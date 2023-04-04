a81aaae0db2318faa3593c8c47dc80d987821db3
lyouta <lyouta@spartaproject.com>
2023-04-04 21:04:22 +0900

    first commit

---
+++ pysparta/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class PySpartaAPI:
+	def runOffline(self):
+		print('sparta running offline...')
+		
+	def __init__(self):
+		print('API initialing...')
---
+++ pysparta/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+class PySpartaGUI:
+	def __init__(self):
+		print('GUI initialing...')
+
+if __name__ == '__main__':
+	PySpartaGUI()
