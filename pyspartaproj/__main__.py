6e188cfffd4f249ad9aeb481ab041920e36d7050
lyouta <lyouta@spartaproject.com>
2023-04-05 02:44:19 +0900

    add module directory path

--- pyspartaproj/__main__.py
+++ pyspartaproj/__main__.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 
-if __name__ == '__main__':
+def show_application():
 	import pysparta
+
 	sparta_gui = pysparta.PySpartaGUI()
 	sparta_gui.show()
+
+def add_current_path():
+	import os
+	module_path =__file__
+	directory_path = os.path.dirname(module_path)
+
+	import sys
+	sys.path.append(directory_path)
+
+def startup():
+	add_current_path()
+	show_application()
+
+if __name__ == '__main__':
+	startup()
