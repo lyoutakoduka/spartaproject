f70bc4d0cd667215ab2507d12c910f73e1d9af1f
lyouta <lyouta@spartaproject.com>
2023-04-06 15:52:56 +0900

    :sparkles: Introduce new class for silent mode.

--- pyspartaproj/__init__.py
+++ pyspartaproj/__init__.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+class PySpartaSilent:
+    def run(self):
+        print('run sparta silently')
+
+    def shutdown(self):
+        print('shutdown sparta')
+
+    def delete(self):
+        print('delete sparta')
+        self.still_alive = False
+
+    def __del__(self):
+        if self.still_alive:
+            self.delete()
+
+    def __init__(self):
+        self.still_alive = True
+
+
 class PySpartaAPI:
     def runOffline(self):
         print('running offline...')
