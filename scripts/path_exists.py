1e59d38a004cdfa602ae41291176102f8ce9caf5
lyouta <lyouta@spartaproject.com>
2023-04-20 14:06:11 +0900

    ✨ added module to check paths exists

---
+++ scripts/path_exists.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from pathlib import Path
+from typing import List
+
+from scripts.same_bools import pair_true
+
+_Strs = List[str]
+_Bools = List[bool]
+
+
+def check_exists(paths: _Strs) -> _Bools:
+    return [Path(path).exists() for path in paths]
+
+
+def main() -> bool:
+    EXISTS_EXPECTED: _Bools = [False, False, False, True]
+    FILE_TYPES: _Strs = ['red', 'green', 'blue', 'empty']
+
+    full_paths: _Strs = [
+        str(Path(__file__).with_name(f'debug_{type}.py'))
+        for type in FILE_TYPES
+    ]
+
+    file_exists: _Bools = check_exists(full_paths)
+
+    result:  bool = pair_true(EXISTS_EXPECTED, file_exists)
+
+    return result
+
+
+if __name__ == '__main__':
+    sys.exit(not main())
