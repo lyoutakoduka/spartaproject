77180d77eb3c555c53b08b606d0fe874b261f214
lyouta <lyouta@spartaproject.com>
2023-04-22 06:04:24 +0900

    🧪 divided to test and function

--- scripts/same_elements.py
+++ scripts/same_elements.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 from typing import List
 
 _Bools = List[bool]
@@ -10,18 +9,3 @@ _Bools = List[bool]
 def all_true(elements: _Bools) -> bool:
     elements = list(set(elements))
     return 1 == len(elements) and elements[0]
-
-
-def main() -> bool:
-    result: bool = all_true([
-        0 == 0,
-        '1' == '1',
-        True == True,
-        2.0 == 2.0,
-    ])
-
-    return result
-
-
-if __name__ == '__main__':
-    sys.exit(not main())
