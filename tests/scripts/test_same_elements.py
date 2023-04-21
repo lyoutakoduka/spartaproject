afa1a665e11c071036a5afe9f0d24e479b06dd8f
lyouta <lyouta@spartaproject.com>
2023-04-22 08:20:47 +0900

    🩹 added bool type return

--- tests/scripts/test_same_elements.py
+++ tests/scripts/test_same_elements.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sparta.scripts.same_elements import all_true
+from scripts.same_elements import all_true
 
 
-def test() -> None:
-    result: bool = all_true([
+def test() -> bool:
+    assert all_true([
         0 == 0,
         '1' == '1',
         True == True,
         2.0 == 2.0,
     ])
 
-    assert result
+    return True
