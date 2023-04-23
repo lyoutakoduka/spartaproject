af28c0362d46cb1137d8bac14133940b888c3d97
lyouta <lyouta@spartaproject.com>
2023-04-23 20:08:30 +0900

    Feat: function for dictionary type bool object

--- scripts/bools/compare_value.py
+++ scripts/bools/compare_value.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import List
-
-from project.sparta.scripts.bools.same_value import bool_same_array
+from typing import List, Dict
 
 _Bools = List[bool]
 _Ints = List[int]
+_Strs = List[str]
+_BoolsList = List[_Bools]
+_StrsList = List[_Strs]
+_BoolPair = Dict[str, bool]
+_BoolType = _Bools | _BoolPair
 
 
-def bool_compare_array(lefts: _Bools, rights: _Bools) -> bool:
-    counts: _Ints = list(set([len(flags) for flags in [lefts, rights]]))
-    count: int = len(counts)
+def _check_args_size(lefts: _BoolType, rights: _BoolType) -> None:
+    flag_counts: _Ints = list(set([len(flags) for flags in [lefts, rights]]))
+    count: int = len(flag_counts)
 
     if 1 != count:
         raise ValueError('size')
 
-    if 0 == counts[0]:
+    if 0 == flag_counts[0]:
         raise ValueError('empty')
 
-    return bool_same_array([left == right for left, right in zip(lefts, rights)])
+
+def bool_compare_array(lefts: _Bools, rights: _Bools) -> bool:
+    _check_args_size(lefts, rights)
+    return lefts == rights
+
+
+def bool_compare_pair(lefts: _BoolPair, rights: _BoolPair) -> bool:
+    _check_args_size(lefts, rights)
+
+    sorted_keys: _StrsList = [
+        sorted(flags.keys())
+        for flags in [lefts, rights]
+    ]
+
+    if sorted_keys[0] != sorted_keys[1]:
+        raise KeyError('unmatch')
+
+    sorted_flags: _BoolsList = [
+        [flags[key] for key in keys]
+        for keys, flags in zip(sorted_keys, [lefts, rights])
+    ]
+
+    return sorted_flags[0] == sorted_flags[1]
