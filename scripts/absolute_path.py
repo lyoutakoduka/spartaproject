acb964523365a015d9daad418ea96f87e6c1a13f
lyouta <lyouta@spartaproject.com>
2023-04-23 13:57:00 +0900

    Change: argument type, str to Path of pathlib

--- scripts/absolute_path.py
+++ scripts/absolute_path.py
@@ -4,13 +4,12 @@
 from pathlib import Path
 from typing import List
 
-_Strs = List[str]
+_Paths = List[Path]
 
 
-def convert_path(relative_path: str) -> str:
-    path: Path = Path(relative_path)
-    return str(path.absolute())  # resolve() ignore symbolic link
+def convert_path(relative_path: Path) -> Path:
+    return relative_path.absolute()  # resolve() ignore symbolic link
 
 
-def convert_paths(relative_paths: _Strs) -> _Strs:
+def convert_paths(relative_paths: _Paths) -> _Paths:
     return [convert_path(path) for path in relative_paths]
--- tests/scripts/test_absolute_path.py
+++ tests/scripts/test_absolute_path.py
@@ -8,34 +8,28 @@ from scripts.same_elements import all_true
 from scripts.path_exists import check_paths, check_path
 from scripts.absolute_path import convert_paths, convert_path
 
-_Strs = List[str]
-_StrsList = List[_Strs]
+_Paths = List[Path]
 
-_BASE_PATH: _Strs = ['project', 'sparta']
-_EMPTY_PATH: _Strs = _BASE_PATH + ['scripts', 'debug_empty.py']
+_BASE_PATH: Path = Path('project', 'sparta')
+_EMPTY_PATH: Path = _BASE_PATH.joinpath('scripts', 'debug_empty.py')
 
 
 def test_ignore() -> None:
-    path: str = __file__
+    path: Path = Path(__file__)
     assert path == convert_path(path)
 
 
 def test_single() -> None:
-    assert check_path(convert_path(str(Path(*_EMPTY_PATH))))
+    assert check_path(convert_path(_EMPTY_PATH))
 
 
 def test_multi() -> None:
-    RELATIVE_PATH: _StrsList = [
+    RELATIVE_PATH: _Paths = [
         _EMPTY_PATH,
-        _BASE_PATH + ['debug_wrapper.py']
+        _BASE_PATH.joinpath('debug_wrapper.py'),
     ]
 
-    relative_paths: _Strs = [
-        str(Path(*relative_path))
-        for relative_path in RELATIVE_PATH
-    ]
-
-    assert all_true(check_paths(convert_paths(relative_paths)))
+    assert all_true(check_paths(convert_paths(RELATIVE_PATH)))
 
 
 def main() -> bool:
