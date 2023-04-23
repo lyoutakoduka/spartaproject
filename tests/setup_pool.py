9883a0ca227bbd31e1953917d8d15c36d6b45981
lyouta <lyouta@spartaproject.com>
2023-04-23 12:37:32 +0900

    Remove: because using pytest module

--- tests/setup_pool.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from type_info import InitialContext
-
-
-def run(_test_conditions: InitialContext) -> bool:
-    print(_test_conditions)
-    return True
