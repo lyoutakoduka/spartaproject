5dad3e5b27ec81a575c1728315cd068c2fd80b91
lyouta <lyouta@spartaproject.com>
2023-04-23 20:50:14 +0900

    Test: add test case for dictionary type arguments

--- tests/scripts/paths/test_check_exists.py
+++ tests/scripts/paths/test_check_exists.py
@@ -2,25 +2,47 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
-from scripts.bools.compare_value import bool_compare_array
-from scripts.paths.check_exists import path_exists, path_array_exists
+from scripts.bools.compare_value import bool_compare_array, bool_compare_pair
+from scripts.paths.check_exists import path_exists, path_array_exists, path_pair_exists
 
+_Bools = List[bool]
+_BoolPair = Dict[str, bool]
 _Paths = List[Path]
-current_path = Path(__file__)
+_PathPair = Dict[str, Path]
+
+CURRENT_PATH: Path = Path(__file__)
+UNKNOWN_PATH: Path = CURRENT_PATH.with_name('unknown.py')
 
 
 def test_single() -> None:
-    assert path_exists(current_path)
+    assert path_exists(CURRENT_PATH)
 
 
-def test_multi() -> None:
-    paths: _Paths = [current_path, current_path.with_name('unknown.py')]
-    assert bool_compare_array([True, False], path_array_exists(paths))
+def test_array() -> None:
+    PATHS: _Paths = [
+        CURRENT_PATH,
+        UNKNOWN_PATH
+    ]
+    EXPECTS: _Bools = [True, False]
+
+    assert bool_compare_array(EXPECTS, path_array_exists(PATHS))
+
+
+def test_pair() -> None:
+    PATHS: _PathPair = {
+        'R': CURRENT_PATH,
+        'G': UNKNOWN_PATH,
+        'B': CURRENT_PATH.parent,
+    }
+    EXPECTS: _BoolPair = {'R': True, 'G': False, 'B': True}
+
+    assert bool_compare_pair(EXPECTS, path_pair_exists(PATHS))
 
 
 def main() -> bool:
     test_single()
-    test_multi()
+    test_array()
+    test_pair()
     return True
--- tests/scripts/paths/test_get_absolute.py
+++ tests/scripts/paths/test_get_absolute.py
@@ -2,16 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 
-from scripts.bools.same_value import bool_same_array
-from scripts.paths.check_exists import path_exists, path_array_exists
-from scripts.paths.get_absolute import path_absolute, path_array_absolute
+from scripts.bools.compare_value import bool_compare_array, bool_compare_pair
+from scripts.paths.check_exists import path_exists, path_array_exists, path_pair_exists
+from scripts.paths.get_absolute import path_absolute, path_array_absolute, path_pair_absolute
 
+_Bools = List[bool]
+_BoolPair = Dict[str, bool]
 _Paths = List[Path]
+_PathPair = Dict[str, Path]
+
 
 _BASE_PATH: Path = Path('project', 'sparta')
-_EMPTY_PATH: Path = _BASE_PATH.joinpath('scripts', 'debug_empty.py')
+_EMPTY_HEAD: Path = Path('scripts', 'debug_empty.py')
+_EMPTY_PATH: Path = _BASE_PATH.joinpath(_EMPTY_HEAD)
 
 
 def test_ignore() -> None:
@@ -23,19 +28,31 @@ def test_single() -> None:
     assert path_exists(path_absolute(_EMPTY_PATH))
 
 
-def test_multi() -> None:
-    RELATIVE_PATH: _Paths = [
-        _EMPTY_PATH,
-        _BASE_PATH.joinpath('debug_wrapper.py'),
+def test_array() -> None:
+    RELATIVE_PATHS: _Paths = [
+        _BASE_PATH,
+        _EMPTY_HEAD,
     ]
+    EXPECTS: _Bools = [True, False]
 
-    assert bool_same_array(
-        path_array_exists(
-            path_array_absolute(RELATIVE_PATH)))
+    absolute_paths: _Paths = path_array_absolute(RELATIVE_PATHS)
+    assert bool_compare_array(EXPECTS, path_array_exists(absolute_paths))
+
+
+def test_pair() -> None:
+    RELATIVE_PATHS: _PathPair = {
+        'R': _EMPTY_PATH,
+        'G': _EMPTY_HEAD,
+        'B': _EMPTY_PATH,
+    }
+    EXPECTS: _BoolPair = {'R': True, 'G': False, 'B': True}
+
+    absolute_paths: _PathPair = path_pair_absolute(RELATIVE_PATHS)
+    assert bool_compare_pair(EXPECTS, path_pair_exists(absolute_paths))
 
 
 def main() -> bool:
     test_ignore()
     test_single()
-    test_multi()
+    test_array()
     return True
