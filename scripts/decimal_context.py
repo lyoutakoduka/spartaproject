10eb15b2e8d576022672f1cf1795af9e91942f74
lyouta <lyouta@spartaproject.com>
2023-04-24 15:39:44 +0900

    Feat: initial context for decimals arithmetic

---
+++ scripts/decimal_context.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from decimal import setcontext, Context, ROUND_FLOOR, FloatOperation
+
+
+def set_decimal_context() -> None:
+    context: Context = Context(prec=32, rounding=ROUND_FLOOR)
+    context.traps[FloatOperation] = True
+    setcontext(context)
