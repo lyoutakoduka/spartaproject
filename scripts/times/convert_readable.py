9a4546207e6bf7f8c14e7cc62bb8974131476690
lyouta <lyouta@spartaproject.com>
2023-04-24 15:47:37 +0900

    Use: decimal module instead of builtin float

--- scripts/times/convert_readable.py
+++ scripts/times/convert_readable.py
@@ -1,104 +1,76 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from math import modf
+from decimal import Decimal
 from datetime import datetime, timedelta
-from typing import List, TypedDict
+from typing import List, Dict
+
+from scripts.decimal_context import set_decimal_context
 
 _Strs = List[str]
+_IntPair = Dict[str, int]
+
+set_decimal_context()
 
 
-class _Formatter(TypedDict):
-    id: str
-    count: float
+def _get_datetime_counts(counter: datetime) -> _IntPair:
+    counts: _IntPair = {
+        'year': counter.year,
+        'month': counter.month,
+        'day': counter.day,
+    }
+
+    counts = {id: count - 1 for id, count in counts.items()}
+
+    counts.update({
+        'hour': counter.hour,
+        'minute':  counter.minute,
+        'second': counter.second,
+        'micro':  counter.microsecond,
+    })
+
+    return counts
 
 
-_Formatters = List[_Formatter]
+def _get_micro_second_text(second: Decimal, counts: _IntPair, order: int, order_limit: int) -> str:
+    count_text: str = str(counts['micro'])
 
+    if '0' != count_text:
+        count_text = str(second).split('.')[-1]
 
-def _add_without_float_error(second: float, micro_sec: float) -> float:
-    micro_scale: float = 1.0e+6
-    second_int: int = int(second * micro_scale + micro_sec)
-
-    return second_int / micro_scale
-
-
-def _merge_second(counter: datetime) -> float:
-    second: float = counter.second
-    micro_sec: float = counter.microsecond
-
-    if 0 == micro_sec:
-        return second
-
-    return _add_without_float_error(second, micro_sec)
-
-
-def _get_format(counter: datetime) -> _Formatters:
-    return [
-        {'id': 'year', 'count': counter.year - 1},
-        {'id': 'month', 'count': counter.month - 1},
-        {'id': 'day', 'count': counter.day - 1},
-        {'id': 'hour', 'count': counter.hour},
-        {'id': 'minute', 'count': counter.minute},
-        {'id': 'second', 'count': _merge_second(counter)},
-    ]
-
-
-def _format_second_float(count_float: float, order: int, order_limit: int) -> float:
-    count_float = count_float * pow(10, order)
-    count_float = round(count_float)
-    return count_float * pow(10, order_limit - order)
-
-
-def _format_second_str(count_text: str, order: int, order_limit: int) -> str:
-    count_text = count_text.zfill(order_limit)
+    count_text += '0' * order_limit
     return count_text[:order]
 
 
-def _format_second_text(count_float: float, order: int) -> str:
+def _get_decimal_count_texts(second: Decimal, counts: _IntPair, order: int) -> str:
+    second_numbers: _Strs = [str(counts['second'])]
+
     ORDER_LIMIT: int = 6
 
     if ORDER_LIMIT < order:
         order = ORDER_LIMIT
 
-    count_float = _format_second_float(count_float, order, ORDER_LIMIT)
-    return _format_second_str(str(count_float), order, ORDER_LIMIT)
+    if 0 < order:
+        second_numbers += [
+            _get_micro_second_text(second, counts, order, ORDER_LIMIT)]
+
+    return '.'.join(second_numbers) + 's'
 
 
-def _get_second_text(count: float, order: int) -> str:
-    if 0 == order:
-        return str(round(count))
-
-    count_float, count_int = modf(count)
-    clipped_numbers = [
-        str(int(count_int)),
-        _format_second_text(count_float, order)
+def _get_int_count_texts(counts: _IntPair) -> _Strs:
+    int_types: _Strs = ['year', 'month', 'day', 'hour', 'minute']
+    return [
+        str(counts[type]) + type[0]
+        for type in int_types
+        if 0 < counts[type]
     ]
 
-    return '.'.join(clipped_numbers)
 
+def readable_time(second: Decimal, order: int = 0) -> str:
+    counts: _IntPair = _get_datetime_counts(
+        datetime.min + timedelta(seconds=float(second)))
 
-def _get_count_texts(formatters: _Formatters, order: int) -> _Strs:
-    count_texts: _Strs = []
+    count_texts: _Strs = _get_int_count_texts(counts)
+    count_texts += [_get_decimal_count_texts(second, counts, order)]
 
-    for formatter in formatters:
-        id: str = formatter['id']
-        count: float = formatter['count']
-        count_text: str = str(count)
-
-        if 'second' == id:
-            count_text = _get_second_text(count, order)
-        else:
-            if count == 0:
-                continue
-
-        count_texts += [count_text + id[0]]
-
-    return count_texts
-
-
-def readable_time(second: float, order: int = 0) -> str:
-    formatters: _Formatters = _get_format(
-        datetime.min + timedelta(seconds=second))
-
-    return ' '.join(_get_count_texts(formatters, order))
+    return ' '.join(count_texts)
--- tests/scripts/times/test_convert_readable.py
+++ tests/scripts/times/test_convert_readable.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import Dict
+from typing import Dict, List
+from decimal import Decimal
 
 from scripts.bools.same_value import bool_same_array
 from scripts.times.convert_readable import readable_time
+from scripts.decimal_context import set_decimal_context
 
+_Strs = List[str]
+_Decimals = List[Decimal]
 _IntPair = Dict[str, int]
-_FloatPair = Dict[str, float]
+
+set_decimal_context()
 
 
 def test_datetime() -> None:
-    INPUT_UTC_EPOCH: float = 63849679147
+    INPUT_UTC_EPOCH: Decimal = Decimal('63849679147.012345')
     EXPECT: str = "2023y 3m 24d 21h 59m 7s"
 
     assert EXPECT == readable_time(INPUT_UTC_EPOCH)
 
 
 def test_day() -> None:
-    SECOND: float = 1
-    MINUTE: float = SECOND * 60
-    HOUR: float = MINUTE * 60
-    DAY: float = HOUR * 24
+    SECOND: int = 1
+    MINUTE: int = SECOND * 60
+    HOUR: int = MINUTE * 60
+    DAY: int = HOUR * 24
 
-    INPUT_TIMES: _FloatPair = {
+    INPUT_TIMES: _IntPair = {
         "1s": SECOND,
         "1m 0s": MINUTE,
         "1m 1s": MINUTE + SECOND,
@@ -37,44 +42,46 @@ def test_day() -> None:
     }
 
     assert bool_same_array([
-        expect == readable_time(input)
-        for expect, input in INPUT_TIMES.items()
+        expected == readable_time(Decimal(str(input)))
+        for expected, input in INPUT_TIMES.items()
     ])
 
 
 def test_second() -> None:
-    INPUT_TIMES: _FloatPair = {
-        "1.000000s": 1.0,
-        "0.100000s": 0.1,
-        "0.010000s": 0.01,
-        "0.001000s": 0.001,
-        "0.000100s": 0.0001,
-        "0.000010s": 0.00001,
-        "0.000001s": 0.000001,
-        "0.000000s": 0.0000001,
-    }
+    INPUTS: _Decimals = [Decimal('0.1') ** Decimal(str(i)) for i in range(9)]
+
+    EXPECTED: _Strs = [
+        "1.000000s",
+        "0.100000s",
+        "0.010000s",
+        "0.001000s",
+        "0.000100s",
+        "0.000010s",
+        "0.000001s",
+        "0.000000s",
+    ]
 
     assert bool_same_array([
-        expect == readable_time(input, order=6)
-        for expect, input in INPUT_TIMES.items()
+        expected == readable_time(input, order=6)
+        for expected, input in zip(EXPECTED, INPUTS)
     ])
 
 
 def test_order() -> None:
-    INPUT_TIME: float = 0.6666666
+    INPUT_TIME: Decimal = Decimal('0.6666666')
     INPUT_ORDERS: _IntPair = {
-        "1s": 0,
-        "0.7s": 1,
-        "0.67s": 2,
-        "0.667s": 3,
-        "0.6667s": 4,
-        "0.66667s": 5,
-        "0.666667s": 6,
+        "0s": 0,
+        "0.6s": 1,
+        "0.66s": 2,
+        "0.666s": 3,
+        "0.6666s": 4,
+        "0.66666s": 5,
+        "0.666666s": 6,
     }
 
     assert bool_same_array([
-        expect == readable_time(INPUT_TIME, order=order)
-        for expect, order in INPUT_ORDERS.items()
+        expected == readable_time(INPUT_TIME, order=order)
+        for expected, order in INPUT_ORDERS.items()
     ])
 
 
