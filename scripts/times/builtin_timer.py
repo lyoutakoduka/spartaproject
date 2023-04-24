5f5669fa0bc1a5b6f013213546fba1f07bad991c
lyouta <lyouta@spartaproject.com>
2023-04-24 21:03:12 +0900

    Test: give initial value closer to builtin timer

--- scripts/times/builtin_timer.py
+++ scripts/times/builtin_timer.py
@@ -13,14 +13,17 @@ class TimerSelect:
     def __init__(self, override: bool = False, interval: Decimal = Decimal('1')) -> None:
         self._override: bool = override
         self._interval: Decimal = interval
-        self._count: Decimal = Decimal('0')
 
-    def __call__(self) -> Decimal:
-        if not self._override:
-            return Decimal(str(time.time()))
-
-        self._count += self._interval
-        return self._count
+        self.APRIL_1_2023_EPOCH: Decimal = Decimal('1680307200')
+        self._count: Decimal = self.APRIL_1_2023_EPOCH
 
     def current(self) -> Decimal:
         return self._count
+
+    def __call__(self) -> Decimal:
+        if self._override:
+            self._count += self._interval
+        else:
+            self._count = Decimal(str(time.time()))
+
+        return self.current()
