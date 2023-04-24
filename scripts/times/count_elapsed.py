f09cff3b5fe6e336f288430a156d02201455d8fc
lyouta <lyouta@spartaproject.com>
2023-04-24 19:23:32 +0900

    Feat: module that have timer and log functionality

---
+++ scripts/times/count_elapsed.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from decimal import Decimal
+from typing import List
+
+from scripts.decimal_context import set_decimal_context
+from scripts.times.builtin_timer import TimerSelect
+from scripts.times.convert_readable import readable_time
+
+_Strs = List[str]
+
+set_decimal_context()
+
+
+class LogTimer:
+    def __init__(self) -> None:
+        self.restart()
+
+    def timer_current(self) -> Decimal:
+        return self._timer.current()
+
+    def increase_timer(self) -> None:
+        self._timer()
+
+    def restart(
+        self,
+        override: bool = False,
+        timer_interval: Decimal = Decimal('0.01'),
+        interval: Decimal = Decimal('0.1'),
+        order: int = 1
+    ):
+        self._timer: TimerSelect = TimerSelect(
+            override=override, interval=timer_interval)
+        self._start_time: Decimal = self.timer_current()
+
+        self._old_time: int = 0
+        self._interval: Decimal = interval
+
+        self._order: int = order
+
+    def _is_force_show(self, elapsed: Decimal) -> bool:
+        current_interval: int = int(elapsed / self._interval)
+        count_changed: bool = current_interval != self._old_time
+        self._old_time = current_interval
+
+        return count_changed
+
+    def show(self, force: bool = False, header: _Strs = [], footer: _Strs = []) -> None:
+        elapsed: Decimal = self.timer_current() - self._start_time
+
+        if force or self._is_force_show(elapsed):
+            elapsed_text: str = readable_time(elapsed, order=self._order)
+            print(' '.join(header + [elapsed_text] + footer))
---
+++ tests/scripts/times/test_count_elapsed.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import Callable
+from decimal import Decimal
+
+from scripts.off_stdout import StdoutText
+from scripts.format_texts import format_indent
+from scripts.decimal_context import set_decimal_context
+from scripts.times.count_elapsed import LogTimer
+
+set_decimal_context()
+
+
+def _stdout_check(
+    expected: str,
+    count: int,
+    restart: Callable[[LogTimer], None],
+    show: Callable[[LogTimer, int], None],
+) -> None:
+
+    timer = LogTimer()
+    restart(timer)
+
+    stdout_text = StdoutText()
+
+    @stdout_text.deco
+    def _show_log() -> None:
+        for i in range(count):
+            show(timer, i)
+            timer.increase_timer()
+    _show_log()
+
+    assert format_indent(expected, stdout=True) == stdout_text.stdout
+
+
+def test_day() -> None:
+    EXPECTED: str = """
+        1h 0s
+        2h 0s
+        3h 0s
+        4h 0s
+        5h 0s
+        6h 0s
+        7h 0s
+        8h 0s
+        9h 0s
+        10h 0s
+        11h 0s
+        12h 0s
+    """
+
+    minutes: int = 60
+    hour: int = minutes * 60
+    day: int = hour * 12
+    increase_count: int = day + 1
+
+    def restart_timer(timer: LogTimer) -> None:
+        timer.restart(
+            override=True,
+            timer_interval=Decimal(str(1)),
+            interval=Decimal(str(hour)),
+            order=0,
+        )
+
+    def show_timer(timer: LogTimer, _: int) -> None:
+        timer.show()
+
+    _stdout_check(EXPECTED, increase_count, restart_timer, show_timer)
+
+
+def test_show() -> None:
+    EXPECTED: str = """
+        i=10 Almost 0.1s have passed...
+        i=20 Almost 0.2s have passed...
+        i=30 Almost 0.3s have passed...
+        i=40 Almost 0.4s have passed...
+        i=50 Almost 0.5s have passed...
+        i=60 Almost 0.6s have passed...
+        i=70 Almost 0.7s have passed...
+        i=80 Almost 0.8s have passed...
+        i=90 Almost 0.9s have passed...
+    """
+
+    increase_count: int = 100
+
+    def restart_timer(timer: LogTimer) -> None:
+        timer.restart(override=True)
+
+    def show_timer(timer: LogTimer, index: int) -> None:
+        timer.show(
+            header=[f'i={index}', 'Almost'],
+            footer=['have', 'passed...'],
+        )
+
+    _stdout_check(EXPECTED, increase_count, restart_timer, show_timer)
+
+
+def test_force() -> None:
+    EXPECTED: str = """
+        i=0
+        0.00s
+        i=1
+        0.01s
+        i=2
+        0.02s
+        i=3
+        0.03s
+        i=4
+        0.04s
+    """
+
+    increase_count: int = 5
+
+    def restart_timer(timer: LogTimer) -> None:
+        timer.restart(override=True, order=2)
+
+    def show_timer(timer: LogTimer, index: int) -> None:
+        print(f'i={index}')
+        timer.show(force=True)
+
+    _stdout_check(EXPECTED, increase_count, restart_timer, show_timer)
+
+
+def main() -> bool:
+    test_day()
+    test_show()
+    test_force()
+    return True
