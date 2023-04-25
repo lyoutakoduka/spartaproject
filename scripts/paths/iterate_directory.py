72bc1eefc34343fa6a6708777d93b3d0e854d626
lyouta <lyouta@spartaproject.com>
2023-04-25 22:27:06 +0900

    Feat: module to walk and filter inside directory

---
+++ scripts/paths/iterate_directory.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import Generator
+from pathlib import Path
+
+_PathGene = Generator[Path, None, None]
+
+DEFAULT_FILTER: str = '**/*'
+
+
+def _create_filter(depth: int, file: bool, directory: bool, suffix: str) -> str:
+    if 0 < depth:
+        filter = '*/' * depth
+    else:
+        filter = '**/'
+
+    if file and directory:
+        filter += '*'
+    elif file and not directory:
+        filter += '*.' + suffix
+
+    return filter
+
+
+def walk_iterator(
+    root: Path,
+    depth: int = 0,
+    file: bool = True,
+    directory: bool = True,
+    suffix: str = '*',
+    filter: str = DEFAULT_FILTER,
+) -> _PathGene:
+
+    start_iter = True
+
+    if DEFAULT_FILTER == filter:
+        if not file and not directory:
+            start_iter = False
+        else:
+            filter = _create_filter(depth, file, directory, suffix)
+
+    if start_iter:
+        for path in root.glob(filter):
+            if root != path:
+                yield path
---
+++ tests/scripts/paths/test_iterate_directory.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import List, Callable, Generator
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from sparta.scripts.paths.get_relative import path_array_relative
+from sparta.scripts.paths.create_tmp_tree import create_tree
+from sparta.scripts.paths.iterate_directory import walk_iterator
+
+_Paths = List[Path]
+_Strs = List[str]
+_StrList = List[_Strs]
+_PathGene = Generator[Path, None, None]
+
+TREE_DEEP: int = 3
+
+NAME_DIR_1: str = 'dir001'
+NAME_DIR_2: str = 'dir002'
+NAME_DIRS: _Strs = [NAME_DIR_1, NAME_DIR_2]
+NAME_DIR_EMPTY: str = 'empty'
+
+NAME_INI: str = 'file.ini'
+NAME_JSON: str = 'file.json'
+NAME_TEXT: str = 'file.txt'
+
+
+def _check_walk_result(expected: _StrList, path_gene: _PathGene, root_path: Path) -> None:
+    results: _Paths = path_array_relative(list(path_gene), root_path=root_path)
+    expected_paths: _Paths = [Path(*path_names) for path_names in expected]
+
+    assert expected_paths == results
+
+
+def _inside_tmp_directory(expected: _StrList, func: Callable[[Path], _PathGene]) -> None:
+    with TemporaryDirectory() as tmp_path:
+        root_path: Path = Path(tmp_path)
+        create_tree(root_path, tree_deep=TREE_DEEP)
+        _check_walk_result(expected, func(Path(root_path)), root_path)
+
+
+def test_all() -> None:
+    EXPECTED: _StrList = [
+        [NAME_DIR_1],
+        [NAME_DIR_EMPTY],
+        [NAME_INI],
+        [NAME_JSON],
+        [NAME_TEXT],
+        NAME_DIRS,
+        [NAME_DIR_1, NAME_DIR_EMPTY],
+        [NAME_DIR_1, NAME_INI],
+        [NAME_DIR_1, NAME_JSON],
+        [NAME_DIR_1, NAME_TEXT],
+        NAME_DIRS + [NAME_DIR_EMPTY],
+        NAME_DIRS + [NAME_INI],
+        NAME_DIRS + [NAME_JSON],
+        NAME_DIRS + [NAME_TEXT],
+    ]
+
+    def make_tree(root_path: Path) -> _PathGene:
+        return walk_iterator(root_path)
+    _inside_tmp_directory(EXPECTED, make_tree)
+
+
+def test_depth() -> None:
+    EXPECTED: _StrList = [
+        NAME_DIRS + [NAME_DIR_EMPTY],
+        NAME_DIRS + [NAME_INI],
+        NAME_DIRS + [NAME_JSON],
+        NAME_DIRS + [NAME_TEXT],
+    ]
+
+    def make_tree(root_path: Path) -> _PathGene:
+        return walk_iterator(root_path, depth=2)
+    _inside_tmp_directory(EXPECTED, make_tree)
+
+
+def test_directory() -> None:
+    EXPECTED: _StrList = [
+        [NAME_INI],
+        [NAME_JSON],
+        [NAME_TEXT],
+        [NAME_DIR_1, NAME_INI],
+        [NAME_DIR_1, NAME_JSON],
+        [NAME_DIR_1, NAME_TEXT],
+        NAME_DIRS + [NAME_INI],
+        NAME_DIRS + [NAME_JSON],
+        NAME_DIRS + [NAME_TEXT],
+    ]
+
+    def make_tree(root_path: Path) -> _PathGene:
+        return walk_iterator(root_path, directory=False)
+    _inside_tmp_directory(EXPECTED, make_tree)
+
+
+def test_file() -> None:
+    EXPECTED: _StrList = [
+        [NAME_DIR_1],
+        NAME_DIRS,
+        NAME_DIRS + [NAME_DIR_EMPTY],
+        [NAME_DIR_1, NAME_DIR_EMPTY],
+        [NAME_DIR_EMPTY],
+    ]
+
+    def make_tree(root_path: Path) -> _PathGene:
+        return walk_iterator(root_path, file=False)
+    _inside_tmp_directory(EXPECTED, make_tree)
+
+
+def test_suffix() -> None:
+    EXPECTED: _StrList = [
+        [NAME_JSON],
+        [NAME_DIR_1, NAME_JSON],
+        NAME_DIRS + [NAME_JSON],
+    ]
+
+    def make_tree(root_path: Path) -> _PathGene:
+        return walk_iterator(root_path, directory=False, suffix='json')
+    _inside_tmp_directory(EXPECTED, make_tree)
+
+
+def test_filter() -> None:
+    EXPECTED: _StrList = [NAME_DIRS + [NAME_TEXT]]
+
+    def make_tree(root_path: Path) -> _PathGene:
+        return walk_iterator(root_path, filter='*/*/*.txt')
+    _inside_tmp_directory(EXPECTED, make_tree)
+
+
+def main() -> bool:
+    test_all()
+    test_depth()
+    test_directory()
+    test_file()
+    test_suffix()
+    test_filter()
+    return True
