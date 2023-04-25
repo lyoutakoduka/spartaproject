a3ca3f970e686f246a48a23c9b95b3b8b129efa2
lyouta <lyouta@spartaproject.com>
2023-04-26 03:51:57 +0900

    Update: corresponded to False array

--- scripts/bools/same_value.py
+++ scripts/bools/same_value.py
@@ -7,7 +7,7 @@ _Bools = List[bool]
 _BoolPair = Dict[str, bool]
 
 
-def bool_same_array(flags: _Bools) -> bool:
+def bool_same_array(flags: _Bools, invert: bool = False) -> bool:
     if 0 == len(flags):
         raise ValueError('empty')
 
@@ -16,7 +16,7 @@ def bool_same_array(flags: _Bools) -> bool:
     if 1 != len(flags):
         raise ValueError('true and false')
 
-    if not flags[0]:
+    if not invert ^ flags[0]:
         raise ValueError('false')
 
     return True
--- tests/scripts/bools/test_same_value.py
+++ tests/scripts/bools/test_same_value.py
@@ -25,11 +25,16 @@ def test_array() -> None:
     assert bool_same_array([True, True, True])
 
 
+def test_invert() -> None:
+    assert bool_same_array([False, False, False], invert=True)
+
+
 def test_pair() -> None:
     assert bool_same_pair({'R': True, 'G': True, 'B': True})
 
 
 def main() -> bool:
     test_array()
+    test_invert()
     test_pair()
     return True
