d43b2b6ec840439222d190f087d1d64b9e849847
lyouta <lyouta@spartaproject.com>
2023-04-27 22:12:48 +0900

    Add: user defined types

---
+++ contexts/callable_context.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import TypeVar, ParamSpec
+from typing import Callable  # type: ignore #TODO don't use here, but necessary
+
+CR = TypeVar('CR')
+CP = ParamSpec('CP')
