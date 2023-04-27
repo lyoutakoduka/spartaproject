24c7df27fd1f16951f68b2ab8434d5572b6a9ed1
lyouta <lyouta@spartaproject.com>
2023-04-27 23:07:11 +0900

    Add: test for global context  of decimal module

---
+++ tests/contexts/test_decimal_context.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pytest import raises
+from decimal import getcontext, setcontext, FloatOperation, Context
+
+from contexts.string_context import Strs
+from contexts.decimal_context import Decimal, set_decimal_context
+
+
+def test_float() -> None:
+    with raises(FloatOperation):
+        set_decimal_context()
+        Decimal(1.0)
+
+
+def test_accuracy() -> None:
+    def get_changed() -> Strs:
+        context = getcontext()
+        return [str(context.prec), context.rounding]
+
+    setcontext(Context())
+    before: Strs = get_changed()
+    set_decimal_context()
+    after: Strs = get_changed()
+
+    assert before != after
+
+
+def main() -> bool:
+    test_accuracy()
+    return True
