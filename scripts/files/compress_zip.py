1a6ff5303fa29d765591003923bc29fb9cc7f23f
lyouta <lyouta@spartaproject.com>
2023-04-30 00:55:03 +0900

    Feat: add module to create zip archive

---
+++ scripts/files/compress_zip.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from zipfile import ZipFile, ZIP_LZMA, ZIP_STORED
+
+from contexts.decimal_context import Decimal, set_decimal_context
+from contexts.string_context import Strs
+from contexts.path_context import Path, Paths
+from scripts.paths.get_relative import path_relative
+from scripts.paths.create_directory import path_mkdir
+from scripts.paths.iterate_directory import walk_iterator
+
+set_decimal_context()
+
+
+def _default() -> int:
+    limit_byte: int = 1
+    for _ in range(3):
+        limit_byte *= (2**10)
+    return limit_byte * 4
+
+
+class ArchiveZip:
+    def __init__(
+            self, output_root: Path,
+            archive_id: str = '',
+            limit_byte: int = _default(),
+            compress: bool = False,
+    ) -> None:
+        self._output_root: Path = output_root
+
+        self._limit_byte: Decimal = Decimal(str(limit_byte))
+        self._compress: bool = compress
+
+        self._init_archive_id(archive_id)
+        self._init_walk_history()
+        self._init_archive_byte()
+
+    def __del__(self):
+        self._file_zip.close()
+
+    def _init_archive_id(self, archive_id: str) -> None:
+        if 0 == len(archive_id):
+            archive_id = self._output_root.name
+        self._archive_id = archive_id
+
+    def _init_walk_history(self) -> None:
+        self._walk_directories: Paths = []
+        self._walk_files: Paths = []
+        self._archived: Paths = []
+
+    def _init_archive_byte(self) -> None:
+        self._output_index: int = 0
+        self._reset_archive_byte()
+
+    def result(self) -> Paths:
+        return self._archived
+
+    def _get_archive_path(self) -> Path:
+        file_names: Strs = [self._archive_id]
+
+        if 0 < self._output_index:
+            file_names += [str(self._output_index).zfill(4)]
+        self._output_index += 1
+
+        return Path(self._output_root, '_'.join(file_names)).with_suffix('.zip')
+
+    def _reset_archive_byte(self) -> None:
+        self._archive_byte: Decimal = Decimal('0')
+
+        path_mkdir(self._output_root)
+        self._archived += [self._get_archive_path()]
+
+        self._file_zip = ZipFile(
+            self._archived[-1],
+            mode='w',
+            compression=ZIP_LZMA if self._compress else ZIP_STORED,
+        )
+
+    def _add_file_to_archive(self, is_dir: bool, target: Path, root: Path) -> None:
+        with open(target, 'rb') as file_read:
+            self._file_zip.writestr(
+                str(path_relative(target, root_path=root)),
+                file_read.read()
+            )
+
+    def _update_archive_byte(self, target: Path, root: Path) -> None:
+        target_byte: Decimal = Decimal(str(target.stat().st_size))
+
+        if self._limit_byte < target_byte:
+            ...
+        else:
+            next_byte: Decimal = self._archive_byte + target_byte
+
+            if self._limit_byte < next_byte:
+                self._reset_archive_byte()
+            self._archive_byte += target_byte
+
+            self._add_file_to_archive(False, target, root)
+
+    def _not_still_archived(self, is_dir: bool, target: Path) -> bool:
+        archived: Paths = self._walk_directories if is_dir else self._walk_files
+
+        not_still: bool = target not in archived
+        if not_still:
+            archived += [target]
+
+        return not_still
+
+    def _add_archive_child(self, target: Path, root: Path) -> None:
+        if target.is_dir():
+            if self._not_still_archived(True, target):
+                self._add_file_to_archive(True, target, root)
+
+                for path in walk_iterator(target):
+                    self._add_archive_child(path, root)
+        else:
+            if self._not_still_archived(False, target):
+                self._update_archive_byte(target, root)
+
+    def add_archive(self, archive_target: Path, archive_root: Path = Path('')) -> None:
+        if archive_target.is_relative_to(archive_root):
+            self._add_archive_child(archive_target, archive_root)
+        elif archive_target.is_dir():
+            self._add_archive_child(archive_target, archive_target)
+        else:
+            self._add_archive_child(archive_target, archive_target.parent)
---
+++ tests/scripts/files/test_compress_zip.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from shutil import unpack_archive
+from typing import Callable
+from tempfile import TemporaryDirectory
+
+from contexts.integer_context import Ints, Ints2
+from contexts.path_context import Path, Paths, Paths2
+from scripts.files.compress_zip import ArchiveZip
+from scripts.paths.get_relative import path_array_relative
+from scripts.paths.create_tmp_tree import create_tree
+from scripts.paths.iterate_directory import walk_iterator
+
+
+def _get_input_paths(walk_paths: Paths) -> Paths:
+    inputs: Paths = []
+
+    for walk_path in walk_paths:
+        inputs += [walk_path]
+        if walk_path.is_dir():
+            for path in walk_iterator(walk_path):
+                inputs += [path]
+
+    return inputs
+
+
+def _get_output_paths(result_raw: Paths, tmp_path: Path) -> Paths:
+    outputs: Paths = []
+
+    extract_root: Path = Path(tmp_path, 'extract')
+    for result_path in result_raw:
+        unpack_archive(result_path, extract_dir=extract_root)
+        for path in walk_iterator(extract_root):
+            outputs += [path]
+
+    return outputs
+
+
+def _compare_path_count(inputs: Paths, outputs: Paths) -> None:
+    counts: Ints = [len(paths) for paths in [inputs, outputs]]
+    assert counts[0] == counts[1]
+
+
+def _compare_path_name(sorted_paths: Paths2, tmp_path: Path) -> None:
+    relative_paths: Paths2 = [
+        path_array_relative(paths, root_path=Path(tmp_path, directory))
+        for directory, paths in zip(['tree', 'extract'], sorted_paths)
+    ]
+
+    assert relative_paths[0] == relative_paths[1]
+
+
+def _compare_file_size(sorted_paths: Paths2) -> None:
+    file_size_pair: Ints2 = [
+        [path.stat().st_size for path in paths if path.is_file()]
+        for paths in sorted_paths
+    ]
+
+    assert file_size_pair[0] == file_size_pair[1]
+
+
+def _check_archive_result(
+    result_raw: Paths,
+    tmp_path: Path,
+    walk_paths: Paths,
+) -> None:
+
+    inputs: Paths = _get_input_paths(walk_paths)
+    outputs: Paths = _get_output_paths(result_raw, tmp_path)
+    sorted_paths: Paths2 = [sorted(paths) for paths in [inputs, outputs]]
+
+    _compare_path_count(inputs, outputs)
+    _compare_path_name(sorted_paths, tmp_path)
+    _compare_file_size(sorted_paths)
+
+
+def _inside_tmp_directory(func: Callable[[Path, Path, Paths], Paths]) -> None:
+    with TemporaryDirectory() as tmp_path:
+        tree_root: Path = Path(tmp_path, 'tree')
+        create_tree(tree_root, tree_deep=4, tree_weight=4)
+
+        archive_root: Path = Path(tmp_path, 'archive')
+        walk_paths: Paths = []
+        _check_archive_result(
+            func(archive_root, tree_root, walk_paths),
+            Path(tmp_path), walk_paths,
+        )
+
+
+def test_simple() -> None:
+    def make_tree(archive_root: Path, tree_root: Path, walk_paths: Paths) -> Paths:
+        archive_zip = ArchiveZip(archive_root)
+
+        for path in walk_iterator(tree_root, directory=False, depth=1):
+            archive_zip.add_archive(path)
+            walk_paths += [path]
+
+        return archive_zip.result()
+
+    _inside_tmp_directory(make_tree)
+
+
+def main() -> bool:
+    test_simple()
+    return True
