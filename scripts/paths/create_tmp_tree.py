acba5064fc0c9a5507c3b6bf2225f2263d4798e5
lyouta <lyouta@spartaproject.com>
2023-04-29 20:53:05 +0900

    Update: control size of file in the tree easily

--- scripts/paths/create_tmp_tree.py
+++ scripts/paths/create_tmp_tree.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from contexts.string_context import Strs
 from contexts.path_context import Path
 from scripts.files.export_config import config_export, Config
 from scripts.files.export_json import json_export, Json
@@ -10,49 +9,72 @@ from scripts.paths.create_directory import path_mkdir
 _NAME: str = 'file'
 
 
-def _sample_text(root: Path) -> None:
-    line_count: int = 1
-    line_width: int = 64
-    line_count *= 10
-    index_order: int = len(str(line_count))
-    width: int = line_width - index_order
-    line_text: str = '-' * width
+def _sample_text(root: Path, weight: int) -> None:
+    line_width: int = 10
+    index_order: int = len(str(weight))
+    line_text: str = '-' * (line_width - index_order)
 
     INPUT: str = '\n'.join([
         str(i).zfill(index_order) + line_text
-        for i in range(line_count)
+        for i in range(weight)
     ])
 
     with open(Path(root, _NAME + '.txt'), 'w') as file:
         file.write(INPUT)
 
 
-def _sample_config(root: Path) -> None:
+def _sample_config(root: Path, weight: int) -> None:
+    width: int = 10
+    section_order: int = len(str(weight))
+    line_text: str = '-' * width
+
     INPUT: Config = {
-        'section': {'option' + str(i): str(i) for i in range(3)}
+        str(i).zfill(section_order): {
+            str(j).zfill(section_order): line_text
+            for j in range(weight)
+        }
+        for i in range(weight)
     }
+
     config_export(Path(root, _NAME + '.ini'), INPUT)
 
 
-def _sample_json(root: Path) -> None:
-    INPUT: Json = {
-        'section': {'option' + str(i): str(i) for i in range(3)}
-    }
-    json_export(Path(root, _NAME + '.json'), INPUT)
+def _sample_json(root: Path, weight: int) -> None:
+    width: int = 10
+    section_order: int = len(str(weight))
+    line_text: str = '-' * width
+
+    def func(count: int) -> Json:
+        if 0 < count:
+            return {
+                str(i).zfill(section_order): func(count - 1)
+                for i in range(weight)
+            }
+        return line_text
+
+    input: Json = func(weight)
+
+    json_export(Path(root, _NAME + '.json'), input)
 
 
-def _recursive_tree(root: Path, tree_deep: int, deep: int):
+def _recursive_tree(root: Path, tree_deep: int, deep: int, weight: int):
     path_mkdir(root)
     path_mkdir(Path(root, 'empty'))
-    _sample_text(root)
-    _sample_config(root)
-    _sample_json(root)
+    _sample_text(root, weight)
+    _sample_config(root, weight)
+    _sample_json(root, weight)
 
     if 1 < deep:
         directory_name: Path = Path('dir' + str(tree_deep - deep + 1).zfill(3))
-        _recursive_tree(Path(root, directory_name), tree_deep, deep - 1)
+
+        _recursive_tree(
+            Path(root, directory_name),
+            tree_deep,
+            deep - 1,
+            weight
+        )
 
 
-def create_tree(root_path: Path, tree_deep: int = 1) -> None:
+def create_tree(root_path: Path, tree_deep: int = 1, weight: int = 1) -> None:
     if 1 <= tree_deep <= 10:
-        _recursive_tree(root_path, tree_deep, tree_deep)
+        _recursive_tree(root_path, tree_deep, tree_deep, weight)
