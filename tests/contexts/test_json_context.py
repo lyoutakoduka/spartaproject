8631905ce307ab52aa8f542746df9f8464c4c400
lyouta <lyouta@spartaproject.com>
2023-04-29 13:23:28 +0900

    Update: add template of function

--- tests/contexts/test_json_context.py
+++ tests/contexts/test_json_context.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+def _check_type_structure() -> bool:
+    return True
+
+
+def test_safe() -> None:
+    assert _check_type_structure()
+
+
+def test_type() -> None:
+    assert _check_type_structure()
+
+
 def main() -> bool:
+    test_safe()
+    test_type()
     return True
