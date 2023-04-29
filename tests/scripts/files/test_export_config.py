9806e700ebc45f2f38bcbb63f3e20838829f14d9
lyouta <lyouta@spartaproject.com>
2023-04-29 16:10:36 +0900

    Fix: simplify test of the file exporter function

--- tests/scripts/files/test_export_config.py
+++ tests/scripts/files/test_export_config.py
@@ -14,30 +14,7 @@ from scripts.files.export_config import config_dump, config_export
 from scripts.format_texts import format_indent
 
 
-_CONFIG_INPUT: Config = {
-    'section': {
-        'path': Path('root'),
-        'decimal': Decimal('0.1'),
-        'str': '1',
-        'float': 1.0,
-        'int': 1,
-        'bool': True,
-    }
-}
-
-# 2 space indent
-_EXPECTED_SRC: str = """
-    [section]
-    path = root
-    decimal = 0.1
-    str = 1
-    float = 1.0
-    int = 1
-    bool = True
-"""
-
-
-def test_dump() -> None:
+def test_section() -> None:
     INPUT: Section = {
         'path': Path('root'),
         'decimal': Decimal('0.1'),
@@ -60,17 +37,6 @@ def test_dump() -> None:
 
     expected: str = format_indent(EXPECTED)
     assert expected == config_dump(config_input)
-
-
-def test_export() -> None:
-    expected: str = format_indent(_EXPECTED_SRC)
-
-    with TemporaryDirectory() as tmp_path:
-        export_path: Path = Path(tmp_path, 'tmp.json')
-        config_export(export_path, _CONFIG_INPUT)
-
-        with open(export_path, 'r') as file:
-            assert expected == file.read()
 
 
 def test_lower() -> None:
@@ -132,9 +98,31 @@ def test_type() -> None:
     assert expected == config_dump(INPUT)
 
 
+def test_export() -> None:
+    INPUT: Config = {
+        'section': {
+            'option': 'value',
+        }
+    }
+
+    EXPECTED: str = """
+        [section]
+        option = value
+    """
+
+    expected: str = format_indent(EXPECTED)
+
+    with TemporaryDirectory() as tmp_path:
+        export_path: Path = Path(tmp_path, 'tmp.json')
+        config_export(export_path, INPUT)
+
+        with open(export_path, 'r') as file:
+            assert expected == file.read()
+
+
 def main() -> bool:
-    test_dump()
-    test_export()
+    test_section()
     test_lower()
     test_type()
+    test_export()
     return True
