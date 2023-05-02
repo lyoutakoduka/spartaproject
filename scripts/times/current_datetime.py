749f52616f1d49f44d5040e14b9bde9fd6b382b6
lyouta <lyouta@spartaproject.com>
2023-05-02 20:54:55 +0900

    Change: use inside API to convert timestamp.

--- scripts/times/current_datetime.py
+++ scripts/times/current_datetime.py
@@ -2,12 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
-from dateutil.tz import gettz
 
+from scripts.times.from_timestamp import time_from_timestamp
 from scripts.times.builtin_timer import TimerSelect
 
 
 def get_current_time(override: bool = False, jst: bool = False) -> datetime:
     timer = TimerSelect(override=override)
-    current_time: datetime = datetime.fromtimestamp(float(timer()))
-    return current_time.astimezone(gettz('Asia/Tokyo' if jst else 'UTC'))
+    return time_from_timestamp(timer())
