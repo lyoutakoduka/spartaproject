def957f9085ebe69e2741561ba6004c5761cf880
lyouta <lyouta@spartaproject.com>
2023-05-02 20:52:15 +0900

    Change: return value and args as datetime object

--- scripts/times/offset_timezone.py
+++ scripts/times/offset_timezone.py
@@ -4,17 +4,11 @@
 from datetime import datetime, timedelta, timezone
 
 
-def _offset_time(time_object: datetime) -> datetime:
-    offset: timedelta | None = time_object.utcoffset()
+def offset_time(time: datetime) -> datetime:
+    offset: timedelta | None = time.utcoffset()
 
     if offset is None:
         raise TypeError
 
-    time_offset: datetime = time_object - offset
+    time_offset: datetime = time - offset
     return time_offset.replace(tzinfo=timezone.utc)
-
-
-def offset_time(time: str) -> str:
-    time_object: datetime = datetime.fromisoformat(time)
-    time_object = _offset_time(time_object)
-    return time_object.isoformat()
--- tests/scripts/times/test_offset_timezone.py
+++ tests/scripts/times/test_offset_timezone.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from contexts.time_context import datetime, Times
 from scripts.times.offset_timezone import offset_time
 
 
-def test_pass() -> None:
+def test_timezone() -> None:
     INPUT_UTC: str = '2023-04-15T20:09:30.936886+00:00'
     INPUT_JST: str = '2023-04-16T05:09:30.936886+09:00'
 
-    utc: str = offset_time(INPUT_UTC)
-    utc_converted: str = offset_time(INPUT_JST)
+    times: Times = [
+        offset_time(datetime.fromisoformat(input))
+        for input in [INPUT_UTC, INPUT_JST]
+    ]
 
-    assert utc == utc_converted
+    assert times[0] == times[1]
 
 
 def main() -> bool:
-    test_pass()
+    test_timezone()
     return True
