6c9ce8a5785a15c719431224a571ebb36404b599
lyouta <lyouta@spartaproject.com>
2023-05-03 08:58:50 +0900

    Update: unknown timezone support

--- scripts/times/offset_timezone.py
+++ scripts/times/offset_timezone.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime, timedelta, timezone
+from dateutil.tz import gettz
 
 
 def offset_time(time: datetime) -> datetime:
     offset: timedelta | None = time.utcoffset()
 
     if offset is None:
-        raise TypeError
+        return datetime.fromisoformat(time.isoformat() + '+00:00')
 
     time_offset: datetime = time - offset
     return time_offset.replace(tzinfo=timezone.utc)
