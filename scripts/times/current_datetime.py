33d90019fc2f0c29af7a8719d1e39ee3d62739f6
lyouta <lyouta@spartaproject.com>
2023-05-02 19:59:00 +0900

    Change: return value as default datetime object

--- scripts/times/current_datetime.py
+++ scripts/times/current_datetime.py
@@ -7,12 +7,7 @@ from dateutil.tz import gettz
 from scripts.times.builtin_timer import TimerSelect
 
 
-def get_current_time(override: bool = False, jst: bool = False) -> str:
+def get_current_time(override: bool = False, jst: bool = False) -> datetime:
     timer = TimerSelect(override=override)
     current_time: datetime = datetime.fromtimestamp(float(timer()))
-    current_time = current_time.astimezone(gettz('UTC'))
-
-    if jst:
-        current_time = current_time.astimezone(gettz('Asia/Tokyo'))
-
-    return current_time.isoformat()
+    return current_time.astimezone(gettz('Asia/Tokyo' if jst else 'UTC'))
--- tests/scripts/times/test_current_datetime.py
+++ tests/scripts/times/test_current_datetime.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 from scripts.times.current_datetime import get_current_time
+
+
+def _common_test(time: datetime, expected: str) -> None:
+    assert datetime.fromisoformat(expected) == time
 
 
 def test_utc() -> None:
     EXPECTED: str = '2023-04-01T00:00:00+00:00'
-    assert EXPECTED == get_current_time(override=True)
+    _common_test(get_current_time(override=True), EXPECTED)
 
 
 def test_jst() -> None:
     EXPECTED: str = '2023-04-01T09:00:00+09:00'
-    assert EXPECTED == get_current_time(override=True, jst=True)
+    _common_test(get_current_time(override=True, jst=True), EXPECTED)
 
 
 def main() -> bool:
