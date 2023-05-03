00e3b62d45183bdc1516cf298fb418164d80036c
lyouta <lyouta@spartaproject.com>
2023-05-04 07:19:38 +0900

    Support: integer time tuple

--- contexts/integer_context.py
+++ contexts/integer_context.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 Ints = List[int]
+IntTuple = Tuple[int, ...]
 IntPair = Dict[str, int]
 Ints2 = List[Ints]
