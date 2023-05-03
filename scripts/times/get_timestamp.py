51b71a48976532ea642d9f01a00c81a0cca8a65e
lyouta <lyouta@spartaproject.com>
2023-05-03 22:45:37 +0900

    Support: get create time from timestamp

--- scripts/times/get_timestamp.py
+++ scripts/times/get_timestamp.py
@@ -8,5 +8,13 @@ from datetime import datetime
 from scripts.times.from_timestamp import time_from_timestamp
 
 
+def _convert_timestamp(time: float, jst: bool) -> datetime:
+    return time_from_timestamp(Decimal(str(time)), jst=jst)
+
+
+def get_create(path: Path, jst: bool = False) -> datetime:
+    return _convert_timestamp(path.stat().st_ctime, jst)
+
+
 def get_latest(path: Path, jst: bool = False) -> datetime:
-    return time_from_timestamp(Decimal(str(path.stat().st_mtime)))
+    return _convert_timestamp(path.stat().st_mtime, jst)
--- tests/scripts/times/test_get_timestamp.py
+++ tests/scripts/times/test_get_timestamp.py
@@ -3,17 +3,16 @@
 
 from typing import Callable
 from pathlib import Path
-from datetime import datetime
 from tempfile import TemporaryDirectory
 
 from contexts.json_context import Json
+from contexts.time_context import datetime, Times
 from scripts.files.export_json import json_export
-from scripts.times.set_timestamp import set_latest
-from scripts.times.get_timestamp import get_latest
+from scripts.times.get_timestamp import get_latest, get_create
 
 
-_INPUT_UTC: str = '2023-04-15T20:09:30.936886+00:00'
-_time_utc: datetime = datetime.fromisoformat(_INPUT_UTC)
+def _common_test(times: Times) -> None:
+    assert times[0] == times[1]
 
 
 def _inside_tmp_directory(func: Callable[[Path], None]) -> None:
@@ -22,23 +21,25 @@ def _inside_tmp_directory(func: Callable[[Path], None]) -> None:
     with TemporaryDirectory() as tmp_path:
         path: Path = Path(tmp_path, 'tmp.json')
         json_export(path, INPUT_JSON)
-        set_latest(path, _time_utc)
         func(path)
 
 
 def test_utc() -> None:
     def individual_test(path: Path) -> None:
-        assert _time_utc == get_latest(path)
+        _common_test([func(path) for func in [get_latest, get_create]])
 
     _inside_tmp_directory(individual_test)
 
 
 def test_jst() -> None:
-    INPUT_JST: str = '2023-04-16T05:09:30.936886+09:00'
-    _time_jst: datetime = datetime.fromisoformat(INPUT_JST)
-
     def individual_test(path: Path) -> None:
-        assert _time_jst == get_latest(path, jst=True)
+        times: Times = [
+            func(path, jst=True)
+            for func in [get_latest, get_create]
+        ]
+        _common_test(times)
+
+        assert '9:00:00' == str(times[0].utcoffset())
 
     _inside_tmp_directory(individual_test)
 
