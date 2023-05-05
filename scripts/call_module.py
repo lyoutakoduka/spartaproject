1c7de3b6a9d41f9020e05cbb17c17d1e62d4e10d
lyouta <lyouta@spartaproject.com>
2023-05-05 20:09:21 +0900

    Update: not depends on directory of call source

--- scripts/call_module.py
+++ scripts/call_module.py
@@ -35,25 +35,38 @@ def _replace_file_name(head: str, module_path: Path) -> Path:
     return module_path.with_name(head + module_path.name)
 
 
-def _replace_to_test_root(test_added_path: Path) -> Path:
-    ROOT_PATH: Path = Path('project', 'sparta')
-    root_path: Path = path_absolute(ROOT_PATH)
-    return Path(
-        root_path,
-        'tests',
-        test_added_path.relative_to(root_path)
-    )
+def _switch_test_root(head_added_path: Path, root_path: Path) -> Path:
+    return Path(root_path, 'tests', head_added_path.relative_to(root_path))
+
+
+def _update_module_path(
+    call_context: PathPair,
+    root_names: Strs,
+    head_added_path: Path,
+    index: int,
+) -> bool:
+    root_path: Path = path_absolute(Path(*root_names[:index + 1]))
+    if not head_added_path.is_relative_to(root_path):
+        return False
+
+    module_path: Path = _switch_test_root(head_added_path, root_path)
+    if not module_path.exists():
+        return False
+
+    call_context.update({'module': module_path})
+    return True
 
 
 def _check_test_path(call_context: PathPair) -> bool:
-    TEST_HEAD: str = 'test' + '_'
+    HEAD: str = 'test' + '_'
+    if call_context['src'].name.startswith(HEAD):
+        return False
 
-    if not call_context['src'].name.startswith(TEST_HEAD):
-        test_module_path: Path = _replace_to_test_root(
-            _replace_file_name(TEST_HEAD, call_context['module']))
+    ROOT_NAMES: Strs = ['project', 'sparta']
+    head_added_path: Path = _replace_file_name(HEAD, call_context['module'])
 
-        if test_module_path.exists():
-            call_context.update({'module': test_module_path})
+    for i in range(len(ROOT_NAMES)):
+        if _update_module_path(call_context, ROOT_NAMES, head_added_path, i):
             return True
 
     return False
