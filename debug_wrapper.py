d28c3f314a8146a4fe7dac9d11b2a42cfe7f26ab
lyouta <lyouta@spartaproject.com>
2023-05-05 12:31:11 +0900

    Update: corresponded to inside API updating

--- debug_wrapper.py
+++ debug_wrapper.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from sys import exit, argv
+from pathlib import Path
 
 from contexts.string_context import Strs
 from scripts.call_module import call_function
@@ -11,7 +12,7 @@ def main() -> bool:
     arguments: Strs = argv
 
     if 2 == len(arguments):
-        return call_function(arguments[0], arguments[1])
+        return call_function(Path(arguments[0]), Path(arguments[1]))
 
     return True  # TODO: untestable
 
