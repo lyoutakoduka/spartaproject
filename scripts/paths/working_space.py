ed9998132211f8d0e10cab773a674716a6b2c5bb
lyouta <lyouta@spartaproject.com>
2023-05-12 12:59:34 +0900

    Feat: module to create current datetime folder

---
+++ scripts/paths/working_space.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from datetime import datetime
+
+from contexts.integer_context import Ints2
+from contexts.string_context import Strs
+from scripts.paths.get_absolute import path_absolute
+from scripts.paths.create_directory import path_mkdir
+from scripts.times.current_datetime import get_current_time
+
+
+def get_time_data(time: datetime) -> Ints2:
+    return [
+        [4, time.year],
+        [2, time.month],
+        [2, time.day],
+        [2, time.hour],
+        [2, time.minute],
+        [2, time.second],
+        [6, time.microsecond],
+    ]
+
+
+def current_working_space(root: Path, override: bool = False, jst: bool = False) -> Path:
+    time_texts: Strs = [
+        str(time_count).zfill(order)
+        for order, time_count in get_time_data(get_current_time(override=override, jst=jst))
+    ]
+
+    time_path: Path = path_absolute(Path(root, *time_texts))
+    path_mkdir(time_path)
+
+    return time_path
---
+++ tests/scripts/paths/test_working_space.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from scripts.paths.get_relative import path_relative
+from scripts.paths.working_space import current_working_space
+
+
+def test_pass() -> None:
+    NAME: str = '.trash'
+    EXPECTED: Path = Path(NAME, '2023', '04', '01', '00', '00', '00', '000000')
+
+    time_path: Path = current_working_space(Path(NAME), override=True)
+
+    assert time_path.exists()
+    assert EXPECTED == path_relative(time_path)
+
+
+def main() -> bool:
+    test_pass()
+    return True
