4a61e484cf0fcc8575d43af745c6c893e423ba49
lyouta <lyouta@spartaproject.com>
2023-05-14 09:01:58 +0900

    Update: communize initial value of arguments

--- scripts/paths/get_relative.py
+++ scripts/paths/get_relative.py
@@ -4,17 +4,16 @@
 from contexts.path_context import Path, Paths, PathPair
 
 
-def _default() -> Path:
-    return Path.cwd()
+def path_relative(absolute_path: Path, root_path: Path = Path()) -> Path:
+    if '.' == str(root_path):
+        root_path = Path.cwd()
 
-
-def path_relative(absolute_path: Path, root_path: Path = _default()) -> Path:
     return absolute_path.relative_to(root_path)
 
 
-def path_array_relative(absolute_paths: Paths, root_path: Path = _default()) -> Paths:
-    return [path_relative(path, root_path) for path in absolute_paths]
+def path_array_relative(absolute_paths: Paths, root_path: Path = Path()) -> Paths:
+    return [path_relative(path, root_path=root_path) for path in absolute_paths]
 
 
-def path_pair_relative(absolute_pair: PathPair, root_path: Path = _default()) -> PathPair:
-    return {key: path_relative(path, root_path) for key, path in absolute_pair.items()}
+def path_pair_relative(absolute_pair: PathPair, root_path: Path = Path()) -> PathPair:
+    return {key: path_relative(path, root_path=root_path) for key, path in absolute_pair.items()}
