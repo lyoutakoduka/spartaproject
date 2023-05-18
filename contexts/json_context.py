aad5ff767d1cd00f3743c46677822b0e313c8f4b
lyouta <lyouta@spartaproject.com>
2023-05-19 03:48:10 +0900

    Add: user defined list and dict types

--- contexts/json_context.py
+++ contexts/json_context.py
@@ -3,8 +3,29 @@
 
 from decimal import Decimal
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Union
 
 Single = None | bool | int | float | str | Decimal | Path
 
 Json = Single | Dict[str, 'Json'] | List['Json']
+
+Array = Union[
+    List[bool],
+    List[int],
+    List[float],
+    List[str],
+    List[Decimal],
+    List[Path],
+]
+
+Pair = Union[
+    Dict[str, bool],
+    Dict[str, int],
+    Dict[str, float],
+    Dict[str, str],
+    Dict[str, Decimal],
+    Dict[str, Path],
+]
+
+Array2 = List[Array]
+Pair2 = Dict[str, Pair]
