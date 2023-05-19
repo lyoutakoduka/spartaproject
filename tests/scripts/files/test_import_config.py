b349b95401fb84d77cb92e8b2b3148b45b2a79fe
lyouta <lyouta@spartaproject.com>
2023-05-20 08:17:05 +0900

    Refactor: represent input text by one liner

--- tests/scripts/files/test_import_config.py
+++ tests/scripts/files/test_import_config.py
@@ -8,7 +8,6 @@ from tempfile import TemporaryDirectory
 from contexts.config_context import Config, Basic
 from scripts.files.export_file import text_export
 from scripts.files.import_config import config_load, config_import
-from scripts.format_texts import format_indent
 
 
 def _get_section(input: str) -> Basic:
@@ -17,57 +16,42 @@ def _get_section(input: str) -> Basic:
 
 
 def test_bool() -> None:
-    INPUT: str = """
-        [section]
-        option = True
-    """
+    INPUT: str = "[section] \n option = True"
     assert _get_section(INPUT)
 
 
 def test_int() -> None:
-    INPUT: str = """
-        [section]
-        option = 1
-    """
-    assert 1 == _get_section(INPUT)
+    INPUT: str = "[section] \n option = 1"
+    EXPECTED: int = 1
+    assert EXPECTED == _get_section(INPUT)
 
 
 def test_decimal() -> None:
-    INPUT: str = """
-        [section]
-        option = 1.0
-    """
-    assert Decimal('1.0') == _get_section(INPUT)
+    INPUT: str = "[section] \n option = 1.0"
+    EXPECTED: Decimal = Decimal('1.0')
+    assert EXPECTED == _get_section(INPUT)
 
 
 def test_string() -> None:
-    INPUT: str = """
-        [section]
-        option = text
-    """
-    assert 'text' == _get_section(INPUT)
+    INPUT: str = "[section] \n option = text"
+    EXPECTED: str = 'text'
+    assert EXPECTED == _get_section(INPUT)
 
 
 def test_path() -> None:
-    INPUT: str = """
-        [section]
-        path = text
-    """
+    INPUT: str = "[section] \n path = text"
+    EXPECTED: Path = Path('text')
     config: Config = config_load(INPUT)
-    assert Path('text') == config['section']['path']
+    assert EXPECTED == config['section']['path']
 
 
 def test_import() -> None:
-    INPUT: str = """
-        [section]
-        option = text
-    """
-    input: str = format_indent(INPUT)
-
+    INPUT: str = "[section] \n option = text"
+    EXPECTED: str = 'text'
     with TemporaryDirectory() as tmp_path:
-        config_path: Path = text_export(Path(tmp_path, 'tmp.ini'), input)
+        config_path: Path = text_export(Path(tmp_path, 'tmp.ini'), INPUT)
         config: Config = config_import(config_path)
-        assert 'text' == config['section']['option']
+        assert EXPECTED == config['section']['option']
 
 
 def main() -> bool:
