56f4fc91d084effbbe65fce279427f3ff0cfead2
lyouta <lyouta@spartaproject.com>
2023-05-21 05:17:03 +0900

    Add: user defined type

--- contexts/string_context.py
+++ contexts/string_context.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 StrPair = Dict[str, str]
+StrTuple = Tuple[str, str]
 Strs = List[str]
 Trans = Dict[int, str]
 
