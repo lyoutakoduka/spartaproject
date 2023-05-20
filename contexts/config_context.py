ad6fb03604af189bd332b2a527c28547b2c01c6e
lyouta <lyouta@spartaproject.com>
2023-05-20 17:02:53 +0900

    Support: user defined type structured by same type

--- contexts/config_context.py
+++ contexts/config_context.py
@@ -5,10 +5,7 @@ from typing import Dict
 from decimal import Decimal
 from pathlib import Path
 
-_Default = bool | int | float | str
-_Extend = Decimal | Path
-
-Basic = _Default | _Extend
+Basic = bool | int | float | str | Decimal | Path
 
 _BasicPair = Dict[str, Basic]
 _BoolPair = Dict[str, bool]
@@ -18,9 +15,14 @@ _IntPair = Dict[str, int]
 _PathPair = Dict[str, Path]
 _StrPair = Dict[str, str]
 
-_Section = \
-    _BoolPair | _IntPair | _FloatPair | _StrPair |\
-    _DecPair | _PathPair |\
-    _BasicPair
+_Section = _BoolPair | _IntPair | _FloatPair | _StrPair | _DecPair | _PathPair | _BasicPair
 
-Config = Dict[str, _Section]
+_BasicPair2 = Dict[str, _Section]
+_BoolPair2 = Dict[str, _BoolPair]
+_DecPair2 = Dict[str, _DecPair]
+_FloatPair2 = Dict[str, _FloatPair]
+_IntPair2 = Dict[str, _IntPair]
+_PathPair2 = Dict[str, _PathPair]
+_StrPair2 = Dict[str, _StrPair]
+
+Config = _BoolPair2 | _IntPair2 | _FloatPair2 | _StrPair2 | _DecPair2 | _PathPair2 | _BasicPair2
