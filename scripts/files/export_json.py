c1d2e2167128b2607f078a2e1226430f1ddbcb10
lyouta <lyouta@spartaproject.com>
2023-05-21 14:39:57 +0900

    Unsupport: extend type that json can't treat

--- scripts/files/export_json.py
+++ scripts/files/export_json.py
@@ -1,39 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from decimal import Decimal
 from json import dumps
 from pathlib import Path
-from typing import List, Dict
 
-from contexts.json_context import Json, Single
+from contexts.json_context import Json
 from contexts.string_context import StrTuple
 from scripts.files.export_file import text_export
 
 
-def _convert_unknown(content: Single) -> Single:
-    if isinstance(content, Path):
-        return str(content)
-
-    if isinstance(content, Decimal):
-        return float(content)
-
-    return content
-
-
-def _serialize_json(content: Json) -> Json:
-    if isinstance(content, Dict):
-        return {key: _serialize_json(value) for key, value in content.items()}
-
-    if isinstance(content, List):
-        return [_serialize_json(value) for value in content]
-
-    return _convert_unknown(content)
-
-
 def json_dump(content: Json, compress: bool = False) -> str:
-    content = _serialize_json(content)
-
     separators: StrTuple | None = (',', ':') if compress else None
     indent: int | None = None if compress else 2
 
