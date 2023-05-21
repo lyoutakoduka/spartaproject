c5e5694a4b8eca8d3c428dad2daed6bc9fa2e1e2
lyouta <lyouta@spartaproject.com>
2023-05-21 14:47:14 +0900

    Rename: suitable module name

--- scripts/bools/compare_json.py
+++ scripts/bools/compare_json.py
@@ -6,4 +6,5 @@ from scripts.files.export_json import json_dump
 
 
 def is_same_dict(left: Json, right: Json) -> bool:
-    return 1 == len(list(set([json_dump(input, compress=True) for input in [left, right]])))
+    return 1 == len(
+        list(set([json_dump(input, compress=True) for input in [left, right]])))
