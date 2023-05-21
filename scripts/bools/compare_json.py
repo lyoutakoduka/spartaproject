e885fd8d5be125d2c9160e6db3934228ffefd794
lyouta <lyouta@spartaproject.com>
2023-05-21 14:59:58 +0900

    Rename: suitable function name

--- scripts/bools/compare_json.py
+++ scripts/bools/compare_json.py
@@ -5,7 +5,7 @@ from contexts.json_context import Json
 from scripts.files.export_json import json_dump
 
 
-def is_same_dict(left: Json, right: Json) -> bool:
+def is_same_json(left: Json, right: Json) -> bool:
     return 1 == len(list(set([
         json_dump(input, compress=True)
         for input in [left, right]
--- tests/scripts/bools/test_compare_dict.py
+++ tests/scripts/bools/test_compare_dict.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from project.sparta.scripts.bools.compare_json import is_same_dict
+from project.sparta.scripts.bools.compare_json import is_same_json
 
 
 def test_simple() -> None:
-    assert is_same_dict({'A': True}, {'A': True})
+    assert is_same_json({'A': True}, {'A': True})
 
 
 def test_nest() -> None:
-    assert is_same_dict({'A': {'B': True}}, {'A': {'B': True}})
+    assert is_same_json({'A': {'B': True}}, {'A': {'B': True}})
 
 
 def test_multi() -> None:
-    assert is_same_dict({'A': True, 'B': False}, {'B': False, 'A': True})
+    assert is_same_json({'A': True, 'B': False}, {'B': False, 'A': True})
 
 
 def test_array() -> None:
-    assert is_same_dict({'A': ['B', 'C']}, {'A': ['B', 'C']})
+    assert is_same_json({'A': ['B', 'C']}, {'A': ['B', 'C']})
 
 
 def test_type() -> None:
-    assert is_same_dict(
+    assert is_same_json(
         {
             'A': [None, True, 0, 0.1, 'test'],
         },
