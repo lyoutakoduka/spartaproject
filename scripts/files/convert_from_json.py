7045a05695f492b718c9279a4b111ce3f8d98e74
lyouta <lyouta@spartaproject.com>
2023-05-24 08:13:10 +0900

    Feat: convert to structured by same type from json

---
+++ scripts/files/convert_from_json.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from typing import List, Dict
+
+from contexts.bool_context import Bools, Bools2, BoolPair, BoolPair2
+from contexts.integer_context import Ints, Ints2, IntPair, IntPair2
+from contexts.string_context import Strs, Strs2, StrPair, StrPair2
+from contexts.decimal_context import Decimal, Decs, Decs2, DecPair, DecPair2
+from contexts.path_context import Path, Paths, Paths2, PathPair, PathPair2
+from contexts.json_context import Json, Single
+
+
+def _convert_unknown(input: Single, key: str) -> Single:
+    if isinstance(input, str):
+        if 'path' in key:
+            return Path(input)
+
+    if isinstance(input, float):
+        return Decimal(str(input))
+
+    return input
+
+
+def from_safe_json(input: Json, key: str = '') -> Json:
+    if isinstance(input, Dict):
+        return {
+            key: from_safe_json(value, key=key)
+            for key, value in input.items()
+        }
+
+    if isinstance(input, List):
+        return [from_safe_json(value) for value in input]
+
+    return _convert_unknown(input, key)
+
+
+def _to_decimal(input: float) -> Decimal:
+    return Decimal(str(input))
+
+
+def _to_path(input: str) -> Path:
+    return Path(input)
+
+
+def bool_array_from_json(input: Json) -> Bools:
+    if not isinstance(input, List):
+        return []
+    return [value for value in input if isinstance(value, bool)]
+
+
+def integer_array_from_json(input: Json) -> Ints:
+    if not isinstance(input, List):
+        return []
+    return [value for value in input if isinstance(value, int)]
+
+
+def string_array_from_json(input: Json) -> Strs:
+    if not isinstance(input, List):
+        return []
+    return [value for value in input if isinstance(value, str)]
+
+
+def decimal_array_from_json(input: Json) -> Decs:
+    if not isinstance(input, List):
+        return []
+    return [
+        _to_decimal(value) for value in input if isinstance(value, float)
+    ]
+
+
+def path_array_from_json(input: Json) -> Paths:
+    if not isinstance(input, List):
+        return []
+    return [_to_path(value) for value in input if isinstance(value, str)]
+
+
+def bool_pair_from_json(input: Json) -> BoolPair:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: value for key, value in input.items() if isinstance(value, bool)
+    }
+
+
+def integer_pair_from_json(input: Json) -> IntPair:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: value for key, value in input.items() if isinstance(value, int)
+    }
+
+
+def string_pair_from_json(input: Json) -> StrPair:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: value for key, value in input.items() if isinstance(value, str)
+    }
+
+
+def decimal_pair_from_json(input: Json) -> DecPair:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: _to_decimal(value)
+        for key, value in input.items()
+        if isinstance(value, float)
+    }
+
+
+def path_pair_from_json(input: Json) -> PathPair:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: _to_path(value)
+        for key, value in input.items()
+        if isinstance(value, str)
+    }
+
+
+def bool_array2_from_json(input: Json) -> Bools2:
+    if not isinstance(input, List):
+        return []
+    return [bool_array_from_json(value) for value in input]
+
+
+def integer_array2_from_json(input: Json) -> Ints2:
+    if not isinstance(input, List):
+        return []
+    return [integer_array_from_json(value) for value in input]
+
+
+def string_array2_from_json(input: Json) -> Strs2:
+    if not isinstance(input, List):
+        return []
+    return [string_array_from_json(value) for value in input]
+
+
+def decimal_array2_from_json(input: Json) -> Decs2:
+    if not isinstance(input, List):
+        return []
+    return [decimal_array_from_json(value) for value in input]
+
+
+def path_array2_from_json(input: Json) -> Paths2:
+    if not isinstance(input, List):
+        return []
+    return [path_array_from_json(value) for value in input]
+
+
+def bool_pair2_from_json(input: Json) -> BoolPair2:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: bool_pair_from_json(value) for key, value in input.items()
+    }
+
+
+def integer_pair2_from_json(input: Json) -> IntPair2:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: integer_pair_from_json(value) for key, value in input.items()
+    }
+
+
+def string_pair2_from_json(input: Json) -> StrPair2:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: string_pair_from_json(value) for key, value in input.items()
+    }
+
+
+def decimal_pair2_from_json(input: Json) -> DecPair2:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: decimal_pair_from_json(value) for key, value in input.items()
+    }
+
+
+def path_pair2_from_json(input: Json) -> PathPair2:
+    if not isinstance(input, Dict):
+        return {}
+    return {
+        key: path_pair_from_json(value) for key, value in input.items()
+    }
---
+++ tests/scripts/files/test_convert_from_json.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from decimal import Decimal
+from pathlib import Path
+from typing import List, Dict
+
+from contexts.json_context import Json, Single, Array, Array2, Pair, Pair2
+from scripts.files.convert_from_json import (
+    bool_array_from_json,
+    integer_array_from_json,
+    string_array_from_json,
+    decimal_array_from_json,
+    path_array_from_json,
+    bool_pair_from_json,
+    integer_pair_from_json,
+    string_pair_from_json,
+    decimal_pair_from_json,
+    path_pair_from_json,
+    bool_array2_from_json,
+    integer_array2_from_json,
+    string_array2_from_json,
+    decimal_array2_from_json,
+    path_array2_from_json,
+    bool_pair2_from_json,
+    integer_pair2_from_json,
+    string_pair2_from_json,
+    decimal_pair2_from_json,
+    path_pair2_from_json,
+    from_safe_json,
+)
+
+
+def _common_test(input: Single, result: Single) -> None:
+    assert input == result
+
+
+def _common_test_array(input: Single, result: Array) -> None:
+    _common_test(input, result[0])
+
+
+def _common_test_array2(input: Single, result: Array2) -> None:
+    _common_test_array(input, result[0])
+
+
+def _common_test_pair(input: Single, result: Pair) -> None:
+    _common_test(input, result['B'])
+
+
+def _common_test_pair2(input: Single, result: Pair2) -> None:
+    _common_test_pair(input, result['A'])
+
+
+def test_bool_array() -> None:
+    INPUT: bool = True
+    input1: Json = [INPUT]
+    input2: Json = [input1]
+    _common_test_array(INPUT, bool_array_from_json(input1))
+    _common_test_array2(INPUT, bool_array2_from_json(input2))
+
+
+def test_bool_pair() -> None:
+    INPUT: bool = True
+    input1: Json = {'B': INPUT}
+    input2: Json = {'A': input1}
+    _common_test_pair(INPUT, bool_pair_from_json(input1))
+    _common_test_pair2(INPUT, bool_pair2_from_json(input2))
+
+
+def test_integer_array() -> None:
+    INPUT: int = 1
+    input1: Json = [INPUT]
+    input2: Json = [input1]
+    _common_test_array(INPUT, integer_array_from_json(input1))
+    _common_test_array2(INPUT, integer_array2_from_json(input2))
+
+
+def test_integer_pair() -> None:
+    INPUT: int = 1
+    input1: Json = {'B': INPUT}
+    input2: Json = {'A': input1}
+    _common_test_pair(INPUT, integer_pair_from_json(input1))
+    _common_test_pair2(INPUT, integer_pair2_from_json(input2))
+
+
+def test_string_array() -> None:
+    INPUT: str = 'test'
+    input1: Json = [INPUT]
+    input2: Json = [input1]
+    _common_test_array(INPUT, string_array_from_json(input1))
+    _common_test_array2(INPUT, string_array2_from_json(input2))
+
+
+def test_string_pair() -> None:
+    INPUT: str = 'test'
+    input1: Json = {'B': INPUT}
+    input2: Json = {'A': input1}
+    _common_test_pair(INPUT, string_pair_from_json(input1))
+    _common_test_pair2(INPUT, string_pair2_from_json(input2))
+
+
+def test_decimal_array() -> None:
+    INPUT: Decimal = Decimal('1.0')
+    input1: Json = [float(INPUT)]
+    input2: Json = [input1]
+    _common_test_array(INPUT, decimal_array_from_json(input1))
+    _common_test_array2(INPUT, decimal_array2_from_json(input2))
+
+
+def test_decimal_pair() -> None:
+    INPUT: Decimal = Decimal('1.0')
+    input1: Json = {'B': float(INPUT)}
+    input2: Json = {'A': input1}
+    _common_test_pair(INPUT, decimal_pair_from_json(input1))
+    _common_test_pair2(INPUT, decimal_pair2_from_json(input2))
+
+
+def test_path_array() -> None:
+    INPUT: Path = Path('root')
+    input1: Json = [str(INPUT)]
+    input2: Json = [input1]
+    _common_test_array(INPUT, path_array_from_json(input1))
+    _common_test_array2(INPUT, path_array2_from_json(input2))
+
+
+def test_path_pair() -> None:
+    INPUT: Path = Path('root')
+    input1: Json = {'B': str(INPUT)}
+    input2: Json = {'A': input1}
+    _common_test_pair(INPUT, path_pair_from_json(input1))
+    _common_test_pair2(INPUT, path_pair2_from_json(input2))
+
+
+def test_tree() -> None:
+    INPUT_DECIMAL: Decimal = Decimal('1.0')
+    INPUT_PATH: Path = Path('root')
+    input: Json = {'A': {
+        'B': [None, float(INPUT_DECIMAL)], 'path': str(INPUT_PATH)
+    }}
+    result: Json = from_safe_json(input)
+
+    assert isinstance(result, Dict)
+    result_outside: Json = result['A']
+
+    assert isinstance(result_outside, Dict)
+    assert INPUT_PATH == result_outside['path']
+    result_inside: Json = result_outside['B']
+
+    assert isinstance(result_inside, List)
+    input_inside: Json = [None, INPUT_DECIMAL]
+    assert input_inside == result_inside
+
+
+def main() -> bool:
+    test_bool_array()
+    test_bool_pair()
+    test_integer_array()
+    test_integer_pair()
+    test_string_array()
+    test_string_pair()
+    test_decimal_array()
+    test_decimal_pair()
+    test_path_array()
+    test_path_pair()
+    test_tree()
+    return True
