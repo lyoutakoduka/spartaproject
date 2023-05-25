2725e7fe298f03c57b1a2bb0cbd0782ce730bb37
lyouta <lyouta@spartaproject.com>
2023-05-26 03:52:15 +0900

    Feat: module to create temporary single file

---
+++ scripts/paths/create_temporary_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from scripts.files.export_json import json_export
+from scripts.paths.create_directory_parent import create_directory_parent
+
+
+def create_temporary_file(file_root: Path) -> Path:
+    NAME: str = 'temporary'
+    file_path: Path = Path(file_root, NAME + '.json')
+    create_directory_parent(file_path)
+    return json_export(file_path, NAME)
---
+++ tests/scripts/paths/test_create_temporary_file.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Callable
+
+from scripts.paths.create_temporary_file import create_temporary_file
+
+
+def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
+    with TemporaryDirectory() as temporary_path:
+        function(Path(temporary_path))
+
+
+def test_pass() -> None:
+    def individual_test(temporary_path: Path) -> None:
+        EXPECTED: Path = Path(temporary_path, 'temporary.json')
+        file_path: Path = create_temporary_file(temporary_path)
+        assert EXPECTED == file_path
+        assert file_path.exists()
+
+    _inside_temporary_directory(individual_test)
+
+
+def main() -> bool:
+    test_pass()
+    return True
