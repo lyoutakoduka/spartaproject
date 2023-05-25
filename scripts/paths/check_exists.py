516565aa5673000d61851fd52af74ecf12dbe309
lyouta <lyouta@spartaproject.com>
2023-05-26 00:34:40 +0900

    Rename: suitable function names

--- scripts/paths/check_exists.py
+++ scripts/paths/check_exists.py
@@ -5,13 +5,13 @@ from contexts.bool_context import Bools, BoolPair
 from contexts.path_context import Path, Paths, PathPair
 
 
-def _exists(path: Path) -> bool:
+def _check_exists(path: Path) -> bool:
     return path.exists()
 
 
-def path_array_exists(paths: Paths) -> Bools:
-    return [_exists(path) for path in paths]
+def check_exists_array(paths: Paths) -> Bools:
+    return [_check_exists(path) for path in paths]
 
 
-def path_pair_exists(paths: PathPair) -> BoolPair:
-    return {key: _exists(path) for key, path in paths.items()}
+def check_exists_pair(paths: PathPair) -> BoolPair:
+    return {key: _check_exists(path) for key, path in paths.items()}
--- tests/scripts/paths/test_check_exists.py
+++ tests/scripts/paths/test_check_exists.py
@@ -4,7 +4,7 @@
 from contexts.bool_context import Bools, BoolPair
 from contexts.path_context import Path, Paths, PathPair
 from scripts.bools.compare_value import bool_compare_array, bool_compare_pair
-from scripts.paths.check_exists import path_array_exists, path_pair_exists
+from scripts.paths.check_exists import check_exists_array, check_exists_pair
 
 _CURRENT_PATH: Path = Path(__file__)
 _UNKNOWN_PATH: Path = _CURRENT_PATH.with_name('unknown.py')
@@ -14,7 +14,7 @@ def test_array() -> None:
     PATHS: Paths = [_CURRENT_PATH, _UNKNOWN_PATH]
     EXPECTS: Bools = [True, False]
 
-    assert bool_compare_array(EXPECTS, path_array_exists(PATHS))
+    assert bool_compare_array(EXPECTS, check_exists_array(PATHS))
 
 
 def test_pair() -> None:
@@ -25,7 +25,7 @@ def test_pair() -> None:
     }
     EXPECTS: BoolPair = {'R': True, 'G': False, 'B': True}
 
-    assert bool_compare_pair(EXPECTS, path_pair_exists(PATHS))
+    assert bool_compare_pair(EXPECTS, check_exists_pair(PATHS))
 
 
 def main() -> bool:
