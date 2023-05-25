1b936c6db9f1da247b0e6b78a67daee03c878276
lyouta <lyouta@spartaproject.com>
2023-05-26 00:48:03 +0900

    Rename: suitable function names

--- tests/scripts/paths/test_parent_directory.py
+++ tests/scripts/paths/test_parent_directory.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Callable
 
-from scripts.paths.parent_directory import create_parent_dir
+from scripts.paths.parent_directory import create_parent_directory
 
 
 def _inside_tmp_directory(func: Callable[[Path], None]) -> None:
@@ -16,7 +16,7 @@ def _inside_tmp_directory(func: Callable[[Path], None]) -> None:
 def test_pass() -> None:
     def individual_test(tmp_root: Path) -> None:
         EXPECTED: Path = Path(tmp_root, 'parent')
-        parent_path: Path = create_parent_dir(Path(EXPECTED, 'tmp.json'))
+        parent_path: Path = create_parent_directory(Path(EXPECTED, 'tmp.json'))
 
         assert EXPECTED == parent_path
         assert parent_path.exists()
