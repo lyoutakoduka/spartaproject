bc7b9bdd3e27b82c1c9e376930af7ac3b9f3d6cd
lyouta <lyouta@spartaproject.com>
2023-05-28 07:43:30 +0900

    Feat: module to record file history

---
+++ scripts/paths/safe_file_history.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from itertools import count
+
+from contexts.path_context import Path, PathPair2
+from contexts.json_context import Json
+from scripts.files.convert_to_json import multiple2_to_json
+from scripts.files.export_json import json_export
+from scripts.paths.working_space import current_working_space
+from scripts.times.current_datetime import get_current_time
+
+
+class FileHistory:
+    def __init__(self, history_path: Path = Path()) -> None:
+        self._history: PathPair2 = {}
+        self.history_path: Path = self._init_history_path(history_path)
+
+    def __del__(self) -> None:
+        self.pop_history()
+
+    def _init_history_path(self, path: Path) -> Path:
+        if '.' != str(path):
+            return path
+        return current_working_space(Path('.trash'), jst=True)
+
+    def _export_history(self, history: Json) -> Path:
+        return json_export(Path(self.history_path, 'rename.json'), history)
+
+    def pop_history(self) -> Path:
+        if 0 == len(self._history):
+            return self.history_path
+
+        history: Json = multiple2_to_json(self._history)
+        self._history.clear()
+        return self._export_history(history)
+
+    def _get_key_time(self) -> str:
+        time: str = get_current_time(jst=True).isoformat()
+
+        for i in count():
+            time_index: str = '_'.join([time, str(i).zfill(4)])
+            if time_index not in self._history:
+                return time_index
+        return ''
+
+    def add_history(self, source_path: Path, destination_path: Path) -> None:
+        self._history[self._get_key_time()] = {
+            'source': source_path,
+            'destination': destination_path,
+        }
---
+++ tests/scripts/paths/test_safe_file_history.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from tempfile import TemporaryDirectory
+
+from contexts.bool_context import Bools
+from contexts.path_context import Path, Paths2, PathPair2
+from scripts.bools.same_value import bool_same_array
+from scripts.files.convert_from_json import path_pair2_from_json
+from scripts.files.import_json import json_import
+from scripts.paths.safe_file_history import FileHistory
+
+
+def _compare_path_count(source: Paths2, destination: PathPair2) -> bool:
+    return 1 == len(set([len(history) for history in [source, destination]]))
+
+
+def _compare_path_name(source: Paths2, destination: PathPair2) -> bool:
+    same_paths: Bools = []
+    for lefts, (_, rights) in zip(source, sorted(destination.items())):
+        for i, type in enumerate(['source', 'destination']):
+            same_paths += [lefts[i] == rights[type]]
+    return bool_same_array(same_paths)
+
+
+def _common_test(source: Paths2, rename_path: Path) -> None:
+    destination: PathPair2 = path_pair2_from_json(json_import(rename_path))
+
+    assert _compare_path_count(source, destination)
+    assert _compare_path_name(source, destination)
+
+
+def _add_single_history(
+    file_history: FileHistory, source_history: Paths2, name: str,
+) -> None:
+    source_path: Path = Path(__file__).parent.with_name('source.json')
+    destination_path: Path = source_path.with_stem(name)
+    file_history.add_history(source_path, destination_path)
+    source_history += [[source_path, destination_path]]
+
+
+def test_single() -> None:
+    file_history = FileHistory()
+    source_history: Paths2 = []
+    _add_single_history(file_history, source_history, 'destination')
+
+    _common_test(source_history, file_history.pop_history())
+
+
+def test_array() -> None:
+    file_history = FileHistory()
+    source_history: Paths2 = []
+    for i in range(10):
+        _add_single_history(file_history, source_history, str(i).zfill(4))
+
+    _common_test(source_history, file_history.pop_history())
+
+
+def test_history() -> None:
+    with TemporaryDirectory() as temporary_path:
+        temporary_root = Path(temporary_path)
+        file_history = FileHistory(history_path=temporary_root)
+        source_history: Paths2 = []
+        _add_single_history(file_history, source_history, 'destination')
+
+        history_path: Path = file_history.pop_history()
+        _common_test(source_history, history_path)
+        assert history_path.is_relative_to(temporary_root)
+
+
+def main() -> bool:
+    test_single()
+    test_array()
+    test_history()
+    return True
