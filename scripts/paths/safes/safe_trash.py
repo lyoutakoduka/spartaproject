c2eb58866966f975254916c8a36e05c3de7f678c
lyouta <lyouta@spartaproject.com>
2023-05-29 04:34:54 +0900

    Relink: module import

--- scripts/paths/safes/safe_trash.py
+++ scripts/paths/safes/safe_trash.py
@@ -5,7 +5,7 @@ from shutil import move
 
 from contexts.path_context import Path, Paths
 from scripts.directories.create_directory_parent import create_directory_parent
-from scripts.directories.working_space import current_working_space
+from scripts.directories.create_directory_working import current_working_space
 from scripts.paths.get_relative import get_relative
 
 
