5ccaa7610a42a697c207bc1ac4b7d291949c166d
lyouta <lyouta@spartaproject.com>
2023-05-29 13:52:39 +0900

    Categorize: put together to directory [text]

--- test/script/file/text/test_import_file.py
+++ test/script/file/text/test_import_file.py
@@ -5,33 +5,32 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Callable
 
-from script.file.export_file import text_export, byte_export
+from script.file.text.export_file import text_export
+from script.file.text.import_file import text_import, byte_import
+
+_INPUT: str = 'test'
 
 
-def _common_test(text_path: Path, count: int) -> None:
-    assert text_path.stat().st_size == count
+def _common_test(result: str) -> None:
+    assert _INPUT == result
 
 
 def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
     with TemporaryDirectory() as temporary_path:
-        text_path: Path = Path(temporary_path, 'temporary.txt')
-        function(text_path)
+        function(text_export(Path(temporary_path, 'temporary.txt'), _INPUT))
 
 
 def test_text() -> None:
-    INPUT: str = 'test'
-
     def individual_test(text_path: Path) -> None:
-        _common_test(text_export(text_path, INPUT), len(INPUT))
+        _common_test(text_import(text_path))
 
     _inside_temporary_directory(individual_test)
 
 
 def test_byte() -> None:
-    INPUT: bytes = b'test'
-
     def individual_test(text_path: Path) -> None:
-        _common_test(byte_export(text_path, INPUT), len(INPUT))
+        result: bytes = byte_import(text_path)
+        _common_test(result.decode('utf-8'))
 
     _inside_temporary_directory(individual_test)
 
