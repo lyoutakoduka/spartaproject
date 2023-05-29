354b8643f1bd0db8ede78e41d0bd080818569bf2
lyouta <lyouta@spartaproject.com>
2023-05-29 14:11:06 +0900

    Relink: module import [path]

--- test/script/path/test_iterate_directory.py
+++ test/script/path/test_iterate_directory.py
@@ -6,9 +6,9 @@ from typing import Callable
 
 from context.default.string_context import Strs, Strs2
 from context.extension.path_context import Path, Paths, PathGene
-from script.path.create_temporary_tree import create_temporary_tree
 from script.path.get_relative import get_relative_array
 from script.path.iterate_directory import walk_iterator
+from script.path.temporary.create_temporary_tree import create_temporary_tree
 
 _TREE_DEEP: int = 3
 
