98614d24f93e94f5c44c5506e3c4a348a3ec8758
lyouta <lyouta@spartaproject.com>
2023-05-29 09:30:05 +0900

    Relink: module import [safes]

--- script/paths/safes/safe_file_history.py
+++ script/paths/safes/safe_file_history.py
@@ -8,7 +8,7 @@ from context.path_context import Path, PathPair2
 from script.directory.create_directory_working import current_working_space
 from script.files.jsons.convert_to_json import multiple2_to_json
 from script.files.jsons.export_json import json_export
-from script.times.current_datetime import get_current_time
+from script.time.current_datetime import get_current_time
 
 
 class FileHistory:
