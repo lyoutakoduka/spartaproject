73adb9353d13d07fd685a89cbcb9176e608737fa
lyouta <lyouta@spartaproject.com>
2023-05-29 09:18:11 +0900

    Relink: module import [jsons]

--- test/script/files/jsons/test_convert_from_json.py
+++ test/script/files/jsons/test_convert_from_json.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from typing import List, Dict
 
 from context.json_context import Json, Single, Array, Array2, Pair, Pair2
-from script.bools.same_value import bool_same_array
+from script.bool.same_value import bool_same_array
 from script.files.jsons.convert_from_json import (
     bool_array_from_json,
     integer_array_from_json,
