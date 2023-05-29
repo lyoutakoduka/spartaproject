dd0ebe888ba7694e315163b6497ee61e1555799a
lyouta <lyouta@spartaproject.com>
2023-05-29 09:40:07 +0900

    Relink: module import [bool]

--- script/bool/compare_json.py
+++ script/bool/compare_json.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from context.json_context import Json
-from script.files.jsons.export_json import json_dump
+from script.files.json.export_json import json_dump
 
 
 def is_same_json(left: Json, right: Json) -> bool:
