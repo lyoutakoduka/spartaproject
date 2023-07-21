73e77a2f38f92c9820a8c687b1400eda36bd25cc
lyouta <lyouta@spartaproject.com>
2023-07-21 09:18:44 +0900

    Feat: module to get shortcut path

---
+++ test/script/file/shortcut/test_get_shortcut_path.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from script.file.shortcut.get_shortcut_path import get_shortcut_path
+
+
+def test_name() -> None:
+    shortcut_target: Path = Path(__file__)
+    target_root: Path = shortcut_target.parent
+    expected: Path = shortcut_target.with_name(shortcut_target.name + '.lnk')
+
+    assert expected == get_shortcut_path(shortcut_target, target_root)
+
+
+def main() -> bool:
+    test_name()
+    return True
