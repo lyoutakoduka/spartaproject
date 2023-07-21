08d662cee985f04a89de64daf9041e45250a7bd8
lyouta <lyouta@spartaproject.com>
2023-07-21 09:21:19 +0900

    Add: test to check existing shortcut path

--- script/file/shortcut/create_shortcut.py
+++ script/file/shortcut/create_shortcut.py
@@ -6,12 +6,16 @@ from win32com.client import Dispatch
 from win32com.client.dynamic import CDispatch
 
 
-def create_shortcut(shortcut_target: Path, target_root: Path) -> Path:
-    shortcut_path: Path = Path(target_root, shortcut_target.name + '.lnk')
+def create_shortcut(shortcut_target: Path, shortcut_path: Path) -> bool:
+    if not shortcut_target.exists():
+        raise FileNotFoundError(shortcut_target)
+
+    if shortcut_path.exists():
+        return False
 
     shell: CDispatch = Dispatch('WScript.Shell')
     shortcut: CDispatch = shell.CreateShortcut(shortcut_path.as_posix())
     shortcut.Targetpath = 'file:/' + shortcut_target.as_posix()
     shortcut.Save()
 
-    return shortcut_path
+    return True
--- test/script/file/shortcut/test_create_shortcut.py
+++ test/script/file/shortcut/test_create_shortcut.py
@@ -3,15 +3,18 @@
 
 from pathlib import Path
 from platform import uname
+from pytest import raises
 from tempfile import TemporaryDirectory
 from typing import Callable
 
 from script.file.shortcut.create_shortcut import create_shortcut
+from script.file.shortcut.get_shortcut_path import get_shortcut_path
 from script.path.temporary.create_temporary_file import create_temporary_file
 
 
 def _common_test(shortcut_target: Path, shortcut_root: Path) -> None:
-    shortcut_path: Path = create_shortcut(shortcut_target, shortcut_root)
+    shortcut_path: Path = get_shortcut_path(shortcut_target, shortcut_root)
+    create_shortcut(shortcut_target, shortcut_path)
 
     assert shortcut_path.exists()
     assert shortcut_target.name == shortcut_path.stem
@@ -35,6 +38,15 @@ def test_file() -> None:
 def test_directory() -> None:
     def individual_test(temporary_root: Path) -> None:
         _common_test(temporary_root, temporary_root)
+
+    _inside_temporary_directory(individual_test)
+
+
+def test_exist() -> None:
+    def individual_test(temporary_root: Path) -> None:
+        empty_path: Path = Path('empty')
+        with raises(FileNotFoundError, match=str(empty_path)):
+            _common_test(empty_path, temporary_root)
 
     _inside_temporary_directory(individual_test)
 
