0f717ff35a870817635ef2cecdc0b1ee305abe37
lyouta <lyouta@spartaproject.com>
2023-08-06 05:04:24 +0900

    Feat: module to connect server

---
+++ script/server/connect_server.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from paramiko import SSHClient, SFTPClient, AutoAddPolicy, Channel
+from pathlib import Path
+from time import sleep
+from typing import Dict
+
+from context.default.integer_context import IntPair
+from context.default.string_context import Strs, Strs2, StrPair
+from context.extension.decimal_context import Decimal, set_decimal_context
+from context.file.json_context import Json
+from script.file.json.import_json import json_import
+from script.path.modify.get_absolute import get_absolute
+
+set_decimal_context()
+
+
+class ConnectServer:
+    def _filter_condition(self, key: str, ftp_context: Json) -> Json | None:
+        if not isinstance(ftp_context, Dict):
+            return None
+
+        if key not in ftp_context:
+            return None
+
+        return ftp_context[key]
+
+    def _get_type_text(self, ftp_context: Json) -> StrPair:
+        context_texts: StrPair = {}
+
+        for key in [
+            'host', 'username', 'privateKeyPath', 'remotePath', 'context'
+        ]:
+            if value := self._filter_condition(key, ftp_context):
+                if isinstance(value, str):
+                    context_texts[key] = value
+
+        return context_texts
+
+    def _get_type_number(self, ftp_context: Json) -> IntPair:
+        context_numbers: IntPair = {}
+
+        for key in ['connectTimeout', 'port']:
+            if value := self._filter_condition(key, ftp_context):
+                if isinstance(value, int):
+                    context_numbers[key] = value
+
+        return context_numbers
+
+    def _get_ftp_context(self) -> Json:
+        ftp_context: Json = json_import(
+            get_absolute(Path('.vscode', 'sftp.json'))
+        )
+
+        self._texts: StrPair = self._get_type_text(ftp_context)
+        self._numbers: IntPair = self._get_type_number(ftp_context)
+
+    def _initialize_connect(self) -> None:
+        self._ssh: SSHClient | None = None
+        self._shell: Channel | None = None
+        self._sftp: SFTPClient | None = None
+
+    def __init__(self) -> None:
+        self._initialize_connect()
+        self._get_ftp_context()
+
+        self._EXPECTED: Strs = ['private', 'public']
+
+    def __del__(self) -> None:
+        if self._ssh is not None:
+            self._ssh.close()
+
+        if self._sftp is not None:
+            self._sftp.close()
+
+        self._initialize_connect()
+
+    def _create_ssh(self) -> None:
+        self._ssh = SSHClient()
+
+        self._ssh.load_system_host_keys()
+        self._ssh.set_missing_host_key_policy(AutoAddPolicy())
+
+        milliseconds: int = self._numbers['connectTimeout']
+        seconds: Decimal = Decimal(str(milliseconds)) / Decimal('1000.0')
+
+        self._ssh.connect(
+            hostname=self._texts['host'],
+            port=self._numbers['port'],
+            username=self._texts['username'],
+            key_filename=self._texts['privateKeyPath'],
+            timeout=float(seconds)
+        )
+
+    def _sleep(self) -> None:
+        sleep(0.01)
+
+    def _receive_ssh(self) -> Strs:
+        if self._shell is None:
+            return []
+
+        self._sleep()
+
+        while not self._shell.recv_ready():
+            self._sleep()
+
+        byte: bytes = self._shell.recv(9999)
+        text: str = byte.decode('utf-8')
+
+        lines: Strs = text.splitlines()
+        return lines[2:-1]
+
+    def _execute_ssh(self, commands: Strs) -> None:
+        if self._shell is not None:
+            command: str = ' '.join(commands) + '\n'
+            self._shell.send(command.encode('utf-8'))
+
+    def _ssh_remote_path(self) -> None:
+        self._execute_ssh(['cd', self._texts['remotePath']])
+        self._receive_ssh()
+
+    def _correct_path(self, expected: Strs, result: Strs) -> bool:
+        name_sorted: Strs2 = [sorted(name) for name in [expected, result]]
+
+        return name_sorted[0] == name_sorted[1]
+
+    def _ssh_correct_path(self) -> bool:
+        self._execute_ssh(['ls', '-1', '-p'])
+
+        return self._correct_path(
+            [name + '/' for name in self._EXPECTED], self._receive_ssh()
+        )
+
+    def _connect_ssh(self) -> bool:
+        self._create_ssh()
+
+        if self._ssh is not None:
+            self._shell = self._ssh.invoke_shell()
+
+        self._receive_ssh()
+
+        self._ssh_remote_path()
+        return self._ssh_correct_path()
+
+    def _receive_sftp(self) -> Strs:
+        if self._sftp is None:
+            return []
+
+        return self._sftp.listdir()
+
+    def _sftp_correct_path(self) -> bool:
+        if self._sftp is not None:
+            return self._correct_path(self._EXPECTED, self._receive_sftp())
+
+        return False
+
+    def _sftp_remote_path(self) -> None:
+        if self._sftp is not None:
+            self._sftp.chdir(self._texts['remotePath'])
+
+    def _create_sftp(self) -> None:
+        if self._ssh is not None:
+            self._sftp = self._ssh.open_sftp()
+
+    def _connect_sftp(self) -> bool:
+        self._create_sftp()
+
+        self._sftp_remote_path()
+        return self._sftp_correct_path()
+
+    def connect(self) -> bool:
+        if self._connect_ssh():
+            if self._connect_sftp():
+                return True
+
+        return False
---
+++ test/script/server/test_connect_server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from script.server.connect_server import ConnectServer
+
+
+def test_connect() -> None:
+    server: ConnectServer = ConnectServer()
+    assert server.connect()
+
+
+def main() -> bool:
+    test_connect()
+    return True
