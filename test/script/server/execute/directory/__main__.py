7746cce62ecfd73c59fd91eedcd7973ee90451ac
lyouta <lyouta@spartaproject.com>
2023-08-30 08:05:09 +0900

    Feat: test code for python execution through ssh

---
+++ test/script/server/execute/directory/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main() -> None:
+    for i in range(3):
+        print('directory' + str(i))
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/script/server/execute/file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main() -> None:
+    for i in range(3):
+        print('file' + str(i))
+
+
+if __name__ == "__main__":
+    main()
