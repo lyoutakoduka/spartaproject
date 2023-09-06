0c8ed7f3548a8c65fe1197789f7c7e5e6137db25
lyouta <lyouta@spartaproject.com>
2023-09-06 15:02:22 +0900

    Feat: method to convert path to string

--- script/server/context_server.py
+++ script/server/context_server.py
@@ -61,3 +61,7 @@ class ContextServer:
     def get_path(self, type: str) -> Path:
         context: PathPair = path_pair_from_json(self._current_context)
         return context[type]
+
+    def _get_path_string(self, type: str) -> str:
+        path: Path = self.get_path(type)
+        return path.as_posix()
