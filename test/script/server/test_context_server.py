266f88268082b412a84ec2a5f1862a98b0cb9de5
lyouta <lyouta@spartaproject.com>
2023-09-07 21:09:46 +0900

    Remove: test of unused method

--- test/script/server/test_context_server.py
+++ test/script/server/test_context_server.py
@@ -48,18 +48,9 @@ def test_path() -> None:
     _common_test(individual_test)
 
 
-def test_path_string() -> None:
-    def individual_test(server: ContextServer) -> None:
-        type: str = 'private_key'
-        assert server.get_path(type) == Path(server.get_path_string(type))
-
-    _common_test(individual_test)
-
-
 def main() -> bool:
     test_key()
     test_integer()
     test_string()
     test_path()
-    test_path_string()
     return True
