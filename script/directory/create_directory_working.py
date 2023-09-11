9c3586ef4481260cc6d10effc8938bcab7f42891
lyouta <lyouta@spartaproject.com>
2023-09-11 19:22:55 +0900

    Feat: function to get directory for working space

--- script/directory/create_directory_working.py
+++ script/directory/create_directory_working.py
@@ -23,14 +23,18 @@ def _get_time_data(time: datetime) -> Ints2:
     ]
 
 
-def create_working_space(
-    root: Path, override: bool = False, jst: bool = False
-) -> Path:
-    time_texts: Strs = [
+def get_working_space(override: bool = False, jst: bool = False) -> Strs:
+    return [
         str(time_count).zfill(order)
         for order, time_count in _get_time_data(
             get_current_time(override=override, jst=jst)
         )
     ]
 
+
+def create_working_space(
+    root: Path, override: bool = False, jst: bool = False
+) -> Path:
+    time_texts: Strs = get_working_space(override=override, jst=jst)
+    print(time_texts)
     return create_directory(get_absolute(Path(root, *time_texts)))
