445ab0690048009f05672fe7b1e1dc7999de44a4
lyouta <lyouta@spartaproject.com>
2023-09-11 18:14:18 +0900

    Refactor: create working space for each test

--- test/script/server/test_upload_server.py
+++ test/script/server/test_upload_server.py
@@ -15,23 +15,25 @@ def _common_test(server: UploadServer, source_path: Path) -> None:
 
 
 def _inside_temporary_directory(
-    function: Callable[[UploadServer, Path], None]
+    function: Callable[[UploadServer], None]
 ) -> None:
     server: UploadServer = UploadServer()
 
     if server.connect():
-        function(server, server.get_working_space())
+        function(server)
 
 
 def test_file() -> None:
-    def individual_test(server: UploadServer, temporary_path: Path) -> None:
+    def individual_test(server: UploadServer) -> None:
+        temporary_path: Path = server.get_working_space()
         _common_test(server, create_temporary_file(temporary_path))
 
     _inside_temporary_directory(individual_test)
 
 
 def test_directory() -> None:
-    def individual_test(server: UploadServer, temporary_path: Path) -> None:
+    def individual_test(server: UploadServer) -> None:
+        temporary_path: Path = server.get_working_space()
         source_path: Path = create_directory(Path(temporary_path, 'directory'))
         _common_test(server, source_path)
 
@@ -39,7 +41,8 @@ def test_directory() -> None:
 
 
 def test_tree() -> None:
-    def individual_test(server: UploadServer, temporary_path: Path) -> None:
+    def individual_test(server: UploadServer) -> None:
+        temporary_path: Path = server.get_working_space()
         source_path: Path = Path(temporary_path, 'tree')
         create_temporary_tree(source_path, tree_deep=2)
         _common_test(server, source_path)
