8c7eb125d639fc6e12718110301036507e396261
lyouta <lyouta@spartaproject.com>
2023-09-17 23:26:35 +0900

    Doc: about module and function

--- script/execute/debug_empty.py
+++ script/execute/debug_empty.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Performance test for only function call."""
+
+
 def main() -> bool:
+    """Just return True.
+
+    #! No test code
+
+    Returns:
+        bool: success if get to the end of function
+    """
     return True
