a594cb9363cb7a56fbf1b678b59212f593216e27
lyouta <lyouta@spartaproject.com>
2023-09-18 00:22:30 +0900

    Doc: about module and function

--- test/script/execute/test_call_module.py
+++ test/script/execute/test_call_module.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Call designated function of designated module."""
+
 from pathlib import Path
 
 from pytest import raises
@@ -11,20 +13,28 @@ _UNKNOWN: str = 'unknown'
 
 
 def test_unknown_module() -> None:
+    """Unknown function calling of designated module."""
     error_path = Path(_SOURCE_PATH).with_name(_UNKNOWN + '.py')
     with raises(FileNotFoundError, match='unknown'):
         call_function(_SOURCE_PATH, error_path)
 
 
 def test_unknown_function() -> None:
+    """Main function calling of unknown module."""
     with raises(ModuleNotFoundError, match=_UNKNOWN):
         call_function(_SOURCE_PATH, _SOURCE_PATH, function=_UNKNOWN)
 
 
 def test_pass() -> None:
+    """Main function calling of designated module."""
     assert call_function(_SOURCE_PATH, _SOURCE_PATH)
 
 
 def main() -> bool:
+    """Test all public functions.
+
+    Returns:
+        bool: success if get to the end of function
+    """
     test_pass()
     return True
