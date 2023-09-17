8f37fac2f0587d41a1d4c335c67054c3b8371167
lyouta <lyouta@spartaproject.com>
2023-09-18 04:16:00 +0900

    Sort: import order in ascending order [safe]

--- test/script/path/safe/test_safe_file_history.py
+++ test/script/path/safe/test_safe_file_history.py
@@ -4,7 +4,7 @@
 from tempfile import TemporaryDirectory
 
 from context.default.bool_context import Bools
-from context.extension.path_context import Path, Paths2, PathPair2
+from context.extension.path_context import Path, PathPair2, Paths2
 from script.bool.same_value import bool_same_array
 from script.file.json.convert_from_json import path_pair2_from_json
 from script.file.json.import_json import json_import
