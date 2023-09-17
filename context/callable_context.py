cd5b31e0dad374a85f3e7a9622f9902e37c030d9
lyouta <lyouta@spartaproject.com>
2023-09-18 03:57:39 +0900

    Sort: import order in ascending order [context]

--- context/callable_context.py
+++ context/callable_context.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import TypeVar, ParamSpec
 from typing import Callable  # type: ignore #TODO don't use here, but necessary
+from typing import ParamSpec, TypeVar
 
 CR = TypeVar('CR')
 CP = ParamSpec('CP')
