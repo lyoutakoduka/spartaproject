a25b0fa6008d48c92ad36f83e6a79c69716e7a2f
lyouta <lyouta@spartaproject.com>
2023-09-18 04:16:45 +0900

    Sort: import order in ascending order [path]

--- test/script/path/test_check_exists.py
+++ test/script/path/test_check_exists.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from context.default.bool_context import Bools, BoolPair
-from context.extension.path_context import Path, Paths, PathPair
+from context.default.bool_context import BoolPair, Bools
+from context.extension.path_context import Path, PathPair, Paths
 from script.bool.compare_value import bool_compare_array, bool_compare_pair
 from script.path.check_exists import check_exists_array, check_exists_pair
 
--- test/script/path/test_iterate_directory.py
+++ test/script/path/test_iterate_directory.py
@@ -5,7 +5,7 @@ from tempfile import TemporaryDirectory
 from typing import Callable
 
 from context.default.string_context import Strs, Strs2
-from context.extension.path_context import Path, Paths, PathGene
+from context.extension.path_context import Path, PathGene, Paths
 from script.path.iterate_directory import walk_iterator
 from script.path.modify.get_relative import get_relative_array
 from script.path.temporary.create_temporary_tree import create_temporary_tree
