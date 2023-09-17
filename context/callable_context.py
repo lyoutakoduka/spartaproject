19760b8df08a064e0779740c3267fb11468d0d5a
lyouta <lyouta@spartaproject.com>
2023-09-18 04:37:06 +0900

    Doc: ignore linter error

--- context/callable_context.py
+++ context/callable_context.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import Callable  # type: ignore # Don't use here, but necessary
+from typing import Callable  # ! # noqa: F401 # type: ignore
 from typing import ParamSpec, TypeVar
 
 CR = TypeVar('CR')
