6c9f09ff26a37bc8444ab64bd47edb172f1548ac
lyouta <lyouta@spartaproject.com>
2023-09-18 03:58:33 +0900

    Doc: remove unnecessary information

--- context/callable_context.py
+++ context/callable_context.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import Callable  # type: ignore #TODO don't use here, but necessary
+from typing import Callable  # type: ignore # Don't use here, but necessary
 from typing import ParamSpec, TypeVar
 
 CR = TypeVar('CR')
