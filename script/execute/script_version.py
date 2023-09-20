5c0a1364424146afebe86c344604f7c660c44e35
lyouta <lyouta@spartaproject.com>
2023-09-21 06:00:38 +0900

    Feat: function to convert version from string

--- script/execute/script_version.py
+++ script/execute/script_version.py
@@ -7,3 +7,7 @@ from spartaproject.context.default.integer_context import Ints
 
 def version_to_string(versions: Ints) -> str:
     return '.'.join([str(number) for number in versions])
+
+
+def version_from_string(version: str) -> Ints:
+    return [int(number) for number in version.split('.')]
--- test/script/execute/test_script_version.py
+++ test/script/execute/test_script_version.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from spartaproject.script.execute.script_version import version_to_string
+from spartaproject.script.execute.script_version import (version_from_string,
+                                                         version_to_string)
 
 
 def test_string() -> None:
     EXPECTED: str = '0.0.0'
     assert EXPECTED == version_to_string([0, 0, 0])
+
+
+def test_number() -> None:
+    EXPECTED: Ints = [0, 0, 0]
+    assert EXPECTED == version_from_string('0.0.0')
 
 
 def main() -> bool:
@@ -16,4 +22,5 @@ def main() -> bool:
         bool: success if get to the end of function
     """
     test_string()
+    test_number()
     return True
