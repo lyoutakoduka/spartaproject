ea59f8da70e5e13dc0154c54234b0e12401deae5
lyouta <lyouta@spartaproject.com>
2023-09-22 10:20:27 +0900

    Refactor: formatting by Black

--- setup.py
+++ setup.py
@@ -5,39 +5,35 @@ from codecs import open
 
 from setuptools import setup
 
-_PACKAGE_NAME = 'pyspartaproj'
-_EMAIL = 'lyouta@spartaproject.com'
-_AUTHOR_NAME = 'lyouta'
-_SEARCH_KEYWORDS = 'sparta spartaproject pyspartaproj'
-_PACKAGE_VERSION = '0.0.1'
-_PACKAGE_DESCRIPTION = 'spartaproject'
-_PRODUCT_URL = 'https://github.com/lyoutakoduka'
-_CONTENT_TYPE = 'text/markdown'
-_LICENSE_TYPE = 'MIT'
+_PACKAGE_NAME = "pyspartaproj"
+_EMAIL = "lyouta@spartaproject.com"
+_AUTHOR_NAME = "lyouta"
+_SEARCH_KEYWORDS = "sparta spartaproject pyspartaproj"
+_PACKAGE_VERSION = "0.0.1"
+_PACKAGE_DESCRIPTION = "spartaproject"
+_PRODUCT_URL = "https://github.com/lyoutakoduka"
+_CONTENT_TYPE = "text/markdown"
+_LICENSE_TYPE = "MIT"
 
-_README_PATH = 'README.md'
+_README_PATH = "README.md"
 
-_INSTALL_REQUIRES = [
-    'PySide6>=6.4.3',
-    'PyOpenGL>=3.1.6',
-    'numpy>=1.24.2'
-]
+_INSTALL_REQUIRES = ["PySide6>=6.4.3", "PyOpenGL>=3.1.6", "numpy>=1.24.2"]
 
 _CLASSIFIERS = [
-    'Development Status :: 1 - Planning',
-    'Environment :: GPU',
-    'Environment :: X11 Applications :: Qt',
-    'License :: OSI Approved :: MIT License',
-    'Natural Language :: English',
-    'Operating System :: Microsoft :: Windows :: Windows 11',
-    'Operating System :: POSIX :: Linux',
-    'Programming Language :: Python :: 3.11',
-    'Programming Language :: C++',
-    'Topic :: Artistic Software',
-    'Topic :: Multimedia :: Graphics :: 3D Modeling'
+    "Development Status :: 1 - Planning",
+    "Environment :: GPU",
+    "Environment :: X11 Applications :: Qt",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: Microsoft :: Windows :: Windows 11",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: C++",
+    "Topic :: Artistic Software",
+    "Topic :: Multimedia :: Graphics :: 3D Modeling",
 ]
 
-_ENCODING_TYPE = 'utf-8'
+_ENCODING_TYPE = "utf-8"
 
 
 with open(_README_PATH, encoding=_ENCODING_TYPE) as file:
@@ -56,5 +52,5 @@ setup(
     keywords=_SEARCH_KEYWORDS,
     packages=[_PACKAGE_NAME],
     install_requires=_INSTALL_REQUIRES,
-    classifiers=_CLASSIFIERS
+    classifiers=_CLASSIFIERS,
 )
