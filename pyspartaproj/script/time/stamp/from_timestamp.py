f4eb66c086d27b7cfd57931740732231e8dd7833
lyouta <lyouta@spartaproject.com>
2023-09-27 04:41:57 +0900

    Refactor: format text by Black and isort.

--- pyspartaproj/script/time/stamp/from_timestamp.py
+++ pyspartaproj/script/time/stamp/from_timestamp.py
@@ -9,4 +9,4 @@ from dateutil.tz import gettz
 
 def time_from_timestamp(timestamp: Decimal, jst: bool = False) -> datetime:
     date_time: datetime = datetime.fromtimestamp(float(timestamp))
-    return date_time.astimezone(gettz('Asia/Tokyo' if jst else 'UTC'))
+    return date_time.astimezone(gettz("Asia/Tokyo" if jst else "UTC"))
