5e8797efee9a75a07f4792b8e3f48397d5ee3b24
lyouta <lyouta@spartaproject.com>
2023-09-27 15:38:26 +0900

    Refactor: format text by Black and isort.

--- test/script/path/safe/test_safe_trash.py
+++ test/script/path/safe/test_safe_trash.py
@@ -8,16 +8,19 @@ from typing import Callable
 from pyspartaproj.context.default.bool_context import BoolPair
 from pyspartaproj.context.extension.path_context import PathPair2, Paths
 from pyspartaproj.script.bool.same_value import bool_same_array
-from pyspartaproj.script.file.json.convert_from_json import \
-    path_pair2_from_json
+from pyspartaproj.script.file.json.convert_from_json import (
+    path_pair2_from_json,
+)
 from pyspartaproj.script.file.json.import_json import json_import
 from pyspartaproj.script.path.check_exists import check_exists_pair
 from pyspartaproj.script.path.iterate_directory import walk_iterator
 from pyspartaproj.script.path.safe.safe_trash import SafeTrash
-from pyspartaproj.script.path.temporary.create_temporary_file import \
-    create_temporary_file
-from pyspartaproj.script.path.temporary.create_temporary_tree import \
-    create_temporary_tree
+from pyspartaproj.script.path.temporary.create_temporary_file import (
+    create_temporary_file,
+)
+from pyspartaproj.script.path.temporary.create_temporary_tree import (
+    create_temporary_tree,
+)
 
 
 def _common_test(history_size: int, history_path: Path) -> None:
@@ -26,9 +29,9 @@ def _common_test(history_size: int, history_path: Path) -> None:
 
     for _, path_pair in history.items():
         exists_pair: BoolPair = check_exists_pair(path_pair)
-        assert bool_same_array([
-            not exists_pair['source'], exists_pair['destination']
-        ])
+        assert bool_same_array(
+            [not exists_pair["source"], exists_pair["destination"]]
+        )
 
 
 def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
@@ -72,6 +75,7 @@ def test_tree() -> None:
 
 def test_select() -> None:
     with TemporaryDirectory() as temporary_path:
+
         def individual_test(temporary_root: Path) -> None:
             create_temporary_tree(temporary_root)
 
