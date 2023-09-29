dfafec778e6da8f9fac14180a7713318500bf514
lyouta <lyouta@spartaproject.com>
2023-09-30 00:42:55 +0900

    Refactor: move method position to suitable

--- pyspartaproj/script/server/local/context_server.py
+++ pyspartaproj/script/server/local/context_server.py
@@ -18,25 +18,6 @@ from pyspartaproj.script.file.json.import_json import json_import
 
 
 class ContextServer:
-    def get_context_table(self, type: str) -> Strs:
-        if "integer" == type:
-            context_integer: IntPair = integer_pair_from_json(
-                self._current_context
-            )
-            return list(context_integer.keys())
-
-        elif "string" == type:
-            context_string: StrPair = string_pair_from_json(
-                self._current_context
-            )
-            return list(context_string.keys())
-
-        elif "path" == type:
-            context_path: PathPair = path_pair_from_json(self._current_context)
-            return list(context_path.keys())
-
-        return []
-
     def revert_default(self) -> None:
         self._current_context = deepcopy(self._default_context)
 
@@ -61,6 +42,25 @@ class ContextServer:
     def get_path(self, type: str) -> Path:
         context: PathPair = path_pair_from_json(self._current_context)
         return context[type]
+
+    def get_context_table(self, type: str) -> Strs:
+        if "integer" == type:
+            context_integer: IntPair = integer_pair_from_json(
+                self._current_context
+            )
+            return list(context_integer.keys())
+
+        elif "string" == type:
+            context_string: StrPair = string_pair_from_json(
+                self._current_context
+            )
+            return list(context_string.keys())
+
+        elif "path" == type:
+            context_path: PathPair = path_pair_from_json(self._current_context)
+            return list(context_path.keys())
+
+        return []
 
     def set_path(self, type: str, path: Path) -> bool:
         if type in self.get_context_table("path"):
