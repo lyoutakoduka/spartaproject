79d0eee8a4ed854b05c22752d5f93b2257d2839e
lyouta <lyouta@spartaproject.com>
2023-09-29 12:17:06 +0900

    Refactor: rename variable to suitable

--- pyspartaproj/script/file/config/export_config.py
+++ pyspartaproj/script/file/config/export_config.py
@@ -9,14 +9,14 @@ from pyspartaproj.context.file.config_context import Config
 from pyspartaproj.script.file.text.export_file import text_export
 
 
-def _cleanup_text(test: str, compress: bool) -> str:
-    test = test.strip()
+def _cleanup_text(text: str, compress: bool) -> str:
+    text = text.strip()
 
     if not compress:
-        return test
+        return text
 
-    test = test.replace("\n" * 2, "\n")
-    return test.replace(" = ", "=")
+    text = text.replace("\n" * 2, "\n")
+    return text.replace(" = ", "=")
 
 
 def config_dump(input: Config, compress: bool = False) -> str:
