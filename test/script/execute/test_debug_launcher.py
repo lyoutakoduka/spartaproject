72464aaac36851c3b320da8b4c664e09daebbdf8
lyouta <lyouta@spartaproject.com>
2023-10-01 22:02:25 +0900

    Feat: test for executing selected script

---
+++ test/script/execute/test_debug_launcher.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Connector to for execution and debugging from VSCode."""
+
+
+from pyspartaproj.script.execute.debug_launcher import debug_test
+
+
+def test_callable() -> None:
+    """Used for test for function call."""
+    assert debug_test()
+
+
+def main() -> bool:
+    """Test all public functions.
+
+    Returns:
+        bool: success if get to the end of function
+    """
+    test_callable()
+    return True
