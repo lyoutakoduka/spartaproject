b2640e8f33a4b61a921c140a1e6632eef2dd36de
lyouta <lyouta@spartaproject.com>
2023-10-02 16:41:19 +0900

    Feat: module to manage feature flags

---
+++ pyspartaproj/script/execute/feature_flags.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Module to manage feature flags."""
+
+from logging import Formatter, Handler, Logger, StreamHandler, getLogger
+
+
+def _config_logger(handler_name: str, handler: Handler) -> Logger:
+    logger: Logger = getLogger(handler_name)
+    logger.addHandler(handler)
+    handler.setFormatter(
+        Formatter(f"%(levelname)s (%(asctime)s) [%(name)s] %(message)s")
+    )
+    return logger
+
+
+def in_development(file: str | None = None) -> bool:
+    """Check feature flag.
+
+    Args:
+        flag (str | None, optional): Defaults to None.
+            file path is required if in development.
+
+    Returns:
+        bool: return True if in development.
+    """
+    if file is None:
+        return False
+
+    _config_logger("feature_flags", StreamHandler()).info(file)
+
+    return True
---
+++ test/script/execute/test_feature_flags.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test of feature flags module."""
+
+from pyspartaproj.script.execute.feature_flags import in_development
+
+
+def test_develop() -> None:
+    """Test when development environment."""
+    assert in_development(__file__)
+
+
+def test_production() -> None:
+    """Test when production environment."""
+    assert not in_development()
+
+
+def main() -> bool:
+    """All test of feature flags module.
+
+    Returns:
+        bool: success if get to the end of function
+    """
+    test_develop()
+    test_production()
+    return True
