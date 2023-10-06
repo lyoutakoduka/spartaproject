587b40d5efb2527bc92b54a35a819aaac3c977f7
lyouta <lyouta@spartaproject.com>
2023-10-06 23:56:00 +0900

    Fix: reconnect module importing

--- test/script/string/test_off_stdout.py
+++ test/script/string/test_off_stdout.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pyspartaproj.script.off_stdout import StdoutText
 from pyspartaproj.script.string.format_texts import format_indent
+from pyspartaproj.script.string.off_stdout import StdoutText
 
 
 def test_messages() -> None:
