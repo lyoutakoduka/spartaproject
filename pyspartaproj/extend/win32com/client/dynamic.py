b7238e859bf5da721b2b1f9422f81bb5ed38342d
lyouta <lyouta@spartaproject.com>
2023-10-06 16:41:45 +0900

    Fix: remove module that can call Windows APIs

--- pyspartaproj/extend/win32com/client/dynamic.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""Wrapper module to separate pure python code and other.
-
-win32com: pywin32
-"""
-
-from win32com.client.dynamic import CDispatch  # noqa: F401 # type: ignore
