98ab3b7e46782213b57bdf749671ac99739f92f8
lyouta <lyouta@spartaproject.com>
2023-10-07 19:21:48 +0900

    Feat: get path of script for reading shortcut

--- pyspartaproj/script/file/shortcut/read_shortcut.py
+++ pyspartaproj/script/file/shortcut/read_shortcut.py
@@ -4,6 +4,10 @@
 from pathlib import Path
 
 
+def _get_script_path() -> Path:
+    return Path(Path(__file__).parent, "execute", "read.ps1")
+
+
 def read_shortcut(shortcut_path: Path) -> Path:
     if not shortcut_path.exists():
         raise FileNotFoundError()
