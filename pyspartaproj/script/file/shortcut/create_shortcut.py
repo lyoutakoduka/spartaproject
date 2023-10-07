fd49554e1b8fc0dd0ce914b0836429ef7eee3425
lyouta <lyouta@spartaproject.com>
2023-10-07 11:25:36 +0900

    Feat: converting path to string for PowerShell

--- pyspartaproj/script/file/shortcut/create_shortcut.py
+++ pyspartaproj/script/file/shortcut/create_shortcut.py
@@ -4,6 +4,10 @@
 from pathlib import Path
 
 
+def _get_path_string(path: Path) -> str:
+    return str(path)  # Not as_posix()
+
+
 def create_shortcut(shortcut_target: Path, shortcut_path: Path) -> bool:
     if not shortcut_target.exists():
         raise FileNotFoundError(shortcut_target)
