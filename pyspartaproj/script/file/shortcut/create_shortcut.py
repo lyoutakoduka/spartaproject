f38dbe9ddc9e9482fe5b6ed25deabb969e4db05a
lyouta <lyouta@spartaproject.com>
2023-10-07 11:28:23 +0900

    Feat: get path of script for creating shortcut

--- pyspartaproj/script/file/shortcut/create_shortcut.py
+++ pyspartaproj/script/file/shortcut/create_shortcut.py
@@ -8,6 +8,15 @@ def _get_path_string(path: Path) -> str:
     return str(path)  # Not as_posix()
 
 
+def _get_script_path() -> Path:
+    current: Path = Path(__file__)
+    return Path(
+        current.parent,
+        "execute",
+        current.with_suffix(".ps1").name,
+    )
+
+
 def create_shortcut(shortcut_target: Path, shortcut_path: Path) -> bool:
     if not shortcut_target.exists():
         raise FileNotFoundError(shortcut_target)
