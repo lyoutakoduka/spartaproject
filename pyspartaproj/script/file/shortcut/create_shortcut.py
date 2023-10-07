9627ce54c4e442464ed11a6ef6746b7a26127bc6
lyouta <lyouta@spartaproject.com>
2023-10-07 17:15:38 +0900

    Refactor: remove unused functions

--- pyspartaproj/script/file/shortcut/create_shortcut.py
+++ pyspartaproj/script/file/shortcut/create_shortcut.py
@@ -13,26 +13,8 @@ from pyspartaproj.script.execute.execute_powershell import (
 from pyspartaproj.script.path.safe.safe_trash import SafeTrash
 
 
-def _get_path_string(path: Path) -> str:
-    return str(path)  # Not as_posix()
-
-
 def _get_script_path() -> Path:
     return Path(Path(__file__).parent, "execute", "create.ps1")
-
-
-def _get_quoted_paths(shortcut_target: Path, shortcut_path: Path) -> Strs:
-    return [
-        _get_path_string(path).join(["'"] * 2)
-        for path in [
-            shortcut_target,
-            shortcut_path,
-        ]
-    ]
-
-
-def _get_powershell_command(commands_execute: Strs) -> str:
-    return " ".join(commands_execute).join(['"'] * 2)
 
 
 def _get_shortcut_command(shortcut_target: Path, shortcut_path: Path) -> str:
