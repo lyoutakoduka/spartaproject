8027bbca19c931c8ecf6b29a4776f9148075bb75
lyouta <lyouta@spartaproject.com>
2023-10-07 11:30:27 +0900

    Feat: convert path to string for PowerShell

--- pyspartaproj/script/file/shortcut/create_shortcut.py
+++ pyspartaproj/script/file/shortcut/create_shortcut.py
@@ -3,6 +3,8 @@
 
 from pathlib import Path
 
+from pyspartaproj.context.default.string_context import Strs
+
 
 def _get_path_string(path: Path) -> str:
     return str(path)  # Not as_posix()
@@ -15,6 +17,16 @@ def _get_script_path() -> Path:
         "execute",
         current.with_suffix(".ps1").name,
     )
+
+
+def _get_quoted_paths(shortcut_target: Path, shortcut_path: Path) -> Strs:
+    return [
+        _get_path_string(path).join(["'"] * 2)
+        for path in [
+            shortcut_target,
+            shortcut_path,
+        ]
+    ]
 
 
 def create_shortcut(shortcut_target: Path, shortcut_path: Path) -> bool:
