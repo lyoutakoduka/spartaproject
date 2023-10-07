40bb123c4ff0abc06123dc42e84a37e44b4697d4
lyouta <lyouta@spartaproject.com>
2023-10-07 09:42:07 +0900

    Doc: add to module and function.

--- test/script/execute/test_execute_powershell.py
+++ test/script/execute/test_execute_powershell.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test to execute commands in PowerShell."""
 
 from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.execute.execute_powershell import execute_powershell
 
 
 def test_write() -> None:
+    """Test for Write-Output that is shown three line number."""
+
     expected: Strs = [str(i).zfill(3) for i in range(3)]
     command: str = "; ".join(["Write-Output " + text for text in expected])
 
