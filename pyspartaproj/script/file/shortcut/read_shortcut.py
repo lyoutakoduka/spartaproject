d021d189ae3b66dbf857692e446c0802f3918a92
lyouta <lyouta@spartaproject.com>
2023-10-07 19:23:13 +0900

    Feat: convert path to string for PowerShell

--- pyspartaproj/script/file/shortcut/read_shortcut.py
+++ pyspartaproj/script/file/shortcut/read_shortcut.py
@@ -3,9 +3,24 @@
 
 from pathlib import Path
 
+from pyspartaproj.script.execute.execute_powershell import (
+    get_path_string,
+    get_quoted_paths,
+    get_script_executable,
+)
+
 
 def _get_script_path() -> Path:
     return Path(Path(__file__).parent, "execute", "read.ps1")
+
+
+def _get_shortcut_command(shortcut_path: Path) -> str:
+    return get_script_executable(
+        [
+            get_path_string(_get_script_path()),
+            get_quoted_paths(shortcut_path),
+        ]
+    )
 
 
 def read_shortcut(shortcut_path: Path) -> Path:
