8e8c5436f25969707d906d967ba1c00b128b9fd1
lyouta <lyouta@spartaproject.com>
2023-10-08 23:36:31 +0900

    Refactor: variable must write lowercase @text

--- test/script/file/text/test_export_file.py
+++ test/script/file/text/test_export_file.py
@@ -19,19 +19,19 @@ def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
 
 
 def test_text() -> None:
-    INPUT: str = "test"
+    input: str = "test"
 
     def individual_test(text_path: Path) -> None:
-        _common_test(text_export(text_path, INPUT), len(INPUT))
+        _common_test(text_export(text_path, input), len(input))
 
     _inside_temporary_directory(individual_test)
 
 
 def test_byte() -> None:
-    INPUT: bytes = b"test"
+    input: bytes = b"test"
 
     def individual_test(text_path: Path) -> None:
-        _common_test(byte_export(text_path, INPUT), len(INPUT))
+        _common_test(byte_export(text_path, input), len(input))
 
     _inside_temporary_directory(individual_test)
 
--- test/script/file/text/test_import_file.py
+++ test/script/file/text/test_import_file.py
@@ -8,16 +8,16 @@ from typing import Callable
 from pyspartaproj.script.file.text.export_file import text_export
 from pyspartaproj.script.file.text.import_file import byte_import, text_import
 
-_INPUT: str = "test"
+_input: str = "test"
 
 
 def _common_test(result: str) -> None:
-    assert _INPUT == result
+    assert _input == result
 
 
 def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
     with TemporaryDirectory() as temporary_path:
-        function(text_export(Path(temporary_path, "temporary.txt"), _INPUT))
+        function(text_export(Path(temporary_path, "temporary.txt"), _input))
 
 
 def test_text() -> None:
