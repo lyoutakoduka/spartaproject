cb2d12bada0da9303f4e07e038d51264176f414f
lyouta <lyouta@spartaproject.com>
2023-10-09 00:51:34 +0900

    Fix: resolve duplicate variable name

--- test/script/file/archive/test_decompress_zip.py
+++ test/script/file/archive/test_decompress_zip.py
@@ -141,8 +141,9 @@ def test_limit() -> None:
 
 
 def test_timestamp() -> None:
-    expected: str = "2023-04-15T20:09:30.936886+00:00"
-    expected: datetime = datetime.fromisoformat(expected)
+    expected: datetime = datetime.fromisoformat(
+        "2023-04-15T20:09:30.936886+00:00"
+    )
 
     def individual_test(temporary_root: Path) -> None:
         tree_root: Path = Path(temporary_root, "tree")
