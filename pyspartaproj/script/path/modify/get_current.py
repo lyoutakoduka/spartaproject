e61b68f623f49d049bd114253ee217201666f3b5
lyouta <lyouta@spartaproject.com>
2023-10-11 03:58:28 +0900

    Fix: symbolic link on Linux is not support

--- pyspartaproj/script/path/modify/get_current.py
+++ pyspartaproj/script/path/modify/get_current.py
@@ -5,29 +5,11 @@
 
 from pathlib import Path
 
-from pyspartaproj.context.default.string_context import Strs
-from pyspartaproj.script.shell.execute_command import execute_command
-
-
-def get_current_symbolic() -> Path:
-    """Get current working directory including symbolic link.
-
-    Raises:
-        ValueError: when fail to get path
-
-    Returns:
-        Path: current working directory as symbolic link
-    """
-    results: Strs = execute_command(["pwd"])
-
-    if 1 == len(results):
-        return Path(results[0])
-
-    raise ValueError
-
 
 def get_current() -> Path:
     """Get current working directory.
+
+    Call from symbolic link on Linux is not support
 
     Returns:
         Path: current working directory
