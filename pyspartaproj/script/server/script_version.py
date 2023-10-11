ab10c6c83419446a93f1f77008245dfdd35f8be4
lyouta <lyouta@spartaproject.com>
2023-10-11 21:30:26 +0900

    Doc: add module and function document string

--- pyspartaproj/script/server/script_version.py
+++ pyspartaproj/script/server/script_version.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to get version information of Python interpreter."""
+
 from pathlib import Path
 
 from pyspartaproj.script.shell.execute_command import execute_command
 
 
 def get_version_name(version: str) -> str:
+    """Function to convert version string as default directory name.
+
+    Args:
+        version (str): version information formatted like "3.12.0"
+
+    Returns:
+        str: formatted version string like "Python-3.12.0"
+    """
     return "python".capitalize() + "-" + version
 
 
 def get_interpreter_version(executable: Path) -> str:
+    """Function to get version information of specific interpreter.
+
+    Args:
+        executable (Path): interpreter path you want to get version
+
+    Returns:
+        str: version information formatted like "3.12.0"
+    """
     return execute_command([str(executable), "-V"])[0].split(" ")[-1]
