135fe331ab29f449f28c3fc68993af75939a1585
lyouta <lyouta@spartaproject.com>
2023-10-12 04:35:29 +0900

    Feat: function to remove new lines at line ends #9

--- pyspartaproj/script/shell/execute_command.py
+++ pyspartaproj/script/shell/execute_command.py
@@ -6,6 +6,14 @@ from subprocess import PIPE, Popen
 from pyspartaproj.context.default.string_context import StrGene, Strs
 
 
+def _cleanup_new_lines(text: str) -> str:
+    for new_line in reversed("\r\n"):
+        if text.endswith(new_line):
+            text = text[:-1]
+
+    return text
+
+
 def execute_command(commands: Strs) -> StrGene:
     subprocess = Popen(
         " ".join(commands), stdout=PIPE, stderr=PIPE, shell=True
@@ -16,7 +24,7 @@ def execute_command(commands: Strs) -> StrGene:
     while True:
         line: bytes = subprocess.stdout.readline()
         if line:
-            yield line.decode().replace("\n", "")
+            yield _cleanup_new_lines(line.decode())
         else:
             if subprocess.poll() is not None:
                 break
