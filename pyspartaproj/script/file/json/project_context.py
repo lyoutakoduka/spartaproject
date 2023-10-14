ff49705372be4d63c2c3b86602b00994955fbb1c
lyouta <lyouta@spartaproject.com>
2023-10-15 03:01:00 +0900

    Feat: module to import project config file #11

---
+++ pyspartaproj/script/file/json/project_context.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from pyspartaproj.context.default.integer_context import IntPair
+from pyspartaproj.context.file.json_context import Json
+from pyspartaproj.script.file.json.convert_from_json import (
+    integer_pair2_from_json,
+    path_pair_from_json,
+)
+from pyspartaproj.script.file.json.import_json import json_import
+
+
+class ProjectContext:
+    def _get_context_path(self, forward: Path | None) -> Path:
+        if forward is None:
+            return Path("pyspartaproj", "resource", "project_context.json")
+
+        return forward
+
+    def _load_context(self, forward: Path) -> Json:
+        return json_import(
+            path_pair_from_json(json_import(forward))["forward.path"]
+        )
+
+    def _serialize_path(self, base_context: Json) -> None:
+        self._integer_context = integer_pair2_from_json(base_context)
+
+    def __init__(self, forward: Path | None = None) -> None:
+        self._serialize_path(
+            self._load_context(self._get_context_path(forward))
+        )
+
+    def get_integer_context(self, group: str) -> IntPair:
+        return self._integer_context[group]
---
+++ test/script/file/json/test_project_context.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from pyspartaproj.context.default.integer_context import IntPair
+from pyspartaproj.context.default.string_context import Strs2
+from pyspartaproj.script.file.json.project_context import ProjectContext
+
+
+def _common_test(keys_pair: Strs2) -> None:
+    assert 1 == len(set([str(sorted(keys)) for keys in keys_pair]))
+
+
+def test_integer() -> None:
+    expected: IntPair = {"index": 0, "count": 1}
+
+    project = ProjectContext(
+        forward=Path(Path(__file__).parent, "resource", "forward.json")
+    )
+    integer_context: IntPair = project.get_integer_context("test")
+
+    _common_test([list(items.keys()) for items in [expected, integer_context]])
+
+    for key, value in expected.items():
+        assert value == integer_context[key]
+
+
+def main() -> bool:
+    test_integer()
+    return True
