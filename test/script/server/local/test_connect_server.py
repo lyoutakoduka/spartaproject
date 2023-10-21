045debf006a0944e14294f96064d07dce39fb02b
lyouta <lyouta@spartaproject.com>
2023-10-21 11:43:13 +0900

    Doc: Add module document string #13

--- test/script/server/local/test_connect_server.py
+++ test/script/server/local/test_connect_server.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to use SSH and SFTP functionality."""
+
 from pyspartaproj.script.server.local.connect_server import ConnectServer
 
 
 def test_connect() -> None:
+    """Test to connect server by using SSH and SFTP functionality."""
     server: ConnectServer = ConnectServer()
     assert server.connect()
 
 
 def main() -> bool:
+    """Run all tests.
+
+    Returns:
+        bool: success if get to the end of function
+    """
     test_connect()
     return True
