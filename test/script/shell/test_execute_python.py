91704df6a9499fa7f42f37faff19b5573318b633
lyouta <lyouta@spartaproject.com>
2023-10-26 23:54:53 +0900

    Update: convert path for Python execution #15

--- test/script/shell/test_execute_python.py
+++ test/script/shell/test_execute_python.py
@@ -5,13 +5,16 @@
 
 
 from pyspartaproj.script.path.modify.get_resource import get_resource
-from pyspartaproj.script.shell.execute_python import execute_python
+from pyspartaproj.script.shell.execute_python import (
+    execute_python,
+    get_script_string,
+)
 
 
 def test_command() -> None:
     """Test to execute Python script that return version of interpreter."""
     assert [str(i).zfill(3) for i in range(3)] == list(
-        execute_python([get_resource(["indices.py"]).as_posix()])
+        execute_python([get_script_string(get_resource(["indices.py"]))])
     )
 
 
