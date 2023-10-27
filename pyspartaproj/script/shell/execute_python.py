cf616422fd0c583b47189d4151e7dabcb5a8127e
lyouta <lyouta@spartaproject.com>
2023-10-27 15:19:16 +0900

    Doc: update module document string #15

--- pyspartaproj/script/shell/execute_python.py
+++ pyspartaproj/script/shell/execute_python.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Module to execute Python according to OS."""
+"""Module to execute Python corresponding to OS."""
 
 from pathlib import Path
 
@@ -11,27 +11,35 @@ from pyspartaproj.script.shell.execute_command import execute_command
 
 
 def get_interpreter_path() -> Path:
-    """Function to get interpreter path of Python according to OS.
+    """Function to get interpreter path of Python corresponding to OS.
 
     Returns:
-        Path: relative path of Python interpreter
+        Path: Relative path of Python interpreter.
     """
     project = ProjectContext()
     return project.merge_platform_path("filter", "platform", "interpreter")
 
 
 def get_script_string(path: Path) -> str:
+    """Convert to the format which is necessary for executing script in Python.
+
+    Args:
+        path (Path): Path you want to convert.
+
+    Returns:
+        str: Convert path which can executed in Python.
+    """
     return str(path)  # Not as_posix()
 
 
 def execute_python(commands: Strs) -> StrGene:
-    """Execute Python according to OS.
+    """Execute Python corresponding to OS.
 
     Args:
         commands (Strs): Script you want execute and arguments of itself
 
     Returns:
-        StrGene: Stdout of Script path you want execute
+        StrGene: Generator for getting stdout of the script you want execute.
     """
     return execute_command(
         [get_script_string(get_interpreter_path())] + commands
