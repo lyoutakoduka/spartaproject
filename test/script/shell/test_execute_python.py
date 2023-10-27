eca67253722c337509d747a94f6808c307b04d7a
lyouta <lyouta@spartaproject.com>
2023-10-27 15:34:04 +0900

    Feat: convert path to execute script in Python #15

--- test/script/shell/test_execute_python.py
+++ test/script/shell/test_execute_python.py
@@ -4,12 +4,23 @@
 """Test to execute Python according to OS."""
 
 from pathlib import Path
+from platform import uname
 
+from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.path.modify.get_resource import get_resource
 from pyspartaproj.script.shell.execute_python import (
     execute_python,
     get_script_string,
 )
+
+
+def test_path() -> None:
+    path_elements: Strs = ["A", "B", "C"]
+    identifier: str = "/" if "Linux" == uname().system else "\\"
+
+    assert identifier.join(path_elements) == get_script_string(
+        Path(*path_elements)
+    )
 
 
 def test_command() -> None:
@@ -25,5 +36,6 @@ def main() -> bool:
     Returns:
         bool: success if get to the end of function
     """
+    test_path()
     test_command()
     return True
