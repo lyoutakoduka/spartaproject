8b31a6c3f27b0784ad01df3a5b0753802e4d5f73
lyouta <lyouta@spartaproject.com>
2023-10-30 19:52:16 +0900

    Doc: add module document string #15

--- test/script/file/shortcut/test_create_shortcut.py
+++ test/script/file/shortcut/test_create_shortcut.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to create Windows shortcut from PowerShell."""
+
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Callable
@@ -26,6 +28,8 @@ def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
 
 
 def test_file() -> None:
+    """Test to create file type shortcut of Windows from PowerShell."""
+
     def individual_test(temporary_root: Path) -> None:
         source_path: Path = create_temporary_file(temporary_root)
         _common_test(source_path, temporary_root)
@@ -34,6 +38,8 @@ def test_file() -> None:
 
 
 def test_directory() -> None:
+    """Test to create directory type shortcut of Windows from PowerShell."""
+
     def individual_test(temporary_root: Path) -> None:
         _common_test(temporary_root, temporary_root)
 
@@ -41,6 +47,8 @@ def test_directory() -> None:
 
 
 def test_exist() -> None:
+    """Test to exists shortcut file before create it."""
+
     def individual_test(temporary_root: Path) -> None:
         with raises(FileNotFoundError):
             _common_test(Path("empty"), temporary_root)
@@ -49,6 +57,11 @@ def test_exist() -> None:
 
 
 def main() -> bool:
+    """Run all tests.
+
+    Returns:
+        bool: success if get to the end of function
+    """
     test_file()
     test_directory()
     test_exist()
