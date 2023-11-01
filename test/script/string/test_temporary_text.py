c005ca86ace7281ad24472e316ab33739c68a127
lyouta <lyouta@spartaproject.com>
2023-11-01 21:57:16 +0900

    Feat: add test for order argument #19

--- test/script/string/test_temporary_text.py
+++ test/script/string/test_temporary_text.py
@@ -8,6 +8,11 @@ def test_count() -> None:
     assert ["0", "1", "2"] == temporary_text(3, 1)
 
 
+def test_order() -> None:
+    assert ["000"] == temporary_text(1, 3)
+
+
 def main() -> bool:
     test_count()
+    test_order()
     return True
