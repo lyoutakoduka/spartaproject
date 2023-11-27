c51bde3a075d1fb5e8e2911e817353e31dd2fe55
lyouta <lyouta@spartaproject.com>
2023-11-27 22:56:34 +0900

    Doc: add module document string #19

--- pyspartaproj/script/string/temporary_text.py
+++ pyspartaproj/script/string/temporary_text.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to show simple numbers as string type for module test."""
+
 from pyspartaproj.context.default.string_context import Strs
 
 
 def temporary_text(count: int, order: int) -> Strs:
+    """Function to show simple numbers as string.
+
+    Args:
+        count (int): Count of numbers which is start from zero.
+        digit (int): Digit of numbers as string type which is filled by zero.
+
+        e.g. Following text is shown if "count" is 3, and "digit" is 4.
+
+        0000
+        0001
+        0002
+
+    Returns:
+        Strs: Numbers as string type which is filled by zero.
+
+    """
     return [str(i).zfill(order) for i in range(count)]
