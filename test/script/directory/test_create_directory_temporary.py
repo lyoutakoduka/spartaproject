197d41e976c1468eeb1e03c92165b32989ce6499
lyouta <lyouta@spartaproject.com>
2023-11-30 04:32:49 +0900

    Doc: add module document string #21

--- test/script/directory/test_create_directory_temporary.py
+++ test/script/directory/test_create_directory_temporary.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to create temporary working directory shared in class."""
+
 from pathlib import Path
 
 from pyspartaproj.script.directory.create_directory_temporary import WorkSpace
@@ -11,11 +13,17 @@ class TemporaryWorkSpace(WorkSpace):
 
 
 def test_create() -> None:
+    """Test to check existing of temporary working directory."""
     work_space = TemporaryWorkSpace()
     work_space_root: Path = work_space.get_root()
     assert work_space_root.exists()
 
 
 def main() -> bool:
+    """Run all tests.
+
+    Returns:
+        bool: Success if get to the end of function.
+    """
     test_create()
     return True
