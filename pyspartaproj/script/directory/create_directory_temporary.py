908ca5a9bce5352384170ce7d515b7958e1e7eef
lyouta <lyouta@spartaproject.com>
2023-11-29 15:42:38 +0900

    Doc: add module document string #21

--- pyspartaproj/script/directory/create_directory_temporary.py
+++ pyspartaproj/script/directory/create_directory_temporary.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to create temporary working directory shared in class."""
+
 from pathlib import Path
 from shutil import rmtree
 from tempfile import mkdtemp
 
 
 class WorkSpace:
+    """Class to create temporary working directory shared in class."""
+
     def __init__(self) -> None:
+        """Create temporary working directory."""
         self._work_space_root = Path(mkdtemp())
 
     def __del__(self) -> None:
+        """Remove temporary working directory."""
         rmtree(str(self._work_space_root))
 
     def get_root(self) -> Path:
+        """Get root path of temporary working directory."""
         return self._work_space_root
