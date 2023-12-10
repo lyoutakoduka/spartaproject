6f3028162ee5ad45b01f00dfedeaac329c4b6fea
lyouta <lyouta@spartaproject.com>
2023-12-11 06:28:14 +0900

    Refactor: remove unused function #23

--- pyspartaproj/script/time/stamp/get_timestamp.py
+++ pyspartaproj/script/time/stamp/get_timestamp.py
@@ -15,10 +15,6 @@ def _convert_timestamp(time: float, jst: bool) -> datetime:
     return time_from_timestamp(Decimal(str(time)), jst=jst)
 
 
-def get_access(path: Path, jst: bool = False) -> datetime:
-    return _convert_timestamp(path.stat().st_atime, jst)
-
-
 def get_latest(
     path: Path, jst: bool = False, access: bool = False
 ) -> datetime:
