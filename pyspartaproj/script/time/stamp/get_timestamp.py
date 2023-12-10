c806965f7af1035df7657ebac45a012508272863
lyouta <lyouta@spartaproject.com>
2023-12-11 06:26:42 +0900

    Refactor: add type hint #23

--- pyspartaproj/script/time/stamp/get_timestamp.py
+++ pyspartaproj/script/time/stamp/get_timestamp.py
@@ -3,6 +3,7 @@
 
 from datetime import datetime
 from decimal import Decimal
+from os import stat_result
 from pathlib import Path
 
 from pyspartaproj.context.default.string_context import StrPair
@@ -21,7 +22,7 @@ def get_access(path: Path, jst: bool = False) -> datetime:
 def get_latest(
     path: Path, jst: bool = False, access: bool = False
 ) -> datetime:
-    status = path.stat()
+    status: stat_result = path.stat()
     return _convert_timestamp(
         status.st_atime if access else status.st_mtime, jst
     )
