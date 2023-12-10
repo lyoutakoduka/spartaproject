d806632b3540c26af9ef47504f238c7f1b8e43e2
lyouta <lyouta@spartaproject.com>
2023-12-11 02:21:18 +0900

    Update: support JST timezone for latest date #23

--- pyspartaproj/script/time/stamp/get_timestamp.py
+++ pyspartaproj/script/time/stamp/get_timestamp.py
@@ -22,5 +22,10 @@ def get_latest(path: Path, jst: bool = False) -> datetime:
     return _convert_timestamp(path.stat().st_mtime, jst)
 
 
-def get_directory_latest(walk_generator: PathGene) -> StrPair:
-    return {str(path): get_latest(path).isoformat() for path in walk_generator}
+def get_directory_latest(
+    walk_generator: PathGene, jst: bool = False
+) -> StrPair:
+    return {
+        str(path): get_latest(path, jst=jst).isoformat()
+        for path in walk_generator
+    }
