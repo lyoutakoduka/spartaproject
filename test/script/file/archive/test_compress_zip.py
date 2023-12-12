15e2012ef818c2112663c185f6a0ae0286fb2d56
lyouta <lyouta@spartaproject.com>
2023-12-12 20:08:17 +0900

    Fix: reconnect module importing: archive #23

--- test/script/file/archive/test_compress_zip.py
+++ test/script/file/archive/test_compress_zip.py
@@ -10,8 +10,8 @@ from typing import Callable
 from pyspartaproj.context.default.integer_context import Ints2
 from pyspartaproj.context.extension.decimal_context import Decs
 from pyspartaproj.context.extension.path_context import Paths, Paths2
+from pyspartaproj.script.decimal.initialize_decimal import initialize_decimal
 from pyspartaproj.script.file.archive.compress_zip import CompressZip
-from pyspartaproj.script.initialize_decimal import initialize_decimal
 from pyspartaproj.script.path.iterate_directory import walk_iterator
 from pyspartaproj.script.path.modify.get_relative import get_relative_array
 from pyspartaproj.script.path.temporary.create_temporary_tree import (
