2bd80f5bcdf690c4addc1d5cea32eddd5154d09d
lyouta <lyouta@spartaproject.com>
2023-12-15 05:01:52 +0900

    Doc: add module document string #23

--- test/script/decimal/test_initialize_decimal.py
+++ test/script/decimal/test_initialize_decimal.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to help calculating by Decimal type."""
+
 from decimal import Context, Decimal, FloatOperation, getcontext, setcontext
 
 from pyspartaproj.context.default.string_context import Strs
@@ -9,12 +11,15 @@ from pyspartaproj.script.decimal.initialize_decimal import initialize_decimal
 
 
 def test_float() -> None:
+    """Test for assign float to Decimal type directly."""
     with raises(FloatOperation):
         initialize_decimal()
         Decimal(1.0)
 
 
 def test_accuracy() -> None:
+    """Test for accuracy of calculating Decimal type."""
+
     def get_changed() -> Strs:
         context = getcontext()
         return [str(context.prec), context.rounding]
@@ -28,6 +33,11 @@ def test_accuracy() -> None:
 
 
 def main() -> bool:
+    """Run all tests.
+
+    Returns:
+        bool: Success if get to the end of function.
+    """
     test_accuracy()
     test_float()
     return True
