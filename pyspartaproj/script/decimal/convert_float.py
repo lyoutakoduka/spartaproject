5a825adbc62c8d37c49442083885b94b09e1c0cb
lyouta <lyouta@spartaproject.com>
2023-12-15 01:35:01 +0900

    Doc: add module document string #23

--- pyspartaproj/script/decimal/convert_float.py
+++ pyspartaproj/script/decimal/convert_float.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to convert data from type Decimal to type float."""
+
 from pyspartaproj.context.default.float_context import Floats
 from pyspartaproj.context.extension.decimal_context import Decs
 
 
 def convert_float_array(numbers: Decs) -> Floats:
+    """Function to convert array from type Decimal to type float.
+
+    Args:
+        numbers (Decs): Decimal array you want to convert.
+
+    Returns:
+        Floats: Converted float array.
+    """
     return [float(number) for number in numbers]
