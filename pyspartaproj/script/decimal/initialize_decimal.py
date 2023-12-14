330c44d55232404d5323280e4975770187b54527
lyouta <lyouta@spartaproject.com>
2023-12-15 05:01:45 +0900

    Doc: add module document string #23

--- pyspartaproj/script/decimal/initialize_decimal.py
+++ pyspartaproj/script/decimal/initialize_decimal.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to help calculating by Decimal type."""
 
 from decimal import ROUND_FLOOR, Context, FloatOperation, setcontext
 
 
 def initialize_decimal() -> None:
+    """Function must running before calculating Decimal type."""
     context: Context = Context(prec=32, rounding=ROUND_FLOOR)
     context.traps[FloatOperation] = True
     setcontext(context)
