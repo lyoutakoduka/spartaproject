a2f632cf9f4ecc152e38e22742bf8cdd3274a826
lyouta <lyouta@spartaproject.com>
2023-12-14 15:40:08 +0900

    Feat: module to edit internal of archive file #23

---
+++ pyspartaproj/script/file/archive/edit_zip.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Module to edit internal of zip archive file."""
+
+from pathlib import Path
+
+from pyspartaproj.context.default.string_context import StrPair
+from pyspartaproj.context.extension.path_context import Paths
+from pyspartaproj.script.bool.compare_json import is_same_json
+from pyspartaproj.script.directory.create_directory_temporary import WorkSpace
+from pyspartaproj.script.file.archive.compress_zip import CompressZip
+from pyspartaproj.script.file.archive.decompress_zip import DecompressZip
+from pyspartaproj.script.file.json.convert_to_json import multiple_to_json
+from pyspartaproj.script.path.iterate_directory import walk_iterator
+from pyspartaproj.script.path.safe.safe_trash import SafeTrash
+from pyspartaproj.script.time.stamp.get_timestamp import get_directory_latest
+
+
+class EditZip(WorkSpace):
+    """Class to edit internal of zip archive file.
+
+    WorkSpace: Class to create temporary working directory shared in class.
+    """
+
+    def _initialize_variables(
+        self, archive_path: Path, limit_byte: int
+    ) -> None:
+        self._still_removed: bool = False
+        self._archive_path: Path = archive_path
+        self._limit_byte: int = limit_byte
+
+    def _get_archive_stamp(self) -> StrPair:
+        return get_directory_latest(walk_iterator(self.get_root()))
+
+    def _is_difference_archive(self) -> StrPair | None:
+        archive_stamp: StrPair = self._get_archive_stamp()
+        if is_same_json(
+            *[
+                multiple_to_json(stamp)
+                for stamp in [self._archive_stamp, archive_stamp]
+            ]
+        ):
+            return None
+
+        return archive_stamp
+
+    def _cleanup_before_override(self) -> None:
+        safe_trash = SafeTrash()
+
+        for path in self._decompressed:
+            safe_trash.trash(path)
+
+    def _compress_archive(self, archive_stamp: StrPair) -> Paths:
+        self._cleanup_before_override()
+
+        compress_zip = CompressZip(
+            self._archive_path.parent, limit_byte=self._limit_byte
+        )
+
+        for path_text in archive_stamp.keys():
+            compress_zip.compress_archive(
+                Path(path_text), archive_root=self.get_root()
+            )
+
+        return compress_zip.close_archived()
+
+    def _decompress_archive(self) -> None:
+        decompress_zip = DecompressZip(self.get_root())
+        self._decompressed: Paths = decompress_zip.sequential_archives(
+            self._archive_path
+        )
+
+        for path in self._decompressed:
+            decompress_zip.decompress_archive(path)
+
+    def _initialize_archive(self) -> None:
+        self._decompress_archive()
+        self._archive_stamp: StrPair = self._get_archive_stamp()
+
+    def _finalize_archive(self) -> Paths | None:
+        archived: Paths | None = None
+
+        if archive_stamp := self._is_difference_archive():
+            archived = self._compress_archive(archive_stamp)
+
+        super().__del__()
+
+        return archived
+
+    def close_archive(self) -> Paths | None:
+        if self._still_removed:
+            return None
+
+        self._still_removed = True
+
+        return self._finalize_archive()
+
+    def __del__(self) -> None:
+        self.close_archive()
+
+    def __init__(self, archive_path: Path, limit_byte: int = 0) -> None:
+        super().__init__()
+        self._initialize_variables(archive_path, limit_byte)
+        self._initialize_archive()
+
+    def get_decompressed_root(self) -> Path:
+        return self.get_root()
---
+++ test/script/file/archive/test_edit_zip.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Callable
+
+from pyspartaproj.context.default.string_context import StrPair
+from pyspartaproj.context.extension.path_context import PathPair, Paths
+from pyspartaproj.interface.pytest import fail
+from pyspartaproj.script.bool.compare_json import is_same_json
+from pyspartaproj.script.file.archive.compress_zip import CompressZip
+from pyspartaproj.script.file.archive.decompress_zip import DecompressZip
+from pyspartaproj.script.file.archive.edit_zip import EditZip
+from pyspartaproj.script.file.json.convert_to_json import multiple_to_json
+from pyspartaproj.script.path.iterate_directory import walk_iterator
+from pyspartaproj.script.path.modify.get_relative import get_relative
+from pyspartaproj.script.path.safe.safe_rename import SafeRename
+from pyspartaproj.script.path.safe.safe_trash import SafeTrash
+from pyspartaproj.script.path.temporary.create_temporary_file import (
+    create_temporary_file,
+)
+from pyspartaproj.script.path.temporary.create_temporary_tree import (
+    create_temporary_tree,
+)
+from pyspartaproj.script.time.stamp.get_timestamp import get_directory_latest
+
+
+def _add_archive(temporary_root: Path, compress_zip: CompressZip) -> Path:
+    for path in walk_iterator(Path(temporary_root, "before")):
+        compress_zip.compress_archive(path)
+
+    return compress_zip.close_archived()[0]
+
+
+def _get_stamp_key(path_text: str, stamp_root: Path) -> str:
+    return str(get_relative(Path(path_text), root_path=stamp_root))
+
+
+def _get_stamp_value(path_text: str, time: str) -> str:
+    return "directory" if Path(path_text).is_dir() else time
+
+
+def _get_archive_stamp(stamp_root: Path) -> StrPair:
+    stamp: StrPair = get_directory_latest(walk_iterator(stamp_root))
+
+    return {
+        _get_stamp_key(key, stamp_root): _get_stamp_value(key, time)
+        for key, time in stamp.items()
+    }
+
+
+def _create_archive(temporary_root: Path) -> StrPair:
+    before_root: Path = Path(temporary_root, "before")
+    create_temporary_tree(before_root)
+    return _get_archive_stamp(before_root)
+
+
+def _inside_temporary_directory(
+    function: Callable[[Path, StrPair], None]
+) -> None:
+    with TemporaryDirectory() as temporary_path:
+        temporary_root: Path = Path(temporary_path)
+        function(temporary_root, _create_archive(temporary_root))
+
+
+def _add_to_archived(archive_root: Path) -> Path:
+    return create_temporary_file(archive_root)
+
+
+def _remove_from_archived(archive_root: Path) -> Path:
+    safe_trash = SafeTrash()
+    return safe_trash.trash(Path(archive_root, "file.txt"))
+
+
+def _update_to_archived(archive_root: Path) -> Path:
+    file_name: str = "file.ini"
+    rename_path: Path = Path(archive_root, file_name)
+
+    safe_rename = SafeRename()
+    safe_rename.rename(rename_path, rename_path.with_name("rename"))
+
+    return rename_path
+
+
+def _edit_to_archived(archive_root: Path) -> PathPair:
+    edit_history: PathPair = {
+        "add": _add_to_archived(archive_root),
+        "remove": _remove_from_archived(archive_root),
+        "update": _update_to_archived(archive_root),
+    }
+
+    return {
+        key: get_relative(path, root_path=archive_root)
+        for key, path in edit_history.items()
+    }
+
+
+def _decompress_archive(after_root: Path, archived: Paths) -> None:
+    decompress_zip = DecompressZip(after_root)
+
+    for archived_path in archived:
+        decompress_zip.decompress_archive(archived_path)
+
+
+def _remove_stamp_after(path_text: str, stamp_after: StrPair) -> None:
+    del stamp_after[path_text]
+
+
+def _remove_time_stamp(edit_history: PathPair, stamp_after: StrPair) -> None:
+    _remove_stamp_after(str(edit_history["add"]), stamp_after)
+
+
+def _add_stamp_after(
+    edit_type: str,
+    edit_history: PathPair,
+    stamp_before: StrPair,
+    stamp_after: StrPair,
+) -> None:
+    path_text: str = str(edit_history[edit_type])
+    stamp_after[path_text] = stamp_before[path_text]
+
+
+def _add_time_stamp(
+    edit_history: PathPair, stamp_before: StrPair, stamp_after: StrPair
+) -> None:
+    _add_stamp_after("remove", edit_history, stamp_before, stamp_after)
+
+
+def _update_time_stamp(
+    edit_history: PathPair, stamp_before: StrPair, stamp_after: StrPair
+) -> None:
+    _remove_stamp_after("rename", stamp_after)
+    _add_stamp_after("update", edit_history, stamp_before, stamp_after)
+
+
+def _edit_time_stamp(
+    edit_history: PathPair, stamp_before: StrPair, stamp_after: StrPair
+) -> None:
+    _remove_time_stamp(edit_history, stamp_after)
+    _add_time_stamp(edit_history, stamp_before, stamp_after)
+    _update_time_stamp(edit_history, stamp_before, stamp_after)
+
+
+def _compare_time_stamp(stamp_before: StrPair, stamp_after: StrPair) -> bool:
+    return is_same_json(
+        *[multiple_to_json(stamp) for stamp in [stamp_before, stamp_after]]
+    )
+
+
+def _get_stamp_after(
+    temporary_root: Path,
+    stamp_before: StrPair,
+    edit_history: PathPair,
+    archived: Paths,
+) -> StrPair:
+    after_root: Path = Path(temporary_root, "after")
+    _decompress_archive(after_root, archived)
+
+    stamp_after: StrPair = _get_archive_stamp(after_root)
+    _edit_time_stamp(edit_history, stamp_before, stamp_after)
+
+    return stamp_after
+
+
+def _common_test(
+    temporary_root: Path, stamp_before: StrPair, edit_zip: EditZip
+) -> None:
+    edit_history: PathPair = _edit_to_archived(
+        edit_zip.get_decompressed_root()
+    )
+
+    if archived := edit_zip.close_archive():
+        assert _compare_time_stamp(
+            stamp_before,
+            _get_stamp_after(
+                temporary_root, stamp_before, edit_history, archived
+            ),
+        )
+    else:
+        fail()
+
+
+def test_single() -> None:
+    def individual_test(temporary_root: Path, stamp_before: StrPair) -> None:
+        _common_test(
+            temporary_root,
+            stamp_before,
+            EditZip(
+                _add_archive(
+                    temporary_root,
+                    CompressZip(Path(temporary_root, "archive")),
+                ),
+            ),
+        )
+
+    _inside_temporary_directory(individual_test)
+
+
+def main() -> bool:
+    """All test of feature flags module.
+
+    Returns:
+        bool: Success if get to the end of function.
+    """
+    test_single()
+    return True
