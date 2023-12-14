6d3eecc643b1fafbd06e01c2b1c255ce068446f7
lyouta <lyouta@spartaproject.com>
2023-12-15 01:35:06 +0900

    Doc: add module document string #23

--- test/script/decimal/test_convert_float.py
+++ test/script/decimal/test_convert_float.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to convert data from type Decimal to type float."""
+
 from decimal import Decimal
 
 from pyspartaproj.context.default.float_context import Floats
@@ -8,6 +10,7 @@ from pyspartaproj.script.decimal.convert_float import convert_float_array
 
 
 def test_array() -> None:
+    """Test to convert array from type Decimal to type float."""
     expected: Floats = [float(i) for i in range(3)]
     assert expected == convert_float_array(
         [Decimal(str(value)) for value in expected]
@@ -15,5 +18,10 @@ def test_array() -> None:
 
 
 def main() -> bool:
+    """Run all tests.
+
+    Returns:
+        bool: Success if get to the end of function.
+    """
     test_array()
     return True
