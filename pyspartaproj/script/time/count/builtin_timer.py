4654472bb714c93a8039776eb496fbf0f37cd3ab
lyouta <lyouta@spartaproject.com>
2023-12-22 00:38:20 +0900

    Doc: Add module document string #23

--- pyspartaproj/script/time/count/builtin_timer.py
+++ pyspartaproj/script/time/count/builtin_timer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to count time like a timer."""
+
 from datetime import datetime, timezone
 from decimal import Decimal
 
@@ -10,6 +12,8 @@ initialize_decimal()
 
 
 class TimerSelect:
+    """Class to count time like a timer."""
+
     def _initialize_variable(self, override: bool, interval: Decimal) -> None:
         self._override: bool = override
         self._interval: Decimal = interval
@@ -28,9 +32,15 @@ class TimerSelect:
             self._count = self._get_current()
 
     def increase_timer(self) -> None:
+        """Increase time count by specific interval."""
         self._count += self._interval
 
     def __call__(self) -> Decimal:
+        """Get current time based on specific initial time count.
+
+        Returns:
+            Decimal: Current time of timer.
+        """
         if self._override:
             return self._count
 
@@ -39,6 +49,16 @@ class TimerSelect:
     def __init__(
         self, override: bool = False, interval: Decimal | None = None
     ) -> None:
+        """Initialize variables and timer count.
+
+        Args:
+            override (bool, optional): Defaults to False.
+                Override initial time count to "2023/4/1:12:00:00-00 (AM)".
+                Default is current datetime, it's mainly used for test.
+
+            interval (Decimal | None, optional): Defaults to None.
+                Interval of timer count, use 1 if None.
+        """
         if interval is None:
             interval = Decimal("1")
 
