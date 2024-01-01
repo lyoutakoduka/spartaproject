89060d3403df156f43b09caa366fdaf08ac5e958
lyouta <lyouta@spartaproject.com>
2024-01-01 21:06:49 +0900

    Refactor: Move functionality about latest time #23

--- test/script/time/stamp/test_get_file_epoch.py
+++ test/script/time/stamp/test_get_file_epoch.py
@@ -5,15 +5,16 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Callable
 
-from pyspartaproj.context.extension.decimal_context import Decs
 from pyspartaproj.script.path.temporary.create_temporary_file import (
     create_temporary_file,
 )
 from pyspartaproj.script.time.stamp.get_file_epoch import get_file_epoch
 
 
-def _common_test(times: Decs) -> None:
-    assert 1 == len(set(times))
+def _common_test(path: Path) -> None:
+    assert 1 == len(
+        set([get_file_epoch(path, access=status) for status in [False, True]])
+    )
 
 
 def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
@@ -23,13 +24,7 @@ def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
 
 def test_create() -> None:
     def individual_test(path: Path) -> None:
-        file_path: Path = create_temporary_file(path)
-        _common_test(
-            [
-                get_file_epoch(file_path, access=status)
-                for status in [False, True]
-            ]
-        )
+        _common_test(create_temporary_file(path))
 
     _inside_temporary_directory(individual_test)
 
