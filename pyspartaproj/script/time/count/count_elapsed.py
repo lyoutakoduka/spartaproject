f231841de575c36765c1f36930e95cc9437a3821
lyouta <lyouta@spartaproject.com>
2024-01-01 14:27:14 +0900

    Doc: Add module document string #23

--- pyspartaproj/script/time/count/count_elapsed.py
+++ pyspartaproj/script/time/count/count_elapsed.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to count time and show by readable format."""
 
 from decimal import Decimal
 
@@ -12,6 +13,8 @@ initialize_decimal()
 
 
 class LogTimer:
+    """Class to count time, and show by readable format."""
+
     def _initialize_variables(
         self,
         override: bool,
@@ -48,6 +51,7 @@ class LogTimer:
         return None
 
     def increase_timer(self) -> None:
+        """Increase time count by specific interval."""
         self._timer.increase_timer()
 
     def restart(
@@ -57,6 +61,45 @@ class LogTimer:
         interval: Decimal | None = None,
         digit: int = 1,
     ) -> None:
+        """Restart timer functionality, it's become initial status.
+
+        Args:
+            override (bool, optional): Defaults to False.
+                Override initial time count to "2023/4/1:12:00:00-00 (AM)".
+                Use for argument "override" on class "TimerSelect".
+
+            timer_interval (Decimal | None, optional): Defaults to None.
+                Interval of timer count.
+                Use for argument "interval" on class "TimerSelect".
+
+            interval (Decimal | None, optional): Defaults to None.
+                Interval which is use for showing timer count.
+
+            digit (int, optional): Defaults to 1.
+                Digit of timer count when showing.
+
+        Use this class as like follow script,
+            if you want to get current datetime represented by readable time.
+
+        >>> import time
+        >>> timer = LogTimer() # Create class instance.
+        >>> timer.get_readable_time() # Because still in default interval.
+        None
+        >>> time.sleep(1) # Wait 1 second.
+        >>> timer.get_readable_time() # Timer count can shown.
+        1.0s
+
+        If in the test environment.
+
+        >>> timer = LogTimer()
+        >>> timer.restart(override=True)
+        >>> timer.increase_timer() # Increase timer count by 0.5.
+        >>> timer.get_readable_time() # Because still in default interval.
+        None
+        >>> timer.increase_timer() # Increase timer count by 0.5.
+        >>> timer.get_readable_time() # Timer count can shown.
+        1.0s
+        """
         if timer_interval is None:
             timer_interval = Decimal("0.01")
 
