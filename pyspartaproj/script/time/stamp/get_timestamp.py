22f0d134e93a36e78d6efa4f681fb8ca207fed54
lyouta <lyouta@spartaproject.com>
2024-01-02 14:18:20 +0900

    Doc: Add module document string #23

--- pyspartaproj/script/time/stamp/get_timestamp.py
+++ pyspartaproj/script/time/stamp/get_timestamp.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to get latest datetime of file or directory as time object."""
+
 from datetime import datetime
 from decimal import Decimal
 from pathlib import Path
@@ -18,6 +20,22 @@ def _convert_timestamp(time: float, jst: bool) -> datetime:
 def get_latest(
     path: Path, jst: bool = False, access: bool = False
 ) -> datetime:
+    """Get latest datetime of file or directory as time object.
+
+    Args:
+        path (Path): Path of file or directory you want to get datetime.
+            It's used for argument "path" of function "get_file_epoch".
+
+        access (bool, optional): Defaults to False.
+            Return update time if it's False, and access time if True.
+            It's used for argument "access" of function "get_file_epoch".
+
+        jst (bool, optional): Defaults to False.
+            Return latest datetime as JST timezone, if it's True.
+
+    Returns:
+        datetime: Latest datetime as time object.
+    """
     return _convert_timestamp(
         float(get_file_epoch(path, access=access)), jst=jst
     )
@@ -26,6 +44,21 @@ def get_latest(
 def get_directory_latest(
     walk_generator: PathGene, jst: bool = False, access: bool = False
 ) -> StrPair:
+    """Get array of latest datetime in selected directory as time object.
+
+    Args:
+        walk_generator (PathGene):
+            Path generator of directory you want to get latest datetime inside.
+
+        access (bool, optional): Defaults to False.
+            Return update time if it's False, and access time if True.
+
+        jst (bool, optional): Defaults to False.
+            Return latest datetime as JST timezone, if it's True.
+
+    Returns:
+        StrPair: Dictionary constructed by string path and latest datetime.
+    """
     return {
         str(path): get_latest(path, jst=jst, access=access).isoformat()
         for path in walk_generator
