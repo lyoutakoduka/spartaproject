8495d7da27de10edef44ee3f786c07a287cc2f6f
lyouta <lyouta@spartaproject.com>
2024-01-05 16:37:41 +0900

    Doc: Update module document string #23

--- pyspartaproj/script/time/stamp/set_timestamp.py
+++ pyspartaproj/script/time/stamp/set_timestamp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Module to set latest datetime of file or directory by time object."""
+"""Module to set latest date time of file or directory by time object."""
 
 from datetime import datetime
 from decimal import Decimal
@@ -20,18 +20,18 @@ def _convert_timestamp(time: datetime) -> Decimal:
 
 
 def set_latest(path: Path, time: datetime, access: bool = False) -> Path:
-    """Set latest datetime of file or directory by time object.
+    """Set latest date time of file or directory by time object.
 
     Args:
-        path (Path): Path of file or directory you want to set datetime.
+        path (Path): Path of file or directory you want to set date time.
 
-        time (datetime): Latest datetime you want to set.
+        time (datetime): Latest date time you want to set.
 
         access (bool, optional): Defaults to False.
             Set update time if it's False, and access time if True.
 
     Returns:
-        Path: Path of file or directory you set latest datetime.
+        Path: Path of file or directory you set latest date time.
     """
     path_times: Decs = [
         _convert_timestamp(time),
