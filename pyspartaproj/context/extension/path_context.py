6f3f04f7641dc2a826b7c19f5adee200ed44039d
lyouta <lyouta@spartaproject.com>
2024-01-06 23:53:51 +0900

    Doc: Add module document string #25

--- pyspartaproj/context/extension/path_context.py
+++ pyspartaproj/context/extension/path_context.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""User defined types about type "Path"."""
+
 from pathlib import Path
 from typing import Dict, Generator, List
 
--- pyspartaproj/context/extension/time_context.py
+++ pyspartaproj/context/extension/time_context.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""User defined types about type "datetime"."""
+
 from datetime import datetime
 from typing import List
 
