664c72da52740db40f49ca02f65686b60ace49a3
lyouta <lyouta@spartaproject.com>
2024-01-13 04:48:12 +0900

    Feat: Compare 2 dictionaries about time stamp #27

--- pyspartaproj/script/time/stamp/get_timestamp.py
+++ pyspartaproj/script/time/stamp/get_timestamp.py
@@ -10,6 +10,7 @@ from pathlib import Path
 from pyspartaproj.context.extension.path_context import PathGene
 from pyspartaproj.context.extension.time_context import TimePair
 from pyspartaproj.context.file.json_context import Json
+from pyspartaproj.script.bool.compare_json import is_same_json
 from pyspartaproj.script.file.json.convert_to_json import multiple_to_json
 from pyspartaproj.script.time.stamp.from_timestamp import time_from_timestamp
 from pyspartaproj.script.time.stamp.get_file_epoch import get_file_epoch
@@ -78,6 +79,20 @@ def get_latest(
         return _convert_timestamp(float(time), jst=jst)
 
     return get_invalid_time()
+
+
+def is_same_stamp(left: TimePair, right: TimePair) -> bool:
+    """Compare 2 dictionaries which store path and time stamp of the path.
+
+    Args:
+        left (TimePair): Time stamp of the path you want to compare.
+
+        right (TimePair): Time stamp of the path you want to compare.
+
+    Returns:
+        bool: Return True if 2 dictionaries are same value.
+    """
+    return is_same_json(*[_get_stamp_json(times) for times in [left, right]])
 
 
 def get_directory_latest(
