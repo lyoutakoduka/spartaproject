059d49a518fda114caa21b26a718821ab87b19e1
lyouta <lyouta@spartaproject.com>
2024-01-19 12:04:34 +0900

    Refactor: Change order of user defined class. #31

--- pyspartaproj/context/file/config_context.py
+++ pyspartaproj/context/file/config_context.py
@@ -9,33 +9,33 @@ from typing import Dict, Union
 
 Basic = bool | int | float | str | Decimal | Path
 
-BasicPair = Dict[str, Basic]
 _BoolPair = Dict[str, bool]
-_DecPair = Dict[str, Decimal]
-_FloatPair = Dict[str, float]
 _IntPair = Dict[str, int]
-_PathPair = Dict[str, Path]
+_FloatPair = Dict[str, float]
+_DecPair = Dict[str, Decimal]
 _StrPair = Dict[str, str]
+_PathPair = Dict[str, Path]
+BasicPair = Dict[str, Basic]
 
 SectionPair = Union[
-    _BoolPair, _IntPair, _FloatPair, _StrPair, _DecPair, _PathPair
+    _BoolPair, _IntPair, _FloatPair, _DecPair, _StrPair, _PathPair
 ]
 
-BasicPair2 = Dict[str, BasicPair]
 _BoolPair2 = Dict[str, _BoolPair]
-_DecPair2 = Dict[str, _DecPair]
-_FloatPair2 = Dict[str, _FloatPair]
 _IntPair2 = Dict[str, _IntPair]
-_PathPair2 = Dict[str, _PathPair]
+_FloatPair2 = Dict[str, _FloatPair]
+_DecPair2 = Dict[str, _DecPair]
 _StrPair2 = Dict[str, _StrPair]
+_PathPair2 = Dict[str, _PathPair]
+BasicPair2 = Dict[str, BasicPair]
 SectionPair2 = Dict[str, SectionPair]
 
 Config = Union[
     _BoolPair2,
     _IntPair2,
     _FloatPair2,
-    _StrPair2,
     _DecPair2,
+    _StrPair2,
     _PathPair2,
     BasicPair2,
     SectionPair2,
