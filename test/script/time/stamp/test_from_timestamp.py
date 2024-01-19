79984ec05c964078830612686ff3f296a9946bc0
lyouta <lyouta@spartaproject.com>
2024-01-19 23:46:53 +0900

    Feat: Function to get time stamp used for test. #31

--- test/script/time/stamp/test_from_timestamp.py
+++ test/script/time/stamp/test_from_timestamp.py
@@ -15,14 +15,18 @@ def _get_input_time() -> datetime:
     return datetime.fromisoformat("2023-04-15T20:09:30.936886+00:00")
 
 
+def _get_timestamp() -> Decimal:
+    return Decimal(str(_get_input_time().timestamp()))
+
+
 def test_utc() -> None:
-    assert _get_input_time() == time_from_timestamp(_timestamp)
+    assert _get_input_time() == time_from_timestamp(_get_timestamp())
 
 
 def test_jst() -> None:
     expected: str = "2023-04-16T05:09:30.936886+09:00"
     expected_time: datetime = datetime.fromisoformat(expected)
-    assert expected_time == time_from_timestamp(_timestamp, jst=True)
+    assert expected_time == time_from_timestamp(_get_timestamp(), jst=True)
 
 
 def main() -> bool:
