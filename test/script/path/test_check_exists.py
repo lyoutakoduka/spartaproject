09483dfd9975e3d7c4c2e0335d9b9767f5ee72c1
lyouta <lyouta@spartaproject.com>
2024-01-20 15:19:34 +0900

    Refactor: Remove unused variables. #31

--- test/script/path/test_check_exists.py
+++ test/script/path/test_check_exists.py
@@ -15,9 +15,6 @@ from pyspartaproj.script.path.check_exists import (
 )
 from pyspartaproj.script.stack_frame import current_frame
 
-_current_path: Path = Path(__file__)
-_unknown_path: Path = _current_path.with_name("unknown.py")
-
 
 def _get_current_file() -> Path:
     return current_frame()["file"]
