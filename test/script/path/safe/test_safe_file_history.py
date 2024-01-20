32d515b09f686b4a0f361cc5fa3da0ffd24ce1f9
lyouta <lyouta@spartaproject.com>
2024-01-20 15:57:22 +0900

    Refactor: Use path of current executed file. #31

--- test/script/path/safe/test_safe_file_history.py
+++ test/script/path/safe/test_safe_file_history.py
@@ -44,7 +44,7 @@ def _common_test(source: Paths2, rename_path: Path) -> None:
 def _add_single_history(
     file_history: FileHistory, source_history: Paths2, name: str
 ) -> None:
-    source_path: Path = Path(__file__).parent.with_name("source.json")
+    source_path: Path = _get_current_file().parent.with_name("source.json")
     destination_path: Path = source_path.with_stem(name)
 
     file_history.add_history(source_path, destination_path)
