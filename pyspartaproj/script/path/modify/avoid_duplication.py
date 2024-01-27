2846dcabe8fe165711f55200961af35ffe68f32c
lyouta <lyouta@spartaproject.com>
2024-01-28 00:20:36 +0900

    Doc: Add module document string. #31

--- pyspartaproj/script/path/modify/avoid_duplication.py
+++ pyspartaproj/script/path/modify/avoid_duplication.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to convert path which is avoiding existing path."""
+
 from pathlib import Path
 
 
 def get_avoid_path(path: Path) -> Path:
+    """Function to convert path which is avoiding existing path.
+
+    Args:
+        path (Path): Path you want to convert with avoiding existing path.
+
+    Returns:
+        Path: Path which is avoiding existing path.
+    """
     while path.exists():
         path = path.with_name(path.name + "_")
 
