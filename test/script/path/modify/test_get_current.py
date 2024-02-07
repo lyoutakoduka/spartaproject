2611483ffe87eb5a689d7bdd47ef5471f992440e
lyouta <lyouta@spartaproject.com>
2024-02-08 01:57:54 +0900

    Doc: Add module document string. #31

--- test/script/path/modify/test_get_current.py
+++ test/script/path/modify/test_get_current.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to get current working directory."""
+
 from pyspartaproj.script.path.modify.get_current import get_current
 
 
 def test_current() -> None:
+    """Test to cet current working directory."""
     assert get_current().exists()
 
 
 def main() -> bool:
+    """Run all tests.
+
+    Returns:
+        bool: Success if get to the end of function.
+    """
     test_current()
     return True
