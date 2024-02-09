dccaa6a99c2a4fd1d1993e4d51886bdc30e5b107
lyouta <lyouta@spartaproject.com>
2024-02-09 17:32:18 +0900

    Doc: Add module document string. #31

--- pyspartaproj/script/path/temporary/create_temporary_file.py
+++ pyspartaproj/script/path/temporary/create_temporary_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to create empty temporary file as json format."""
+
 from pathlib import Path
 
 from pyspartaproj.script.directory.create_directory_parent import (
@@ -10,6 +12,15 @@ from pyspartaproj.script.file.json.export_json import json_export
 
 
 def create_temporary_file(file_root: Path) -> Path:
+    """Create empty temporary file as json format.
+
+    Args:
+        file_root (Path):
+            Path of directory which empty temporary file is created.
+
+    Returns:
+        Path: Path of created empty temporary file.
+    """
     name: str = "temporary"
     file_path: Path = Path(file_root, name + ".json")
     create_directory_parent(file_path)
