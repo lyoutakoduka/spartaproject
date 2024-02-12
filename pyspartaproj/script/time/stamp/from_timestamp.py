87acb425035300378ac8f2db17830453914dc4dc
lyouta <lyouta@spartaproject.com>
2024-02-12 17:19:22 +0900

    Doc: Add module document string. #31

--- pyspartaproj/script/time/stamp/from_timestamp.py
+++ pyspartaproj/script/time/stamp/from_timestamp.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to convert time data from epoch format to datetime object."""
+
 from datetime import datetime
 from decimal import Decimal
 
@@ -8,5 +10,16 @@ from pyspartaproj.interface.dateutil.tz import gettz
 
 
 def time_from_timestamp(timestamp: Decimal, jst: bool = False) -> datetime:
+    """Convert time data from epoch format to datetime object.
+
+    Args:
+        timestamp (Decimal): Epoch format time you want to convert.
+
+        jst (bool, optional): Defaults to False.
+            If True, you can get datetime object as JST timezone.
+
+    Returns:
+        datetime: Converted datetime object.
+    """
     date_time: datetime = datetime.fromtimestamp(float(timestamp))
     return date_time.astimezone(gettz("Asia/Tokyo" if jst else "UTC"))
