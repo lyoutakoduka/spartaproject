c20fdbf3135fa19259e5d43fe1350d5d00de1993
lyouta <lyouta@spartaproject.com>
2024-02-17 03:26:31 +0900

    Feat: Add user defined types using TypedDict. #33

---
+++ pyspartaproj/context/typed/user_context.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""User defined types using class "TypedDict"."""
+
+from pathlib import Path
+from typing import TypedDict
+
+from pyspartaproj.context.extension.path_context import Paths
+
+
+class ArchiveStatus(TypedDict):
+    """Class to represent an archive information
+
+    It's used for test to take out directory from inside of archive.
+    """
+
+    archive: Path
+    expected: Paths
