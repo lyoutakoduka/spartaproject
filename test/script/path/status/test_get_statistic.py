8341afd839d47fe5666ae7b5397a7527ec9fd8bd
lyouta <lyouta@spartaproject.com>
2024-02-28 19:46:08 +0900

    Feat: Test to get list of file size from paths.

--- test/script/path/status/test_get_statistic.py
+++ test/script/path/status/test_get_statistic.py
@@ -7,8 +7,13 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Callable
 
+from pyspartaproj.context.default.integer_context import Ints
+from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.file.text.export_file import text_export
-from pyspartaproj.script.path.status.get_statistic import get_file_size
+from pyspartaproj.script.path.status.get_statistic import (
+    get_file_size,
+    get_file_size_array,
+)
 
 
 def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
@@ -28,6 +33,23 @@ def test_single() -> None:
     _inside_temporary_directory(individual_test)
 
 
+def test_array() -> None:
+    texts: Strs = ["first", "second", "third"]
+    expected: Ints = [len(text) for text in texts]
+
+    def individual_test(temporary_root: Path) -> None:
+        result: Ints = get_file_size_array(
+            [
+                text_export(Path(temporary_root, text + ".txt"), text)
+                for text in texts
+            ]
+        )
+
+        assert expected == result
+
+    _inside_temporary_directory(individual_test)
+
+
 def main() -> bool:
     """Run all tests.
 
@@ -35,4 +57,5 @@ def main() -> bool:
         bool: Success if get to the end of function.
     """
     test_single()
+    test_array()
     return True
