9080c7b2c92b6d172d7da1ed037925514d784e23
lyouta <lyouta@spartaproject.com>
2024-02-28 09:36:52 +0900

    Doc: Add module document string. #33

--- pyspartaproj/script/path/status/get_statistic.py
+++ pyspartaproj/script/path/status/get_statistic.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to get statistics about file."""
+
 from pathlib import Path
 
 
 def get_file_size(path: Path) -> int:
+    """Get size of file.
+
+    Args:
+        path (Path): Path of file you want to get size.
+
+    Returns:
+        int: File size.
+    """
     return path.stat().st_size
