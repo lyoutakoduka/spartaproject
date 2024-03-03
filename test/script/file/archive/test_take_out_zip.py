a6ffe121e505328eb6e179b5f612f04a85f84485
lyouta <lyouta@spartaproject.com>
2024-03-04 08:37:31 +0900

    Refactor: Use function of same functionality. #33

--- test/script/file/archive/test_take_out_zip.py
+++ test/script/file/archive/test_take_out_zip.py
@@ -169,7 +169,7 @@ def _create_archive_mix(working: PathPair) -> ArchiveStatus:
 def _get_take_out_list(working: PathPair) -> Paths:
     file_paths: Paths = []
 
-    for file_root in _add_temporary_directory(working["source"], _get_types()):
+    for file_root in _add_directories_test(working):
         file_paths += [file_root, create_temporary_file(file_root)]
 
     return file_paths
