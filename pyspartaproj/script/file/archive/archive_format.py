eb3da13907216f9b374a5a52a3c406312f512e2a
lyouta <lyouta@spartaproject.com>
2024-03-05 07:56:46 +0900

    Feat: Rename file format to archive type. #33

--- pyspartaproj/script/file/archive/archive_format.py
+++ pyspartaproj/script/file/archive/archive_format.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+
 
 def get_format() -> str:
     return "zip"
+
+
+def rename_format(path: Path) -> Path:
+    return path.with_suffix("." + get_format())
--- test/script/file/archive/test_archive_format.py
+++ test/script/file/archive/test_archive_format.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pyspartaproj.script.file.archive.archive_format import get_format
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Callable
+
+from pyspartaproj.script.file.archive.archive_format import (
+    get_format,
+    rename_format,
+)
+
+
+def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
+    with TemporaryDirectory() as temporary_path:
+        function(Path(temporary_path))
+
+
+def _get_format() -> str:
+    return "zip"
 
 
 def test_format() -> None:
-    assert "zip" == get_format()
+    assert _get_format() == get_format()
+
+
+def test_rename() -> None:
+    def individual_test(temporary_root: Path) -> None:
+        expected: Path = temporary_root.with_suffix("." + _get_format())
+        assert expected == rename_format(temporary_root)
+
+    _inside_temporary_directory(individual_test)
 
 
 def main() -> bool:
     test_format()
+    test_rename()
     return True
