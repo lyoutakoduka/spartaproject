82ab145e99f49642ed77b138c744fe7886c0a986
lyouta <lyouta@spartaproject.com>
2024-03-13 15:18:35 +0900

    Feat: Add sample tree for protected archive. #33

--- test/script/file/archive/test_take_out_archive.py
+++ test/script/file/archive/test_take_out_archive.py
@@ -252,6 +252,10 @@ def _create_archive_specific(working: PathPair) -> ArchiveStatus:
     )
 
 
+def _get_take_out_protect(working: PathPair) -> Paths:
+    return _add_test_tree_simple(working)
+
+
 def _replace_path_root(archive_path: Path, archive_root: Path) -> Paths:
     return get_absolute_array(
         _get_relative_archive(archive_path), root_path=archive_root
