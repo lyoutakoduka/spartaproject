2fd115fde6443c77714aa46d919513ecf9f4ffb3
lyouta <lyouta@spartaproject.com>
2024-03-21 23:26:00 +0900

    Feat: Get instance of compressed archive. #35

--- test/script/file/archive/test_compress_archive.py
+++ test/script/file/archive/test_compress_archive.py
@@ -171,6 +171,10 @@ def _get_archive(temporary_root: Path) -> CompressArchive:
     return CompressArchive(_get_archive_root(temporary_root))
 
 
+def _get_archive_compress(temporary_root: Path) -> CompressArchive:
+    return CompressArchive(_get_archive_root(temporary_root), compress=True)
+
+
 def test_empty() -> None:
     """Test to create empty archive."""
 
@@ -252,9 +256,7 @@ def test_compress() -> None:
     def individual_test(temporary_root: Path) -> None:
         tree_root: Path = _create_tree_compress(temporary_root)
 
-        compress_archive = CompressArchive(
-            _get_archive_root(temporary_root), compress=True
-        )
+        compress_archive = _get_archive_compress(temporary_root)
 
         walk_paths: Paths = list(
             walk_iterator(tree_root, directory=False, suffix="json")
