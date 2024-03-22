19ee0fce1812b0f7ce6d82b68655dffc76705a1d
lyouta <lyouta@spartaproject.com>
2024-03-22 16:31:28 +0900

    Refactor: Remove unnecessary variables. #35

--- test/script/file/archive/test_compress_archive.py
+++ test/script/file/archive/test_compress_archive.py
@@ -279,16 +279,15 @@ def test_tree() -> None:
 
     def individual_test(temporary_root: Path) -> None:
         tree_root: Path = _create_tree_tree(temporary_root)
-
-        compress_archive: CompressArchive = _get_archive(temporary_root)
-
         walk_paths: Paths = _get_walk_paths_tree(tree_root)
 
-        archive_paths: Paths = _finalize_archive(
-            tree_root, walk_paths, compress_archive
+        _common_test(
+            _finalize_archive(
+                tree_root, walk_paths, _get_archive(temporary_root)
+            ),
+            temporary_root,
+            walk_paths,
         )
-
-        _common_test(archive_paths, temporary_root, walk_paths)
 
     _inside_temporary_directory(individual_test)
 
