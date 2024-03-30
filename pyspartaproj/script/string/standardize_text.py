4222ab8acefef7e7af0c7da3389f15563a2f1e54
lyouta <lyouta@spartaproject.com>
2024-03-31 00:01:21 +0900

    Feat: Remove under bar of the both ends. #39

--- pyspartaproj/script/string/standardize_text.py
+++ pyspartaproj/script/string/standardize_text.py
@@ -13,11 +13,15 @@ def _convert_under(text: str) -> str:
     return text
 
 
+def _convert_strip(text: str) -> str:
+    return text.strip("_")
+
+
 def standardize_text(text: str) -> str:
     text = _convert_lower(text)
 
     text = _convert_under(text)
 
-    text = text.strip("_")
+    text = _convert_strip(text)
 
     return text
