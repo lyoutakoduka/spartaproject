131b25f5c67575d390307ae9dbeab0d574e142ce
lyouta <lyouta@spartaproject.com>
2024-04-11 15:25:49 +0900

    Doc: Add module document string. #41

--- test/script/string/test_convert_type.py
+++ test/script/string/test_convert_type.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to convert text to specific type."""
 
 from pyspartaproj.script.string.convert_type import convert_integer
 
 
 def test_number() -> None:
+    """Test to convert text to type "integer"."""
     assert 1 == convert_integer("0001")
 
 
 def main() -> bool:
+    """Run all tests.
+
+    Returns:
+        bool: Success if get to the end of function.
+    """
     test_number()
     return True
