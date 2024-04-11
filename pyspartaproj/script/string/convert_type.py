f136ef4bc0f50e935b5734a5ca95ee7e516e408b
lyouta <lyouta@spartaproject.com>
2024-04-11 15:21:51 +0900

    Doc: Add module document string. #41

--- pyspartaproj/script/string/convert_type.py
+++ pyspartaproj/script/string/convert_type.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to convert text to specific type."""
+
 
 def convert_integer(index: str) -> int | None:
+    """Convert text to type "integer".
+
+    Args:
+        index (str): Text you want to convert.
+
+    Returns:
+        int | None: Converted index from text.
+    """
     try:
         return int(index)
     except BaseException:
