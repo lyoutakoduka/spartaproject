9155775616d6d4b063b5a0594ab745f1f42a69a8
lyouta <lyouta@spartaproject.com>
2024-04-11 14:05:02 +0900

    Feat: Split function to compare base name. #41

--- test/script/string/test_base_name_elements.py
+++ test/script/string/test_base_name_elements.py
@@ -8,9 +8,17 @@ from pyspartaproj.context.typed.user_context import BaseName
 from pyspartaproj.script.string.base_name_elements import BaseNameElements
 
 
-def _compare_elements(name: str, index: int, name_elements: BaseName) -> None:
+def _compare_name(name: str, name_elements: BaseName) -> None:
     assert name == name_elements["name"]
+
+
+def _compare_index(index: int, name_elements: BaseName) -> None:
     assert index == name_elements["index"]
+
+
+def _compare_elements(name: str, index: int, name_elements: BaseName) -> None:
+    _compare_name(name, name_elements)
+    _compare_index(index, name_elements)
 
 
 def _get_identifier() -> str:
