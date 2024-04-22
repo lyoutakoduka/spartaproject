240eba2c192f816c393ae7affc2ba1cee6605c53
lyouta <lyouta@spartaproject.com>
2024-04-23 05:24:07 +0900

    Feat: Test to check path which is relative. #47

--- test/script/path/modify/test_get_relative.py
+++ test/script/path/modify/test_get_relative.py
@@ -5,9 +5,11 @@
 
 from pathlib import Path
 
+from pyspartaproj.context.default.bool_context import Bools
 from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.context.extension.path_context import PathPair, Paths
 from pyspartaproj.interface.pytest import raises
+from pyspartaproj.script.bool.compare_value import bool_compare_array
 from pyspartaproj.script.bool.same_value import bool_same_array
 from pyspartaproj.script.path.modify.get_absolute import (
     get_absolute,
@@ -18,6 +20,7 @@ from pyspartaproj.script.path.modify.get_relative import (
     get_relative,
     get_relative_array,
     get_relative_pair,
+    is_relative,
 )
 
 
@@ -31,6 +34,18 @@ def _get_current_file() -> Path:
 
 def _to_pair(path_types: Strs, paths: Paths) -> PathPair:
     return {path_type: path for path_type, path in zip(path_types, paths)}
+
+
+def test_check() -> None:
+    current: Path = _get_current_file()
+    expected: Bools = [True, False]
+
+    result: Bools = [
+        is_relative(path, root_path=current.parent)
+        for path in [current, _get_error()]
+    ]
+
+    assert bool_compare_array(expected, result)
 
 
 def test_unmatch() -> None:
@@ -81,6 +96,7 @@ def main() -> bool:
     Returns:
         bool: Success if get to the end of function.
     """
+    test_check()
     test_unmatch()
     test_single()
     test_root()
