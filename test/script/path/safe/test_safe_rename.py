b5863fbf1b3cf17e0cb82218186985249f6bc275
lyouta <lyouta@spartaproject.com>
2024-04-23 21:06:55 +0900

    Feat: Confirm file operation history is empty. #47

--- test/script/path/safe/test_safe_rename.py
+++ test/script/path/safe/test_safe_rename.py
@@ -22,13 +22,17 @@ from pyspartaproj.script.path.temporary.create_temporary_tree import (
 )
 
 
-def _common_test(history: PathPair2 | None) -> None:
+def _compare_empty(history: PathPair2 | None) -> PathPair2:
     if history is None:
         fail()
 
     assert 1 == len(history)
 
-    for _, path_pair in history.items():
+    return history
+
+
+def _common_test(history: PathPair2 | None) -> None:
+    for _, path_pair in _compare_empty(history).items():
         exists_pair: BoolPair = check_exists_pair(path_pair)
         assert bool_same_array(
             [not exists_pair["source.path"], exists_pair["destination.path"]]
