42f05d1a22635c3205c125323ecdbee03c362708
lyouta <lyouta@spartaproject.com>
2024-04-27 17:51:46 +0900

    Feat: Create sub directory into working space. #47

--- test/script/directory/test_work_space.py
+++ test/script/directory/test_work_space.py
@@ -30,6 +30,15 @@ def test_root() -> None:
     assert work_space_root.exists()
 
 
+def test_directory() -> None:
+    """Test for path of temporary working space you specified."""
+
+    def individual_test(temporary_root: Path) -> None:
+        _compare_directory(WorkSpace(working_root=temporary_root))
+
+    _inside_temporary_directory(individual_test)
+
+
 def main() -> bool:
     """Run all tests.
 
@@ -37,4 +46,5 @@ def main() -> bool:
         bool: Success if get to the end of function.
     """
     test_root()
+    test_directory()
     return True
