9e083237e9ca76850d5bf36bc6af1c71b31b2886
lyouta <lyouta@spartaproject.com>
2024-04-28 08:36:24 +0900

    Refactor: Rename function to suitable. #47

--- test/script/directory/test_work_space.py
+++ test/script/directory/test_work_space.py
@@ -16,7 +16,7 @@ def _relative_test(path: Path, root: Path) -> None:
     assert is_relative(path, root_path=root)
 
 
-def _compare_relative(temporary_root: Path, work_space: WorkSpace) -> None:
+def _compare_path(temporary_root: Path, work_space: WorkSpace) -> None:
     _relative_test(work_space.get_working_root(), temporary_root)
 
 
@@ -35,9 +35,7 @@ def test_path() -> None:
     """Test to check path of temporary working space you specified."""
 
     def individual_test(temporary_root: Path) -> None:
-        _compare_relative(
-            temporary_root, WorkSpace(working_root=temporary_root)
-        )
+        _compare_path(temporary_root, WorkSpace(working_root=temporary_root))
 
     _inside_temporary_directory(individual_test)
 
