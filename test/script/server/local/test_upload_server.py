8bf6add723154f915e537f3e55fd5be2ee4ca8df
lyouta <lyouta@spartaproject.com>
2024-05-10 14:18:25 +0900

    Refactor: Rename function to suitable. #47

--- test/script/server/local/test_upload_server.py
+++ test/script/server/local/test_upload_server.py
@@ -18,7 +18,7 @@ from pyspartaproj.script.path.temporary.create_temporary_tree import (
 from pyspartaproj.script.server.local.upload_server import UploadServer
 
 
-def _common_test(server: UploadServer, source_path: Path) -> None:
+def _upload_path(server: UploadServer, source_path: Path) -> None:
     assert server.upload(source_path)
 
 
@@ -44,7 +44,7 @@ def test_file() -> None:
     server: UploadServer = _get_server()
     _is_connect(server)
 
-    _common_test(server, create_temporary_file(server.get_working_root()))
+    _upload_path(server, create_temporary_file(server.get_working_root()))
 
 
 def test_directory() -> None:
@@ -52,7 +52,7 @@ def test_directory() -> None:
     server: UploadServer = _get_server()
     _is_connect(server)
 
-    _common_test(
+    _upload_path(
         server, create_directory(Path(server.get_working_root(), "directory"))
     )
 
@@ -62,7 +62,7 @@ def test_tree() -> None:
     server: UploadServer = _get_server()
     _is_connect(server)
 
-    _common_test(
+    _upload_path(
         server,
         create_temporary_tree(
             Path(server.get_working_root(), "tree"), tree_deep=2
