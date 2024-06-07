3b178d9a3a3cb25862ef880d4ffdf762a5114949
lyouta <lyouta@spartaproject.com>
2024-06-08 05:53:52 +0900

    Refactor: Function that is same functionality. #47

--- test/script/path/safe/test_safe_trash.py
+++ test/script/path/safe/test_safe_trash.py
@@ -197,7 +197,7 @@ def test_work() -> None:
 
 def test_different() -> None:
     def individual_test(temporary_root: Path) -> None:
-        safe_trash = SafeTrash(trash_root=temporary_root, override=True)
+        safe_trash: SafeTrash = _get_remove_trash(temporary_root)
         _compare_path_not(*_get_relative_roots(_get_trash_roots(safe_trash)))
 
     _inside_temporary_directory(individual_test)
