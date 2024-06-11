9f62b60e72068980cbb069feccb19c6e88beb1be
lyouta <lyouta@spartaproject.com>
2024-06-12 02:19:19 +0900

    Fix: Open archive before edit it. #47

--- test/script/file/archive/test_edit_archive.py
+++ test/script/file/archive/test_edit_archive.py
@@ -473,7 +473,11 @@ def test_path() -> None:
         _create_source(temporary_root)
 
         archive_path: Path = _get_archive_path(temporary_root)
-        _path_test(archive_path, _get_edit_path(archive_path))
+
+        edit_archive: EditArchive = _get_edit()
+        edit_archive.open_archive(archive_path=archive_path)
+
+        _path_test(archive_path, edit_archive)
 
     _inside_temporary_directory(individual_test)
 
