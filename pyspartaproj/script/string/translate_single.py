ff89747f9d5c3fbc7d423071e3ae068b824b8e0c
lyouta <lyouta@spartaproject.com>
2024-06-22 20:19:22 +0900

    Feat: Fill characters by using hex table. #57

--- pyspartaproj/script/string/translate_single.py
+++ pyspartaproj/script/string/translate_single.py
@@ -15,3 +15,10 @@ def _get_hex_tables() -> Ints3:
         [[0xFF41, 0xFF5A], [0x61, 0x7A]],
         [[0xFF10, 0xFF19], [0x30, 0x39]],
     ]
+
+
+def _fill_hex_tables(hex_tables: Ints3) -> Ints3:
+    return [
+        [_fill_character(hex_span) for hex_span in hex_table]
+        for hex_table in hex_tables
+    ]
