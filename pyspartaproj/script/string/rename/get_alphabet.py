bef2c6c0fed945cfbf9a7d5c224a4c839e2320d0
lyouta <lyouta@spartaproject.com>
2024-06-24 09:57:35 +0900

    Refactor: Remove unused functions. #57

--- pyspartaproj/script/string/rename/get_alphabet.py
+++ pyspartaproj/script/string/rename/get_alphabet.py
@@ -7,33 +7,6 @@ from pyspartaproj.context.default.string_context import Strs, Strs2
 from pyspartaproj.context.typed.user_context import Alphabets
 
 
-def _to_characters(numbers: Ints) -> Strs:
-    return [chr(number) for number in numbers]
-
-
-def _fill_character(characters: Ints) -> Ints:
-    return list(range(characters[0], characters[1] + 1))
-
-
-def _get_hex_tables(multiple: bool) -> Ints2:
-    if multiple:
-        return [[0xFF21, 0xFF3A], [0xFF41, 0xFF5A], [0xFF10, 0xFF19]]
-    else:
-        return [[0x41, 0x5A], [0x61, 0x7A], [0x30, 0x39]]
-
-
-def _fill_hex_tables(hex_table: Ints2) -> Ints2:
-    return [_fill_character(hex_span) for hex_span in hex_table]
-
-
-def _get_alphabet_table(filled_table: Ints2) -> Strs2:
-    return [_to_characters(numbers) for numbers in filled_table]
-
-
-def _get_table(multiple: bool) -> Strs2:
-    return _get_alphabet_table(_fill_hex_tables(_get_hex_tables(multiple)))
-
-
 def _struct_alphabet(
     big: Strs, small: Strs, number: Strs, other: Strs
 ) -> Alphabets:
