d450198312e60c38361fb1c66d0f1edb24683bc5
lyouta <lyouta@spartaproject.com>
2024-06-26 12:37:17 +0900

    Feat: Compare both ends of character table. #57

--- test/script/string/rename/test_get_alphabet.py
+++ test/script/string/rename/test_get_alphabet.py
@@ -6,3 +6,7 @@ from pyspartaproj.context.default.string_context import Strs2
 
 def _compare_counts(tables: Strs2) -> None:
     assert [26, 26, 10, 33] == [len(table) for table in tables]
+
+
+def _compare_filtered(expected: Strs2, tables: Strs2) -> None:
+    assert expected == [[table[-i] for i in range(2)] for table in tables]
