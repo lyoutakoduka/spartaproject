0d6cc2d781f77645c4dada64d6fc90bf65dec643
lyouta <lyouta@spartaproject.com>
2024-06-30 19:02:34 +0900

    Feat: Compare specific split identifier. #59

--- test/script/string/rename/test_split_identifier.py
+++ test/script/string/rename/test_split_identifier.py
@@ -4,12 +4,16 @@
 from pyspartaproj.script.string.rename.split_identifier import SplitIdentifier
 
 
+def _compare_identifier(
+    identifier: str, split_identifier: SplitIdentifier
+) -> None:
+    assert identifier == split_identifier.get_identifier()
+
+
 def test_path() -> None:
-    split_identifier = SplitIdentifier()
-    assert "_" == split_identifier.get_identifier()
+    _compare_identifier("_", SplitIdentifier())
 
 
 def test_specific() -> None:
     identifier: str = "-"
-    split_identifier = SplitIdentifier(identifier=identifier)
-    assert identifier == split_identifier.get_identifier()
+    _compare_identifier(identifier, SplitIdentifier(identifier=identifier))
