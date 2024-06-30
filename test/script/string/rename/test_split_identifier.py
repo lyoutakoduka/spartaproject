6fa8d03d4ddc45934aff35a9df9fd996c1d495ae
lyouta <lyouta@spartaproject.com>
2024-06-30 18:58:14 +0900

    Feat: Test for user defined split identifier. #59

--- test/script/string/rename/test_split_identifier.py
+++ test/script/string/rename/test_split_identifier.py
@@ -7,3 +7,9 @@ from pyspartaproj.script.string.rename.split_identifier import SplitIdentifier
 def test_path() -> None:
     split_identifier = SplitIdentifier()
     assert "_" == split_identifier.get_identifier()
+
+
+def test_specific() -> None:
+    identifier: str = "-"
+    split_identifier = SplitIdentifier(identifier=identifier)
+    assert identifier == split_identifier.get_identifier()
