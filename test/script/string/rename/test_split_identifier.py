7b4ae6e63c6d80d4e4ebb495aee3b8b851e1b06f
lyouta <lyouta@spartaproject.com>
2024-07-01 15:39:23 +0900

    Doc: Update module document string. #59

--- test/script/string/rename/test_split_identifier.py
+++ test/script/string/rename/test_split_identifier.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to convert string by using the split identifier."""
+
 from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.string.rename.split_identifier import SplitIdentifier
 
@@ -12,22 +14,27 @@ def _compare_identifier(
 
 
 def test_path() -> None:
+    """Test to get default split identifier."""
     _compare_identifier("_", SplitIdentifier())
 
 
 def test_specific() -> None:
+    """Test to get selected split identifier."""
     identifier: str = "-"
     _compare_identifier(identifier, SplitIdentifier(identifier=identifier))
 
 
 def test_strip() -> None:
-    """Test to remove under bar of the both ends."""
+    """Test to remove the split identifier of the both ends."""
     expected: str = "test"
     assert expected == SplitIdentifier().convert_strip("__" + expected + "__")
 
 
 def test_identifier() -> None:
-    """Test to convert some characters to the split identifier."""
+    """Test to convert some characters to the split identifier.
+
+    Candidates are characters other than alphabets and numbers.
+    """
     names: Strs = ["first", "second", "third"]
 
     for identifier in [" ", ".", "-", "~"]:
