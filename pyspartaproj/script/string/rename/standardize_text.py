29a3ed6338e5a8152d26f500427f3bd95e80f06a
lyouta <lyouta@spartaproject.com>
2024-07-01 18:57:39 +0900

    Update: Change functionality to class. #59

--- pyspartaproj/script/string/rename/standardize_text.py
+++ pyspartaproj/script/string/rename/standardize_text.py
@@ -4,28 +4,26 @@
 """Module to standardize string for key of dictionary."""
 
 
-def _convert_lower(text: str) -> str:
-    return text.lower()
+class StandardizeText:
+    def _convert_lower(text: str) -> str:
+        return text.lower()
 
+    def _convert_under(text: str) -> str:
+        for identifier in [" ", ".", "-"]:
+            text = text.replace(identifier, "_")
 
-def _convert_under(text: str) -> str:
-    for identifier in [" ", ".", "-"]:
-        text = text.replace(identifier, "_")
+        return text
 
-    return text
+    def _convert_strip(text: str) -> str:
+        return text.strip("_")
 
+    def standardize_text(text: str) -> str:
+        """Function to standardize string for key of dictionary.
 
-def _convert_strip(text: str) -> str:
-    return text.strip("_")
+        Args:
+            text (str): Text you want to standardize.
 
-
-def standardize_text(text: str) -> str:
-    """Function to standardize string for key of dictionary.
-
-    Args:
-        text (str): Text you want to standardize.
-
-    Returns:
-        str: Standardize text.
-    """
-    return _convert_strip(_convert_under(_convert_lower(text)))
+        Returns:
+            str: Standardize text.
+        """
+        return _convert_strip(_convert_under(_convert_lower(text)))
