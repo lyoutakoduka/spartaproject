d7a64fae00eabbfed5be769b7140ca480946a3ae
lyouta <lyouta@spartaproject.com>
2024-07-02 01:40:44 +0900

    Refactor: Remove unused methods. #59

--- pyspartaproj/script/string/rename/standardize_text.py
+++ pyspartaproj/script/string/rename/standardize_text.py
@@ -10,15 +10,6 @@ class StandardizeText(SplitIdentifier):
     def _convert_lower(self, text: str) -> str:
         return text.lower()
 
-    def _convert_under(self, text: str) -> str:
-        for identifier in [" ", ".", "-"]:
-            text = text.replace(identifier, "_")
-
-        return text
-
-    def _convert_strip(self, text: str) -> str:
-        return text.strip("_")
-
     def standardize_text(self, text: str) -> str:
         """Function to standardize string for key of dictionary.
 
