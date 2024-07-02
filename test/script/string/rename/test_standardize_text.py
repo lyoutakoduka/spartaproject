360d0ed779ffe9043abfd434640a89602f286156
lyouta <lyouta@spartaproject.com>
2024-07-02 16:30:50 +0900

    Feat: Compare standardized text. #59

--- test/script/string/rename/test_standardize_text.py
+++ test/script/string/rename/test_standardize_text.py
@@ -6,19 +6,27 @@
 from pyspartaproj.script.string.rename.standardize_text import StandardizeText
 
 
+def _compare_text(
+    expected: str, source: str, standardize: StandardizeText
+) -> None:
+    assert expected == standardize.standardize(source)
+
+
 def test_lower() -> None:
     """Test to convert upper case string to lower case."""
-    assert "test" == StandardizeText(lower=True).standardize("TEST")
+    _compare_text("test", "TEST", StandardizeText(lower=True))
 
 
 def test_under() -> None:
     """Test to convert some characters to under bar."""
     for identifier in [" ", ".", "-"]:
-        assert "test_name" == StandardizeText(under=True).standardize(
-            identifier.join(["test", "name"])
+        _compare_text(
+            "test_name",
+            identifier.join(["test", "name"]),
+            StandardizeText(under=True),
         )
 
 
 def test_strip() -> None:
     """Test to remove under bar of the both ends."""
-    assert "test" == StandardizeText(strip=True).standardize("__test__")
+    _compare_text("test", "__test__", StandardizeText(strip=True))
