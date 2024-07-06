fec195b345d8dfef0a6b4d0d8fe68401eaefeb0f
lyouta <lyouta@spartaproject.com>
2024-07-06 10:26:16 +0900

    Feat: Confirm that text are included in table. #63

---
+++ test/script/string/rename/test_filter_table.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from pyspartaproj.context.default.string_context import Strs
+from pyspartaproj.script.string.rename.filter_table import FilterTable
+
+
+def _filter_text(texts: Strs, filter_table: FilterTable) -> bool:
+    return filter_table.contain("".join(texts))
