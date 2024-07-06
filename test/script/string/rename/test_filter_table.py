ef16570815a7b53a025a92b9da412ae546eb0fc8
lyouta <lyouta@spartaproject.com>
2024-07-06 10:27:54 +0900

    Feat: Test to confirm text included in table. #63

--- test/script/string/rename/test_filter_table.py
+++ test/script/string/rename/test_filter_table.py
@@ -8,3 +8,7 @@ from pyspartaproj.script.string.rename.filter_table import FilterTable
 
 def _filter_text(texts: Strs, filter_table: FilterTable) -> bool:
     return filter_table.contain("".join(texts))
+
+
+def _enable_text(texts: Strs, filter_table: FilterTable) -> None:
+    assert _filter_text(texts, filter_table)
