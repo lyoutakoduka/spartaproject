f20c487f968f7a106932080a22d0a623ed32a17f
lyouta <lyouta@spartaproject.com>
2024-07-06 10:00:08 +0900

    Feat: Serialize merged character tables. #63

--- pyspartaproj/script/string/rename/filter_table.py
+++ pyspartaproj/script/string/rename/filter_table.py
@@ -2,11 +2,20 @@
 # -*- coding: utf-8 -*-
 
 
+from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.string.rename.grouped_characters import (
     GroupedCharacters,
 )
 
 
 class FilterTable(GroupedCharacters):
+    def _serialize_tables(self) -> Strs:
+        serialized: Strs = []
+
+        for table in self.get_merged_tables():
+            serialized += table
+
+        return serialized
+
     def __init__(self, multiple: bool = False) -> None:
         super().__init__(multiple=multiple)
