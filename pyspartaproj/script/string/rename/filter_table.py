7f51d4b25abbd14e5268e6b758d181ddbd2c9613
lyouta <lyouta@spartaproject.com>
2024-07-07 08:31:04 +0900

    Doc: Add module document string. #63

--- pyspartaproj/script/string/rename/filter_table.py
+++ pyspartaproj/script/string/rename/filter_table.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to confirm that characters in selected string.
+
+Each character should be included in user defined character tables.
+"""
 
 from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.string.rename.grouped_characters import (
@@ -9,6 +13,11 @@ from pyspartaproj.script.string.rename.grouped_characters import (
 
 
 class FilterTable(GroupedCharacters):
+    """Class to confirm that characters in selected string.
+
+    Each character should be included in user defined character tables.
+    """
+
     def _serialize_tables(self) -> Strs:
         serialized: Strs = []
 
@@ -21,6 +30,16 @@ class FilterTable(GroupedCharacters):
         self._serialized: Strs = self._serialize_tables()
 
     def contain(self, text: str) -> bool:
+        """Confirm that characters in selected string.
+
+        Each character should be included in user defined character tables.
+
+        Args:
+            text (str): String you want to confirm.
+
+        Returns:
+            bool: True if all characters are exists in character tables.
+        """
         for single in text:
             if single not in self._serialized:
                 return False
@@ -28,6 +47,14 @@ class FilterTable(GroupedCharacters):
         return True
 
     def __init__(self, multiple: bool = False) -> None:
+        """Initialize super class and variables.
+
+        Args:
+            multiple (bool, optional): Defaults to False.
+                True if you want to select character tables
+                    which is constructed by multiple byte.
+                It's used for argument "multiple" of class "GroupedCharacters".
+        """
         super().__init__(multiple=multiple)
 
         self._initialize_variables_filter()
