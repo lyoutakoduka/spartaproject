4a0118a0576cc7a48f14de86f680e41db52c04fa
lyouta <lyouta@spartaproject.com>
2024-07-07 20:09:50 +0900

    Doc: Add module document string. #63

--- test/script/string/rename/test_filter_table.py
+++ test/script/string/rename/test_filter_table.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to confirm that characters in selected string.
+
+Each character should be included in user defined character tables.
+"""
 
 from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.string.rename.filter_table import FilterTable
@@ -23,14 +27,17 @@ def _get_zero(multiple: bool = False) -> str:
 
 
 def test_ascii() -> None:
+    """Test to confirm that characters in Ascii table are included."""
     _enable_text(["A", "a", _get_zero()], FilterTable())
 
 
 def test_other() -> None:
+    """Test to confirm that characters not in Ascii table are included."""
     _disable_text(["À", "à", _get_zero()], FilterTable())
 
 
 def test_multiple() -> None:
+    """Test to confirm that multiple byte characters are included."""
     _enable_text(
         ["\uff21", "\uff41", _get_zero(multiple=True)],
         FilterTable(multiple=True),
@@ -38,6 +45,7 @@ def test_multiple() -> None:
 
 
 def test_jp() -> None:
+    """Test to confirm that Japanese characters are included."""
     _disable_text(
         ["\u3042", "\u3044", _get_zero(multiple=True)],
         FilterTable(multiple=True),
