81fdb2beb2ab9ee71910648746b7cb5cdd0f40d0
lyouta <lyouta@spartaproject.com>
2024-07-14 20:44:52 +0900

    Feat: Execute any Python script. #65

--- test/script/shell/test_execute_python.py
+++ test/script/shell/test_execute_python.py
@@ -26,6 +26,10 @@ def _get_config_file() -> Path:
     return get_resource(local_path=Path("execute_python", "forward.json"))
 
 
+def _execute_python(commands: Strs) -> Strs:
+    return list(execute_python(commands, forward=_get_config_file()))
+
+
 def _get_script_text(script_text: str) -> str:
     return get_script_string(get_resource(local_path=Path(script_text)))
 
