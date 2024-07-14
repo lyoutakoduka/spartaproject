dd9ded9d6a4375fc24c9a5cb5c55a07060882f54
lyouta <lyouta@spartaproject.com>
2024-07-15 03:31:27 +0900

    Update: Add arguments about context file. #65

--- pyspartaproj/script/server/local/upload_server.py
+++ pyspartaproj/script/server/local/upload_server.py
@@ -127,6 +127,8 @@ class UploadServer(ConnectServer):
         local_root: Path | None = None,
         override: bool = False,
         jst: bool = False,
+        forward: Path | None = None,
+        platform: str | None = None,
     ) -> None:
         """Initialize super class.
 
@@ -143,4 +145,10 @@ class UploadServer(ConnectServer):
                 If True, you can get datetime object as JST time zone.
                 It's used for argument "jst" of class "ConnectServer".
         """
-        super().__init__(local_root=local_root, override=override, jst=jst)
+        super().__init__(
+            local_root=local_root,
+            override=override,
+            jst=jst,
+            forward=forward,
+            platform=platform,
+        )
