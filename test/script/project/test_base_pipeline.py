7a924fbcd220ff70755c6b0eeb2ee24c999f1d70
lyouta <lyouta@spartaproject.com>
2024-07-17 20:52:31 +0900

    Feat: Convert log on stdout to string. #65

--- test/script/project/test_base_pipeline.py
+++ test/script/project/test_base_pipeline.py
@@ -3,8 +3,23 @@
 
 from pathlib import Path
 
+from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.path.modify.get_resource import get_resource
+from pyspartaproj.script.project.base_pipeline import BasePipeline
+from pyspartaproj.script.string.off_stdout import StdoutText
 
 
 def _get_config_file() -> Path:
     return get_resource(local_path=Path("base_pipeline", "forward.json"))
+
+
+def _convert_print(messages: Strs, pipeline: BasePipeline) -> str:
+    stdout_text = StdoutText()
+
+    @stdout_text.decorator
+    def _messages() -> None:
+        pipeline.show_log(messages)
+
+    _messages()
+
+    return stdout_text.show()
