3ef6b902171882f8cd49f838b001ba8c3fdef517
lyouta <lyouta@spartaproject.com>
2024-07-18 00:22:32 +0900

    Doc: Add module document string. #65

--- pyspartaproj/script/project/base_pipeline.py
+++ pyspartaproj/script/project/base_pipeline.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to handling I/O functionality of any script called pipeline."""
+
 from pathlib import Path
 
 from pyspartaproj.context.default.string_context import Strs
@@ -9,6 +11,8 @@ from pyspartaproj.script.time.count.log_timer import LogTimer
 
 
 class BasePipeline(ProjectContext, LogTimer):
+    """Class to handling I/O functionality of any script called pipeline."""
+
     def _initialize_super_class(
         self, platform: str | None, forward: Path | None
     ) -> None:
@@ -23,9 +27,27 @@ class BasePipeline(ProjectContext, LogTimer):
             self._show_message(message_timer + ": " + message)
 
     def show_log(self, messages: Strs) -> None:
+        """Show message as log to stdout.
+
+        Args:
+            messages (Strs): Message list which will merged by space character.
+        """
         self._log_with_timer(" ".join(messages))
 
     def __init__(
         self, platform: str | None = None, forward: Path | None = None
     ) -> None:
+        """Initialize super class and variables.
+
+        Args:
+            platform (str | None, optional): Defaults to None.
+                You can select an execution platform from "linux" or "windows".
+                Current execution platform is selected if argument is None.
+                It's used for argument "platform" of class "ProjectContext".
+
+            forward (Path | None, optional): Defaults to None.
+                Path of setting file in order to place
+                    project context file to any place.
+                It's used for argument "forward" of class "ProjectContext".
+        """
         self._initialize_super_class(platform, forward)
