564e85cb0bf461d9b22e1e1bb3ce935c2d89507e
lyouta <lyouta@spartaproject.com>
2024-07-22 02:39:36 +0900

    Doc: Add module document string. #69

--- pyspartaproj/script/string/encoding/find_encoding.py
+++ pyspartaproj/script/string/encoding/find_encoding.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to find character encoding from string automatically."""
+
 from chardet.universaldetector import UniversalDetector
 
 
 def find_encoding(byte: bytes) -> str:
+    """Find character encoding from string automatically.
+
+    Args:
+        byte (bytes): Byte data you want to get character encoding.
+
+    Returns:
+        str: Character encoding of the byte data.
+    """
     encoding: str = "utf-8"
 
     detector = UniversalDetector()
