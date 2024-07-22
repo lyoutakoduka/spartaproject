01783ada47df4ab9c7fee4a1788baf56d18c6bac
lyouta <lyouta@spartaproject.com>
2024-07-22 11:32:53 +0900

    Doc: Add module document string. #69

--- test/script/string/encoding/test_set_encoding.py
+++ test/script/string/encoding/test_set_encoding.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to encode string by specific character encoding."""
 
 from pyspartaproj.script.string.encoding.set_encoding import set_encoding
 
@@ -10,6 +11,7 @@ def _compare_encoding(expected: bytes, result: bytes) -> None:
 
 
 def test_utf() -> None:
+    """Test to encode string by default character encoding."""
     source_text: str = "\u3042"
     expected: bytes = b"\xe3\x81\x82"
 
@@ -17,6 +19,7 @@ def test_utf() -> None:
 
 
 def test_sjis() -> None:
+    """Test to encode string by specific character encoding."""
     source_text: str = "\u3042"
     expected: bytes = b"\x82\xa0"
 
