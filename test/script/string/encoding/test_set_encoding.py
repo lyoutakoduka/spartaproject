2a35da04146ce44942ee1f4a4d9590fb0290529e
lyouta <lyouta@spartaproject.com>
2024-07-22 11:09:38 +0900

    Feat: Compare default character encoding. #69

--- test/script/string/encoding/test_set_encoding.py
+++ test/script/string/encoding/test_set_encoding.py
@@ -2,5 +2,15 @@
 # -*- coding: utf-8 -*-
 
 
+from pyspartaproj.script.string.encoding.set_encoding import set_encoding
+
+
 def _compare_encoding(expected: bytes, result: bytes) -> None:
     assert expected == result
+
+
+def test_utf() -> None:
+    source_text: str = "\u3042"
+    expected: bytes = b"\xe3\x81\x82"
+
+    _compare_encoding(expected, set_encoding(source_text))
