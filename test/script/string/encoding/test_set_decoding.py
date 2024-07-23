4df995bbd4a16276a8761d38c2acba8900ac830f
lyouta <lyouta@spartaproject.com>
2024-07-23 22:02:21 +0900

    Feat: Decode byte data by default encoding. #69

--- test/script/string/encoding/test_set_decoding.py
+++ test/script/string/encoding/test_set_decoding.py
@@ -2,5 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
+from pyspartaproj.script.string.encoding.set_decoding import set_decoding
+
+
 def _compare_decoding(result: str) -> None:
     assert "\u3042" == result
+
+
+def test_utf() -> None:
+    source: bytes = b"\xe3\x81\x82"
+
+    _compare_decoding(set_decoding(source))
