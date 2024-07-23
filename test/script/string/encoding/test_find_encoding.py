227dbb65d7080e703ebc7fd9133bed1d137550d2
lyouta <lyouta@spartaproject.com>
2024-07-24 04:44:06 +0900

    Refactor: Use same functionality. #69

--- test/script/string/encoding/test_find_encoding.py
+++ test/script/string/encoding/test_find_encoding.py
@@ -4,10 +4,13 @@
 """Test module to find character encoding from string automatically."""
 
 from pyspartaproj.script.string.encoding.find_encoding import find_encoding
+from pyspartaproj.script.string.encoding.set_encoding import set_encoding
 
 
 def _common_test(encoding: str) -> None:
-    assert encoding == find_encoding(chr(12354).encode(encoding))
+    assert encoding == find_encoding(
+        set_encoding(chr(12354), encoding=encoding)
+    )
 
 
 def test_utf() -> None:
@@ -22,4 +25,7 @@ def test_sjis() -> None:
 
 def test_other() -> None:
     """Test to find character encoding which is others."""
-    assert "shift-jis" != find_encoding(chr(12354).encode("euc-jp"))
+
+    assert "shift-jis" != find_encoding(
+        set_encoding(chr(12354), encoding="euc-jp")
+    )
