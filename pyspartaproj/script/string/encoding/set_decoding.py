ab0abc68be851b8d9ae372a2a971d002a6a57cbd
lyouta <lyouta@spartaproject.com>
2024-07-23 21:55:24 +0900

    Doc: Add module document string. #69

--- pyspartaproj/script/string/encoding/set_decoding.py
+++ pyspartaproj/script/string/encoding/set_decoding.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to decode byte data by specific character encoding."""
 
 from pyspartaproj.script.string.encoding.find_encoding import find_encoding
 
 
 def set_decoding(byte: bytes, encoding: str | None = None) -> str:
+    """Decode byte data by specific character encoding.
+
+    Args:
+        byte (bytes): Byte data you want to decode.
+
+        encoding (str | None, optional): Defaults to None.
+            Character encoding used for decoding forcibly.
+
+    Returns:
+        str: Decoded string.
+    """
     if encoding is None:
         encoding = find_encoding(byte)
 
