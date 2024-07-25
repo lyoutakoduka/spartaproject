9f3ddeb9e628f6b303a982fb01e40e954c144d2b
lyouta <lyouta@spartaproject.com>
2024-07-25 14:23:00 +0900

    Doc: Add module document string. #69

--- test/script/file/json/test_import_json.py
+++ test/script/file/json/test_import_json.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to import Json file or load Json data."""
+
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Dict
@@ -18,31 +20,37 @@ def _common_test(expected: Single, source: str) -> None:
 
 
 def test_none() -> None:
+    """Test to load Json data as None data."""
     expected: None = None
     _common_test(expected, "null")
 
 
 def test_bool() -> None:
+    """Test to load Json data as type boolean."""
     expected: bool = True
     _common_test(expected, "true")
 
 
 def test_integer() -> None:
+    """Test to load Json data as type integer."""
     expected: int = 1
     _common_test(expected, str(expected))
 
 
 def test_float() -> None:
+    """Test to load Json data as type decimal."""
     expected: float = 0.1
     _common_test(expected, str(expected))
 
 
 def test_string() -> None:
+    """Test to load Json data as type string."""
     expected: str = "test"
     _common_test(expected, '"%s"' % expected)
 
 
 def test_export() -> None:
+    """Test to import Json file as format "json"."""
     expected: Json = [None, True, 1, "test"]
 
     with TemporaryDirectory() as temporary_path:
