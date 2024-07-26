44b7c9cc99617db3a37f48a1ef378c94c4fa695b
lyouta <lyouta@spartaproject.com>
2024-07-26 16:43:05 +0900

    Feat: Get characters which will added. #71

--- pyspartaproj/script/string/rename/grouped_characters.py
+++ pyspartaproj/script/string/rename/grouped_characters.py
@@ -4,7 +4,7 @@
 """Module to get character tables constructed by multiple or single byte."""
 
 from pyspartaproj.context.default.integer_context import Ints
-from pyspartaproj.context.default.string_context import Strs, Strs2
+from pyspartaproj.context.default.string_context import Strs, Strs2, StrsPair
 from pyspartaproj.context.typed.user_context import CharacterTable
 
 
@@ -18,6 +18,17 @@ class GroupedCharacters:
             index_base += 65248
 
         return index_base
+
+    def _get_special_pair(self) -> StrsPair:
+        return {
+            " ": ["\u3000"],
+            ".": ["\u30fb", "\u3002", "\u2026"],
+            ",": ["\u3001"],
+            "[": ["\u300c", "\u3010", "\u300e"],
+            "]": ["\u300d", "\u3011", "\u300f"],
+            '"': ["\u201d", "\u201c"],
+            "'": ["\u2019", "\u2018"],
+        }
 
     def _get_special_tables(self, multiple: bool) -> Strs:
         return ["\u3000" if multiple else " "]
