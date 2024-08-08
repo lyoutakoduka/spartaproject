ca5c8e5a9784d292e8a8fc0abcaa5b509c574fb7
lyouta <lyouta@spartaproject.com>
2024-08-08 17:27:00 +0900

    Feat: Create archive including multiple byte. #75

--- test/script/file/archive/test_compress_archive.py
+++ test/script/file/archive/test_compress_archive.py
@@ -14,10 +14,13 @@ from pyspartaproj.context.default.string_context import StrPair
 from pyspartaproj.context.extension.decimal_context import Decs
 from pyspartaproj.context.extension.path_context import Paths, Paths2
 from pyspartaproj.script.decimal.initialize_decimal import initialize_decimal
+from pyspartaproj.script.directory.create_parent import create_parent
 from pyspartaproj.script.file.archive.compress_archive import CompressArchive
 from pyspartaproj.script.file.json.convert_from_json import (
     string_pair_from_json,
 )
+from pyspartaproj.script.file.json.convert_to_json import multiple_to_json
+from pyspartaproj.script.file.json.export_json import json_export
 from pyspartaproj.script.file.json.import_json import json_import
 from pyspartaproj.script.path.iterate_directory import walk_iterator
 from pyspartaproj.script.path.modify.get_relative import get_relative_array
@@ -281,6 +284,11 @@ def _get_walk_paths_limit(tree_root: Path) -> Paths:
 
 def _get_walk_paths_heavy(tree_root: Path) -> Paths:
     return list(walk_iterator(tree_root, directory=False, suffix="json"))
+
+
+def _export_multiple(config_path: Path, config_data: StrPair) -> None:
+    create_parent(config_path)
+    json_export(config_path, multiple_to_json(config_data))
 
 
 def test_empty() -> None:
