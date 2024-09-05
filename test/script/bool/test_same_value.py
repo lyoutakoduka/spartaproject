d652dcebe4544abc43c382588003c543eadf898b
lyouta <lyouta@spartaproject.com>
2024-09-05 18:50:37 +0900

    Doc: Update module document string. #75

--- test/script/bool/test_same_value.py
+++ test/script/bool/test_same_value.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to confirm that type bool values are same and True."""
+
 from pyspartaproj.context.default.bool_context import BoolPair, Bools
 from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.script.bool.same_value import bool_same_array, bool_same_pair
@@ -30,28 +32,35 @@ def _expected_pair(status: bool) -> BoolPair:
 
 
 def test_empty() -> None:
+    """Test for list of bool value as empty."""
     _confirm_error(bool_same_array([]))
 
 
 def test_mixed() -> None:
+    """Test for list of bool value which is mix of True and False."""
     _confirm_error(bool_same_array([False, True, False]))
 
 
 def test_false() -> None:
+    """Test for list of bool value which is all False."""
     _confirm_error(bool_same_array(_expected_list(False)))
 
 
 def test_array() -> None:
+    """Test for list of bool value which is all True."""
     _confirm(bool_same_array(_expected_list(True)))
 
 
 def test_invert() -> None:
+    """Test for list of bool value with invert option."""
     _confirm(bool_same_array(_expected_list(False), invert=True))
 
 
 def test_pair() -> None:
+    """Test for pair of bool value which is all True."""
     _confirm(bool_same_pair(_expected_pair(True)))
 
 
 def test_pair_invert() -> None:
+    """Test for pair of bool value with invert option."""
     _confirm(bool_same_pair(_expected_pair(False), invert=True))
