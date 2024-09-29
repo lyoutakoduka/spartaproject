1d3f730ba700ebfff03a6a4dbbdff6acf3129b54
lyouta <lyouta@spartaproject.com>
2024-09-29 20:46:23 +0900

    Feat: Test for path including April 1, 2023. #79

--- test/script/time/stamp/test_initial_date_time.py
+++ test/script/time/stamp/test_initial_date_time.py
@@ -2,9 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from decimal import Decimal
+from pathlib import Path
 
-from pyspartaproj.script.time.stamp.initial_date_time import get_initial_epoch
+from pyspartaproj.script.time.stamp.initial_date_time import (
+    get_initial_date_time,
+    get_initial_epoch,
+)
 
 
 def test_epoch() -> None:
     assert get_initial_epoch() == Decimal("1680307200")
+
+
+def test_utc() -> None:
+    assert get_initial_date_time() == Path(
+        "2023", "04", "01", "00", "00", "00", "000000"
+    )
