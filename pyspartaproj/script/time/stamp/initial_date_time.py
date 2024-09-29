ac3e35da619ac3454b6fd1d06b3c90962036b610
lyouta <lyouta@spartaproject.com>
2024-09-29 20:24:29 +0900

    Feat: Get path including time April 1, 2023. #79

--- pyspartaproj/script/time/stamp/initial_date_time.py
+++ pyspartaproj/script/time/stamp/initial_date_time.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from decimal import Decimal
+from pathlib import Path
 
 
 def get_initial_epoch() -> Decimal:
     return Decimal("1680307200")
+
+
+def get_initial_date_time(jst: bool = False) -> Path:
+    return Path(
+        "2023", "04", "01", str(9 if jst else 0).zfill(2), "00", "00", "000000"
+    )
