f42853f66c9204038e0bd774aeb1d9fe3f11fb5e
lyouta <lyouta@spartaproject.com>
2024-09-30 13:21:29 +0900

    Refactor: Use path about time. directory. #79

--- test/script/directory/test_date_time_space.py
+++ test/script/directory/test_date_time_space.py
@@ -11,22 +11,23 @@ from pyspartaproj.script.directory.date_time_space import (
     get_working_space,
 )
 from pyspartaproj.script.path.modify.get_relative import get_relative
+from pyspartaproj.script.time.stamp.initial_date_time import (
+    get_initial_date_time,
+)
 
 
 def test_name() -> None:
     """Test to get path including string of current date time."""
-    expected: Path = Path("2023", "04", "01", "00", "00", "00", "000000")
-
-    assert expected == get_working_space(override=True)
+    assert get_initial_date_time() == get_working_space(override=True)
 
 
 def test_create() -> None:
     """Test to create temporary working space including date time string."""
-    expected: Path = Path("2023", "04", "01", "00", "00", "00", "000000")
-
     with TemporaryDirectory() as temporary_directory:
         temporary_path: Path = Path(temporary_directory)
         time_path: Path = create_working_space(temporary_path, override=True)
 
         assert time_path.exists()
-        assert expected == get_relative(time_path, root_path=temporary_path)
+        assert get_initial_date_time() == get_relative(
+            time_path, root_path=temporary_path
+        )
