865c5700e5998fc97728d4cd2744a68cdd7946aa
lyouta <lyouta@spartaproject.com>
2024-10-03 20:39:42 +0900

    Feat: Split test function about time and path. #85

---
+++ test/script/time/path/test_get_time_path.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test module to get constant value including time information."""
+
+from pathlib import Path
+
+from pyspartaproj.script.time.path.get_time_path import get_initial_date_time
+
+
+def test_utc() -> None:
+    """Test to get path represent April 1, 2023 as UTC time zone."""
+    assert get_initial_date_time() == Path(
+        "2023", "04", "01", "00", "00", "00", "000000"
+    )
+
+
+def test_jst() -> None:
+    """Test to get path represent April 1, 2023 as JST time zone."""
+    assert get_initial_date_time(jst=True) == Path(
+        "2023", "04", "01", "09", "00", "00", "000000"
+    )
