aa8291c52efa828dbb617306363b5f94f3dcfcd1
lyouta <lyouta@spartaproject.com>
2024-10-03 20:37:45 +0900

    Fix: Remove duplicated time function. #85

--- pyspartaproj/script/time/stamp/initial_date_time.py
+++ pyspartaproj/script/time/stamp/initial_date_time.py
@@ -4,19 +4,8 @@
 """Module to get constant value including time information."""
 
 from decimal import Decimal
-from pathlib import Path
 
 
 def get_initial_epoch() -> Decimal:
     """Get UNIX epoch represent April 1, 2023."""
     return Decimal("1680307200")
-
-
-def get_initial_date_time(jst: bool = False) -> Path:
-    """Get path represent April 1, 2023.
-
-    You can select time zone from UTC or JST.
-    """
-    return Path(
-        "2023", "04", "01", str(9 if jst else 0).zfill(2), "00", "00", "000000"
-    )
