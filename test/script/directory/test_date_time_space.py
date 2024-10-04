876b0e4c95dc17e33dd356f01a67a6f9e69958b9
lyouta <lyouta@spartaproject.com>
2024-10-04 21:15:54 +0900

    Fix: Reconnect to updated module. directory. #85

--- test/script/directory/test_date_time_space.py
+++ test/script/directory/test_date_time_space.py
@@ -11,11 +11,11 @@ from pyspartaproj.script.directory.date_time_space import (
     get_working_space,
 )
 from pyspartaproj.script.path.modify.get_relative import get_relative
-from pyspartaproj.script.time.path.get_time_path import get_initial_date_time
+from pyspartaproj.script.time.path.get_time_path import get_initial_time_path
 
 
 def _compare_time_path(result: Path) -> None:
-    assert get_initial_date_time() == result
+    assert get_initial_time_path() == result
 
 
 def test_name() -> None:
