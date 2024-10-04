40ca89465802b4dcbc171f595635b6c7001017ca
lyouta <lyouta@spartaproject.com>
2024-10-04 11:50:22 +0900

    Fix: Reconnect to updated module. archive. #85

--- test/script/file/archive/test_edit_archive.py
+++ test/script/file/archive/test_edit_archive.py
@@ -31,13 +31,11 @@ from pyspartaproj.script.path.temporary.create_temporary_file import (
 from pyspartaproj.script.path.temporary.create_temporary_tree import (
     create_temporary_tree,
 )
+from pyspartaproj.script.time.path.get_time_path import get_initial_date_time
 from pyspartaproj.script.time.path.get_timestamp import (
     get_directory_latest,
     get_invalid_time,
     is_same_stamp,
-)
-from pyspartaproj.script.time.stamp.initial_date_time import (
-    get_initial_date_time,
 )
 
 
