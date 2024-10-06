69928d7549da2259779938e7e4224a46c9286e39
lyouta <lyouta@spartaproject.com>
2024-10-07 07:03:32 +0900

    Feat: Get year information for test. #87

---
+++ test/script/time/stamp/test_format_iso_date.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pyspartaproj.context.default.integer_context import IntPair
+
+
+def _get_source_year() -> IntPair:
+    return {"year": 2023, "month": 4, "day": 1}
