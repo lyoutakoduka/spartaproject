f1caae7fe2463e7335e7a590eff47eb950a6564b
lyouta <lyouta@spartaproject.com>
2024-10-07 07:03:59 +0900

    Feat: Get hour information for test. #87

--- test/script/time/stamp/test_format_iso_date.py
+++ test/script/time/stamp/test_format_iso_date.py
@@ -6,3 +6,7 @@ from pyspartaproj.context.default.integer_context import IntPair
 
 def _get_source_year() -> IntPair:
     return {"year": 2023, "month": 4, "day": 1}
+
+
+def _get_source_hour() -> IntPair:
+    return {"hour": 4, "minute": 51, "second": 30, "millisecond": 123}
