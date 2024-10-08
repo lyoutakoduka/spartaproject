9e0f19b3ef274d5828b25570b4aa6e7f96b3949b
lyouta <lyouta@spartaproject.com>
2024-10-09 08:17:57 +0900

    Feat: Groups about Year of ISO date format. #87

--- pyspartaproj/script/time/format/create_iso_date.py
+++ pyspartaproj/script/time/format/create_iso_date.py
@@ -6,3 +6,7 @@ from pyspartaproj.context.default.string_context import Strs
 
 def _get_groups() -> Strs:
     return ["year", "hour", "zone"]
+
+
+def _get_types_year() -> Strs:
+    return ["year", "month", "day"]
