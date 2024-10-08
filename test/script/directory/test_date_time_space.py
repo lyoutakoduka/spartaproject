e0d27c46b8d4ab99bf255bff88db555cf9deb2c7
lyouta <lyouta@spartaproject.com>
2024-10-08 18:52:58 +0900

    Update: Split test function to as new module. #87

--- test/script/directory/test_date_time_space.py
+++ test/script/directory/test_date_time_space.py
@@ -6,21 +6,13 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from pyspartaproj.script.directory.date_time_space import (
-    create_working_space,
-    get_working_space,
-)
+from pyspartaproj.script.directory.date_time_space import create_working_space
 from pyspartaproj.script.path.modify.get_relative import get_relative
 from pyspartaproj.script.time.path.get_time_path import get_initial_time_path
 
 
 def _compare_time_path(result: Path) -> None:
     assert get_initial_time_path() == result
-
-
-def test_name() -> None:
-    """Test to get path including string of current date time."""
-    _compare_time_path(get_working_space(override=True))
 
 
 def test_create() -> None:
---
+++ test/script/time/path/test_get_current_path.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Test module to create temporary working space including date time string."""
+
+from pathlib import Path
+
+from pyspartaproj.script.directory.date_time_space import get_working_space
+from pyspartaproj.script.time.path.get_time_path import get_initial_time_path
+
+
+def _compare_time_path(result: Path) -> None:
+    assert get_initial_time_path() == result
+
+
+def test_name() -> None:
+    """Test to get path including string of current date time."""
+    _compare_time_path(get_working_space(override=True))
