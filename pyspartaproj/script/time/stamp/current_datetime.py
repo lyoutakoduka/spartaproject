a176b50e0acb2d2e0e007b9730f82c4fbe8ba914
lyouta <lyouta@spartaproject.com>
2024-10-09 06:16:37 +0900

    Fix: Reconnect module importing. stamp. #87

--- pyspartaproj/script/time/stamp/current_datetime.py
+++ pyspartaproj/script/time/stamp/current_datetime.py
@@ -4,7 +4,7 @@
 from datetime import datetime
 
 from pyspartaproj.script.time.count.builtin_timer import TimerSelect
-from pyspartaproj.script.time.stamp.from_timestamp import time_from_timestamp
+from pyspartaproj.script.time.epoch.from_timestamp import time_from_timestamp
 
 
 def get_current_time(override: bool = False, jst: bool = False) -> datetime:
