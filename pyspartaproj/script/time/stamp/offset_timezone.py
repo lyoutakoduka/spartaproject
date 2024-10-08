280efd060b7f01b7aa17f12ea8b55d6a61fc6788
lyouta <lyouta@spartaproject.com>
2024-10-09 06:22:52 +0900

    Refactor: Remove unnecessary variables. #87

--- pyspartaproj/script/time/stamp/offset_timezone.py
+++ pyspartaproj/script/time/stamp/offset_timezone.py
@@ -5,10 +5,8 @@ from datetime import datetime, timedelta, timezone
 
 
 def offset_time(time: datetime) -> datetime:
-    offset: timedelta | None = time.utcoffset()
+    if offset := time.utcoffset():
+        time_offset: datetime = time - offset
+        return time_offset.replace(tzinfo=timezone.utc)
 
-    if offset is None:
-        return datetime.fromisoformat(time.isoformat() + "+00:00")
-
-    time_offset: datetime = time - offset
-    return time_offset.replace(tzinfo=timezone.utc)
+    return datetime.fromisoformat(time.isoformat() + "+00:00")
