7d49591ed9f9cacc1bc03973f19e09727c63b987
lyouta <lyouta@spartaproject.com>
2024-10-10 08:16:00 +0900

    Refactor: Remove unused functions. #87

--- pyspartaproj/script/time/format/format_iso_date.py
+++ pyspartaproj/script/time/format/format_iso_date.py
@@ -25,39 +25,8 @@ def _get_iso_digit() -> IntPair2:
     }
 
 
-def _find_digit(
-    number_group: str, number_type: str, iso_digit: IntPair2
-) -> int:
-    if number_group in iso_digit:
-        digit_group = iso_digit[number_group]
-
-        if number_type in digit_group:
-            return digit_group[number_type]
-
-    return 0
-
-
 def _format_digit(number: int, digit: int) -> str:
     return str(number).zfill(digit)
-
-
-def _format_digit_type(
-    number_group: str, number_type: str, number: int, iso_digit: IntPair2
-) -> str:
-    return _format_digit(
-        number, _find_digit(number_group, number_type, iso_digit)
-    )
-
-
-def _format_digit_group(
-    number_group: str, number_pair: IntPair, iso_digit: IntPair2
-) -> StrPair:
-    return {
-        number_type: _format_digit_type(
-            number_group, number_type, number, iso_digit
-        )
-        for number_type, number in number_pair.items()
-    }
 
 
 def _get_formatted_types(
