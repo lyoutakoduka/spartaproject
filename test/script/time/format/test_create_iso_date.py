a5115dab7ce1edef957272e38be2463242e929fc
lyouta <lyouta@spartaproject.com>
2024-10-12 06:32:02 +0900

    Feat: Get date time groups about Hour. #87

--- test/script/time/format/test_create_iso_date.py
+++ test/script/time/format/test_create_iso_date.py
@@ -6,3 +6,7 @@ from pyspartaproj.context.default.integer_context import IntPair
 
 def _get_years() -> IntPair:
     return {"year": 2023, "month": 4, "day": 1}
+
+
+def _get_hours() -> IntPair:
+    return {"hour": 4, "minute": 51, "second": 30, "millisecond": 123}
