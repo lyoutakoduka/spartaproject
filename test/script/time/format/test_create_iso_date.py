0a08a660303faf8fc6cce7dedf9ff0f3ee677a1b
lyouta <lyouta@spartaproject.com>
2024-10-12 06:31:45 +0900

    Feat: Get date time groups about Year. #87

---
+++ test/script/time/format/test_create_iso_date.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pyspartaproj.context.default.integer_context import IntPair
+
+
+def _get_years() -> IntPair:
+    return {"year": 2023, "month": 4, "day": 1}
