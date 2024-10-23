4131b23ab874af2dbe5d9c5e3e30416f526139e2
lyouta <lyouta@spartaproject.com>
2024-10-23 18:48:28 +0900

    Feat: Offset time by current time zone. #87

--- pyspartaproj/script/time/stamp/offset_timezone.py
+++ pyspartaproj/script/time/stamp/offset_timezone.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
+
+
+def _offset(time: datetime, offset: timedelta) -> datetime:
+    time_offset: datetime = time - offset
+    return time_offset.replace(tzinfo=timezone.utc)
 
 
 def offset_time(time: datetime) -> datetime:
     if offset := time.utcoffset():
-        time_offset: datetime = time - offset
-        return time_offset.replace(tzinfo=timezone.utc)
+        return _offset(time, offset)
 
     return datetime.fromisoformat(time.isoformat() + "+00:00")
