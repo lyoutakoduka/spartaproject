0284a833928b8bd92e25dd10876be6df3c15ed11
lyouta <lyouta@spartaproject.com>
2024-10-24 22:43:01 +0900

    Feat: Get string about Hour. #87

--- pyspartaproj/script/time/directory/get_time_path.py
+++ pyspartaproj/script/time/directory/get_time_path.py
@@ -6,11 +6,13 @@
 from pathlib import Path
 
 
+def _get_hour(jst: bool) -> str:
+    return str(9 if jst else 0).zfill(2)
+
+
 def get_initial_time_path(jst: bool = False) -> Path:
     """Get path represent April 1, 2023.
 
     You can select time zone from UTC or JST.
     """
-    return Path(
-        "2023", "04", "01", str(9 if jst else 0).zfill(2), "00", "00", "000000"
-    )
+    return Path("2023", "04", "01", _get_hour(jst), "00", "00", "000000")
