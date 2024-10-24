e514bd914680cdd544b9366eaea3f942d092340d
lyouta <lyouta@spartaproject.com>
2024-10-24 23:33:29 +0900

    Doc: Add module document string. #87

--- pyspartaproj/script/time/stamp/current_datetime.py
+++ pyspartaproj/script/time/stamp/current_datetime.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to get information about current date time."""
+
 from datetime import datetime
 
 from pyspartaproj.script.time.count.builtin_timer import TimerSelect
@@ -8,4 +10,18 @@ from pyspartaproj.script.time.epoch.from_timestamp import time_from_timestamp
 
 
 def get_current_time(override: bool = False, jst: bool = False) -> datetime:
+    """Get information about current date time.
+
+    Args:
+        override (bool, optional): Defaults to False.
+            Override initial time count to "2023/4/1:12:00:00-00 (AM)".
+            It's used for argument "override" of class "TimerSelect".
+
+        jst (bool, optional): Defaults to False.
+            If True, you can get datetime object as JST time zone.
+            It's used for argument "jst" of function "time_from_timestamp".
+
+    Returns:
+        datetime: _description_
+    """
     return time_from_timestamp(TimerSelect(override=override)(), jst=jst)
