087a6ed89ed3372478bec7a23939d509b7b06466
lyouta <lyouta@spartaproject.com>
2024-10-24 22:45:39 +0900

    Update: Use data time element, not string. #87

--- test/script/time/directory/test_get_time_path.py
+++ test/script/time/directory/test_get_time_path.py
@@ -20,13 +20,9 @@ def _get_time_path(jst: bool = True) -> Path:
 
 def test_utc() -> None:
     """Test to get path represent April 1, 2023 as UTC time zone."""
-    assert get_initial_time_path() == Path(
-        "2023", "04", "01", "00", "00", "00", "000000"
-    )
+    assert get_initial_time_path() == _get_time_path()
 
 
 def test_jst() -> None:
     """Test to get path represent April 1, 2023 as JST time zone."""
-    assert get_initial_time_path(jst=True) == Path(
-        "2023", "04", "01", "09", "00", "00", "000000"
-    )
+    assert get_initial_time_path(jst=True) == _get_time_path(jst=True)
