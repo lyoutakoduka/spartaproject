c59404c3bef05ddcb66e80b9814a7761e3d42287
lyouta <lyouta@spartaproject.com>
2024-10-24 21:51:29 +0900

    Feat: Get date time element about Hour. #87

--- test/script/time/stamp/test_offset_timezone.py
+++ test/script/time/stamp/test_offset_timezone.py
@@ -10,6 +10,10 @@ def _get_year() -> IntPair:
     return {"year": 2023, "month": 4, "day": 15}
 
 
+def _get_hour() -> IntPair:
+    return {"hour": 20, "minute": 9, "second": 30, "micro": 936886}
+
+
 def _common_text(source_time: str) -> None:
     expected_utc: str = "2023-04-15T20:09:30.936886+00:00"
     expected: datetime = datetime.fromisoformat(expected_utc)
