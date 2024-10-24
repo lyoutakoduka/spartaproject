f9b93744c36ad9a371194dbbbd61b91655bd0677
lyouta <lyouta@spartaproject.com>
2024-10-24 21:11:13 +0900

    Refactor: Remove unused functions. #87

--- test/script/time/path/test_set_timestamp.py
+++ test/script/time/path/test_set_timestamp.py
@@ -84,35 +84,13 @@ def _select_zone(jst: bool = False) -> IntPair3:
     return _time_jst() if jst else _time_utc()
 
 
-def _get_time_text(jst: bool = False) -> Strs:
-    times: Strs = [
-        "2023-04-01T00:00:00.000001+00:00",
-        "2023-04-15T20:09:30.936886+00:00",
-    ]
-    times_jst: Strs = [
-        "2023-04-01T09:00:00.000001+09:00",
-        "2023-04-16T05:09:30.936886+09:00",
-    ]
-
-    return times_jst if jst else times
-
-
 def _get_input_time(times: IntPair3) -> TimePair:
     return {group: get_iso_time(time) for group, time in times.items()}
-
-
-def _convert_input_time(times: Strs) -> Times:
-    return [datetime.fromisoformat(time) for time in times]
 
 
 def _set_latest_pair(path: Path, times: IntPair3) -> None:
     for group, time in _get_input_time(times).items():
         set_latest(path, time, access=_is_access(group))
-
-
-def _set_latest_datetime(path: Path, time_texts: Strs) -> None:
-    for status, time in zip([False, True], _convert_input_time(time_texts)):
-        set_latest(path, time, access=status)
 
 
 def _get_latest_pair(path: Path) -> TimePair:
@@ -122,29 +100,12 @@ def _get_latest_pair(path: Path) -> TimePair:
     }
 
 
-def _get_latest_datetime(path: Path) -> Times:
-    return [get_latest(path, access=status) for status in [False, True]]
-
-
-def _get_expected_datetime() -> Times:
-    return _convert_input_time(_get_time_text())
-
-
 def _compare_datetime(path: Path, times: IntPair3) -> None:
     _set_latest_pair(path, times)
 
     for group, expected in _get_input_time(times).items():
         results: TimePair = _get_latest_pair(path)
         assert expected == results[group]
-
-
-def _common_test(path: Path, time_texts: Strs) -> None:
-    _set_latest_datetime(path, time_texts)
-
-    for expected, result in zip(
-        _get_expected_datetime(), _get_latest_datetime(path)
-    ):
-        assert result == expected
 
 
 def _inside_temporary_directory(function: Callable[[Path], None]) -> None:
