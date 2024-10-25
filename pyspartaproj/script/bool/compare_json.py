717b3e38d35b5c9836c3de0b0c39e8d540c8047e
lyouta <lyouta@spartaproject.com>
2024-10-26 08:32:50 +0900

    Feat: Convert string list from Json data. #89

--- pyspartaproj/script/bool/compare_json.py
+++ pyspartaproj/script/bool/compare_json.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pyspartaproj.context.default.string_context import Strs
 from pyspartaproj.context.file.json_context import Json
 from pyspartaproj.script.file.json.export_json import json_dump
 
 
+def _convert_string(left: Json, right: Json) -> Strs:
+    return [json_dump(source, compress=True) for source in [left, right]]
+
+
 def is_same_json(left: Json, right: Json) -> bool:
-    return 1 == len(
-        list(
-            set([json_dump(source, compress=True) for source in [left, right]])
-        )
-    )
+    return 1 == len(list(set(_convert_string(left, right))))
