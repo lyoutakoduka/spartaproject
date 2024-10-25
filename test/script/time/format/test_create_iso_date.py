2605e2ce4df99e17155baf9a27e5f05b2b82df29
lyouta <lyouta@spartaproject.com>
2024-10-25 16:10:05 +0900

    Doc: Update module document string. #87

--- test/script/time/format/test_create_iso_date.py
+++ test/script/time/format/test_create_iso_date.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Test module to convert date time elements to several types."""
+"""Test module to convert date time element to several types."""
 
 from datetime import datetime
 from decimal import Decimal
