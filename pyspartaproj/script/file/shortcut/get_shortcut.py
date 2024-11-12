a6fadc6a6240ad0984f3290a35ceb74d33db5f1e
lyouta <lyouta@spartaproject.com>
2024-11-12 13:27:03 +0900

    Update: Support both file and directory. #99

--- pyspartaproj/script/file/shortcut/get_shortcut.py
+++ pyspartaproj/script/file/shortcut/get_shortcut.py
@@ -18,4 +18,5 @@ def get_shortcut(target_path: Path, shortcut_root: Path) -> Path:
     Returns:
         Path: Path of the shortcut file that is created.
     """
-    return Path(shortcut_root, target_path.name).with_suffix(".lnk")
+    path: Path = Path(shortcut_root, target_path.name)
+    return path.with_suffix(target_path.suffix + ".lnk")
