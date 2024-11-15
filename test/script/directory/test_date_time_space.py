c4ed308689c7da13f5a7d93bef3b62700209e6ae
lyouta <lyouta@spartaproject.com>
2024-11-16 08:01:31 +0900

    Fix: Reconnect module importing. directory. #101

--- test/script/directory/test_date_time_space.py
+++ test/script/directory/test_date_time_space.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from pyspartaproj.script.directory.date_time_space import create_working_space
-from pyspartaproj.script.path.modify.get_relative import get_relative
+from pyspartaproj.script.path.modify.current.get_relative import get_relative
 from pyspartaproj.script.time.directory.get_time_path import (
     get_initial_time_path,
 )
