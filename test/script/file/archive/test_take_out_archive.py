8536c2863cf8e9c66aa2a5db13b5bb366fb896d2
lyouta <lyouta@spartaproject.com>
2024-11-16 08:00:32 +0900

    Fix: Reconnect module importing. archive. #101

--- test/script/file/archive/test_take_out_archive.py
+++ test/script/file/archive/test_take_out_archive.py
@@ -16,11 +16,11 @@ from pyspartaproj.script.file.archive.compress_archive import CompressArchive
 from pyspartaproj.script.file.archive.edit_archive import EditArchive
 from pyspartaproj.script.file.archive.take_out_archive import TakeOutArchive
 from pyspartaproj.script.path.iterate_directory import walk_iterator
-from pyspartaproj.script.path.modify.get_absolute import get_absolute_array
-from pyspartaproj.script.path.modify.get_relative import (
+from pyspartaproj.script.path.modify.current.get_relative import (
     get_relative_array,
     is_relative_array,
 )
+from pyspartaproj.script.path.modify.get_absolute import get_absolute_array
 from pyspartaproj.script.path.temporary.create_temporary_file import (
     create_temporary_file,
 )
