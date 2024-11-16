0bf641458f34450e91eb0ddb2902d8e0594abc98
lyouta <lyouta@spartaproject.com>
2024-11-17 04:51:35 +0900

    Feat: Confirm that path is mounted in Linux. #105

--- pyspartaproj/script/path/modify/mount/has_linux_head.py
+++ pyspartaproj/script/path/modify/mount/has_linux_head.py
@@ -3,6 +3,12 @@
 
 from pathlib import Path
 
+from pyspartaproj.script.path.modify.current.get_relative import is_relative
+
 
 def _get_mount_root() -> Path:
     return Path("/", "mnt")
+
+
+def has_linux_head(path: Path) -> bool:
+    return is_relative(path, root_path=_get_mount_root())
