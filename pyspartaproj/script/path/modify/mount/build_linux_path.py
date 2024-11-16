992dbf63b4a121bbdf031baf42a2dc28c3f99bd0
lyouta <lyouta@spartaproject.com>
2024-11-17 06:14:06 +0900

    Feat: Specific drive that mounted on Linux. #105

--- pyspartaproj/script/path/modify/mount/build_linux_path.py
+++ pyspartaproj/script/path/modify/mount/build_linux_path.py
@@ -6,3 +6,7 @@ from pathlib import Path
 
 def _get_mount_root() -> Path:
     return Path("/", "mnt")
+
+
+def _convert_linux(identifier: str) -> Path:
+    return Path(_get_mount_root(), identifier.lower())
