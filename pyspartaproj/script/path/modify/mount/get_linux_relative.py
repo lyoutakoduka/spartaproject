75ab4744b1b916ea98afc46f8aba67a6e37ae425
lyouta <lyouta@spartaproject.com>
2024-11-17 06:33:08 +0900

    Feat: Get relative path seen from Linux root. #105

--- pyspartaproj/script/path/modify/mount/get_linux_relative.py
+++ pyspartaproj/script/path/modify/mount/get_linux_relative.py
@@ -3,6 +3,12 @@
 
 from pathlib import Path
 
+from pyspartaproj.script.path.modify.current.get_relative import get_relative
+
 
 def _get_mount_root() -> Path:
     return Path("/", "mnt")
+
+
+def get_linux_relative(path: Path) -> Path:
+    return get_relative(path, root_path=_get_mount_root())
