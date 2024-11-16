47070cafd301ece30faef5c1ef33f85fbfaaf329
lyouta <lyouta@spartaproject.com>
2024-11-16 09:14:41 +0900

    Feat: Move function to new module group. #103

---
+++ pyspartaproj/script/path/modify/mount/convert_mount.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+
+def convert_mount_path(path: Path) -> Path:
+    """Convert shared path between Linux and Windows.
+
+    e.g., if you select argument (path) like "/mnt/c/Users/user",
+        "C:/Users/user" is returned.
+
+    Args:
+        path (Path): Linux path which is starts from mount string.
+
+    Returns:
+        Path: Converted Windows path which is starts from drive letter.
+    """
+    path_text: str = path.as_posix()
+    mount: str = "/mnt/"
+
+    if not path_text.startswith(mount):
+        return path
+
+    index: int = len(mount)
+    index_right: int = index + 1
+
+    return Path(path_text[index].capitalize() + ":" + path_text[index_right:])
--- pyspartaproj/script/shell/execute_powershell.py
+++ pyspartaproj/script/shell/execute_powershell.py
@@ -134,27 +134,3 @@ def get_double_quoted_command(commands: Strs) -> str:
         str: Converted command part surrounded by double quotation.
     """
     return " ".join(commands).join(['"'] * 2)
-
-
-def convert_mount_path(path: Path) -> Path:
-    """Convert shared path between Linux and Windows.
-
-    e.g., if you select argument (path) like "/mnt/c/Users/user",
-        "C:/Users/user" is returned.
-
-    Args:
-        path (Path): Linux path which is starts from mount string.
-
-    Returns:
-        Path: Converted Windows path which is starts from drive letter.
-    """
-    path_text: str = path.as_posix()
-    mount: str = "/mnt/"
-
-    if not path_text.startswith(mount):
-        return path
-
-    index: int = len(mount)
-    index_right: int = index + 1
-
-    return Path(path_text[index].capitalize() + ":" + path_text[index_right:])
