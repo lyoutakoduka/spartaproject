44fb90c1651de68517119b00479d92dc998b316a
lyouta <lyouta@spartaproject.com>
2024-11-17 05:12:40 +0900

    Feat: Build path on Windows from elements. #105

--- pyspartaproj/script/path/modify/mount/build_windows_path.py
+++ pyspartaproj/script/path/modify/mount/build_windows_path.py
@@ -6,3 +6,7 @@ from pathlib import Path
 
 def _convert_windows(identifier: str) -> Path:
     return Path(identifier.capitalize() + ":")
+
+
+def build_windows_path(identifier: str, relative_root: Path) -> Path:
+    return Path(_convert_windows(identifier), relative_root)
