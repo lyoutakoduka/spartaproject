cd8fc7f7b37fdc3e03af418aa76387203b467f08
lyouta <lyouta@spartaproject.com>
2024-11-17 08:09:43 +0900

    Feat: Get relative path used for test. #105

--- test/script/path/modify/mount/shared/test_has_linux_head.py
+++ test/script/path/modify/mount/shared/test_has_linux_head.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+
 
 def _get_drive_letter() -> str:
     return "c"
+
+
+def _get_relative_root() -> Path:
+    return Path("root", "body", "head")
