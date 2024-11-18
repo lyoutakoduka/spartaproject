dd11ad71354de883da3833579f994c6bc9b5e8cc
lyouta <lyouta@spartaproject.com>
2024-11-18 14:23:18 +0900

    Feat: Build path on Linux from elements. #105

--- test/script/path/modify/mount/shared/test_get_linux_relative.py
+++ test/script/path/modify/mount/shared/test_get_linux_relative.py
@@ -3,6 +3,10 @@
 
 from pathlib import Path
 
+from pyspartaproj.script.path.modify.mount.build_linux_path import (
+    build_linux_path,
+)
+
 
 def _get_drive_letter() -> str:
     return "c"
@@ -14,3 +18,7 @@ def _get_relative_root() -> Path:
 
 def _get_expected_path() -> Path:
     return Path(_get_drive_letter(), _get_relative_root())
+
+
+def _get_linux_path() -> Path:
+    return build_linux_path(_get_drive_letter(), _get_relative_root())
