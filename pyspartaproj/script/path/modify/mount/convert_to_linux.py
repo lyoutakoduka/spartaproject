85ebc8405e8a9c79a3820c7b3a4818c76e7b381f
lyouta <lyouta@spartaproject.com>
2024-11-19 09:16:55 +0900

    Feat: Convert path from Windows to Linux. #105

--- pyspartaproj/script/path/modify/mount/convert_to_linux.py
+++ pyspartaproj/script/path/modify/mount/convert_to_linux.py
@@ -4,6 +4,12 @@
 from pathlib import Path
 
 from pyspartaproj.context.default.string_context import Strs
+from pyspartaproj.script.path.modify.mount.build_linux_path import (
+    build_linux_path,
+)
+from pyspartaproj.script.path.modify.mount.shared.has_linux_head import (
+    has_linux_head,
+)
 
 
 def _get_relative_strings(path: Path) -> Strs:
@@ -16,3 +22,10 @@ def _get_drive_letter(path: Path) -> str:
 
 def _get_relative_root(path: Path) -> Path:
     return Path(*_get_relative_strings(path)[1:])
+
+
+def convert_to_linux(path: Path) -> Path:
+    if has_linux_head(path):
+        return path
+
+    return build_linux_path(_get_drive_letter(path), _get_relative_root(path))
