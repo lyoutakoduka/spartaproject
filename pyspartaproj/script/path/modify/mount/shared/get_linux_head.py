7a6fa46cb9242c72bf125232420165dbf835fbcc
lyouta <lyouta@spartaproject.com>
2024-11-20 18:41:31 +0900

    Doc: Add module document string. #105

--- pyspartaproj/script/path/modify/mount/shared/get_linux_head.py
+++ pyspartaproj/script/path/modify/mount/shared/get_linux_head.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to get a path that drive of Windows will be mounted."""
+
 from pathlib import Path
 
 
 def get_linux_head() -> Path:
+    """Get a path that drive of Windows will be mounted.
+
+    Returns:
+        Path: Path that drive will be mounted.
+    """
     return Path("/", "mnt")
--- test/script/path/modify/mount/shared/test_get_linux_head.py
+++ test/script/path/modify/mount/shared/test_get_linux_head.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test Module to get a path that drive of Windows will be mounted."""
+
 from pyspartaproj.script.path.modify.mount.shared.get_linux_head import (
     get_linux_head,
 )
 
 
 def test_mount() -> None:
+    """Test to get a path that drive of Windows will be mounted."""
     assert "/mnt" == str(get_linux_head())
