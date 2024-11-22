9634660beb907f411e5ba2349fb198e6b74d7c43
lyouta <lyouta@spartaproject.com>
2024-11-23 03:21:33 +0900

    Feat: Get path that is place of drive letter. #105

--- test/script/path/modify/mount/test_build_linux_path.py
+++ test/script/path/modify/mount/test_build_linux_path.py
@@ -3,6 +3,10 @@
 
 from pathlib import Path
 
+from pyspartaproj.script.path.modify.mount.build_linux_path import (
+    get_linux_head,
+)
+
 
 def _get_drive_letter() -> str:
     return "c"
@@ -10,3 +14,7 @@ def _get_drive_letter() -> str:
 
 def _get_relative_root() -> Path:
     return Path("root", "body", "head")
+
+
+def _get_linux_head() -> Path:
+    return get_linux_head(_get_drive_letter())
