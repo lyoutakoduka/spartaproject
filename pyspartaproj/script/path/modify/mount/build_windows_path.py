c49b6733a82cd125bf31e3e1f6c30f9cec139fb8
lyouta <lyouta@spartaproject.com>
2024-11-22 11:17:55 +0900

    Update: Rename function to suitable. #105

--- pyspartaproj/script/path/modify/mount/build_windows_path.py
+++ pyspartaproj/script/path/modify/mount/build_windows_path.py
@@ -8,5 +8,5 @@ def get_windows_head(identifier: str) -> Path:
     return Path(identifier.capitalize() + ":")
 
 
-def build_windows_path(identifier: str, relative_root: Path) -> Path:
+def get_windows_path(identifier: str, relative_root: Path) -> Path:
     return Path(get_windows_head(identifier), relative_root)
