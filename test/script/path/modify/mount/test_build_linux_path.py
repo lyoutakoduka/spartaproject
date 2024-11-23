728b6c000cec54efe97d1f1bb1873b82227a5eed
lyouta <lyouta@spartaproject.com>
2024-11-23 14:09:46 +0900

    Feat: Build path on Linux from elements. #105

--- test/script/path/modify/mount/test_build_linux_path.py
+++ test/script/path/modify/mount/test_build_linux_path.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from pyspartaproj.script.path.modify.mount.build_linux_path import (
     get_linux_head,
+    get_linux_path,
 )
 
 
@@ -18,3 +19,7 @@ def _get_relative_root() -> Path:
 
 def _get_linux_head() -> Path:
     return get_linux_head(_get_drive_letter())
+
+
+def _get_linux_path() -> Path:
+    return get_linux_path(_get_drive_letter(), _get_relative_root())
