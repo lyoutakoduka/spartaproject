a1589978c4fb7d73c370d3906fe3441c10e6cbcb
lyouta <lyouta@spartaproject.com>
2024-11-23 15:06:06 +0900

    Feat: Build path about Windows from elements. #105

--- test/script/path/modify/mount/test_build_windows_path.py
+++ test/script/path/modify/mount/test_build_windows_path.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from pyspartaproj.script.path.modify.mount.build_windows_path import (
     get_windows_head,
+    get_windows_path,
 )
 
 
@@ -18,3 +19,7 @@ def _get_relative_root() -> Path:
 
 def _get_windows_head() -> Path:
     return get_windows_head(_get_drive_letter())
+
+
+def _get_windows_path() -> Path:
+    return get_windows_path(_get_drive_letter(), _get_relative_root())
