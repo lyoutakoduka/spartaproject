a2f383f3b3ca4bc1d71054f708538c0b16e8f234
lyouta <lyouta@spartaproject.com>
2024-11-23 20:11:14 +0900

    Doc: Add module document string. #105

--- test/script/path/modify/mount/test_build_windows_path.py
+++ test/script/path/modify/mount/test_build_windows_path.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Test module to get path about Windows file system."""
+
 from pathlib import Path
 
 from pyspartaproj.script.path.modify.mount.build_windows_path import (
@@ -38,8 +40,10 @@ def _compare_path(expected: Path, result: Path) -> None:
 
 
 def test_head() -> None:
+    """Test to get root path of drive about Windows file system."""
     _compare_path(_get_expected_head(), _get_windows_head())
 
 
 def test_path() -> None:
+    """Test to generate path about Windows file system from elements."""
     _compare_path(_get_expected_path(), _get_windows_path())
