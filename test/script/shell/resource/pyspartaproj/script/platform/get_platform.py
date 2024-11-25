4736234d9800e306e93491e6947c906280b7c01a
lyouta <lyouta@spartaproject.com>
2024-11-25 23:04:50 +0900

    Doc: Add module document string. #105

--- test/script/shell/resource/pyspartaproj/script/platform/get_platform.py
+++ test/script/shell/resource/pyspartaproj/script/platform/get_platform.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Module to get current executed platform."""
+
 from platform import uname
 
 
 def get_platform() -> str:
+    """Get current executed platform.
+
+    Returns:
+        str: Executed platform.
+    """
     return uname().system.lower()
