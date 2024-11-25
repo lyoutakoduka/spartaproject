84f41d072a1504251c221568c2a3d3be73b4c324
lyouta <lyouta@spartaproject.com>
2024-11-25 21:26:54 +0900

    Feat: Test to check that platform is Linux. #107

--- test/script/platform/test_get_platform.py
+++ test/script/platform/test_get_platform.py
@@ -3,7 +3,10 @@
 
 from platform import uname
 
-from pyspartaproj.script.platform.get_platform import get_platform
+from pyspartaproj.script.platform.get_platform import (
+    get_platform,
+    is_platform_linux,
+)
 
 
 def _get_platform() -> str:
@@ -19,3 +22,8 @@ def _valid_platform(platform: str) -> str | None:
 
 def test_name() -> None:
     assert _valid_platform(_get_platform()) == get_platform()
+
+
+def test_linux() -> None:
+    if "Linux" == _get_platform():
+        assert is_platform_linux()
