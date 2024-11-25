3e714bfc878ee3826e7992de8acefa46b6d6143f
lyouta <lyouta@spartaproject.com>
2024-11-25 19:31:52 +0900

    Feat: Check that executed platform is enable. #107

--- test/script/platform/test_get_platform.py
+++ test/script/platform/test_get_platform.py
@@ -6,3 +6,10 @@ from platform import uname
 
 def _get_platform() -> str:
     return uname().system.lower()
+
+
+def _valid_platform(platform: str) -> str | None:
+    if platform in ["linux", "windows"]:
+        return platform
+
+    return None
