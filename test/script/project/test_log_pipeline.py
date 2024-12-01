860a3f9728d4b116ed2bdea471b57b18e8ea7aaa
lyouta <lyouta@spartaproject.com>
2024-12-02 00:38:41 +0900

    Feat: Get log message as string, not stdout. #111

--- test/script/project/test_log_pipeline.py
+++ test/script/project/test_log_pipeline.py
@@ -3,6 +3,10 @@
 
 from decimal import Decimal
 
+from pyspartaproj.context.default.string_context import Strs
+from pyspartaproj.script.project.log_pipeline import LogPipeline
+from pyspartaproj.script.string.off_stdout import StdoutText
+
 
 def _get_interval() -> Decimal:
     return Decimal("0.3")
@@ -18,3 +22,15 @@ def _get_timer_log(interval: str, messages: str) -> str:
 
 def _get_expected() -> str:
     return _get_timer_log(str(_get_interval()), _get_message())
+
+
+def _convert_log(messages: Strs, pipeline: LogPipeline) -> str:
+    stdout_text = StdoutText()
+
+    @stdout_text.decorator
+    def _messages() -> None:
+        pipeline.show_log(messages, force=True)
+
+    _messages()
+
+    return stdout_text.show()
