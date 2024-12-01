f71ba423c1deb4f961051644786571233f14739d
lyouta <lyouta@spartaproject.com>
2024-12-01 22:19:57 +0900

    Refactor: Use module instead of methods. #111

--- pyspartaproj/script/project/base_pipeline.py
+++ pyspartaproj/script/project/base_pipeline.py
@@ -6,18 +6,19 @@
 from pathlib import Path
 
 from pyspartaproj.context.default.string_context import Strs
+from pyspartaproj.script.project.log_pipeline import LogPipeline
 from pyspartaproj.script.project.project_context import ProjectContext
 from pyspartaproj.script.time.count.log_timer import LogTimer
 
 
-class BasePipeline(ProjectContext, LogTimer):
+class BasePipeline(ProjectContext, LogPipeline):
     """Class to handling I/O functionality of any script called pipeline."""
 
     def __initialize_super_class(
         self, platform: str | None, forward: Path | None
     ) -> None:
         ProjectContext.__init__(self, platform=platform, forward=forward)
-        LogTimer.__init__(self)
+        LogPipeline.__init__(self)
 
     def _show_message(self, message: str) -> None:
         print(message)
