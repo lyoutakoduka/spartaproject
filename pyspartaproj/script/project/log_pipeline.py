c3aae618c62fd407899d738d22fd383fea46f7fc
lyouta <lyouta@spartaproject.com>
2024-12-01 22:01:15 +0900

    Update: Rename method to more private style. #111

--- pyspartaproj/script/project/log_pipeline.py
+++ pyspartaproj/script/project/log_pipeline.py
@@ -5,8 +5,8 @@ from pyspartaproj.script.time.count.log_timer import LogTimer
 
 
 class LogPipeline(LogTimer):
-    def _initialize_super_class(self) -> None:
+    def __initialize_super_class(self) -> None:
         super().__init__()
 
     def __init__(self) -> None:
-        self._initialize_super_class()
+        self.__initialize_super_class()
