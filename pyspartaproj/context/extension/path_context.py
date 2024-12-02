fc58cbd6eaff9c2928bd7e45dc41bff4a99a5a0b
lyouta <lyouta@spartaproject.com>
2024-12-02 23:24:37 +0900

    Feat: Add user defined types about Path. #113

--- pyspartaproj/context/extension/path_context.py
+++ pyspartaproj/context/extension/path_context.py
@@ -4,12 +4,15 @@
 """User defined types about type "Path"."""
 
 from pathlib import Path
-from typing import Dict, Generator, List
+from typing import Callable, Dict, Generator, List
 
 PathGene = Generator[Path, None, None]
 PathPair = Dict[str, Path]
 Paths = List[Path]
+PathFunc = Callable[[Path], None]
+PathBoolFunc = Callable[[Path], bool]
 
+PathGeneFunc = Callable[[], PathGene]
 PathPair2 = Dict[str, PathPair]
 Paths2 = List[Paths]
 PathsPair = Dict[str, Paths]
