823f9b72712509257ade8d6df6a784ad472b9fde
lyouta <lyouta@spartaproject.com>
2024-12-09 05:26:52 +0900

    Doc: Add module document string. #117

--- pyspartaproj/context/custom/log_context.py
+++ pyspartaproj/context/custom/log_context.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""User defined types using class "LogPipeline"."""
+
 from typing import Callable
 
 from pyspartaproj.script.project.log_pipeline import LogPipeline
