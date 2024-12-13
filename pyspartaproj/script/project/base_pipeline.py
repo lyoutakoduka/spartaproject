e5d95081c85eeee9cab6c1b15e2df5845d8dff13
lyouta <lyouta@spartaproject.com>
2024-12-13 18:03:49 +0900

    Update: Add variable to show log message. #131

--- pyspartaproj/script/project/base_pipeline.py
+++ pyspartaproj/script/project/base_pipeline.py
@@ -13,13 +13,16 @@ class BasePipeline(ProjectContext, LogPipeline):
     """Class to handle all functionalities of any script called pipeline."""
 
     def __initialize_super_class(
-        self, platform: str | None, forward: Path | None
+        self, platform: str | None, forward: Path | None, enable_shown: bool
     ) -> None:
         ProjectContext.__init__(self, platform=platform, forward=forward)
-        LogPipeline.__init__(self)
+        LogPipeline.__init__(self, enable_shown=enable_shown)
 
     def __init__(
-        self, platform: str | None = None, forward: Path | None = None
+        self,
+        platform: str | None = None,
+        forward: Path | None = None,
+        enable_shown: bool = False,
     ) -> None:
         """Initialize super class and variables.
 
@@ -34,4 +37,4 @@ class BasePipeline(ProjectContext, LogPipeline):
                     project context file to any place.
                 It's used for argument "forward" of class "ProjectContext".
         """
-        self.__initialize_super_class(platform, forward)
+        self.__initialize_super_class(platform, forward, enable_shown)
