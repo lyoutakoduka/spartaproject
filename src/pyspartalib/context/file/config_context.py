d8e96968e6f20ec3d3a5390e5c8f626aca34473f
lyouta <lyouta@spartaproject.com>
2024-12-14 21:31:23 +0900

    Refactor: User defined types from outside. #133

--- src/pyspartalib/context/file/config_context.py
+++ src/pyspartalib/context/file/config_context.py
@@ -7,36 +7,28 @@ from decimal import Decimal
 from pathlib import Path
 from typing import Dict, Union
 
+from pyspartalib.context.default.bool_context import BoolPair, BoolPair2
+from pyspartalib.context.default.float_context import FloatPair, FloatPair2
+from pyspartalib.context.default.integer_context import IntPair, IntPair2
+from pyspartalib.context.default.string_context import StrPair, StrPair2
+from pyspartalib.context.extension.decimal_context import DecPair, DecPair2
+from pyspartalib.context.extension.path_context import PathPair, PathPair2
+
 Basic = bool | int | float | str | Decimal | Path
 
-_BoolPair = Dict[str, bool]
-_IntPair = Dict[str, int]
-_FloatPair = Dict[str, float]
-_DecPair = Dict[str, Decimal]
-_StrPair = Dict[str, str]
-_PathPair = Dict[str, Path]
 BasicPair = Dict[str, Basic]
+SectionPair = Union[BoolPair, IntPair, FloatPair, DecPair, StrPair, PathPair]
 
-SectionPair = Union[
-    _BoolPair, _IntPair, _FloatPair, _DecPair, _StrPair, _PathPair
-]
-
-_BoolPair2 = Dict[str, _BoolPair]
-_IntPair2 = Dict[str, _IntPair]
-_FloatPair2 = Dict[str, _FloatPair]
-_DecPair2 = Dict[str, _DecPair]
-_StrPair2 = Dict[str, _StrPair]
-_PathPair2 = Dict[str, _PathPair]
 BasicPair2 = Dict[str, BasicPair]
 SectionPair2 = Dict[str, SectionPair]
 
 Config = Union[
-    _BoolPair2,
-    _IntPair2,
-    _FloatPair2,
-    _DecPair2,
-    _StrPair2,
-    _PathPair2,
+    BoolPair2,
+    IntPair2,
+    FloatPair2,
+    DecPair2,
+    StrPair2,
+    PathPair2,
     BasicPair2,
     SectionPair2,
 ]
