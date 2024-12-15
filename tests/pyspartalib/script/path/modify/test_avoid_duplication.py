5430ad0f7293bed01d9956c9d340e2eacfea6026
lyouta <lyouta@spartaproject.com>
2024-12-15 16:12:24 +0900

    Update: Move and rename directory. modify. #133

--- tests/pyspartalib/script/path/modify/test_avoid_duplication.py
+++ tests/pyspartalib/script/path/modify/test_avoid_duplication.py
@@ -6,9 +6,9 @@
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from pyspartaproj.context.extension.path_context import PathFunc
-from pyspartaproj.script.file.json.export_json import json_export
-from pyspartaproj.script.path.modify.avoid_duplication import get_avoid_path
+from pyspartalib.context.extension.path_context import PathFunc
+from pyspartalib.script.file.json.export_json import json_export
+from pyspartalib.script.path.modify.avoid_duplication import get_avoid_path
 
 
 def _common_test(source_path: Path, destination_path: Path) -> None:
--- tests/pyspartalib/script/path/modify/test_get_resource.py
+++ tests/pyspartalib/script/path/modify/test_get_resource.py
@@ -5,12 +5,12 @@
 
 from pathlib import Path
 
-from pyspartaproj.context.default.string_context import Strs
-from pyspartaproj.script.file.json.convert_from_json import (
+from pyspartalib.context.default.string_context import Strs
+from pyspartalib.script.file.json.convert_from_json import (
     string_pair_from_json,
 )
-from pyspartaproj.script.file.json.import_json import json_import
-from pyspartaproj.script.path.modify.get_resource import get_resource
+from pyspartalib.script.file.json.import_json import json_import
+from pyspartalib.script.path.modify.get_resource import get_resource
 
 
 def _common_test(expected: str, path_elements: Strs) -> None:
