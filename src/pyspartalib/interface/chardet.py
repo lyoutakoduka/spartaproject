f0e5da8ac28485d743c7a454c99b1090eff46c67
lyouta <lyouta@spartaproject.com>
2024-12-17 20:33:47 +0900

    Feat: Connect to chardet module from inside. #149

---
+++ src/pyspartalib/interface/chardet.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Wrapper module to separate pure python code and other.
+
+paramiko: chardet
+"""
+
+from chardet import UniversalDetector  # noqa: F401 # type: ignore
