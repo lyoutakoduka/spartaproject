3d402707d10f622d01daec36bba0a0376e9e3f3b
lyouta <lyouta@spartaproject.com>
2024-12-21 03:00:47 +0900

    Refactor: Remove unused tool sections. #163

--- tools/execute_filter.sh
+++ tools/execute_filter.sh
@@ -16,13 +16,7 @@ execute_filter () {
     exec "$interpreter$executable" $argument
 }
 
-if [ "isort" = $filter_group ]; then
-    execute_filter "isort" "--check-only $execute_target"
-elif [ "black" = $filter_group ]; then
-    execute_filter "black" "--check $execute_target"
-elif [ "flake" = $filter_group ]; then
-    execute_filter "pflake8" $execute_target
-elif [ "mypy" = $filter_group ]; then
+if [ "mypy" = $filter_group ]; then
     execute_filter "mypy" $execute_target
 elif [ "pytest" = $filter_group ]; then
     execute_filter "pytest" $execute_target
