3eaaf95eede2c2f21d7c9b0b92d1a254cbab39a9
lyouta <lyouta@spartaproject.com>
2024-12-30 08:00:33 +0900

    Refactor: Unnecessary definition. local. #165

--- packages/pyspartalib/src/pyspartalib/script/server/local/execute_server.py
+++ packages/pyspartalib/src/pyspartalib/script/server/local/execute_server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to execute python code on server you can use ssh connection."""
 
--- packages/pyspartalib/src/pyspartalib/script/server/local/upload_server.py
+++ packages/pyspartalib/src/pyspartalib/script/server/local/upload_server.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to upload file or directory by SFTP functionality."""
 
