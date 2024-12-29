2aa1d70e0c3426ee2f133c5343987b0cc58a6c53
lyouta <lyouta@spartaproject.com>
2024-12-30 07:50:06 +0900

    Refactor: Readable doc-strings. server. #165

--- packages/pyspartalib/src/pyspartalib/script/server/script_version.py
+++ packages/pyspartalib/src/pyspartalib/script/server/script_version.py
@@ -8,24 +8,26 @@ from pyspartalib.script.shell.execute_command import execute_single
 
 
 def get_version_name(version: str) -> str:
-    """Function to convert version string as default directory name.
+    """Convert version string as default directory name.
 
     Args:
         version (str): Version information formatted like "3.12.0".
 
     Returns:
         str: Formatted version string like "Python-3.12.0".
+
     """
     return "python".capitalize() + "-" + version
 
 
 def get_interpreter_version(executable: Path) -> str:
-    """Function to get version information of specific interpreter.
+    """Get version information of specific interpreter.
 
     Args:
         executable (Path): Interpreter path you want to get version.
 
     Returns:
         str: Version information formatted like "3.12.0".
+
     """
     return list(execute_single([str(executable), "-V"]))[0].split(" ")[-1]
