177d33aaeb2431a9cac46b2a3c9c153f1708f959
lyouta <lyouta@spartaproject.com>
2024-12-30 14:03:41 +0900

    Fix: Treat type None return value Correctly. #165

--- packages/pyspartalib/src/pyspartalib/script/server/local/upload_server.py
+++ packages/pyspartalib/src/pyspartalib/script/server/local/upload_server.py
@@ -73,8 +73,9 @@ class UploadServer(ConnectServer):
             sftp.mkdir(path.as_posix())
 
     def _create_upload_tree(self, path: Path) -> None:
-        for path_child in reversed(self._get_upload_tree(path)):
-            self._create_directory(path_child)
+        if paths := self._get_upload_tree(path):
+            for path_child in reversed(paths):
+                self._create_directory(path_child)
 
     def _convert_remote_path(self, local: Path) -> Path:
         return Path(self._get_remote_root(), local)
