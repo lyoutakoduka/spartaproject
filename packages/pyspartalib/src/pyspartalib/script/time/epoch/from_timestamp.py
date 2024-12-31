e6c2eae6abd0bef58739424a451b25f9bd345d68
lyouta <lyouta@spartaproject.com>
2024-12-31 13:58:46 +0900

    Refactor: Get time with time stamp. #165

--- packages/pyspartalib/src/pyspartalib/script/time/epoch/from_timestamp.py
+++ packages/pyspartalib/src/pyspartalib/script/time/epoch/from_timestamp.py
@@ -21,5 +21,7 @@ def time_from_timestamp(timestamp: Decimal, jst: bool = False) -> datetime:
         datetime: Converted datetime object.
 
     """
-    date_time: datetime = datetime.fromtimestamp(float(timestamp))
-    return date_time.astimezone(gettz("Asia/Tokyo" if jst else "UTC"))
+    return datetime.fromtimestamp(
+        float(timestamp),
+        tz=gettz("Asia/Tokyo" if jst else "UTC"),
+    )
