a3fd5c3c81c8b86af2b1be3410f720e5c81e449f
lyouta <lyouta@spartaproject.com>
2024-12-31 13:54:09 +0900

    Doc: Update module document strings. epoch. #165

--- packages/pyspartalib/src/pyspartalib/script/time/epoch/from_timestamp.py
+++ packages/pyspartalib/src/pyspartalib/script/time/epoch/from_timestamp.py
@@ -19,6 +19,7 @@ def time_from_timestamp(timestamp: Decimal, jst: bool = False) -> datetime:
 
     Returns:
         datetime: Converted datetime object.
+
     """
     date_time: datetime = datetime.fromtimestamp(float(timestamp))
     return date_time.astimezone(gettz("Asia/Tokyo" if jst else "UTC"))
