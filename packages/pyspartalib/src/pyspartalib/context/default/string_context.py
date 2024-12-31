2715945bea32b55abf0c6213b9d3ff3280ecaf5d
lyouta <lyouta@spartaproject.com>
2024-12-31 11:23:35 +0900

    Update: Rename type alias to more versatile. #165

--- packages/pyspartalib/src/pyspartalib/context/default/string_context.py
+++ packages/pyspartalib/src/pyspartalib/context/default/string_context.py
@@ -7,7 +7,7 @@ from collections.abc import Generator
 StrPair = dict[str, str]
 StrTuple = tuple[str, str]
 Strs = list[str]
-Trans = dict[int, str]
+IntStrPair = dict[int, str]
 
 StrsPair = dict[str, Strs]
 StrPair2 = dict[str, StrPair]
