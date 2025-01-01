0e060f714ac3146885a62b08c5a652ec361465b3
lyouta <lyouta@spartaproject.com>
2025-01-02 02:53:49 +0900

    Refactor: Unnecessary definition. shared. #165

--- packages/pyspartalib/src/pyspartalib/script/path/modify/mount/shared/get_linux_relative.py
+++ packages/pyspartalib/src/pyspartalib/script/path/modify/mount/shared/get_linux_relative.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to get relative path seen from a mount point of Linux."""
 
--- packages/pyspartalib/src/pyspartalib/script/path/modify/mount/shared/has_linux_head.py
+++ packages/pyspartalib/src/pyspartalib/script/path/modify/mount/shared/has_linux_head.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to confirm that selected path include a mount point of Linux."""
 
