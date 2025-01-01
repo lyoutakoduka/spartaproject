eb98688aadccc55ea64baa9b06da9caf6fb762b7
lyouta <lyouta@spartaproject.com>
2025-01-02 02:39:58 +0900

    Refactor: Unnecessary definition. modify. #165

--- packages/pyspartalib/src/pyspartalib/script/path/modify/avoid_duplication.py
+++ packages/pyspartalib/src/pyspartalib/script/path/modify/avoid_duplication.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to convert path to avoid existing path."""
 
--- packages/pyspartalib/src/pyspartalib/script/path/modify/get_resource.py
+++ packages/pyspartalib/src/pyspartalib/script/path/modify/get_resource.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to get path in local resource directory."""
 
