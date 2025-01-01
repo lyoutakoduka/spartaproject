bfc89604b4424cf9533f17ebbe2b0f2eaf27ba91
lyouta <lyouta@spartaproject.com>
2025-01-02 02:47:15 +0900

    Refactor: Unnecessary definition. mount. #165

--- packages/pyspartalib/src/pyspartalib/script/path/modify/mount/build_linux_path.py
+++ packages/pyspartalib/src/pyspartalib/script/path/modify/mount/build_linux_path.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to get path about Windows mounted on Linux."""
 
--- packages/pyspartalib/src/pyspartalib/script/path/modify/mount/convert_to_windows.py
+++ packages/pyspartalib/src/pyspartalib/script/path/modify/mount/convert_to_windows.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to convert path from format Linux to format Windows."""
 
