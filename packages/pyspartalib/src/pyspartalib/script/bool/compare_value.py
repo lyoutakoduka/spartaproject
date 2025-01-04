fd59793e0990ef4ebcc79ccf2f45c0d5c5655b3a
lyouta <lyouta@spartaproject.com>
2025-01-05 06:02:06 +0900

    Feat: Compare that two bool list is same. #165

--- packages/pyspartalib/src/pyspartalib/script/bool/compare_value.py
+++ packages/pyspartalib/src/pyspartalib/script/bool/compare_value.py
@@ -43,9 +43,14 @@ def _get_sorted_flags(sorted_keys: Strs2, flags_pair: BoolPairs) -> Bools2:
     ]
 
 
+def _same_error(lefts: Bools, rights: Bools) -> bool:
+    return lefts == rights
+
+
 def bool_compare_array(lefts: Bools, rights: Bools) -> bool:
     _check_arguments_size(lefts, rights)
-    return lefts == rights
+
+    return _same_error(lefts, rights)
 
 
 def bool_compare_pair(lefts: BoolPair, rights: BoolPair) -> bool:
