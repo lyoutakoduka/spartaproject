d302ba98dc3b2cd073748afaed41e48cef5f9490
lyouta <lyouta@spartaproject.com>
2025-01-04 15:18:54 +0900

    Feat: Get expected float list for test. #165

--- packages/pyspartalib/tests/pyspartalib/script/decimal/test_convert_float.py
+++ packages/pyspartalib/tests/pyspartalib/script/decimal/test_convert_float.py
@@ -8,9 +8,14 @@ from pyspartalib.context.default.float_context import Floats
 from pyspartalib.script.decimal.convert_float import convert_float_array
 
 
+def _get_expected() -> Floats:
+    return [float(i) for i in range(3)]
+
+
 def test_array() -> None:
     """Test to convert array from type Decimal to type float."""
-    expected: Floats = [float(i) for i in range(3)]
+    expected: Floats = _get_expected()
+
     assert expected == convert_float_array(
         [Decimal(str(value)) for value in expected]
     )
