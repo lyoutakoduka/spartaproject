874725d2bf21ef4f26fc61f65617f6a4a77731c4
lyouta <lyouta@spartaproject.com>
2025-01-04 11:56:57 +0900

    Refactor: Unnecessary definition. bool. #165

--- packages/pyspartalib/tests/pyspartalib/script/bool/test_compare_json.py
+++ packages/pyspartalib/tests/pyspartalib/script/bool/test_compare_json.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Test module to check that two Json objects are same."""
 
--- packages/pyspartalib/tests/pyspartalib/script/bool/test_compare_value.py
+++ packages/pyspartalib/tests/pyspartalib/script/bool/test_compare_value.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from pyspartalib.script.bool.compare_value import (
     bool_compare_array,
