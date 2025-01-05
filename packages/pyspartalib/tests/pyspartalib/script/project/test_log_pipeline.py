566ee3eb3122296a20456350f36738bfaddbb689
lyouta <lyouta@spartaproject.com>
2025-01-05 20:01:18 +0900

    Refactor: Unnecessary definition. project. #165

--- packages/pyspartalib/tests/pyspartalib/script/project/test_log_pipeline.py
+++ packages/pyspartalib/tests/pyspartalib/script/project/test_log_pipeline.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Test module to handle I/O functionalities of any script called pipeline."""
 
--- packages/pyspartalib/tests/pyspartalib/script/project/test_project_context.py
+++ packages/pyspartalib/tests/pyspartalib/script/project/test_project_context.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Test module to import a context of whole project from outside Json."""
 
