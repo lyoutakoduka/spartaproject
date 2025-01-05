fe2057faba0adc8498a4b34c18824260bf2dbd5e
lyouta <lyouta@spartaproject.com>
2025-01-06 05:28:21 +0900

    Feet: Get expected string used for test. #165

--- packages/pyspartalib/tests/pyspartalib/script/stdout/test_logger.py
+++ packages/pyspartalib/tests/pyspartalib/script/stdout/test_logger.py
@@ -6,3 +6,7 @@ from pyspartalib.context.callable_context import Type
 def _difference_error(result: Type, expected: Type) -> None:
     if result != expected:
         raise ValueError
+
+
+def _get_expected() -> str:
+    return "test"
