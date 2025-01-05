3a87d6cab75eb063a95e4eb7f6581aee45b45dc6
lyouta <lyouta@spartaproject.com>
2025-01-06 03:47:48 +0900

    Refactor: Unnecessary definition. tools. #165

--- packages/pyspartalib/tests/pyspartalib/script/shell/resource/tools/find_platform.py
+++ packages/pyspartalib/tests/pyspartalib/script/shell/resource/tools/find_platform.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module for test to execute Python on all platform."""
 
--- packages/pyspartalib/tests/pyspartalib/script/shell/resource/tools/system.py
+++ packages/pyspartalib/tests/pyspartalib/script/shell/resource/tools/system.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Module to show Python system paths."""
 
