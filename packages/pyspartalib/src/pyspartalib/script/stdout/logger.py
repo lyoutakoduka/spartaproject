b7a663de76f140586d4b41ca1f93b0ce52265997
lyouta <lyouta@spartaproject.com>
2025-01-06 05:18:12 +0900

    Doc: Add module document strings. #165

--- packages/pyspartalib/src/pyspartalib/script/stdout/logger.py
+++ packages/pyspartalib/src/pyspartalib/script/stdout/logger.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+"""Module to show log to stdout."""
+
 
 def show_log(text: str) -> None:
+    """Show log to stdout.
+
+    Args:
+        text (str): String you want to show.
+
+    """
     print(text)
