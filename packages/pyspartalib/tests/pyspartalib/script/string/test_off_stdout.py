55fc12416ee4c7af1f2edeb966cad2b50498f076
lyouta <lyouta@spartaproject.com>
2025-01-07 07:56:59 +0900

    Fix: Reconnect moved module correctly. #165

--- packages/pyspartalib/tests/pyspartalib/script/string/test_off_stdout.py
+++ packages/pyspartalib/tests/pyspartalib/script/string/test_off_stdout.py
@@ -3,8 +3,8 @@
 """Test module to redirect stdout to string variable forcibly."""
 
 from pyspartalib.script.stdout.logger import show_log
+from pyspartalib.script.stdout.off_stdout import StdoutText
 from pyspartalib.script.string.format_texts import format_indent
-from pyspartalib.script.string.off_stdout import StdoutText
 
 
 def test_messages() -> None:
