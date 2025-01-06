7f26fbc116db1667a3d23120c349df55ed5f3d3f
lyouta <lyouta@spartaproject.com>
2025-01-07 08:04:55 +0900

    Update: Type to represent simple function. #165

--- packages/pyspartalib/src/pyspartalib/context/type_context.py
+++ packages/pyspartalib/src/pyspartalib/context/type_context.py
@@ -2,7 +2,9 @@
 
 """User defined types about callable object."""
 
+from collections.abc import Callable
 from typing import ParamSpec, TypeVar
 
 Type = TypeVar("Type")
 Param = ParamSpec("Param")
+Func = Callable[[], None]
