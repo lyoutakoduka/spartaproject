cc792e42449c4e962850b9959d2e64ac728ea9b5
lyouta <lyouta@spartaproject.com>
2025-01-07 06:15:21 +0900

    Feat: Error if two values are different. #165

--- packages/pyspartalib/tests/pyspartalib/script/server/test_script_version.py
+++ packages/pyspartalib/tests/pyspartalib/script/server/test_script_version.py
@@ -6,17 +6,26 @@ from pathlib import Path
 from platform import python_version
 from sys import executable
 
+from pyspartalib.context.type_context import Type
 from pyspartalib.script.server.script_version import (
     get_interpreter_version,
     get_version_name,
 )
 
 
+def _difference_error(result: Type, expected: Type) -> None:
+    if result != expected:
+        raise ValueError
+
+
 def test_name() -> None:
     """Test function to compare version string like default directory name."""
-    assert "Python-0.0.0" == get_version_name("0.0.0")
+    _difference_error(get_version_name("0.0.0"), "Python-0.0.0")
 
 
 def test_version() -> None:
     """Test function to compare version information of specific interpreter."""
-    assert python_version() == get_interpreter_version(Path(executable))
+    _difference_error(
+        get_interpreter_version(Path(executable)),
+        python_version(),
+    )
