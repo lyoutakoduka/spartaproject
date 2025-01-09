ab4cd8531190845bdfd5b2f486456e34a3023a4c
lyouta <lyouta@spartaproject.com>
2025-01-09 22:16:19 +0900

    Feat: Error if two values are different. #165

--- packages/pyspartalib/tests/pyspartalib/script/time/epoch/test_from_timestamp.py
+++ packages/pyspartalib/tests/pyspartalib/script/time/epoch/test_from_timestamp.py
@@ -6,11 +6,17 @@ from datetime import datetime
 from decimal import Decimal
 
 from pyspartalib.context.default.integer_context import IntPair2
+from pyspartalib.context.type_context import Type
 from pyspartalib.script.time.epoch.from_timestamp import time_from_timestamp
 from pyspartalib.script.time.format.create_iso_date import (
     get_iso_epoch,
     get_iso_time,
 )
+
+
+def _difference_error(result: Type, expected: Type) -> None:
+    if result != expected:
+        raise ValueError
 
 
 def _get_source() -> IntPair2:
@@ -47,12 +53,12 @@ def _compare_time(expected: datetime, result: datetime) -> None:
 
 def test_utc() -> None:
     """Test to convert time data from epoch format to datetime object."""
-    _compare_time(_get_iso_time(), time_from_timestamp(_get_iso_epoch()))
+    _difference_error(time_from_timestamp(_get_iso_epoch()), _get_iso_time())
 
 
 def test_jst() -> None:
     """Test to convert time data to datetime object as JST time zone."""
-    _compare_time(
-        _get_iso_time_jst(),
+    _difference_error(
         time_from_timestamp(_get_iso_epoch(), jst=True),
+        _get_iso_time_jst(),
     )
--- packages/pyspartalib/tests/pyspartalib/script/time/epoch/test_get_time_stamp.py
+++ packages/pyspartalib/tests/pyspartalib/script/time/epoch/test_get_time_stamp.py
@@ -4,9 +4,15 @@
 
 from decimal import Decimal
 
+from pyspartalib.context.type_context import Type
 from pyspartalib.script.time.epoch.get_time_stamp import get_initial_epoch
+
+
+def _difference_error(result: Type, expected: Type) -> None:
+    if result != expected:
+        raise ValueError
 
 
 def test_epoch() -> None:
     """Test to get UNIX epoch represent April 1, 2023."""
-    assert get_initial_epoch() == Decimal("1680307200")
+    _difference_error(get_initial_epoch(), Decimal("1680307200"))
