1ec1c4b8000a338bf3881d251d37ffe669db7742
lyouta <lyouta@spartaproject.com>
2025-01-11 17:24:55 +0900

    Refactor: Remove redundant code. current. #165

--- packages/pyspartalib/tests/pyspartalib/script/path/modify/current/test_get_absolute.py
+++ packages/pyspartalib/tests/pyspartalib/script/path/modify/current/test_get_absolute.py
@@ -25,7 +25,7 @@ def _get_absolute_current() -> Path:
 
 
 def _to_pair(keys: Strs, paths: Paths) -> PathPair:
-    return {key: path for key, path in zip(keys, paths, strict=True)}
+    return dict(zip(keys, paths, strict=True))
 
 
 def test_ignore() -> None:
--- packages/pyspartalib/tests/pyspartalib/script/path/modify/current/test_get_relative.py
+++ packages/pyspartalib/tests/pyspartalib/script/path/modify/current/test_get_relative.py
@@ -46,10 +46,7 @@ def _get_paths(current: Path) -> Paths:
 
 
 def _to_pair(path_types: Strs, paths: Paths) -> PathPair:
-    return {
-        path_type: path
-        for path_type, path in zip(path_types, paths, strict=True)
-    }
+    return dict(zip(path_types, paths, strict=True))
 
 
 def test_check() -> None:
