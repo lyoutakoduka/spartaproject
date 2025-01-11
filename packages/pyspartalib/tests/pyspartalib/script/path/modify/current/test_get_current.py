528361f1e124cbb7981796562028e910e58ff86a
lyouta <lyouta@spartaproject.com>
2025-01-11 18:16:37 +0900

    Feat: Raise error if path isn't exists. #165

--- packages/pyspartalib/tests/pyspartalib/script/path/modify/current/test_get_current.py
+++ packages/pyspartalib/tests/pyspartalib/script/path/modify/current/test_get_current.py
@@ -2,9 +2,16 @@
 
 """Test module to get current working directory."""
 
+from pathlib import Path
+
 from pyspartalib.script.path.modify.current.get_current import get_current
+
+
+def _no_exists_error(path: Path) -> None:
+    if not path.exists():
+        raise FileNotFoundError
 
 
 def test_current() -> None:
     """Test to cet current working directory."""
-    assert get_current().exists()
+    _no_exists_error(get_current())
