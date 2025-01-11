6e10f5047363ac91fd2bdefb38a096dd39a722a2
lyouta <lyouta@spartaproject.com>
2025-01-11 19:51:54 +0900

    Refactor: Unnecessary encoding declaration. #165

--- packages/pyspartalib/tests/pyspartalib/script/file/archive/test_compress_archive.py
+++ packages/pyspartalib/tests/pyspartalib/script/file/archive/test_compress_archive.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Test module to compress file or directory by archive format."""
 
--- packages/pyspartalib/tests/pyspartalib/script/file/archive/test_decompress_archive.py
+++ packages/pyspartalib/tests/pyspartalib/script/file/archive/test_decompress_archive.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Test module to decompress file or directory by archive format."""
 
