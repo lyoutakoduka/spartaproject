551c31f64aa0809148af944c658580921ccb5332
lyouta <lyouta@spartaproject.com>
2025-01-14 11:57:31 +0900

    Update: Standardize order of type definition. #165

--- packages/pyspartalib/src/pyspartalib/context/file/json_context.py
+++ packages/pyspartalib/src/pyspartalib/context/file/json_context.py
@@ -42,11 +42,11 @@ from pyspartalib.context.extension.path_context import (
     Paths2,
 )
 
-Array = Bools | Ints | Floats | Strs | Decs | Paths
-Array2 = Bools2 | Ints2 | Floats2 | Strs2 | Decs2 | Paths2
-Pair = BoolPair | IntPair | FloatPair | StrPair | DecPair | PathPair
-Pair2 = BoolPair2 | IntPair2 | FloatPair2 | StrPair2 | DecPair2 | PathPair2
-Single = None | bool | int | float | str | Decimal | Path
+Array = Bools | Ints | Floats | Decs | Strs | Paths
+Array2 = Bools2 | Ints2 | Floats2 | Decs2 | Strs2 | Paths2
+Pair = BoolPair | IntPair | FloatPair | DecPair | StrPair | PathPair
+Pair2 = BoolPair2 | IntPair2 | FloatPair2 | DecPair2 | StrPair2 | PathPair2
+Single = None | bool | int | float | Decimal | str | Path
 
 Json = Single | dict[str, "Json"] | list["Json"]
 Multi = Array | Pair
