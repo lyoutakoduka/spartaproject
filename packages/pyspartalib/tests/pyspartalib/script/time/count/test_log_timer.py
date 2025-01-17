85b2423b71566e3e7cd2001614a03ba694ebaff9
lyouta <lyouta@spartaproject.com>
2025-01-17 22:31:37 +0900

    Fix: Reconnect to updated module. #165

--- packages/pyspartalib/tests/pyspartalib/script/time/count/test_log_timer.py
+++ packages/pyspartalib/tests/pyspartalib/script/time/count/test_log_timer.py
@@ -8,7 +8,7 @@ from pyspartalib.context.custom.timer_context import TimerFunc, TimerIntStrFunc
 from pyspartalib.context.default.string_context import Strs
 from pyspartalib.context.type_context import Type
 from pyspartalib.script.decimal.initialize_decimal import initialize_decimal
-from pyspartalib.script.stdout.format_texts import format_indent
+from pyspartalib.script.stdout.format_indent import format_indent
 from pyspartalib.script.time.count.log_timer import LogTimer
 
 initialize_decimal()
