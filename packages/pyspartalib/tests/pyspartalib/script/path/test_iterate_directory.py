8ce2dda71cbd3af7eaf59badcb7d412710409e2d
lyouta <lyouta@spartaproject.com>
2025-01-18 20:57:32 +0900

    Feat: Error if length of list are different. #165

--- packages/pyspartalib/tests/pyspartalib/script/path/test_iterate_directory.py
+++ packages/pyspartalib/tests/pyspartalib/script/path/test_iterate_directory.py
@@ -22,7 +22,7 @@ from pyspartalib.script.path.temporary.create_temporary_tree import (
 
 
 def _length_error(result: Sized, expected: int) -> None:
-    if len(result) == expected:
+    if len(result) != expected:
         raise ValueError
 
 
