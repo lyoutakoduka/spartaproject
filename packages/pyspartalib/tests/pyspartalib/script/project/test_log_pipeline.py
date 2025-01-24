9bfbdceaf9089075bce51c43749b51633bd0900a
lyouta <lyouta@spartaproject.com>
2025-01-24 22:59:52 +0900

    Fix: Reconnect to updated module importing. #165

--- packages/pyspartalib/tests/pyspartalib/script/project/test_log_pipeline.py
+++ packages/pyspartalib/tests/pyspartalib/script/project/test_log_pipeline.py
@@ -4,11 +4,12 @@
 
 from decimal import Decimal
 
-from pyspartalib.context.custom.log_context import LogFunc
 from pyspartalib.context.default.string_context import Strs
 from pyspartalib.context.type_context import Type
 from pyspartalib.script.project.log_pipeline import LogPipeline
 from pyspartalib.script.stdout.off_stdout import OffStdout
+
+from .context.log_context import LogFunc
 
 
 def _none_error(result: Type | None) -> Type:
