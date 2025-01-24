7b791b3dd421cacf6b55c74dc39c55df1fb7e897
lyouta <lyouta@spartaproject.com>
2025-01-25 04:11:07 +0900

    Refactor: Remove unused user defined type. #165

--- packages/pyspartalib/src/pyspartalib/context/typed/user_context.py
+++ packages/pyspartalib/src/pyspartalib/context/typed/user_context.py
@@ -2,22 +2,9 @@
 
 """User defined types using class "TypedDict"."""
 
-from pathlib import Path
 from typing import TypedDict
 
 from pyspartalib.context.default.string_context import Strs
-from pyspartalib.context.extension.path_context import Paths
-
-
-class ArchiveStatus(TypedDict):
-    """Class to represent an archive information.
-
-    It's used for test to take out directory from inside of archive.
-    """
-
-    archive: Path
-    take: Paths
-    keep: Paths
 
 
 class BaseName(TypedDict):
