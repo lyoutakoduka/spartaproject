2c8a81692d2b7e4300d42696f8c4fe1182be6c9a
lyouta <lyouta@spartaproject.com>
2025-01-24 23:46:43 +0900

    Doc: Add module document strings. #165

--- packages/pyspartalib/src/pyspartalib/script/stdout/context/format_context.py
+++ packages/pyspartalib/src/pyspartalib/script/stdout/context/format_context.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+"""Module to represent string with white space character."""
+
 from typing import TypedDict
 
 
 class FormatPair(TypedDict):
+    """Class to represent string with white space character."""
+
     text: str
     count: int
 
