8cd8f9d0597eba07713a5e442d159c18aa259cf8
lyouta <lyouta@spartaproject.com>
2025-01-27 04:31:52 +0900

    Refactor: Remove unused user defined type. #165

--- packages/pyspartalib/src/pyspartalib/context/type_context.py
+++ packages/pyspartalib/src/pyspartalib/context/type_context.py
@@ -2,9 +2,7 @@
 
 """User defined types about callable object."""
 
-from collections.abc import Callable
 from typing import ParamSpec, TypeVar
 
 Type = TypeVar("Type")
 Param = ParamSpec("Param")
-Func = Callable[[], None]
