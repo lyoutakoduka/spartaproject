f0adab06022a396c4532378bd571af4a5213024f
lyouta <lyouta@spartaproject.com>
2025-01-27 03:39:11 +0900

    Refactor: Remove unused user defined type. #165

--- packages/pyspartalib/src/pyspartalib/context/typed/user_context.py
+++ packages/pyspartalib/src/pyspartalib/context/typed/user_context.py
@@ -7,16 +7,6 @@ from typing import TypedDict
 from pyspartalib.context.default.string_context import Strs
 
 
-class BaseName(TypedDict):
-    """Class to represent elements for base name of file.
-
-    It's used for the base name including index string.
-    """
-
-    name: str
-    index: int
-
-
 class CharacterTable(TypedDict):
     """Class to represent 3 types list about alphabets and numbers, and others.
 
