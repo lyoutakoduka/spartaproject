2f3c85bd93c78875629b21eac6f9c1fdc70c4c65
lyouta <lyouta@spartaproject.com>
2025-02-04 12:52:01 +0900

    Feat: Error if length of list are different. #165

---
+++ packages/pyspartalib/tests/pyspartalib/script/platform/test_terminal_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from pyspartalib.context.custom.type_context import Type
+
+
+def _difference_error(result: Type, expected: Type) -> None:
+    if result != expected:
+        raise ValueError
