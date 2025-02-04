ce6ca59b2f9c3177fa5d5d820cc393f31185a311
lyouta <lyouta@spartaproject.com>
2025-02-04 18:17:00 +0900

    Feat: Get current directory on symbolic link. #165

--- packages/pyspartalib/src/pyspartalib/script/path/modify/current/get_current.py
+++ packages/pyspartalib/src/pyspartalib/script/path/modify/current/get_current.py
@@ -6,6 +6,7 @@ from collections.abc import Sized
 from pathlib import Path
 
 from pyspartalib.context.default.string_context import Strs
+from pyspartalib.script.platform.terminal_status import get_terminal
 from pyspartalib.script.shell.execute_command import execute_single
 
 
@@ -28,6 +29,13 @@ def _from_shell() -> Path:
     _length_error(result, 1)
 
     return Path(result[0])
+
+
+def _confirm_symbolic() -> Path:
+    if get_terminal() == "test":
+        raise FileNotFoundError
+
+    return _from_shell()
 
 
 def get_current() -> Path:
