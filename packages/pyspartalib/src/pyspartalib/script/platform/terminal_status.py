71185c17154d16ec7af2b1a52f65c6350361e918
lyouta <lyouta@spartaproject.com>
2025-02-04 12:50:46 +0900

    Feat: Get environment that python executed. #165

--- packages/pyspartalib/src/pyspartalib/script/platform/terminal_status.py
+++ packages/pyspartalib/src/pyspartalib/script/platform/terminal_status.py
@@ -5,3 +5,13 @@ from os import getenv
 
 def _get_environment(key: str) -> bool:
     return getenv(key.upper()) is not None
+
+
+def get_terminal() -> str:
+    if _get_environment("vscode_cwd"):
+        return "test"
+
+    if _get_environment("term_program"):
+        return "vscode"
+
+    return "terminal"
