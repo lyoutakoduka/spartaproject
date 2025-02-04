7e4cdd470f2529246e3de19124823b78dadb4119
lyouta <lyouta@spartaproject.com>
2025-02-04 12:52:43 +0900

    Feat: Raise error if value is None. #165

--- packages/pyspartalib/tests/pyspartalib/script/platform/test_terminal_status.py
+++ packages/pyspartalib/tests/pyspartalib/script/platform/test_terminal_status.py
@@ -6,3 +6,10 @@ from pyspartalib.context.custom.type_context import Type
 def _difference_error(result: Type, expected: Type) -> None:
     if result != expected:
         raise ValueError
+
+
+def _none_error(result: Type | None) -> Type:
+    if result is None:
+        raise ValueError
+
+    return result
