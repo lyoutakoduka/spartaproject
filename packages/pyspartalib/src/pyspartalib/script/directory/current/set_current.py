299d89ca75852626ef8c46c1436955919a1486e4
lyouta <lyouta@spartaproject.com>
2025-02-05 20:05:16 +0900

    Doc: Add module document strings. #165

--- packages/pyspartalib/src/pyspartalib/script/directory/current/set_current.py
+++ packages/pyspartalib/src/pyspartalib/script/directory/current/set_current.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
+"""Module to set current working directory."""
+
 from os import chdir
 from pathlib import Path
 
 
 def set_current(path: Path) -> None:
+    """Set current working directory.
+
+    Args:
+        path (Path): Current working directory you want to set.
+
+    """
     chdir(path)
