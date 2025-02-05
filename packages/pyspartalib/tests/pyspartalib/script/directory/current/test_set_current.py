9d5f8b612134e398a9a000991623bc65ec0df297
lyouta <lyouta@spartaproject.com>
2025-02-05 19:59:34 +0900

    Feat: Error if length of list are different. #165

--- packages/pyspartalib/tests/pyspartalib/script/directory/current/test_set_current.py
+++ packages/pyspartalib/tests/pyspartalib/script/directory/current/test_set_current.py
@@ -2,6 +2,13 @@
 
 from pathlib import Path
 
+from pyspartalib.context.custom.type_context import Type
+
+
+def _difference_error(result: Type, expected: Type) -> None:
+    if result != expected:
+        raise ValueError
+
 
 def _get_current() -> Path:
     return Path().cwd()
