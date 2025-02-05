a395c5f9689b26f7ada8204c97b1302c2d0373cc
lyouta <lyouta@spartaproject.com>
2025-02-05 20:01:02 +0900

    Feat: Create temporary working directory. #165

--- packages/pyspartalib/tests/pyspartalib/script/directory/current/test_set_current.py
+++ packages/pyspartalib/tests/pyspartalib/script/directory/current/test_set_current.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from pyspartalib.context.custom.type_context import Type
+from pyspartalib.context.extension.path_context import PathFunc
 
 
 def _difference_error(result: Type, expected: Type) -> None:
@@ -12,3 +14,8 @@ def _difference_error(result: Type, expected: Type) -> None:
 
 def _get_current() -> Path:
     return Path().cwd()
+
+
+def _inside_temporary_directory(function: PathFunc) -> None:
+    with TemporaryDirectory() as temporary_path:
+        function(Path(temporary_path))
