263d6d3d6231d3659bb376eb25cb53ddccb39081
lyouta <lyouta@spartaproject.com>
2025-02-14 07:21:02 +0900

    Feat: Create symbolic link. #165

--- packages/pyspartalib/src/pyspartalib/script/path/symbolic/create_symbolic.py
+++ packages/pyspartalib/src/pyspartalib/script/path/symbolic/create_symbolic.py
@@ -9,3 +9,8 @@ from pyspartalib.script.path.symbolic.context.symbolic_context import (
 
 def get_symbolic_link(source_root: Path, symbolic_root: Path) -> SymbolicLink:
     return {"source": source_root, "symbolic": symbolic_root}
+
+
+def create_symbolic(source_root: Path, symbolic_root: Path) -> SymbolicLink:
+    symbolic_root.symlink_to(source_root, target_is_directory=True)
+    return get_symbolic_link(source_root, symbolic_root)
