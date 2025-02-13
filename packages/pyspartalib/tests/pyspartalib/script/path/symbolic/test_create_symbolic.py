39d7d4a54a1f09199a4b2817fa79df9a1d616949
lyouta <lyouta@spartaproject.com>
2025-02-14 07:56:10 +0900

    Feat: Create symbolic link for test. #165

--- packages/pyspartalib/tests/pyspartalib/script/path/symbolic/test_create_symbolic.py
+++ packages/pyspartalib/tests/pyspartalib/script/path/symbolic/test_create_symbolic.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
+from pathlib import Path
+
 from pyspartalib.context.custom.type_context import Type
+from pyspartalib.script.path.symbolic.context.symbolic_context import (
+    SymbolicLink,
+)
+from pyspartalib.script.path.symbolic.create_symbolic import create_symbolic
+from pyspartalib.script.path.temporary.create_temporary_tree import (
+    create_temporary_tree,
+)
 
 
 def _difference_error(result: Type, expected: Type) -> None:
@@ -11,3 +20,10 @@ def _difference_error(result: Type, expected: Type) -> None:
 def _status_error(status: bool) -> None:
     if not status:
         raise ValueError
+
+
+def _create_symbolic(path: Path) -> SymbolicLink:
+    return create_symbolic(
+        create_temporary_tree(Path(path, "source"), tree_deep=1),
+        Path(path, "symbolic"),
+    )
