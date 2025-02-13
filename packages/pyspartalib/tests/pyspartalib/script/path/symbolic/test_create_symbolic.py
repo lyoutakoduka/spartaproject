5b43e6f41f78a1d641bb29b42b954d78a077dae9
lyouta <lyouta@spartaproject.com>
2025-02-14 07:45:20 +0900

    Feat: Raise error if status is fail. #165

--- packages/pyspartalib/tests/pyspartalib/script/path/symbolic/test_create_symbolic.py
+++ packages/pyspartalib/tests/pyspartalib/script/path/symbolic/test_create_symbolic.py
@@ -6,3 +6,8 @@ from pyspartalib.context.custom.type_context import Type
 def _difference_error(result: Type, expected: Type) -> None:
     if result != expected:
         raise ValueError
+
+
+def _status_error(status: bool) -> None:
+    if not status:
+        raise ValueError
