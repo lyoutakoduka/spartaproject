0e93131931417525d0f177a391b61a60b9c12a52
lyouta <lyouta@spartaproject.com>
2025-02-14 03:45:33 +0900

    Doc: Add module document strings. #165

--- packages/pyspartalib/src/pyspartalib/script/path/symbolic/context/symbolic_context.py
+++ packages/pyspartalib/src/pyspartalib/script/path/symbolic/context/symbolic_context.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+"""Module to represent paths about symbolic link."""
+
 from pathlib import Path
 from typing import TypedDict
 
 
 class SymbolicLink(TypedDict):
+    """Class to represent paths about symbolic link."""
+
     source: Path
     symbolic: Path
