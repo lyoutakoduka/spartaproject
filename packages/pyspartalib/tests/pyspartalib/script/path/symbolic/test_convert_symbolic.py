d85e37a599c40850529a4f2ecf64291bbd8472ae
lyouta <lyouta@spartaproject.com>
2025-02-15 23:24:18 +0900

    Feat: Get path pair about symbolic link. #165

--- packages/pyspartalib/tests/pyspartalib/script/path/symbolic/test_convert_symbolic.py
+++ packages/pyspartalib/tests/pyspartalib/script/path/symbolic/test_convert_symbolic.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
+
+from pathlib import Path
+
 from pyspartalib.context.custom.type_context import Type
+from pyspartalib.context.extension.path_context import Paths
 
 
 def _difference_error(result: Type, expected: Type) -> None:
     if result != expected:
         raise ValueError
+
+
+def _get_directories(path: Path) -> Paths:
+    return [Path(path, group) for group in ["source", "symbolic"]]
