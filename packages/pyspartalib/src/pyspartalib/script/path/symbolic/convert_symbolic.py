114ea02db95e4ccf875c4162bb241bd8102d5075
lyouta <lyouta@spartaproject.com>
2025-02-15 23:16:28 +0900

    Feat: Convert root path if it can convert. #165

--- packages/pyspartalib/src/pyspartalib/script/path/symbolic/convert_symbolic.py
+++ packages/pyspartalib/src/pyspartalib/script/path/symbolic/convert_symbolic.py
@@ -2,7 +2,10 @@
 
 from pathlib import Path
 
-from pyspartalib.script.path.modify.current.get_relative import get_relative
+from pyspartalib.script.path.modify.current.get_relative import (
+    get_relative,
+    is_relative,
+)
 from pyspartalib.script.path.symbolic.context.symbolic_context import (
     SymbolicLink,
 )
@@ -13,3 +16,13 @@ def _convert_root(target_root: Path, symbolic_link: SymbolicLink) -> Path:
         symbolic_link["symbolic"],
         get_relative(target_root, root_path=symbolic_link["source"]),
     )
+
+
+def convert_symbolic_link(
+    target_root: Path,
+    symbolic_link: SymbolicLink,
+) -> Path:
+    if is_relative(target_root, root_path=symbolic_link["source"]):
+        return _convert_root(target_root, symbolic_link)
+
+    return target_root
