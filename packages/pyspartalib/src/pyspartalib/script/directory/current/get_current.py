8b9b15ec756bb36f1f20f527ef583bbbd665209b
lyouta <lyouta@spartaproject.com>
2025-02-18 09:47:49 +0900

    Refactor: Remove unused functions. #165

--- packages/pyspartalib/src/pyspartalib/script/directory/current/get_current.py
+++ packages/pyspartalib/src/pyspartalib/script/directory/current/get_current.py
@@ -10,32 +10,8 @@ from pyspartalib.script.platform.terminal_status import get_terminal
 from pyspartalib.script.shell.execute_command import execute_single
 
 
-def _length_error(result: Sized, expected: int) -> None:
-    if len(result) != expected:
-        raise ValueError
-
-
 def _from_python() -> Path:
     return Path().cwd()
-
-
-def _get_shell_current() -> Strs:
-    return list(execute_single(["pwd"]))
-
-
-def _from_shell() -> Path:
-    result: Strs = _get_shell_current()
-
-    _length_error(result, 1)
-
-    return Path(result[0])
-
-
-def _confirm_symbolic() -> Path:
-    if get_terminal() == "test":
-        raise FileNotFoundError
-
-    return _from_shell()
 
 
 def get_current() -> Path:
