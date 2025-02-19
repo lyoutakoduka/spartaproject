8350123798b8915b0798d058ad7dd4441ee41041
lyouta <lyouta@spartaproject.com>
2025-02-20 06:07:54 +0900

    Feat: Raise error if status is fail. #165

--- packages/pyspartalib/tests/pyspartalib/script/inherit/test_inherit_with.py
+++ packages/pyspartalib/tests/pyspartalib/script/inherit/test_inherit_with.py
@@ -16,3 +16,8 @@ class TemporaryWith(InheritWith):
 def _difference_error(result: Type, expected: Type) -> None:
     if result != expected:
         raise ValueError
+
+
+def _status_error(status: bool) -> None:
+    if not status:
+        raise ValueError
