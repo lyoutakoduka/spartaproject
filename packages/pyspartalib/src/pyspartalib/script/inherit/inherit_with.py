b7d106e8dbae0be982361ddd70b5d3d236e7eb52
lyouta <lyouta@spartaproject.com>
2025-02-20 19:19:15 +0900

    Doc: Add module document strings. #165

--- packages/pyspartalib/src/pyspartalib/script/inherit/inherit_with.py
+++ packages/pyspartalib/src/pyspartalib/script/inherit/inherit_with.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
+"""Module to use With statement by using custom class."""
+
 from types import TracebackType
 from typing import Self
 
 
 class InheritWith:
+    """Class to use With statement by using custom class."""
+
     def exit(self) -> None:
         return
 
     def __enter__(self) -> Self:
+        """Return instance of itself from As statement.
+
+        Returns:
+            Self: Instance of the class.
+
+        """
         return self
 
     def __exit__(
@@ -17,6 +27,20 @@ class InheritWith:
         exception_value: BaseException | None = None,
         traceback_type: TracebackType | None = None,
     ) -> None:
+        """Call custom method when leaving from With statement.
+
+        Args:
+            exception_type (type[BaseException] | None, optional):
+                Defaults to None.
+                Exception type used for context manager.
+
+            exception_value (BaseException | None, optional): Defaults to None.
+                Exception used for context manager.
+
+            traceback_type (TracebackType | None, optional): Defaults to None.
+                Traceback type used for context manager.
+
+        """
         self.exit()
 
     def __init__(self) -> None:
