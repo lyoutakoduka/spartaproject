4aa725688aa9c18e8e41f9f52bcef8c6e6629e01
lyouta <lyouta@spartaproject.com>
2025-02-22 06:38:42 +0900

    Fix: Use pytest directly to check types. #165

--- packages/pyspartalib/tests/pyspartalib/script/bool/test_compare_value.py
+++ packages/pyspartalib/tests/pyspartalib/script/bool/test_compare_value.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
+import pytest
 from pyspartalib.script.bool.compare_value import (
     bool_compare_array,
     bool_compare_pair,
 )
-from tests.pyspartalib.interface.pytest import raises
 
 
 def test_size() -> None:
-    with raises(ValueError, match="size"):
+    with pytest.raises(ValueError, match="size"):
         bool_compare_array([True], [True, False])
 
 
 def test_empty() -> None:
-    with raises(ValueError, match="empty"):
+    with pytest.raises(ValueError, match="empty"):
         bool_compare_array([], [])
 
 
 def test_key() -> None:
-    with raises(KeyError, match="unmatch"):
+    with pytest.raises(KeyError, match="unmatch"):
         bool_compare_pair(
             {"R": False, "G": True, "B": True},
             {"R": True, "error": False, "B": True},
