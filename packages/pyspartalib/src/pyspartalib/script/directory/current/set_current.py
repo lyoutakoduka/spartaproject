267ee7538fe268daf3106d2fefc299215a963821
lyouta <lyouta@spartaproject.com>
2025-02-21 13:21:38 +0900

    Feat: Set temporary current directory. #165

--- packages/pyspartalib/src/pyspartalib/script/directory/current/set_current.py
+++ packages/pyspartalib/src/pyspartalib/script/directory/current/set_current.py
@@ -5,6 +5,17 @@
 from os import chdir
 from pathlib import Path
 
+from pyspartalib.script.directory.current.get_current import get_current
+from pyspartalib.script.inherit.inherit_with import InheritWith
+
+
+class SetCurrent(InheritWith):
+    def __initialize_variables(self) -> None:
+        self._backup_current_root = get_current()
+
+    def __init__(self) -> None:
+        self.__initialize_variables()
+
 
 def set_current(path: Path) -> None:
     """Set current working directory.
