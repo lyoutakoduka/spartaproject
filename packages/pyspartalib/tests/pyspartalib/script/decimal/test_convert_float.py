2e701c0ea3a83188445198728ac8051260037b8e
lyouta <lyouta@spartaproject.com>
2025-02-24 15:56:02 +0900

    Feat: Raise error if 2 values are different. #165

--- packages/pyspartalib/tests/pyspartalib/script/decimal/test_convert_float.py
+++ packages/pyspartalib/tests/pyspartalib/script/decimal/test_convert_float.py
@@ -4,9 +4,15 @@
 
 from decimal import Decimal
 
+from pyspartalib.context.custom.type_context import Type
 from pyspartalib.context.default.float_context import Floats
 from pyspartalib.context.extension.decimal_context import Decs
 from pyspartalib.script.decimal.convert_float import convert_float_array
+
+
+def _difference_error(result: Type, expected: Type) -> None:
+    if result != expected:
+        raise ValueError
 
 
 def _get_expected() -> Floats:
@@ -21,5 +27,7 @@ def test_array() -> None:
     """Test to convert array from type Decimal to type float."""
     expected: Floats = _get_expected()
 
-    if expected != convert_float_array(_convert_decimal(expected)):
-        raise ValueError
+    _difference_error(
+        convert_float_array(_convert_decimal(expected)),
+        expected,
+    )
