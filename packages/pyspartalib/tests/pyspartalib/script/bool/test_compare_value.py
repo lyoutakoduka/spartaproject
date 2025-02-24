19e4032dca04c7c166b149dead23fcc3af948959
lyouta <lyouta@spartaproject.com>
2025-02-24 15:57:55 +0900

    Feat: Raise error if status is fail. #165

--- packages/pyspartalib/tests/pyspartalib/script/bool/test_compare_value.py
+++ packages/pyspartalib/tests/pyspartalib/script/bool/test_compare_value.py
@@ -7,6 +7,11 @@ from pyspartalib.script.bool.compare_value import (
 )
 
 
+def _fail_error(status: bool) -> None:
+    if not status:
+        raise ValueError
+
+
 def test_size() -> None:
     with pytest.raises(ValueError, match="size"):
         bool_compare_array([True], [True, False])
@@ -26,13 +31,13 @@ def test_key() -> None:
 
 
 def test_array() -> None:
-    if not bool_compare_array([True, False], [True, False]):
-        raise ValueError
+    _fail_error(bool_compare_array([True, False], [True, False]))
 
 
 def test_pair() -> None:
-    if not bool_compare_pair(
-        {"R": True, "G": False, "B": True},
-        {"R": True, "G": False, "B": True},
-    ):
-        raise ValueError
+    _fail_error(
+        bool_compare_pair(
+            {"R": True, "G": False, "B": True},
+            {"R": True, "G": False, "B": True},
+        ),
+    )
