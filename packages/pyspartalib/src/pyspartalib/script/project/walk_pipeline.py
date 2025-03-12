cf489c02bd09438f5080dd9080a206087ea25c24
lyouta <lyouta@spartaproject.com>
2025-03-12 10:28:23 +0900

    Feat: Add entry point as method. #173

--- packages/pyspartalib/src/pyspartalib/script/project/walk_pipeline.py
+++ packages/pyspartalib/src/pyspartalib/script/project/walk_pipeline.py
@@ -7,5 +7,8 @@ class WalkPipeline(LogPipeline):
     def __initialize_super_class(self, enable_shown: bool) -> None:
         LogPipeline.__init__(self, enable_shown=enable_shown)
 
+    def launch_pipeline(self) -> None:
+        pass
+
     def __init__(self, enable_shown: bool) -> None:
         self.__initialize_super_class(enable_shown)
