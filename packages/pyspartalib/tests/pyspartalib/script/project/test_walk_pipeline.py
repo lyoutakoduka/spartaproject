7370ab0c44ab8c46b73523710143f06d4be8197a
lyouta <lyouta@spartaproject.com>
2025-03-12 11:02:30 +0900

    Feat: Method that is override entry point. #173

--- packages/pyspartalib/tests/pyspartalib/script/project/test_walk_pipeline.py
+++ packages/pyspartalib/tests/pyspartalib/script/project/test_walk_pipeline.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 from pyspartalib.script.project.walk_pipeline import WalkPipeline
+from pyspartalib.script.stdout.send_stdout import send_stdout
 
 
 class LaunchTest(WalkPipeline):
     def __initialize_super_class(self) -> None:
         super().__init__(True)
+
+    def launch_pipeline(self) -> None:
+        send_stdout("launch")
 
     def __init__(self) -> None:
         self.__initialize_super_class()
