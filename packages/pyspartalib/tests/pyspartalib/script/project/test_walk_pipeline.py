36b269a10abb6137a5fcf49243847a5050e65f51
lyouta <lyouta@spartaproject.com>
2025-03-12 11:16:49 +0900

    Feat: Reset timer functionality in module. #173

--- packages/pyspartalib/tests/pyspartalib/script/project/test_walk_pipeline.py
+++ packages/pyspartalib/tests/pyspartalib/script/project/test_walk_pipeline.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from pyspartalib.context.custom.type_context import Type
+from pyspartalib.script.project.log_pipeline import LogPipeline
 from pyspartalib.script.project.walk_pipeline import WalkPipeline
 from pyspartalib.script.stdout.send_stdout import send_stdout
 
@@ -27,3 +28,7 @@ def _get_expected_launch() -> str:
         launch
         0.0s: end
     """
+
+
+def _restart_timer(pipeline: LogPipeline) -> None:
+    pipeline.restart(override=True)
