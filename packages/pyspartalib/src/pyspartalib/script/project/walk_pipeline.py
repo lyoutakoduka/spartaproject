ddfe1275fe49251fb1f27430249543d25c73f28a
lyouta <lyouta@spartaproject.com>
2025-03-12 10:26:30 +0900

    Feat: Module to provide an entry point. #173

---
+++ packages/pyspartalib/src/pyspartalib/script/project/walk_pipeline.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from pyspartalib.script.project.log_pipeline import LogPipeline
+
+
+class WalkPipeline(LogPipeline):
+    def __initialize_super_class(self, enable_shown: bool) -> None:
+        LogPipeline.__init__(self, enable_shown=enable_shown)
+
+    def __init__(self, enable_shown: bool) -> None:
+        self.__initialize_super_class(enable_shown)
