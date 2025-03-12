fc6cf28fcd3fb1d2f4c93fe8c3c8abc4e64e0a9b
lyouta <lyouta@spartaproject.com>
2025-03-12 15:31:44 +0900

    Doc: Add module document strings. #173

--- packages/pyspartalib/src/pyspartalib/script/project/walk_pipeline.py
+++ packages/pyspartalib/src/pyspartalib/script/project/walk_pipeline.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+"""Module to iterate contents in a directory like walk module."""
+
 from pyspartalib.script.project.log_pipeline import LogPipeline
 
 
 class WalkPipeline(LogPipeline):
+    """Class to iterate contents in a directory like walk module."""
+
     def __initialize_super_class(self, enable_shown: bool) -> None:
         LogPipeline.__init__(self, enable_shown=enable_shown)
 
@@ -14,4 +18,12 @@ class WalkPipeline(LogPipeline):
         self.launch_pipeline()
 
     def __init__(self, enable_shown: bool) -> None:
+        """Initialize super class and variables.
+
+        Args:
+            enable_shown (bool, optional):
+                Log messages are shown if True.
+                It's used for argument "enable_shown" of class "LogPipeline".
+
+        """
         self.__initialize_super_class(enable_shown)
