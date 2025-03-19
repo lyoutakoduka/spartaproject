b874b2a7417315fcabc7327a35f53f7944c80fc6
lyouta <lyouta@spartaproject.com>
2025-03-19 18:05:10 +0900

    Feat: Get the root of temporary directory. #187

--- packages/pyspartalib/src/pyspartalib/script/directory/working/working_directory.py
+++ packages/pyspartalib/src/pyspartalib/script/directory/working/working_directory.py
@@ -6,3 +6,6 @@ from pathlib import Path
 class WorkingDirectory:
     def _set_working_root(self, working_root: Path) -> None:
         self._working_root = working_root
+
+    def get_working_root(self) -> Path:
+        return self._working_root
