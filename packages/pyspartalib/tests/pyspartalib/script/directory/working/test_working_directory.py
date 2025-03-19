7980985d5897d98bba5f7c0fa3fc450a3623ae10
lyouta <lyouta@spartaproject.com>
2025-03-19 22:14:37 +0900

    Feat: Raise error if input status is fail. #187

--- packages/pyspartalib/tests/pyspartalib/script/directory/working/test_working_directory.py
+++ packages/pyspartalib/tests/pyspartalib/script/directory/working/test_working_directory.py
@@ -6,3 +6,8 @@ from pyspartalib.context.custom.type_context import Type
 def _difference_error(result: Type, expected: Type) -> None:
     if expected != result:
         raise ValueError
+
+
+def _fail_error(status: bool) -> None:
+    if not status:
+        raise ValueError
