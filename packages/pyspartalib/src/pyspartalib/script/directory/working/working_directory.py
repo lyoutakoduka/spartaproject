0a37e743bb0cfd32380b5bdd37652a3f225e9388
lyouta <lyouta@spartaproject.com>
2025-03-19 22:01:29 +0900

    Feat: Keep working root and run the function. #187

--- packages/pyspartalib/src/pyspartalib/script/directory/working/working_directory.py
+++ packages/pyspartalib/src/pyspartalib/script/directory/working/working_directory.py
@@ -2,10 +2,20 @@
 
 from pathlib import Path
 
+from pyspartalib.context.custom.callable_context import BoolFunc
+
 
 class WorkingDirectory:
     def _set_working_root(self, working_root: Path) -> None:
         self._working_root = working_root
+
+    def _execute_function(
+        self,
+        function: BoolFunc,
+        working_root: Path,
+    ) -> bool:
+        self._set_working_root(working_root)
+        return function()
 
     def get_working_root(self) -> Path:
         return self._working_root
