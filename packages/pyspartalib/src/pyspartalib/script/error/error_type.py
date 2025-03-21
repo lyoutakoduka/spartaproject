bc28dc1768532e2584c9a11266644823ddb8b96e
lyouta <lyouta@spartaproject.com>
2025-03-21 14:14:02 +0900

    Feat: Raise a basic error. #189

--- packages/pyspartalib/src/pyspartalib/script/error/error_type.py
+++ packages/pyspartalib/src/pyspartalib/script/error/error_type.py
@@ -4,3 +4,6 @@
 class _Base:
     def _invert(self, result: bool, invert: bool) -> bool:
         return result ^ invert
+
+    def base_value(self, match: str) -> None:
+        raise ValueError(match)
