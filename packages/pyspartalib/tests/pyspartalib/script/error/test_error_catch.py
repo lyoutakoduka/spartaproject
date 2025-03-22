b8f5513c064ec56202aea3b4c6fe1fbce56da64b
lyouta <lyouta@spartaproject.com>
2025-03-23 06:07:55 +0900

    Feat: Test to catch ValueError. #191

--- packages/pyspartalib/tests/pyspartalib/script/error/test_error_catch.py
+++ packages/pyspartalib/tests/pyspartalib/script/error/test_error_catch.py
@@ -10,3 +10,6 @@ class TestValue(ErrorCatch, ErrorRaise):
 
     def _error_value(self) -> None:
         self.error_value(self._get_match())
+
+    def test_value(self) -> None:
+        self.catch_value(self._error_value, self._get_match())
