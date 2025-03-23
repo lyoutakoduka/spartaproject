7a02bcb52a9e48c17f9d96496218e989e513c2f6
lyouta <lyouta@spartaproject.com>
2025-03-24 02:36:30 +0900

    Update: Move the method to the new class. #191

--- packages/pyspartalib/src/pyspartalib/script/frame/stack_frame.py
+++ packages/pyspartalib/src/pyspartalib/script/frame/stack_frame.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from types import FrameType
 from typing import NoReturn
 
+from pyspartalib.script.error.error_raise import ErrorRaise
 from pyspartalib.script.frame.context.frame_context import (
     StackFrame,
     StackFrames,
@@ -18,42 +19,41 @@ def _raise_error(message: str) -> NoReturn:
     raise ValueError(message)
 
 
-def _get_stack_frame(outer_frame: FrameInfo) -> StackFrame:
-    return {
-        "file": Path(outer_frame.filename),
-        "function": outer_frame.function,
-        "line": outer_frame.lineno,
-    }
+class CurrentFrame(ErrorRaise):
+    def _get_stack_frame(self, outer_frame: FrameInfo) -> StackFrame:
+        return {
+            "file": Path(outer_frame.filename),
+            "function": outer_frame.function,
+            "line": outer_frame.lineno,
+        }
 
+    def _get_stack_frames(self, current_frame: FrameType) -> StackFrames:
+        return [
+            self._get_stack_frame(outer_frame)
+            for outer_frame in getouterframes(current_frame)
+        ]
 
-def _get_stack_frames(current_frame: FrameType) -> StackFrames:
-    return [
-        _get_stack_frame(outer_frame)
-        for outer_frame in getouterframes(current_frame)
-    ]
+    def _find_stack_frame_error(self) -> StackFrames:
+        if current_frame := currentframe():
+            return self._get_stack_frames(current_frame)
 
+        _raise_error("frame")
 
-def _find_stack_frame_error() -> StackFrames:
-    if current_frame := currentframe():
-        return _get_stack_frames(current_frame)
+    def _to_relative_path(self, frame: StackFrame) -> StackFrame:
+        frame["file"] = get_relative(frame["file"])
+        return frame
 
-    _raise_error("frame")
+    def current_frame(self, offset: int = 0) -> StackFrame:
+        """Get current frame information in stack frames.
 
+        Args:
+            offset (int, optional): Defaults to 0.
+                Index offset of stack frames based on current frame.
 
-def _to_relative_path(frame: StackFrame) -> StackFrame:
-    frame["file"] = get_relative(frame["file"])
-    return frame
+        Returns:
+            StackFrame: Selected current frame information.
 
-
-def current_frame(offset: int = 0) -> StackFrame:
-    """Get current frame information in stack frames.
-
-    Args:
-        offset (int, optional): Defaults to 0.
-            Index offset of stack frames based on current frame.
-
-    Returns:
-        StackFrame: Selected current frame information.
-
-    """
-    return _to_relative_path(_find_stack_frame_error()[2 + offset])
+        """
+        return self._to_relative_path(
+            self._find_stack_frame_error()[2 + offset],
+        )
