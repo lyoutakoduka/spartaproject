f531d9891070c95ae1c5ae5dc841e074529b733c
lyouta <lyouta@spartaproject.com>
2025-03-24 22:40:01 +0900

    Refactor: Use the method on the super class. #191

--- packages/pyspartalib/tests/pyspartalib/script/shell/test_execute_command.py
+++ packages/pyspartalib/tests/pyspartalib/script/shell/test_execute_command.py
@@ -10,7 +10,11 @@ from pyspartalib.context.default.string_context import Strs
 from pyspartalib.context.extension.path_context import PathFunc
 from pyspartalib.script.directory.create_directory import create_directory
 from pyspartalib.script.directory.current.set_current import SetCurrent
-from pyspartalib.script.error.error_raise import ErrorLength, ErrorNoExists
+from pyspartalib.script.error.error_raise import (
+    ErrorDifference,
+    ErrorLength,
+    ErrorNoExists,
+)
 from pyspartalib.script.shell.execute_command import ExecuteCommand
 
 
@@ -31,7 +35,7 @@ def _inside_temporary_directory(function: PathFunc) -> None:
         function(Path(temporary_path))
 
 
-class _TestShare(ErrorLength, ErrorNoExists):
+class _TestShare(ErrorLength, ErrorNoExists, ErrorDifference):
     def get_single_path(self, result: Strs) -> Path:
         self.error_length(result, 1, "share")
         path: Path = Path(result[0])
@@ -52,9 +56,10 @@ class TestSingle(_TestShare):
 
         def individual_test(temporary_root: Path) -> None:
             with SetCurrent(temporary_root):
-                _difference_error(
+                self.error_difference(
                     self.get_single_path(self._get_current()),
                     temporary_root,
+                    "single",
                 )
 
         _inside_temporary_directory(individual_test)
@@ -79,9 +84,10 @@ class TestMultiple(_TestShare):
             move_root: Path = create_directory(Path(temporary_root, "move"))
 
             with SetCurrent(temporary_root):
-                _difference_error(
+                self.error_difference(
                     self.get_single_path(self._move_and_get(move_root)),
                     move_root,
+                    "multiple",
                 )
 
         _inside_temporary_directory(individual_test)
