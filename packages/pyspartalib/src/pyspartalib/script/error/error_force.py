06c1f3fb586b67dd0f30addaa53d5e36a087edea
lyouta <lyouta@spartaproject.com>
2025-04-01 23:32:20 +0900

    Doc: Add strings of the module document. #192

--- packages/pyspartalib/src/pyspartalib/script/error/error_force.py
+++ packages/pyspartalib/src/pyspartalib/script/error/error_force.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+"""Module to send the signal to raise an errors forcibly."""
+
 from pyspartalib.context.default.string_context import Strs
 
 
 class ErrorForce:
+    """Class to send the signal to raise ana errors forcibly."""
+
     def __initialize_variables(self, error_types: Strs | None) -> None:
         self._error_types: Strs = self._set_fail_types(error_types)
 
@@ -14,7 +18,23 @@ class ErrorForce:
         return error_types
 
     def find_type(self, error_type: str) -> bool:
+        """Send the signal to raise an errors forcibly.
+
+        Args:
+            error_type (str): The error type you raise forcibly.
+
+        Returns:
+            bool: True if the candidates include the error type.
+
+        """
         return error_type in self._error_types
 
     def __init__(self, error_types: Strs | None = None) -> None:
+        """Initialize class variables.
+
+        Args:
+            error_types (Strs | None, optional): Defaults to None.
+                The candidates of error type you raise forcibly.
+
+        """
         self.__initialize_variables(error_types)
