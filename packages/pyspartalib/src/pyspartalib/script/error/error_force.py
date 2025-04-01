389b211ffbdd7444bdf31d00228f338b19d65443
lyouta <lyouta@spartaproject.com>
2025-04-01 13:20:14 +0900

    Feat: Initialize the class variable. #192

--- packages/pyspartalib/src/pyspartalib/script/error/error_force.py
+++ packages/pyspartalib/src/pyspartalib/script/error/error_force.py
@@ -4,8 +4,14 @@ from pyspartalib.context.default.string_context import Strs
 
 
 class ErrorForce:
+    def __initialize_variables(self, error_types: Strs | None) -> None:
+        self._error_types: Strs = self._set_fail_types(error_types)
+
     def _set_fail_types(self, error_types: Strs | None) -> Strs:
         if error_types is None:
             return []
 
         return error_types
+
+    def __init__(self, error_types: Strs | None = None) -> None:
+        self.__initialize_variables(error_types)
