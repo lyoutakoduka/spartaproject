1bf16ccff2b9bbcbb79907bc7f7c82280d8c535e
lyouta <lyouta@spartaproject.com>
2025-04-01 13:45:28 +0900

    Feat: Reproduces an external function. #192

--- packages/pyspartalib/tests/pyspartalib/script/error/test_error_force.py
+++ packages/pyspartalib/tests/pyspartalib/script/error/test_error_force.py
@@ -8,5 +8,8 @@ class _TestForce(ErrorForce):
     def __initialize_super_class(self, error_types: Strs | None) -> None:
         ErrorForce.__init__(self, error_types=error_types)
 
+    def _outside_process(self) -> str | None:
+        return "success"
+
     def __init__(self, error_types: Strs | None = None) -> None:
         self.__initialize_super_class(error_types)
