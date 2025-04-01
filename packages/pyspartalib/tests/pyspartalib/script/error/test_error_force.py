286e02fcb303fec18753e4ad61d8d2b8345cd51e
lyouta <lyouta@spartaproject.com>
2025-04-01 13:41:33 +0900

    Feat: Initialize super class with arguments. #192

--- packages/pyspartalib/tests/pyspartalib/script/error/test_error_force.py
+++ packages/pyspartalib/tests/pyspartalib/script/error/test_error_force.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+from pyspartalib.context.default.string_context import Strs
+from pyspartalib.script.error.error_force import ErrorForce
 
-class _TestForce:
-    def __init__(self) -> None:
-        pass
+
+class _TestForce(ErrorForce):
+    def __initialize_super_class(self, error_types: Strs | None) -> None:
+        ErrorForce.__init__(self, error_types=error_types)
+
+    def __init__(self, error_types: Strs | None = None) -> None:
+        self.__initialize_super_class(error_types)
