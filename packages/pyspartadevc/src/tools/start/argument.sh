456fa4fde8c872e3aeb3e38d3bd940f660d48af0
lyouta <lyouta@spartaproject.com>
2025-05-11 03:58:40 +0900

    Feat: Show help text used for dev-container. #201

---
+++ packages/pyspartadevc/src/tools/start/argument.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+root="$(dirname "$0")"
+
+source "$root/share/log.sh"
+
+usage() {
+    cat <<EOF
+Usage: command [<options>...]
+
+    Options:
+        -c  Create devcontainer
+            Type: boolean
+            Default: false
+
+        -a  Attach to devcontainer
+            Type: boolean
+            Default: false
+
+        -h  Show help
+            Type: boolean
+            Default: false
+
+EOF
+}
