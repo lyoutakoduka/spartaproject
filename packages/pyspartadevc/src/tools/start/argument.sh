ecaf586a25182007515a47f09fd435f764710b73
lyouta <lyouta@spartaproject.com>
2025-05-11 10:43:46 +0900

    Feat: Confirm the three types of arguments. #201

--- packages/pyspartadevc/src/tools/start/argument.sh
+++ packages/pyspartadevc/src/tools/start/argument.sh
@@ -28,3 +28,31 @@ usage_error() {
     show_error 'Invalid option. See help "-h"'
     usage
 }
+
+select_arguments() {
+    local create=""
+    local attach=""
+
+    while getopts "cah" opt; do
+        case "$opt" in
+        c)
+            create="true"
+            ;;
+        a)
+            attach="true"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        \?)
+            usage_error
+            exit 1
+            ;;
+        esac
+    done
+
+    shift $((OPTIND - 1))
+
+    echo "$create,$attach"
+}
