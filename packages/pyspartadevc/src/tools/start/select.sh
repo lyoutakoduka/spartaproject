f897e605b775c05983a820ea9fbc2ebdc492afa3
lyouta <lyouta@spartaproject.com>
2025-05-15 06:35:01 +0900

    Feat: Shared processing before executing. #201

--- packages/pyspartadevc/src/tools/start/select.sh
+++ packages/pyspartadevc/src/tools/start/select.sh
@@ -3,6 +3,8 @@
 root="$(dirname "$0")"
 
 source "$root/start/argument.sh"
+source "$root/share/environment.sh"
+source "$root/start/user.sh"
 
 select_invalid() {
     usage_error
@@ -12,4 +14,9 @@ select_invalid() {
 select_help() {
     usage
     exit 0
+}
+
+shared_process() {
+    set_environment "DEVC_PROJECT" "$1"
+    filter_root
 }
