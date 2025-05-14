18101121e046b198371d9e068a1db2825b8e6b3e
lyouta <lyouta@spartaproject.com>
2025-05-14 13:29:14 +0900

    Update: Record user ID if not default user. #201

--- packages/pyspartadevc/src/tools/start/user.sh
+++ packages/pyspartadevc/src/tools/start/user.sh
@@ -11,3 +11,23 @@ filter_root() {
         exit 1
     fi
 }
+
+set_user() {
+    local identifier="$(id --user)"
+
+    local user_name=""
+    local user_identifier=""
+    local group_identifier=""
+
+    if [ "$identifier" -ne 1000 ]; then
+        show_log "Executed by the none-default user."
+
+        user_name="$(whoami)"
+        user_identifier="$identifier"
+        group_identifier="$(id --group)"
+    fi
+
+    set_environment "DEVC_USER" "$user_name"
+    set_environment "DEVC_UID" "$user_identifier"
+    set_environment "DEVC_GID" "$group_identifier"
+}
