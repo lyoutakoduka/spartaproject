d8648d6e91ce8d1939043a92f59b549cae97a765
lyouta <lyouta@spartaproject.com>
2025-05-16 00:21:08 +0900

    Update: Owner of the dev-container volumes. #201

--- packages/pyspartadevc/src/tools/lifecycle/after.sh
+++ packages/pyspartadevc/src/tools/lifecycle/after.sh
@@ -3,9 +3,12 @@
 root="$(dirname "$0")"
 
 source "$root/share/environment.sh"
+source "$root/lifecycle/owner.sh"
 source "$root/lifecycle/sync.sh"
 
 after_create() {
+    change_path_owner
+
     sync_yarn
     sync_uv "$(get_environment "DEVC_PROJECT")"
 }
