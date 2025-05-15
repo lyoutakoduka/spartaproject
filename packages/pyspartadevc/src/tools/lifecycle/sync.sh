1b1510ae6817a81240b77f9c2055f694b7507b1d
lyouta <lyouta@spartaproject.com>
2025-05-15 18:44:09 +0900

    Refactor: Remove unnecessary line break. #201

--- packages/pyspartadevc/src/tools/lifecycle/sync.sh
+++ packages/pyspartadevc/src/tools/lifecycle/sync.sh
@@ -11,16 +11,12 @@ change_own() {
 
 sync_yarn() {
     show_begin "Sync JavaScript package manager Yarn."
-
     yarn
-
     show_end
 }
 
 sync_uv() {
     show_begin "Sync Python package manager UV."
-
     uv sync --group "$1"
-
     show_end
 }
