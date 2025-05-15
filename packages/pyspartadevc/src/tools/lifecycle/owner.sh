716dc6cfe77400f20dee9e497faef5936006e0a9
lyouta <lyouta@spartaproject.com>
2025-05-16 00:20:01 +0900

    Feat: Change owner to non-root user. #201

--- packages/pyspartadevc/src/tools/lifecycle/owner.sh
+++ packages/pyspartadevc/src/tools/lifecycle/owner.sh
@@ -4,3 +4,13 @@ change_owner() {
     local name="$(whoami)"
     sudo chown "$name":"$name" "$1"
 }
+
+change_path_owner() {
+    show_log "Change directory owner to non-root user."
+
+    local paths=(".temp/cache" ".venv" "node_modules")
+
+    for path in "${paths[@]}"; do
+        change_owner "$path"
+    done
+}
