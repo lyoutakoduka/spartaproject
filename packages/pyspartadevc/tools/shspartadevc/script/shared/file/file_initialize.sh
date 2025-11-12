df9649f89d0d639c9cb27cf3eac203d0670b51fc
lyouta <lyouta@spartaproject.com>
2025-11-13 00:43:06 +0900

    Feat: Add shebang string to the script file. #222

---
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/file/file_initialize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. packages/pyspartadevc/tools/shspartadevc/script/shared/constant/get_constant.sh
+. packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
+
+initialize_text_file() (
+    declare -r _header="$1"
+    declare -r _shebang=$(constant::script_shebang)
+
+    _main() {
+        export_line "${_shebang}" "${_header}"
+    }
+
+    _main
+)
