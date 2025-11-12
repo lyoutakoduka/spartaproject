3540cec0bc798f9380333687a146ab57a1b9b81b
lyouta <lyouta@spartaproject.com>
2025-11-12 22:56:59 +0900

    Feat: Add environment variable about user. #222

---
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_user.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+. packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant_environment.sh
+. packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant.sh
+. packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_create.sh
+
+set_user_identifier() (
+    declare -r _status="$1"
+    declare -r -i _identifier="$2"
+    declare -r _user_key=$(constant::user_key)
+    declare -r _empty=$(constant::empty)
+    declare -r _success=$(constant::status_success)
+
+    _get_user_value() {
+        declare number="${_empty}"
+
+        if [[ "${_status}" = "${_success}" ]]; then
+            number="${_identifier}"
+        fi
+
+        echo "${number}"
+    }
+
+    _main() {
+        declare -r user_value=$(_get_user_value)
+        set_environment "${_user_key}" "${user_value}"
+    }
+
+    _main
+)
