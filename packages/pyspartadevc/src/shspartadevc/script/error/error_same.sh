358f3104ea3d89a85fb712dce94527b5c02fd077
lyouta <lyouta@spartaproject.com>
2025-11-15 02:40:42 +0900

    Feat: Raise error if values are same. #226

---
+++ packages/pyspartadevc/src/shspartadevc/script/error/error_same.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+error_same() (
+    declare -r _status=1
+    declare -r _result="$1"
+    declare -r _expected="$2"
+
+    _main() {
+        if [[ "${_result}" == "${_expected}" ]]; then
+            exit "${_status}"
+        fi
+    }
+
+    _main
+)
