93ad61697c0b482e389be172cc7f6f3983a0a5f9
lyouta <lyouta@spartaproject.com>
2025-11-17 03:01:13 +0900

    Refactor: Use exit status number directly. #233

--- packages/pyspartadevc/src/shspartadevc/script/error/error_same.sh
+++ packages/pyspartadevc/src/shspartadevc/script/error/error_same.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 error_same() {
-    declare -r _status=1
     declare -r _result="$1"
     declare -r _expected="$2"
 
     _main() {
         if [[ "${_result}" == "${_expected}" ]]; then
-            exit "${_status}"
+            exit 1
         fi
     }
 
