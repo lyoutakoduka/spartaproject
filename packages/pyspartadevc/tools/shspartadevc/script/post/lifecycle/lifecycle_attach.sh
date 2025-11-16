b72093a35ace9e359a1c00b39aa96bf72e0952f8
lyouta <lyouta@spartaproject.com>
2025-11-17 08:00:37 +0900

    Refactor: Remove unused module importing. #239

--- packages/pyspartadevc/tools/shspartadevc/script/post/lifecycle/lifecycle_attach.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/post/lifecycle/lifecycle_attach.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-. packages/pyspartadevc/tools/shspartadevc/script/post/constant/get_constant_command.sh
 . packages/pyspartadevc/tools/shspartadevc/script/post/constant/get_constant_message.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/show/show_log.sh
 
