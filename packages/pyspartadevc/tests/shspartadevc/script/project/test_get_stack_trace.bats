734319e839041e5f7566547a519822a07ca346a4
lyouta <lyouta@spartaproject.com>
2025-11-17 11:43:40 +0900

    Refactor: Remove unused functions. #241

--- packages/pyspartadevc/tests/shspartadevc/script/project/test_get_stack_trace.bats
+++ packages/pyspartadevc/tests/shspartadevc/script/project/test_get_stack_trace.bats
@@ -3,16 +3,6 @@
 . packages/pyspartadevc/src/shspartadevc/script/bats/confirm_status.sh
 . packages/pyspartadevc/tests/shspartadevc/script/project/test_get_stack_trace.sh
 
-_confirm_status() {
-    declare -r _expected=0
-    [[ "${status}" -eq "${_expected}" ]]
-}
-
-@test "test_get_stack_trace" {
-    run test
-    _confirm_status
-}
-
 @test "test_base_path" {
     run test_base_path
     shell::confirm_success "${status}"
--- packages/pyspartadevc/tests/shspartadevc/script/project/test_get_stack_trace.sh
+++ packages/pyspartadevc/tests/shspartadevc/script/project/test_get_stack_trace.sh
@@ -3,51 +3,6 @@
 . packages/pyspartadevc/src/shspartadevc/script/error/error_difference.sh
 . packages/pyspartadevc/src/shspartadevc/script/project/get_stack_trace.sh
 
-test() (
-    declare -r -i _offset=-1
-    declare -r _group_path="path"
-    declare -r _group_name="name"
-    declare -r _expected_path="test_get_stack_trace.sh"
-    declare -r _expected_name="_test_base_name"
-    declare -r _expected_path_offset="get_stack_trace.sh"
-    declare -r _expected_name_offset="get_selected_frame"
-
-    _test_base_path() {
-        declare -r result=$(get_selected_frame "${_group_path}")
-        declare -r parent_root=$(basename "${result}")
-
-        shell::error_difference "${parent_root}" "${_expected_path}"
-    }
-
-    _test_base_name() {
-        declare -r result=$(get_selected_frame "${_group_name}")
-
-        shell::error_difference "${result}" "${_expected_name}"
-    }
-
-    _test_offset_path() {
-        declare -r result=$(get_selected_frame "${_group_path}" "${_offset}")
-        declare -r parent_root=$(basename "${result}")
-
-        shell::error_difference "${parent_root}" "${_expected_path_offset}"
-    }
-
-    _test_offset_name() {
-        declare -r result=$(get_selected_frame "${_group_name}" "${_offset}")
-
-        shell::error_difference "${result}" "${_expected_name_offset}"
-    }
-
-    _main() {
-        _test_base_path
-        _test_base_name
-        _test_offset_path
-        _test_offset_name
-    }
-
-    _main
-)
-
 test_base_path() (
     declare -r _group="path"
     declare -r _expected="test_get_stack_trace.sh"
