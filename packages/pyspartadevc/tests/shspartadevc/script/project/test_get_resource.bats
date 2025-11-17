6b58e0ea7e035781f99c298e9a55c38703750c52
lyouta <lyouta@spartaproject.com>
2025-11-17 13:07:31 +0900

    Refactor: Remove unused functions. #241

--- packages/pyspartadevc/tests/shspartadevc/script/project/test_get_resource.bats
+++ packages/pyspartadevc/tests/shspartadevc/script/project/test_get_resource.bats
@@ -3,16 +3,6 @@
 . packages/pyspartadevc/src/shspartadevc/script/bats/confirm_status.sh
 . packages/pyspartadevc/tests/shspartadevc/script/project/test_get_resource.sh
 
-_confirm_status() {
-    declare -r _expected=0
-    [[ "${status}" -eq "${_expected}" ]]
-}
-
-@test "test_get_resource" {
-    run test
-    _confirm_status
-}
-
 @test "test_base" {
     run test_base
     shell::confirm_success "${status}"
--- packages/pyspartadevc/tests/shspartadevc/script/project/test_get_resource.sh
+++ packages/pyspartadevc/tests/shspartadevc/script/project/test_get_resource.sh
@@ -4,46 +4,6 @@
 . packages/pyspartadevc/src/shspartadevc/script/project/get_resource.sh
 . packages/pyspartadevc/src/shspartadevc/script/project/get_stack_trace.sh
 
-test() (
-    declare -r _group="path"
-    declare -r _local_path="root/head.json"
-    declare -r _root_main="packages/pyspartadevc/tests/shspartadevc"
-    declare -r _root_sub="script/project/resource"
-
-    _get_resource_root() {
-        echo "${_root_main}/${_root_sub}"
-    }
-
-    _get_executed_path() {
-        declare -r executed_path=$(get_selected_frame "${_group}")
-        echo "${executed_path}"
-    }
-
-    test_base() {
-        declare -r executed_path=$(_get_executed_path)
-        declare -r result=$(get_resource "${executed_path}")
-        declare -r resource_root=$(_get_resource_root)
-
-        shell::error_difference "${result}" "${resource_root}"
-    }
-
-    test_local() {
-        declare -r executed_path=$(_get_executed_path)
-        declare -r result=$(get_resource "${executed_path}" "${_local_path}")
-        declare -r resource_root=$(_get_resource_root)
-        declare -r expected="${resource_root}/${_local_path}"
-
-        shell::error_difference "${result}" "${expected}"
-    }
-
-    _main() {
-        test_base
-        test_local
-    }
-
-    _main
-)
-
 _get_resource_root() (
     declare -r _root_main="packages/pyspartadevc"
     declare -r _root_sub="tests/shspartadevc"
