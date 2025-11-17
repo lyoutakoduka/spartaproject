66113ec3cd2eb3b79ed2977332eff1de0a6ae50f
lyouta <lyouta@spartaproject.com>
2025-11-18 05:09:44 +0900

    Feat: Get the file path from stack frames. #249

--- packages/pyspartadevc/tests/shspartadevc/script/constant/test_constant_import.sh
+++ packages/pyspartadevc/tests/shspartadevc/script/constant/test_constant_import.sh
@@ -4,13 +4,19 @@
 . packages/pyspartadevc/src/shspartadevc/script/project/get_stack_trace.sh
 . packages/pyspartadevc/src/shspartadevc/script/error/error_difference.sh
 
-test_import() (
+_test_get_frame_path() (
     declare -r _group="path"
+
+    declare -r executed=$(get_selected_frame "${_group}")
+    echo "${executed}"
+)
+
+test_import() (
     declare -r _trim_filter="\n "
     declare -r _expected='{"fruits":{"red":"apple"}}'
 
     _main() {
-        declare -r executed=$(get_selected_frame "${_group}")
+        declare -r executed=$(_test_get_frame_path)
         declare -r result=$(import_constant "${executed}" "${_trim_filter}")
 
         shell::error_difference "${result}" "${_expected}"
