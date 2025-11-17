2a496e48e0a9f067d787d927e9ca3cc129800cc1
lyouta <lyouta@spartaproject.com>
2025-11-17 21:56:31 +0900

    Fix: Reconnect updated function. export. #245

--- packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
@@ -6,12 +6,12 @@ export_line() (
     declare -r _text="$1"
 
     _add_text() {
-        declare -r path=$(get_file_path)
+        declare -r path=$(shell::get_file_path)
         echo "${_text}" >>"${path}"
     }
 
     _main() {
-        declare -r path=$(get_file_path)
+        declare -r path=$(shell::get_file_path)
 
         if [[ -n "${path}" ]]; then
             _add_text
