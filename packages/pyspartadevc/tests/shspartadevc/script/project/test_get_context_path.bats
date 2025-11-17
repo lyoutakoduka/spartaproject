09e0472de309fa7db4930c9b2b57eb34478570c6
lyouta <lyouta@spartaproject.com>
2025-11-17 14:33:16 +0900

    Refactor: Remove unused functions. #241

--- packages/pyspartadevc/tests/shspartadevc/script/project/test_get_context_path.bats
+++ packages/pyspartadevc/tests/shspartadevc/script/project/test_get_context_path.bats
@@ -3,16 +3,6 @@
 . packages/pyspartadevc/src/shspartadevc/script/bats/confirm_status.sh
 . packages/pyspartadevc/tests/shspartadevc/script/project/test_get_context_path.sh
 
-_confirm_status() {
-    declare -r _expected=0
-    [[ "${status}" -eq "${_expected}" ]]
-}
-
-@test "test_get_context_path" {
-    run test
-    _confirm_status
-}
-
 @test "test_base" {
     run test_base
     shell::confirm_success "${status}"
--- packages/pyspartadevc/tests/shspartadevc/script/project/test_get_context_path.sh
+++ packages/pyspartadevc/tests/shspartadevc/script/project/test_get_context_path.sh
@@ -4,51 +4,6 @@
 . packages/pyspartadevc/src/shspartadevc/script/project/get_context_path.sh
 . packages/pyspartadevc/src/shspartadevc/script/project/get_stack_trace.sh
 
-test() (
-    declare -r _group="path"
-    declare -r _local_path="context.json"
-    declare -r _root_main="packages/pyspartadevc/tests/shspartadevc"
-    declare -r _root_sub="script/project/resource/context.json"
-
-    _get_context_path() {
-        echo "${_root_main}/${_root_sub}"
-    }
-
-    _confirm_result() {
-        declare -r result="$1"
-
-        declare -r context_path=$(_get_context_path)
-
-        shell::error_difference "${result}" "${context_path}"
-    }
-
-    _get_executed_path() {
-        declare -r executed=$(get_selected_frame "${_group}")
-        echo "${executed}"
-    }
-
-    _test_base() {
-        declare -r executed=$(_get_executed_path)
-        declare -r result=$(get_context_path "${executed}" "${_local_path}")
-
-        _confirm_result "${result}"
-    }
-
-    _test_forward() {
-        declare -r executed=$(_get_executed_path)
-        declare -r result=$(get_context_path "${executed}")
-
-        _confirm_result "${result}"
-    }
-
-    _main() {
-        _test_base
-        _test_forward
-    }
-
-    _main
-)
-
 _confirm_result() (
     declare -r result="$1"
     declare -r _root_main="packages/pyspartadevc/tests/shspartadevc"
