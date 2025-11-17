894ecb84cec9836c3cb7bf6843b7c312df650514
lyouta <lyouta@spartaproject.com>
2025-11-18 07:18:02 +0900

    Refactor: Add a docstring to the function. #228

--- packages/pyspartadevc/src/shspartadevc/script/constant/constant_filter.sh
+++ packages/pyspartadevc/src/shspartadevc/script/constant/constant_filter.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#*  Args:
+#*      $1 (string): Json formatted text.
+#*
+#*      $2 (string): Group key used to filter the text.
+#*
+#*  Return:
+#*      (string) Json formatted text filtered by group key.
+#*
 filter_constant() (
     declare -r _text_json="$1"
     declare -r _filter="$2"
