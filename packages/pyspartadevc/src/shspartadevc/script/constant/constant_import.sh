dd938a80f0476c1c8459bd589b95d907236ccec4
lyouta <lyouta@spartaproject.com>
2025-11-18 04:50:35 +0900

    Feat: Import the json file and filter text. #249

---
+++ packages/pyspartadevc/src/shspartadevc/script/constant/constant_import.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+. packages/pyspartadevc/src/shspartadevc/script/project/get_context_path.sh
+
+import_constant() (
+    declare -r _executed="$1"
+    declare -r _trim_filter="$2"
+
+    _get_context_path() {
+        declare -r context_path=$(get_context_path "${_executed}")
+        echo "${context_path}"
+    }
+
+    _filter_text() {
+        declare -r text_json="$1"
+
+        declare -r filtered=$(echo "${text_json}" | tr -d "${_trim_filter}")
+        echo "${filtered}"
+    }
+
+    _main() {
+        declare -r context_path=$(_get_context_path)
+        declare -r text_json=$(cat "${context_path}")
+        declare -r result=$(_filter_text "${text_json}")
+
+        echo "${result}"
+    }
+
+    declare -r result=$(_main)
+    echo "${result}"
+)
