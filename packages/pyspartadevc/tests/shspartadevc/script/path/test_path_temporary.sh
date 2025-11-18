77acdd745aa8dbee8b33ec525302f2952c38c680
lyouta <lyouta@spartaproject.com>
2025-11-19 06:58:44 +0900

    Feat: Test to create a temporary file. #259

--- packages/pyspartadevc/tests/shspartadevc/script/path/test_path_temporary.sh
+++ packages/pyspartadevc/tests/shspartadevc/script/path/test_path_temporary.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+. packages/pyspartadevc/src/shspartadevc/script/error/error_exists.sh
 . packages/pyspartadevc/src/shspartadevc/script/path/path_handle.sh
+. packages/pyspartadevc/src/shspartadevc/script/path/path_temporary.sh
 
 _cleanup_temporary_root() (
     declare -r _temporary="$1"
@@ -8,3 +10,22 @@ _cleanup_temporary_root() (
     declare -r parent=$(dirname "${_temporary}")
     end_temporary_root "${parent}"
 )
+
+test_file() (
+    declare -r _group_root="test_temporary/file"
+    declare -r _path_head="path_test"
+    declare -r _suffix="txt"
+
+    _main() {
+        declare -r temporary=$(
+            get_temporary_file "${_group_root}" "${_path_head}" "${_suffix}"
+        )
+
+        shell::error_no_exists "${temporary}"
+        _cleanup_temporary_root "${temporary}"
+    }
+
+    _main
+)
+
+"$@"
