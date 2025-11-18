811551a1f27a006116b0d9c954a1f49598d22d4f
lyouta <lyouta@spartaproject.com>
2025-11-19 01:03:20 +0900

    Refactor: Add docstring to the functions. #253

--- packages/pyspartadevc/src/shspartadevc/script/error/error_difference.sh
+++ packages/pyspartadevc/src/shspartadevc/script/error/error_difference.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#*  Args:
+#*      $1 (string): The value of test result.
+#*
+#*      $2 (string): Expected value for test.
+#*
+#*  Error:
+#*      exit 1, if two values are different.
+#*
 shell::error_difference() {
     declare -r _result="$1"
     declare -r _expected="$2"
@@ -9,6 +17,14 @@ shell::error_difference() {
     fi
 }
 
+#*  Args:
+#*      $1 (string): The value of test result.
+#*
+#*      $2 (string): Expected value for test.
+#*
+#*  Error:
+#*      exit 1, if two values are different.
+#*
 shell::error_same() {
     declare -r _result="$1"
     declare -r _expected="$2"
