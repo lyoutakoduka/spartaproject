b0f327ea4bcac46512bacd8459bdf35341f2677a
lyouta <lyouta@spartaproject.com>
2025-11-20 01:23:42 +0900

    Refactor: Remove the redundant function. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/string/string_assign.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/string/string_assign.sh
@@ -4,10 +4,5 @@ create_assign() (
     declare -r _key="$1"
     declare -r _value="$2"
 
-    _main() {
-        echo "${_key}=${_value}"
-    }
-
-    declare -r result=$(_main)
-    echo "${result}"
+    echo "${_key}=${_value}"
 )
