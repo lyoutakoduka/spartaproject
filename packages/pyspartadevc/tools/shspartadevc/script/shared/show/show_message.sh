d48a6a3c0fea8a4891f75bbc23fc28b20af59ff5
lyouta <lyouta@spartaproject.com>
2025-11-20 04:53:22 +0900

    Refactor: Remove the redundant function. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/show/show_message.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/show/show_message.sh
@@ -9,24 +9,6 @@ show_message() (
     declare -r _message="$2"
     declare -r _package="pyspartadevc"
 
-    _get_message_inside() {
-        declare -r text=$(create_pair "${_package}" "${_group}")
-
-        echo "${text}"
-    }
-
-    _get_message_pair() {
-        declare -r message_inside=$(_get_message_inside)
-        declare -r text=$(add_bracket "${message_inside}")
-
-        echo "${text}"
-    }
-
-    _main() {
-        declare -r message_pair=$(_get_message_pair)
-        echo "${message_pair} ${_message}"
-    }
-
-    declare -r result=$(_main)
-    echo "${result}"
+    declare -r _message_inside=$(create_pair "${_package}" "${_group}")
+    echo "[${_message_inside}] ${_message}"
 )
