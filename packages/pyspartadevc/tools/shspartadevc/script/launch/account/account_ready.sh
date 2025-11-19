fb3eed231aec1b9e93730e3ecfebf2373ef7a542
lyouta <lyouta@spartaproject.com>
2025-11-20 02:06:29 +0900

    Update: Un-use the some outside modules. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/account/account_ready.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/account/account_ready.sh
@@ -10,13 +10,34 @@
 
 ready_identifier() (
     declare -r -i _expected=$(constant::expected_identifier)
+    declare -r _identifier=$(constant::message_identifier)
+    declare -r _comment=$(constant::header_environment)
+    declare -r _name_key=$(constant::name_key)
+    declare -r _user_key=$(constant::user_key)
+    declare -r _identifier_key=$(constant::group_key)
+
+    _set_user_name() (
+        declare -r _user_name=$(whoami)
+        set_environment "${_name_key}" "${_user_name}"
+    )
+
+    _set_user_identifier() (
+        declare -r _user_value=$(id --user)
+        set_environment "${_user_key}" "${_user_value}"
+    )
+
+    _set_group_identifier() (
+        declare -r identifier=$(id --group)
+        set_environment "${_identifier_key}" "${identifier}"
+    )
 
     _set_user_information() {
-        show_identifier
-        environment_comment
-        set_user_name
-        set_user_identifier
-        set_group_identifier
+        show_log "${_identifier}"
+        export_line "${_comment}"
+
+        _set_user_name
+        _set_user_identifier
+        _set_group_identifier
     }
 
     _main() {
