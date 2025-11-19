d1e5e38a5d1d882bd7b8b08f1854e874afd3d313
lyouta <lyouta@spartaproject.com>
2025-11-20 06:39:07 +0900

    Refactor: Remove the unused functions. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/constant/get_constant.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/constant/get_constant.sh
@@ -4,22 +4,6 @@ constant::package_main() {
     echo "pyspartadevc"
 }
 
-constant::package_sub() {
-    echo "shspartadevc"
-}
-
-constant::script_shebang() {
-    echo "#!/bin/bash"
-}
-
 constant::indent() {
     echo "    "
-}
-
-constant::group_log() {
-    echo "log"
-}
-
-constant::group_error() {
-    echo "error"
 }
