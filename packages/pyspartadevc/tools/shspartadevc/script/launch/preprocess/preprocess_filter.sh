60c10dd08b34485b6dee9885dd835ba315fcff2e
lyouta <lyouta@spartaproject.com>
2025-11-20 08:49:25 +0900

    Feat: Filter process by account information. #263

---
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_filter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant.sh
+. packages/pyspartadevc/tools/shspartadevc/script/shared/show/show_message.sh
+
+filter_by_account() (
+    declare -r _expected=$(constant::expected_name)
+    declare -r _message=$(constant::message_user)
+
+    declare -r user_name=$(whoami)
+
+    if [[ "${user_name}" = "${_expected}" ]]; then
+        shell::show_warning "${_message}"
+    fi
+)
