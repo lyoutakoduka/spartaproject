53686d0559eef38062984adaf33647782e5087c6
lyouta <lyouta@spartaproject.com>
2025-11-20 07:31:12 +0900

    Update: Un-use the outside function. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_create.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_create.sh
@@ -2,18 +2,19 @@
 
 . packages/pyspartadevc/src/shspartadevc/script/string/string_quoted.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant.sh
-. packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_export.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_status.sh
+. packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
 
 set_environment() (
     declare -r _quote="\""
+    declare -r _command="export"
     declare -r _expected="true"
     declare -r _key="$1"
     declare -r _value="$2"
 
     _create_environment() {
         declare -r quote_added=$(string_quoted "${_value}" "${_quote}")
-        export_environment "${_key}" "${quote_added}"
+        export_lines "${_command} ${_key}=${quote_added}"
     }
 
     _main() {
