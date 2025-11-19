f5cb717031b8e1abd6676b14db719c84e46dd1ff
lyouta <lyouta@spartaproject.com>
2025-11-20 03:39:41 +0900

    Feat: Path of the temporary launcher script. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant_path.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant_path.sh
@@ -7,3 +7,15 @@ constant::config() {
 constant::current() {
     echo "."
 }
+
+constant::temporary_create() {
+    echo "packages/pyspartadevc/tools/shspartadevc/.temp/devcontainer_create.sh"
+}
+
+constant::temporary_attach() {
+    echo "packages/pyspartadevc/tools/shspartadevc/.temp/devcontainer_attach.sh"
+}
+
+constant::temporary_post() {
+    echo "packages/pyspartadevc/tools/shspartadevc/.temp/devcontainer_post.sh"
+}
