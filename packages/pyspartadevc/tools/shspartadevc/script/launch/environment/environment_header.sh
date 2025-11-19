bc5ecff2d9ac1ab4c98cfe058b9c970c9c61bc70
lyouta <lyouta@spartaproject.com>
2025-11-20 02:07:05 +0900

    Refactor: Remove the unused module. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_header.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
-
-environment_comment() (
-    declare -r _comment=$(constant::header_environment)
-
-    export_line "${_comment}"
-)
