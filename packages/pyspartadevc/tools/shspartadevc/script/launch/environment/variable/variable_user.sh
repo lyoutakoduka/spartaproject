61e8e704e494f9ad9c0447df5d3854f3d2df168e
lyouta <lyouta@spartaproject.com>
2025-11-20 01:40:20 +0900

    Refactor: Remove unused module importing. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/environment/variable/variable_user.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/environment/variable/variable_user.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-. packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant_command.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant_environment.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_create.sh
