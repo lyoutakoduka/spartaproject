5a3480bb20f8bcaa19fc32bc02020d56326162ea
lyouta <lyouta@spartaproject.com>
2025-11-20 07:57:15 +0900

    Refactor: Remove unused imports. command #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/command/command_workspace.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/command/command_workspace.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-. packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant_path.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/constant/get_constant.sh
 
