430286f2964b073b01a9eab4279e5f9a8b61074d
lyouta <lyouta@spartaproject.com>
2025-11-20 07:16:23 +0900

    Refactor: Remove the redundant function. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
@@ -3,17 +3,13 @@
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/path/get_file_path.sh
 
 export_lines() (
-    export_line() {
-        declare -r _text="$1"
+    declare -r _arguments=("$@")
 
-        declare -r path=$(shell::get_file_path)
+    declare -r path=$(shell::get_file_path)
 
-        if [[ -n "${path}" ]]; then
-            echo "${_text}" >>"${path}"
-        fi
-    }
-
-    for text in "$@"; do
-        export_line "${text}"
-    done
+    if [[ -n "${path}" ]]; then
+        for text in "${_arguments[@]}"; do
+            echo "${text}" >>"${path}"
+        done
+    fi
 )
