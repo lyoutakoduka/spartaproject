57d65c88f4008349c495f2337277128747bf3a2e
lyouta <lyouta@spartaproject.com>
2025-11-20 01:48:34 +0900

    Update: Un-use the outside string module. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_export.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/environment/environment_export.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-. packages/pyspartadevc/tools/shspartadevc/script/launch/string/string_assign.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
 
 export_environment() (
@@ -8,8 +7,6 @@ export_environment() (
     declare -r _key="$1"
     declare -r _value="$2"
 
-    declare -r export_pair=$(create_assign "${_key}" "${_value}")
-    declare -r text="${_command} ${export_pair}"
-
-    export_line "${text}"
+    declare -r _text="${_command} ${_key}=${_value}"
+    export_line "${_text}"
 )
