a18e1cb97af7f5407a0c4c1164011ed15e45ac46
lyouta <lyouta@spartaproject.com>
2025-11-20 07:05:07 +0900

    Refactor: Remove the unused module. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_lines.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
-
-export_lines() (
-    for text in "$@"; do
-        export_line "${text}"
-    done
-)
