9a09b2d5c1685301a5eab96d75076cf26ffc1cf1
lyouta <lyouta@spartaproject.com>
2025-11-20 06:33:32 +0900

    Update: Rename function to private type. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/show/show_message.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/show/show_message.sh
@@ -2,7 +2,7 @@
 
 . packages/pyspartadevc/tools/shspartadevc/script/shared/constant/get_constant.sh
 
-show_message() (
+_show_message() (
     declare -r _group="$1"
     declare -r _message="$2"
     declare -r _package=$(constant::package_main)
@@ -14,7 +14,7 @@ shell::show_warning() {
     declare -r _message="$1"
     declare -r _group="error"
 
-    show_message "${_group}" "${_message}"
+    _show_message "${_group}" "${_message}"
 
     exit 1
 }
@@ -23,5 +23,5 @@ show_log() (
     declare -r _message="$1"
     declare -r _group="log"
 
-    show_message "${_group}" "${_message}"
+    _show_message "${_group}" "${_message}"
 )
