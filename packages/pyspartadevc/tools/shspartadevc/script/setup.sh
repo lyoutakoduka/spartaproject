522f2bad544ec6762263d7cb2dcc24a4730d8b97
lyouta <lyouta@spartaproject.com>
2025-11-19 11:07:12 +0900

    Update: Add code to select launcher script. #261

--- packages/pyspartadevc/tools/shspartadevc/script/setup.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/setup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-. packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess_launch.sh
+. packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_launch.sh
+. packages/pyspartadevc/tools/shspartadevc/script/post/lifecycle/lifecycle_attach.sh
+. packages/pyspartadevc/tools/shspartadevc/script/post/lifecycle/lifecycle_create.sh
 
-launch_preprocess "$@"
+_main() {
+    declare -r _create="create"
+    declare -r _attach="attach"
+    declare -r _group="$1"
+    declare -r _arguments=("$@")
+
+    if [[ "${_group}" == "${_create}" ]]; then
+        lifecycle_create
+    elif [[ "${_group}" == "${_attach}" ]]; then
+        lifecycle_attach
+    else
+        preprocess_launch "${_arguments[@]}"
+    fi
+}
+
+_main "$@"
