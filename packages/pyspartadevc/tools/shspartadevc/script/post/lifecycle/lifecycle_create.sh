a193b2580d1345b244578a79e53b7988c5502476
lyouta <lyouta@spartaproject.com>
2025-11-20 04:32:50 +0900

    Update: Use the outside module to sync. #263

--- packages/pyspartadevc/tools/shspartadevc/script/post/lifecycle/lifecycle_create.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/post/lifecycle/lifecycle_create.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
 . packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant_path.sh
+. packages/pyspartadevc/tools/shspartadevc/script/post/command/command_package.sh
 . packages/pyspartadevc/tools/shspartadevc/script/post/constant/get_constant_comment.sh
 . packages/pyspartadevc/tools/shspartadevc/script/post/owner/owner_section.sh
-. packages/pyspartadevc/tools/shspartadevc/script/shared/constant/get_constant_path.sh
-. packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/file_initialize.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/path/set_file_path.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/process/process_begin.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/process/process_end.sh
-
-_create_command_sync() (
-    declare -r _sync_python="uv sync"
-    declare -r _sync_javascript="yarn"
-    declare -r _comment=$(constant::comment_sync)
-
-    export_line "${_comment}"
-    export_line "${_sync_python}"
-    export_line "${_sync_javascript}"
-)
 
 lifecycle_create() (
     declare -r _header=$(constant::header_post)
@@ -26,7 +15,7 @@ lifecycle_create() (
 
     _body_text_file() {
         create_command_owner
-        _create_command_sync
+        command_package_manager
     }
 
     _whole_text_file() {
