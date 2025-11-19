3c55cd8a3b9b8dde745de5d93fbfc200473cc097
lyouta <lyouta@spartaproject.com>
2025-11-19 11:15:55 +0900

    Doc: Remove unnecessary docstrings. #261

--- packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_launch.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_launch.sh
@@ -8,13 +8,6 @@
 . packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_script.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/constant/get_constant_message.sh
 
-#*  Create the script to create or attach a dev-container.
-#*
-#*  Error:
-#*    _filter_by_arguments (function): exit 1
-#*
-#*    _main (function): exit 1
-#*
 preprocess_launch() (
     declare -r _create="create"
     declare -r _attach="attach"
