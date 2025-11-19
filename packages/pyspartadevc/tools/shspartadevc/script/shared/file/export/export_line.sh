d55c9e999bbd7b14692c1b4b89478d92945026c9
lyouta <lyouta@spartaproject.com>
2025-11-20 07:12:09 +0900

    Update: Integrate the functions to one. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
@@ -2,17 +2,17 @@
 
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/path/get_file_path.sh
 
-export_line() (
-    declare -r _text="$1"
-
-    declare -r path=$(shell::get_file_path)
-
-    if [[ -n "${path}" ]]; then
-        echo "${_text}" >>"${path}"
-    fi
-)
-
 export_lines() (
+    export_line() {
+        declare -r _text="$1"
+
+        declare -r path=$(shell::get_file_path)
+
+        if [[ -n "${path}" ]]; then
+            echo "${_text}" >>"${path}"
+        fi
+    }
+
     for text in "$@"; do
         export_line "${text}"
     done
