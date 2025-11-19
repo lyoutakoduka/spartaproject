b5b3923345eec6648533adc87331bd2fa14f694c
lyouta <lyouta@spartaproject.com>
2025-11-20 04:29:41 +0900

    Update: Add command to sync package managers. #263

---
+++ packages/pyspartadevc/tools/shspartadevc/script/post/command/command_package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. packages/pyspartadevc/tools/shspartadevc/script/post/constant/get_constant_comment.sh
+
+command_package_manager() (
+    declare -r _sync_python="uv sync"
+    declare -r _sync_javascript="yarn"
+    declare -r _comment=$(constant::comment_sync)
+
+    export_line "${_comment}"
+    export_line "${_sync_python}"
+    export_line "${_sync_javascript}"
+)
