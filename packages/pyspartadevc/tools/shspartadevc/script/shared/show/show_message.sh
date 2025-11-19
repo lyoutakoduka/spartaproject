06b7a056cc0b4558f0e068880634138c541c329d
lyouta <lyouta@spartaproject.com>
2025-11-20 05:10:04 +0900

    Update: Define the path in outside module. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/show/show_message.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/show/show_message.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 . packages/pyspartadevc/tools/shspartadevc/script/shared/constant/get_constant.sh
-. packages/pyspartadevc/tools/shspartadevc/script/shared/string/string_bracket.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/string/string_pair.sh
 
 show_message() (
     declare -r _group="$1"
     declare -r _message="$2"
-    declare -r _package="pyspartadevc"
+    declare -r _package=$(constant::package_main)
 
     declare -r _message_inside=$(create_pair "${_package}" "${_group}")
     echo "[${_message_inside}] ${_message}"
