032bbc77323d1fc45e101fb5bf4e32d33692ab9a
lyouta <lyouta@spartaproject.com>
2025-11-20 06:55:20 +0900

    Refactor: Remove the redundant function. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_lines.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_lines.sh
@@ -3,11 +3,7 @@
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
 
 export_lines() (
-    _main() {
-        for text in "$@"; do
-            export_line "${text}"
-        done
-    }
-
-    _main "$@"
+    for text in "$@"; do
+        export_line "${text}"
+    done
 )
