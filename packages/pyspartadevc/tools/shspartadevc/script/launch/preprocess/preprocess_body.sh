2e02e8508deb2a13d63f896f4824d1bf7c0bf438
lyouta <lyouta@spartaproject.com>
2025-11-20 09:15:32 +0900

    Update: Move function to inside of subshell. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_body.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_body.sh
@@ -5,17 +5,8 @@
 . packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_command.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/export/export_line.sh
 
-_filter_exists_command() (
-    declare -r _expected="create"
-    declare -r _group="$1"
-
-    if [[ "${_group}" = "${_expected}" ]]; then
-        declare -r command_exists=$(get_command_exists)
-        export_lines "${command_exists}"
-    fi
-)
-
 body_text_file() (
+    declare -r _expected="create"
     declare -r _group="$1"
     declare -r _message=$(constant::header_devcontainer)
 
@@ -23,6 +14,13 @@ body_text_file() (
         declare -r command_devcontainer=$(get_command_devcontainer)
         export_lines "${command_devcontainer}"
     }
+
+    _filter_exists_command() (
+        if [[ "${_group}" = "${_expected}" ]]; then
+            declare -r command_exists=$(get_command_exists)
+            export_lines "${command_exists}"
+        fi
+    )
 
     _add_shared_foot() {
         declare -r command_workspace=$(get_command_workspace)
