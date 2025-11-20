83b615f2eca2fb7b7a647d30f224b5328a462d28
lyouta <lyouta@spartaproject.com>
2025-11-20 09:29:49 +0900

    Refactor: Use outside functions. #263

--- packages/pyspartadevc/tools/shspartadevc/script/post/lifecycle/lifecycle_create.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/post/lifecycle/lifecycle_create.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 . packages/pyspartadevc/tools/shspartadevc/script/post/command/command_owner.sh
-. packages/pyspartadevc/tools/shspartadevc/script/post/command/command_package.sh
 . packages/pyspartadevc/tools/shspartadevc/script/post/constant/get_constant.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/file_initialize.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/path/get_file_path.sh
@@ -12,16 +11,11 @@ lifecycle_create() (
     declare -r _header=$(constant::header_post)
     declare -r _script_post=$(constant::temporary_post)
 
-    _body_text_file() {
-        command_change_owner
-        command_package_manager
-    }
-
     _whole_text_file() {
         begin_text_file
 
         initialize_text_file "${_header}"
-        _body_text_file
+        body_text_file
 
         end_text_file
     }
