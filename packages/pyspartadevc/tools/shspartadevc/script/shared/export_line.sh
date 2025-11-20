93ed98bb3b3a0a9caa13536774c847dd1355c86f
lyouta <lyouta@spartaproject.com>
2025-11-20 13:11:57 +0900

    Refactor: Integrate functions to same group. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/export_line.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/export_line.sh
@@ -13,3 +13,39 @@ export_lines() (
         done
     fi
 )
+
+shell::get_file_path() {
+    declare -g ADDED_FILE_PATH
+
+    echo "${ADDED_FILE_PATH}"
+}
+
+shell::set_file_path() {
+    declare -r path="$1"
+
+    declare -g ADDED_FILE_PATH="${path}"
+}
+
+_show_message() (
+    declare -r _group="$1"
+    declare -r _message="$2"
+    declare -r _package=$(constant::package_main)
+
+    echo "[${_package}:${_group}] ${_message}"
+)
+
+shell::show_warning() {
+    declare -r _message="$1"
+    declare -r _group="error"
+
+    _show_message "${_group}" "${_message}"
+
+    exit 1
+}
+
+show_log() (
+    declare -r _message="$1"
+    declare -r _group="log"
+
+    _show_message "${_group}" "${_message}"
+)
