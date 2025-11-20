aa826ee15ac0d4593cd3831d820cfb8568e9afab
lyouta <lyouta@spartaproject.com>
2025-11-20 12:10:42 +0900

    Refactor: Integrate functions to same group. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/process/process_begin.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/process/process_begin.sh
@@ -4,6 +4,14 @@
 . packages/pyspartadevc/tools/shspartadevc/script/shared/get_file_path.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/process/process_log.sh
 
+show_preprocess_log() (
+    declare -r _group="$1"
+
+    declare -r _path=$(shell::get_file_path)
+
+    show_log "${_group}: ${_path}"
+)
+
 begin_text_file() (
     declare -r _group=$(constant::group_text_remove)
 
@@ -15,6 +23,31 @@ begin_text_file() (
 
     _execute_and_show() {
         _remove_preprocess_script
+        show_preprocess_log "${_group}"
+    }
+
+    _main() {
+        declare -r path=$(shell::get_file_path)
+
+        if [[ -e "${path}" ]]; then
+            _execute_and_show
+        fi
+    }
+
+    _main
+)
+
+end_text_file() (
+    declare -r _group=$(constant::group_text_create)
+
+    _add_executable_permission() {
+        declare -r path=$(shell::get_file_path)
+
+        chmod +x "${path}"
+    }
+
+    _execute_and_show() {
+        _add_executable_permission
         show_preprocess_log "${_group}"
     }
 
