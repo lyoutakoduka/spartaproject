4d09fbe2a0ad0ee317b644f2a4604881f3b1fef8
lyouta <lyouta@spartaproject.com>
2025-11-20 12:31:17 +0900

    Refactor: Integrate functions to same group. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/get_constant.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/get_constant.sh
@@ -15,3 +15,35 @@ constant::group_text_remove() {
 constant::group_text_create() {
     echo "Create"
 }
+
+constant::volume_cache() {
+    echo ".temp/cache"
+}
+
+constant::volume_python() {
+    echo ".venv"
+}
+
+constant::volume_javascript() {
+    echo "node_modules"
+}
+
+constant::temporary_post() {
+    echo "packages/pyspartadevc/tools/shspartadevc/.temp/devcontainer_post.sh"
+}
+
+constant::header_post() {
+    echo "# * Execute this script that finalize to create the dev-container."
+}
+
+constant::comment_owner() {
+    echo "# Change directory owner to non-root user."
+}
+
+constant::comment_sync() {
+    echo "# Sync the package managers."
+}
+
+constant::help_attach() {
+    echo 'Launched the terminal "zsh".'
+}
