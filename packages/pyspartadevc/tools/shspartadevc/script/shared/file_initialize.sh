e984afbd43e033405664d3b440147cc8c80607a5
lyouta <lyouta@spartaproject.com>
2025-11-20 12:48:06 +0900

    Refactor: Remove unused module imports. #263

--- packages/pyspartadevc/tools/shspartadevc/script/shared/file_initialize.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/file_initialize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-. packages/pyspartadevc/tools/shspartadevc/script/shared/get_constant.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/export_line.sh
+. packages/pyspartadevc/tools/shspartadevc/script/shared/get_constant.sh
 
 initialize_text_file() (
     declare -r _header="$1"
