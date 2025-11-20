e1b9150775e81a30c316327e8dc48b6aa2ffacf9
lyouta <lyouta@spartaproject.com>
2025-11-20 11:28:07 +0900

    Refactor: Integrate function to same group. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_script.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_script.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
 
+. packages/pyspartadevc/tools/shspartadevc/script/launch/account/account_ready.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/get_constant.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_body.sh
+. packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_command.sh
+. packages/pyspartadevc/tools/shspartadevc/script/shared/export_line.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file_initialize.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/get_file_path.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/process/process_begin.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/process/process_end.sh
+
+_add_text_file_launch() (
+    declare -r _expected="create"
+    declare -r _group="$1"
+    declare -r _message=$(constant::header_devcontainer)
+
+    _add_command_head() {
+        declare -r command_devcontainer=$(get_command_devcontainer)
+        export_lines "${command_devcontainer}"
+    }
+
+    _add_command_body() (
+        if [[ "${_group}" = "${_expected}" ]]; then
+            declare -r command_exists=$(get_command_exists)
+            export_lines "${command_exists}"
+        fi
+    )
+
+    _add_command_foot() {
+        declare -r command_workspace=$(get_command_workspace)
+        declare -r command_config=$(get_command_config)
+
+        export_lines "${command_workspace}" "${command_config}"
+    }
+
+    _add_command_base() {
+        _add_command_head
+        _add_command_body "${_group}"
+        _add_command_foot
+    }
+
+    _main() {
+        ready_identifier
+        export_lines "${_message}"
+        _add_command_base
+    }
+
+    _main
+)
 
 create_preprocess_script() (
     declare -r _expected="create"
@@ -36,7 +78,7 @@ create_preprocess_script() (
 
         declare -r header=$(_get_header)
         initialize_text_file "${header}"
-        add_text_file_launch "${_group}"
+        _add_text_file_launch "${_group}"
 
         end_text_file
     }
