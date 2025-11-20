9f48581d00ab7148513578736f210f17113faa76
lyouta <lyouta@spartaproject.com>
2025-11-20 09:48:51 +0900

    Update: Integrate the functions to one group. #263

--- packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_script.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_script.sh
@@ -2,7 +2,7 @@
 
 . packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant.sh
 . packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_body.sh
-. packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_head.sh
+. packages/pyspartadevc/tools/shspartadevc/script/shared/file/file_initialize.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/path/get_file_path.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/process/process_begin.sh
 . packages/pyspartadevc/tools/shspartadevc/script/shared/file/process/process_end.sh
@@ -12,6 +12,8 @@ create_preprocess_script() (
     declare -r _group="$1"
     declare -r _script_create=$(constant::temporary_create)
     declare -r _script_attach=$(constant::temporary_attach)
+    declare -r _header_create=$(constant::header_create)
+    declare -r _header_attach=$(constant::header_attach)
 
     _get_preprocess() {
         if [[ "${_group}" = "${_expected}" ]]; then
@@ -21,10 +23,19 @@ create_preprocess_script() (
         fi
     }
 
+    _get_header() {
+        if [[ "${_group}" = "${_expected}" ]]; then
+            echo "${_header_create}"
+        else
+            echo "${_header_attach}"
+        fi
+    }
+
     _whole_text_file() {
         begin_text_file
 
-        head_text_file "${_group}"
+        declare -r header=$(_get_header)
+        initialize_text_file "${header}"
         add_text_file_launch "${_group}"
 
         end_text_file
