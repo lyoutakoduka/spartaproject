541fe5234b99809b5eebbffca8caa2eba98c4206
lyouta <lyouta@spartaproject.com>
2025-11-21 06:26:11 +0900

    Refactor: Remove redundant functions. #266

--- packages/pyspartadevc/src/shspartadevc/script/project/get_stack_trace.sh
+++ packages/pyspartadevc/src/shspartadevc/script/project/get_stack_trace.sh
@@ -1,40 +1,19 @@
 #!/bin/bash
 
 get_selected_frame() (
-    declare -r -i _empty=""
-    declare -r -i _depth=5
+    declare -r -i _depth=3
     declare -r -i _index_default=0
     declare -r _group="$1"
     declare _index_any="$2"
 
-    _get_file_path() {
-        declare -r -i index="$1"
-        echo "${BASH_SOURCE[index]}"
-    }
-
-    _get_function_name() {
-        declare -r -i index="$1"
-        echo "${FUNCNAME[index]}"
-    }
-
     _get_stack_frame() {
         declare -r -i index="$1"
-        declare result="${_empty}"
 
         if [[ "${_group}" = "path" ]]; then
-            result=$(_get_file_path "${index}")
+            echo "${BASH_SOURCE[index]}"
         else
-            result=$(_get_function_name "${index}")
+            echo "${FUNCNAME[index]}"
         fi
-
-        echo "${result}"
-    }
-
-    _get_stack_trace() {
-        declare -r -i index="$1"
-
-        declare -r result="$(_get_stack_frame "${index}")"
-        echo "${result}"
     }
 
     _update_offset() {
@@ -45,25 +24,12 @@ get_selected_frame() (
         fi
     }
 
-    _offset_index() {
-        declare -r -i offset="$1"
-        echo $((offset + _depth))
-    }
-
-    _get_index() {
-        declare -r -i offset=$(_update_offset)
-        declare -r -i index=$(_offset_index "${offset}")
-
-        echo "${index}"
-    }
-
     _main() {
-        declare -r -i index=$(_get_index)
-        declare -r result=$(_get_stack_trace "${index}")
+        declare -r -i offset=$(_update_offset)
+        declare -r -i index=$((offset + _depth))
 
-        echo "${result}"
+        _get_stack_frame "${index}"
     }
 
-    declare -r result=$(_main)
-    echo "${result}"
+    _main
 )
