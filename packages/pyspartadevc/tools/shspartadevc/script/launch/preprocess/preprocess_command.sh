6a298eefdd0ac270600015b155a35d89fe57fbdd
lyouta <lyouta@spartaproject.com>
2025-11-20 09:08:22 +0900

    Feat: Get the command about existing flag. #263

---
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/preprocess/preprocess_command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. packages/pyspartadevc/tools/shspartadevc/script/launch/constant/get_constant.sh
+
+get_command_exists() (
+    declare -r _flag_exists="--remove-existing-container"
+    declare -r _indent=$(constant::indent)
+    declare -r _enter=$(constant::enter)
+
+    echo "${_indent}${_flag_exists}${_enter}"
+)
