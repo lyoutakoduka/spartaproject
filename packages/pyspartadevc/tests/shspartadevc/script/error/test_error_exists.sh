299a23664e572e4159074d599ae6b83b1b9fe56a
lyouta <lyouta@spartaproject.com>
2025-11-21 06:09:59 +0900

    Refactor: Remove redundant empties. #266

--- packages/pyspartadevc/tests/shspartadevc/script/error/test_error_exists.sh
+++ packages/pyspartadevc/tests/shspartadevc/script/error/test_error_exists.sh
@@ -4,13 +4,11 @@
 
 test_no_exists() (
     declare -r _path="packages/pyspartadevc"
-
     shell::error_no_exists "${_path}"
 )
 
 test_exists() (
     declare -r _path="root/file"
-
     shell::error_exists "${_path}"
 )
 
