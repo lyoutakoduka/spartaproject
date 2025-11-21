502619944d2ae80523c079d9b502fa6b417a8fbe
lyouta <lyouta@spartaproject.com>
2025-11-21 19:39:59 +0900

    Remove: The unused functions. #272

--- packages/pyspartadevc/tools/shspartadevc/script/shared/shared_launch.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/shared/shared_launch.sh
@@ -15,16 +15,6 @@ export_lines() (
     fi
 )
 
-shell::get_file_path() {
-    declare -g ADDED_FILE_PATH
-    echo "${ADDED_FILE_PATH}"
-}
-
-shell::set_file_path() {
-    declare -r _path="$1"
-    declare -g ADDED_FILE_PATH="${_path}"
-}
-
 _show_message() (
     declare -r _group="$1"
     declare -r _message="$2"
@@ -56,33 +46,6 @@ initialize_text_file() (
     export_lines "${_shebang}" "${_header}"
 )
 
-_show_preprocess_log() (
-    declare -r _group="$1"
-    declare -r _path=$(shell::get_file_path)
-    show_log "${_group}: ${_path}"
-)
-
-begin_text_file() (
-    declare -r _group=$(constant::group_text_remove)
-
-    _remove_preprocess_script() {
-        declare -r path=$(shell::get_file_path)
-
-        rm "${path}"
-    }
-
-    _main() {
-        declare -r path=$(shell::get_file_path)
-
-        if [[ -e "${path}" ]]; then
-            _remove_preprocess_script
-            _show_preprocess_log "${_group}"
-        fi
-    }
-
-    _main
-)
-
 shell::begin_text_file_#FF_0000_TOP() {
     declare -r _group="preprocess"
     declare -r _head="devcontainer"
@@ -93,26 +56,6 @@ shell::begin_text_file_#FF_0000_TOP() {
     )
     declare -g ADDED_FILE_PATH="${_temporary}"
 }
-
-end_text_file() (
-    declare -r _group=$(constant::group_text_create)
-
-    _add_executable_permission() {
-        declare -r path=$(shell::get_file_path)
-        chmod +x "${path}"
-    }
-
-    _main() {
-        declare -r path=$(shell::get_file_path)
-
-        if [[ -e "${path}" ]]; then
-            _add_executable_permission
-            _show_preprocess_log "${_group}"
-        fi
-    }
-
-    _main
-)
 
 end_text_file_#FF_0000_TOP() {
     declare -g ADDED_FILE_PATH
