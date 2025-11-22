fb20543ac24ffee0cb00b778bd1cc0ddd4c07411
lyouta <lyouta@spartaproject.com>
2025-11-22 11:37:20 +0900

    Fix: Correct expected log message. #273

--- packages/pyspartadevc/tests/shspartadevc/script/string/test_log_message.sh
+++ packages/pyspartadevc/tests/shspartadevc/script/string/test_log_message.sh
@@ -7,7 +7,7 @@ test_fake() (
     declare -r _group="root"
     declare -r _group_sub="head"
     declare -r _message="fake"
-    declare -r _time="1970-01-01T09:00:00+09:00"
+    declare -r _time="1970-01-01T09:00:00Z"
 
     declare -r _result=$(get_message "${_group}" "${_group_sub}" "${_message}")
     declare -r _expected="${_time} [${_group}:${_group_sub}] ${_message}"
