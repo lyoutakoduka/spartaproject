4012a88cc4129dd898052905b206570621fcaf64
lyouta <lyouta@spartaproject.com>
2025-11-22 15:24:55 +0900

    Update: Use new module about log message. #275

--- packages/pyspartadevc/tools/shspartadevc/script/launch/lifecycle_attach.sh
+++ packages/pyspartadevc/tools/shspartadevc/script/launch/lifecycle_attach.sh
@@ -7,10 +7,12 @@
 declare -g FFB275A
 
 lifecycle_attach() (
+    declare -r _group="log"
     declare -r _message=$(constant::help_attach)
+    declare -r _package=$(constant::package_main)
 
     if [[ "${FFB275A}" == "true" ]]; then
-        show_log "${_message}"
+        get_message "${_package}" "${_group}" "${_message}"
     else
         show_log "${_message}"
     fi
